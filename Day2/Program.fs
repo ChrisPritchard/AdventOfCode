87f7349890b2139f31aaf602d4ad6c6b2eed76ce
Christopher <chrispritchard.nz@gmail.com>
2018-12-06 18:09:43 +1300

    initial (incorrect) solution to day 2

--- Day2/Program.fs
+++ Day2/Program.fs
@@ -1,6 +1,16 @@
 ﻿open System
+open System.IO
 
 [<EntryPoint>]
 let main _ =
-    printfn "Hello World from F#!"
+    let lines = 
+        File.ReadAllLines "input.txt" 
+        |> Array.map (Seq.groupBy id >> Seq.map (snd >> Seq.length) >> Set.ofSeq) 
+        |> Array.toList
+    let (twos, threes) = 
+        lines 
+        |> List.fold (fun (tw, th) set -> 
+            (if Set.contains 2 set then tw + 1 else tw),
+            (if Set.contains 2 set then th + 1 else th)) (0, 0)
+    printfn "%i" (twos * threes)
     0

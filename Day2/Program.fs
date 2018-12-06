c19082bc687597d046f05871b44bf975565ff4d3
Christopher <chrispritchard.nz@gmail.com>
2018-12-06 18:14:17 +1300

    Fixed part 1

--- Day2/Program.fs
+++ Day2/Program.fs
@@ -3,6 +3,7 @@ open System.IO
 
 [<EntryPoint>]
 let main _ =
+
     let lines = 
         File.ReadAllLines "input.txt" 
         |> Array.map (Seq.groupBy id >> Seq.map (snd >> Seq.length) >> Set.ofSeq) 
@@ -11,6 +12,6 @@ let main _ =
         lines 
         |> List.fold (fun (tw, th) set -> 
             (if Set.contains 2 set then tw + 1 else tw),
-            (if Set.contains 2 set then th + 1 else th)) (0, 0)
+            (if Set.contains 3 set then th + 1 else th)) (0, 0)
     printfn "%i" (twos * threes)
     0

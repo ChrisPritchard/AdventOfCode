c99f64cc760af52b60a2fd7487924796ac796c70
Christopher <chrispritchard.nz@gmail.com>
2018-12-06 17:36:11 +1300

    Finished implementation of day 1

--- Day1/Program.fs
+++ Day1/Program.fs
@@ -2,29 +2,27 @@
 open System
 open System.IO
 
+let part1 = Seq.sum
 
-
-let part1 file = 
-    File.ReadAllLines file
-    |> Seq.fold (fun n line -> 
-        let num = Int32.Parse(line)
-        n + num) 0
-
-let part2 file = 
-
-    let rec findDup state found remaining =
-        let head = Seq.head remaining
-        let next = state + head
-        if Set.contains next found then next
-        else findDup next (Set.add next found) (Seq.tail remaining)
-
-    let lines = File.ReadAllLines file |> Array.map (Int32.Parse)
-    Seq.initInfinite (fun _ -> lines) 
-    |> Seq.collect id
-    |> findDup 0 (Set.empty.Add(0))
+let part2 lines = 
+    let rec findDup state found =
+        function
+        | head::remaining ->
+                let next = state + head
+                if Set.contains next found then next
+                else findDup next (Set.add next found) remaining
+        | [] -> findDup state found lines
+     
+    findDup 0 (Set.empty.Add(0)) lines
 
 [<EntryPoint>]
 let main _ =
-    printfn "%i" <| part1 "input.txt"
-    printfn "%i" <| part2 "input.txt"
+    let lines = 
+        File.ReadAllLines "input.txt" 
+        |> Array.map (Int32.Parse)
+        |> Array.toList
+    printfn "total lines in input: %i" <| Seq.length lines
+
+    printfn "%i" <| part1 lines
+    printfn "%i" <| part2 lines
     0

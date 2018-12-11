f48773bc41a370f2a9b82d934427271a5ec5e587
Chris Pritchard <chris.pritchard@edpay.nz>
2018-12-12 11:28:58 +1300

    Implemented part 1 of day 5

--- Day5/Program.fs
+++ Day5/Program.fs
@@ -1,14 +1,25 @@
-﻿open System
-open System.IO
+﻿open System.IO
+
+let toUpper = System.Char.ToUpper
 
 let rec part1 polymer =
-    Seq.length polymer
+    let startLength = Seq.length polymer
+    let result =
+        polymer
+        |> Seq.fold (fun soFar next ->
+            match soFar with
+            | last::tail ->
+                if toUpper last = toUpper next && last <> next then tail
+                else next::last::tail
+            | [] -> [next]) []
+    if List.length result = startLength then startLength
+    else part1 (result |> Seq.ofList)
 
 [<EntryPoint>]
 let main _ =
     
-    //let polymer = File.ReadAllText "input.txt"
-    let polymer = "dabAcCaCBAcCcaDA"
+    let polymer = File.ReadAllText "input.txt"
+    //let polymer = "dabAcCaCBAcCcaDA"
 
     printfn "part1: %i" <| part1 polymer
 

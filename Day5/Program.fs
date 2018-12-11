ec5c8589537550e0b7dd6728c3d30c0344f4343a
Chris Pritchard <chris.pritchard@edpay.nz>
2018-12-12 11:21:49 +1300

    scaffolding for day 5

---
+++ Day5/Program.fs
@@ -0,0 +1,15 @@
+﻿open System
+open System.IO
+
+let rec part1 polymer =
+    Seq.length polymer
+
+[<EntryPoint>]
+let main _ =
+    
+    //let polymer = File.ReadAllText "input.txt"
+    let polymer = "dabAcCaCBAcCcaDA"
+
+    printfn "part1: %i" <| part1 polymer
+
+    0

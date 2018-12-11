3e5abf26bb4780c95cfb05ac79ed895f52493891
Chris Pritchard <chris.pritchard@edpay.nz>
2018-12-12 11:09:13 +1300

    successfully implemented part 1

--- Day4/Program.fs
+++ Day4/Program.fs
@@ -10,7 +10,7 @@ type Event =
 
 let pDateElem : Parser<int32, unit> = pint32 .>> (anyOf ['-';' ';':'])
 let pTimeElem = 
-    pDateElem .>>. pDateElem .>>. pDateElem .>>. pDateElem .>>. pDateElem 
+    pDateElem .>>. pDateElem .>>. pDateElem .>>. pDateElem .>>. pint32 
     |>> fun ((((y, m), d), h), M) -> new DateTime(y, m, d, h, M, 0)
 let pTime = pchar '[' >>. pTimeElem .>> pchar ']'
 
@@ -24,32 +24,70 @@ let pLine = pTime .>>. pEvent
 let processLine (line : string) =
     match run pLine line with
     | Success (result, _, _) -> result
-    | _ -> failwith "invalid line"
+    | Failure (error, _, _) -> failwith error
 
 [<EntryPoint>]
 let main _ =
     
-    //let lines = File.ReadAllLines "input.txt"
-    let lines = [|
-        "[1518-11-01 00:00] Guard #10 begins shift"
-        "[1518-11-01 00:05] falls asleep"
-        "[1518-11-01 00:25] wakes up"
-        "[1518-11-01 00:30] falls asleep"
-        "[1518-11-01 00:55] wakes up"
-        "[1518-11-01 23:58] Guard #99 begins shift"
-        "[1518-11-02 00:40] falls asleep"
-        "[1518-11-02 00:50] wakes up"
-        "[1518-11-03 00:05] Guard #10 begins shift"
-        "[1518-11-03 00:24] falls asleep"
-        "[1518-11-03 00:29] wakes up"
-        "[1518-11-04 00:02] Guard #99 begins shift"
-        "[1518-11-04 00:36] falls asleep"
-        "[1518-11-04 00:46] wakes up"
-        "[1518-11-05 00:03] Guard #99 begins shift"
-        "[1518-11-05 00:45] falls asleep"
-        "[1518-11-05 00:55] wakes up"
-    |]
+    let lines = File.ReadAllLines "input.txt"
+    // let lines = [|
+    //     "[1518-11-01 00:00] Guard #10 begins shift"
+    //     "[1518-11-01 00:05] falls asleep"
+    //     "[1518-11-01 00:25] wakes up"
+    //     "[1518-11-01 00:30] falls asleep"
+    //     "[1518-11-01 00:55] wakes up"
+    //     "[1518-11-01 23:58] Guard #99 begins shift"
+    //     "[1518-11-02 00:40] falls asleep"
+    //     "[1518-11-02 00:50] wakes up"
+    //     "[1518-11-03 00:05] Guard #10 begins shift"
+    //     "[1518-11-03 00:24] falls asleep"
+    //     "[1518-11-03 00:29] wakes up"
+    //     "[1518-11-04 00:02] Guard #99 begins shift"
+    //     "[1518-11-04 00:36] falls asleep"
+    //     "[1518-11-04 00:46] wakes up"
+    //     "[1518-11-05 00:03] Guard #99 begins shift"
+    //     "[1518-11-05 00:45] falls asleep"
+    //     "[1518-11-05 00:55] wakes up"
+    // |]
 
-    let processed = lines |> Seq.map processLine |> Seq.toList
+    let processed = lines |> Seq.map processLine |> Seq.sortBy fst |> Seq.toList
 
+    let (sleepByGuard, _, _) = 
+        processed
+        |> List.fold (fun (result, guard, sleepStart) (date, event) ->
+            match event with
+            | NewGuard n -> (result, n, None)
+            | FallsAsleep -> (result, guard, Some date.TimeOfDay.Minutes)
+            | WakesUp -> 
+                let endSleep = date.TimeOfDay.Minutes-1
+                let newResult =
+                    match Map.tryFind guard result, sleepStart with
+                    | Some list, Some sleepStart -> Map.add guard ((sleepStart, endSleep)::list) result
+                    | None, Some sleepStart -> Map.add guard [(sleepStart, endSleep)] result
+                    | _ -> result
+                (newResult, guard, None)) (Map.empty<int, (int * int) list>, 0, None)
+    
+    let id, sleeps = 
+        sleepByGuard 
+        |> Map.toList
+        |> List.sortByDescending (fun (_, sleeps) -> sleeps |> List.sumBy (fun (startSleep, endSleep) -> endSleep - startSleep))
+        |> List.head
+
+    let sleepsPerMinute sleeps minute =
+        sleeps 
+        |> List.filter (fun (startSleep, endSleep) -> minute >= startSleep && minute <= endSleep)
+        |> List.length
+
+    let minute, count = 
+        [0..59] 
+        |> List.map (fun m -> m, sleepsPerMinute sleeps m) 
+        |> List.sortByDescending snd
+        |> List.head 
+
+    printfn "part1: minute %i, count %i, id %i, multiplied %i" 
+        minute 
+        count
+        id 
+        (minute * id)
+    
     0

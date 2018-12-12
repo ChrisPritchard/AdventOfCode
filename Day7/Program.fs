18c171186a58191a0b87293538762c53ad3f26dc
Chris Pritchard <chris.pritchard@edpay.nz>
2018-12-12 15:28:03 +1300

    Scaffolding for day 7

---
+++ Day7/Program.fs
@@ -0,0 +1,20 @@
+﻿open System
+open System.IO
+open FParsec.CharParsers
+open FParsec
+
+let part1 lines =
+    0
+
+let part2 lines =
+    0
+
+[<EntryPoint>]
+let main _ =
+
+    let lines = File.ReadAllLines "input.txt"
+
+    printfn "part 1: %i" <| part1 lines
+    printfn "part 2: %i" <| part2 lines
+
+    0

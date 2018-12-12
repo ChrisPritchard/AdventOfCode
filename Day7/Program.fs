92866c1873ef24d8d37844afaf993e47b0ad05e0
Chris Pritchard <chris.pritchard@edpay.nz>
2018-12-13 10:14:53 +1300

    interim work on day 7 part 2

--- Day7/Program.fs
+++ Day7/Program.fs
@@ -3,28 +3,52 @@ open System.IO
 open FParsec.CharParsers
 open FParsec
 
+let nextAvailable rules finished remaining = 
+    remaining 
+    |> Seq.tryFind (fun char ->
+        rules 
+        |> List.filter (fun (pre, c) -> c = char && not (Seq.contains pre finished)) 
+        |> List.map fst
+        |> List.isEmpty)
+
 let rec part1 (finished : char list) rules =
     function
     | [] -> String.Concat (List.rev finished)
     | remaining ->
-        let next = remaining |> Seq.find (fun char ->
-            rules 
-            |> List.filter (fun (pre, c) -> c = char && not (Seq.contains pre finished)) 
-            |> List.map fst
-            |> List.isEmpty)
-        part1 (next::finished) rules (remaining |> List.filter (fun char -> char <> next))
+        match nextAvailable rules finished remaining with
+        | Some next ->
+            part1 (next::finished) rules (remaining |> List.filter (fun char -> char <> next))
+        | _ -> failwith "invalid input"
 
-let rec part2 (finished : char list) working idle rules =
-    function
-    | [] -> String.Concat (List.rev finished)
-    | remaining ->
-        let newWorking = working |> List.map (fun i -> i - 1) |> List.except [0]
-        let newIdle = idle + (List.length working - List.length newWorking)
-        if newIdle = 0 then
-            part2 finished newWorking newIdle rules remaining
-        else
+let processJobs working idle finished =
+    working 
+    |> List.fold (fun (nw, ni, nf) (char, time) ->
+        match time with
+        | 0 -> nw, ni + 1, char::nf
+        | _ -> (char, time - 1)::nw, ni, nf) 
+        ([], idle, finished)
+
+let rec part2 (finished : char list) working idle baseJobTime rules remaining totalTime =
+    let newTotalTime = totalTime + 1
+    let newWorking, newIdle, newFinished = processJobs working idle finished
             
-    0
+    if List.isEmpty remaining && List.isEmpty newWorking 
+        then newTotalTime
+    else if List.isEmpty remaining || newIdle = 0
+        then part2 newFinished newWorking newIdle baseJobTime rules remaining newTotalTime
+    else
+        let newJobs = [0..newIdle] |> List.fold (fun jobs i -> 
+            let available = remaining |> List.except (jobs |> List.map fst)
+            match nextAvailable rules newFinished available with
+            | None -> jobs
+            | Some n -> 
+                let time = baseJobTime + 1 + (int n - int 'A')
+                (n, time)::jobs) []
+
+        let newRemaining = remaining |> List.except (newJobs |> List.map fst)
+        let newIdle = newIdle - List.length newJobs
+        let newWorking = List.append newJobs newWorking
+        part2 newFinished newWorking newIdle baseJobTime rules newRemaining newTotalTime
 
 [<EntryPoint>]
 let main _ =
@@ -39,8 +63,9 @@ let main _ =
         
     let lines = File.ReadAllLines "input.txt"
     let rules = lines |> Seq.map processLine |> Seq.toList
+    let letters = rules |> List.collect (fun (a, b) -> [a;b]) |> List.distinct |> List.sortBy id
 
-    printfn "part 1: %s" <| part1 [] rules ['A'..'Z']
-    printfn "part 2: %i" <| part2 [] [] 5 rules ['A'..'Z']
+    printfn "part 1: %s" <| part1 [] rules letters
+    printfn "part 2: %i" <| part2 [] [] 2 0 rules letters -1
 
     0

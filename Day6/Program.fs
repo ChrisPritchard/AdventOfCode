1f19efc5be2519fd3653184264c2b4edee030ab9
Chris Pritchard <chris.pritchard@edpay.nz>
2018-12-12 13:15:09 +1300

    Scaffolding for day 6

---
+++ Day6/Program.fs
@@ -0,0 +1,16 @@
+﻿open System.IO
+
+[<EntryPoint>]
+let main _ =
+    
+    let lines = File.ReadAllLines "input.txt"
+    let lines = [|
+        "1, 1"
+        "1, 6"
+        "8, 3"
+        "3, 4"
+        "5, 5"
+        "8, 9"
+    |]
+
+    0

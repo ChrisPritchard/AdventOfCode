bda97d58446c8240f8ec64712e21255afc9288dd
Chris Pritchard <chris.pritchard@edpay.nz>
2018-12-13 13:40:50 +1300

    scaffolding for day 8 with updated readme

---
+++ Day8/Program.fs
@@ -0,0 +1,13 @@
+﻿open System
+open System.IO
+
+let part1 input =
+    0
+
+[<EntryPoint>]
+let main _ =
+
+    let input = (File.ReadAllText "input.txt").Split([|' '|])
+    printfn "part 1: %i" <| part1 input
+
+    0

3ea523f4ec94ce0d37987d3ab974e9fb69045936
Chris Pritchard <chris.pritchard@edpay.nz>
2018-12-13 14:21:46 +1300

    implemented part 1 of day 8

--- Day8/Program.fs
+++ Day8/Program.fs
@@ -1,13 +1,32 @@
 ﻿open System
 open System.IO
 
-let part1 input =
-    0
+type Node = {
+    children: Node list
+    metadata: int list
+}
+
+let rec parseNode input =
+    match input with
+    | nodes::metadata::rest ->
+        let children, remaining =
+            [1..nodes] |> List.fold (fun (nodes, remaining) _ -> 
+                let node, newRemaining = parseNode remaining
+                (node::nodes, newRemaining)) ([], rest)
+        { children = children; metadata = remaining |> List.take metadata }, List.skip metadata remaining
+    | _ -> failwith "invalid input"
+
+let rec part1 node =
+    List.sum node.metadata + List.sumBy part1 node.children
 
 [<EntryPoint>]
 let main _ =
 
-    let input = (File.ReadAllText "input.txt").Split([|' '|])
-    printfn "part 1: %i" <| part1 input
+    let input = 
+        (File.ReadAllText "input.txt").Split([|' '|]) 
+        |> Array.toList |> List.map (Int32.Parse)
+    let tree, _ = parseNode input
+
+    printfn "part 1: %i" <| part1 tree
 
     0

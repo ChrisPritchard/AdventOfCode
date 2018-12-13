d05fe1c1f8978c506f766e8ac3f58a6ccbbfb70c
Chris Pritchard <chris.pritchard@edpay.nz>
2018-12-13 16:36:08 +1300

    switched to test data and implemented parsing for day9

--- Day9/Program.fs
+++ Day9/Program.fs
@@ -10,6 +10,10 @@ let part2 _ = 0
 let main _ =
 
     let input = File.ReadAllText "input.txt"
+    let players, maxMarble = 
+        match run (pint32 .>> pstring " players; last marble is worth " .>>. pint32 .>> pstring " points") input with
+        | Success (r, _, _) -> r
+        | _ -> failwith "invalid input"
 
     printfn "part 1: %i" <| part1 0
     printfn "part 2: %i" <| part2 0

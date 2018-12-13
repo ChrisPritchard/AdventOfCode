9fc13273ba8749e8b9241a50bf40ce3aeeb87c5b
Chris Pritchard <chris.pritchard@edpay.nz>
2018-12-13 16:52:23 +1300

    interim work on part 1 of day 9

--- Day9/Program.fs
+++ Day9/Program.fs
@@ -6,6 +6,16 @@ open FParsec
 let part1 _ = 0
 let part2 _ = 0
 
+let place newMarble (marbles, currentMarbleIndex) =
+    match List.length marbles with
+    | 0 -> [newMarble], 0
+    | 1 -> marbles @ [newMarble], 1
+    | 2 -> marbles.[0]::newMarble::[marbles.[1]], 1
+    | n ->
+        let nextIndex = currentMarbleIndex + 2
+        let adjusted = if nextIndex >= n then nextIndex - n else nextIndex
+        (List.take (adjusted - 1) marbles) @ [newMarble] @ (List.skip (adjusted + 1) marbles), nextIndex
+
 [<EntryPoint>]
 let main _ =
 
@@ -14,6 +24,8 @@ let main _ =
         match run (pint32 .>> pstring " players; last marble is worth " .>>. pint32 .>> pstring " points") input with
         | Success (r, _, _) -> r
         | _ -> failwith "invalid input"
+
+    let board = [0..5] |> List.fold (fun board marble -> place marble board) ([], 0)
 
     printfn "part 1: %i" <| part1 0
     printfn "part 2: %i" <| part2 0

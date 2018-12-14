782c37ea9ec86f1654758050a306388640586c41
Chris Pritchard <chris.pritchard@edpay.nz>
2018-12-14 16:24:08 +1300

    Implemented day 10, parts 1 and 2

--- Day10/Program.fs
+++ Day10/Program.fs
@@ -17,20 +17,26 @@ let main _ =
 
     let tick = List.map (fun ((x, y), (dx, dy)) -> (x + dx, y + dy), (dx, dy))
 
-    let render stars = 
-        let (mx, my, maxx, maxy) = 
-            stars 
-            |> List.fold (fun (ox, oy, omx, omy) ((x, y), _) -> 
-                (if x < ox then x else ox),
-                (if y < oy then y else oy),
-                (if x > omx then x else omx),
-                (if y > omy then y else omy)) 
-                (Int32.MaxValue, Int32.MaxValue, 0, 0)
-        let map = Map.ofList stars
-        [my..maxy] |> List.collect (fun y ->
-            "\n"::([mx..maxx] |> List.map (fun x -> match Map.tryFind (x, y) map with Some _ -> "#" | _ -> " ")))
-            |> System.String.Concat
+    let bounds = 
+        List.fold (fun (ox, oy, omx, omy) ((x, y), _) -> 
+            (if x < ox then x else ox),
+            (if y < oy then y else oy),
+            (if x > omx then x else omx),
+            (if y > omy then y else omy))
+            (Int32.MaxValue, Int32.MaxValue, 0, 0)
 
-    lines |> tick |> tick |> tick |> render |> printfn "%s"
+    let rec advanceTime n stars = 
+        let (mx, my, maxx, maxy) = bounds stars 
+        if maxy - my > 10 then 
+            advanceTime (n + 1L) (tick stars)
+        else
+            let map = stars |> Seq.map fst |> Set.ofSeq
+            n, [my..maxy] |> List.collect (fun y ->
+                "\r\n"::([mx..maxx] |> List.map (fun x -> if Set.contains (x, y) map then "#" else " ")))
+                |> System.String.Concat
+
+    let time, result = advanceTime 0L lines
+    printfn "%i" time
+    printfn "%s" result
 
     0

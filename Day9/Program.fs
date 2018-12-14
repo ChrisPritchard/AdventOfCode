9fd76fc59d924131fc3a9bf0b857942c759968d4
Chris Pritchard <chris.pritchard@edpay.nz>
2018-12-14 14:15:46 +1300

    Changed to use a circular queue-like implementation with mutable props.

--- Day9/Program.fs
+++ Day9/Program.fs
@@ -3,56 +3,61 @@ open System.IO
 open FParsec.CharParsers
 open FParsec
 
-let placeMarble newMarble (marbles, currentMarbleIndex) =
-    match List.length marbles with
-    | 0 -> [newMarble], 0
-    | 1 -> marbles @ [newMarble], 1
-    | 2 -> marbles.[0]::newMarble::[marbles.[1]], 1
-    | n ->
-        let nextIndex = currentMarbleIndex + 2
-        if nextIndex = n then
-            marbles @ [newMarble], nextIndex
-        else if nextIndex = n + 1 then
-            marbles.[0]::newMarble::marbles.[1..], 1
-        else
-            marbles.[0..nextIndex-1] @ [newMarble] @ marbles.[nextIndex..], nextIndex
-
-let scoreMarble (marbles, currentMarbleIndex) =
-    let length = List.length marbles
-    let toRemove = 
-        if currentMarbleIndex < 7 then length - (7 - currentMarbleIndex)
-        else currentMarbleIndex - 7
-    (marbles.[0..toRemove-1] @ marbles.[toRemove+1..], toRemove), marbles.[toRemove]
+type Place = {
+    mutable previous: Place
+    value: int64
+    mutable next: Place
+} with
+    member __.Rotate =
+        function
+        | 0 -> __
+        | n when n > 0 -> __.next.Rotate (n - 1)
+        | n -> __.previous.Rotate (n + 1)
+    member __.Append marble =
+        let newPlace = {
+            next = __.next
+            value = marble
+            previous = __
+        }
+        __.next.previous <- newPlace
+        __.next <- newPlace
+        newPlace
+    member __.Remove =
+        __.previous.next <- __.next
+        __.next.previous <- __.previous
+        __.previous, __.value
      
 let addScore player score scores = 
-    let existing = match Map.tryFind player scores with Some n -> n | _ -> 0
+    let existing = match Map.tryFind player scores with Some n -> n | _ -> 0L
     Map.add player (existing + score) scores
 
 
-let part1 players maxMarble = 
-    let _, finalScores =
-        [1..maxMarble] |> List.fold (fun (board, scores) marble -> 
-            let player = (marble - 1) % players
-            if marble % 23 = 0 then
-                let next, removed = scoreMarble board
-                next, addScore player (removed + marble) scores
-            else
-                placeMarble marble board, scores
-            ) (([0],0), Map.empty)
-    finalScores |> Map.toList |> List.map snd |> List.max
+let maxScore players maxMarble = 
+    let start = { next = Unchecked.defaultof<Place>; value = 0L; previous = Unchecked.defaultof<Place> }
+    start.next <- start
+    start.previous <- start
 
-let part2 _ = 0
+    let _, finalScores =
+        [1L..maxMarble] |> List.fold (fun ((current : Place), scores) marble ->
+            if marble % 23L = 0L then
+                let player = (marble - 1L) % players
+                let next, removed = (current.Rotate -7).Remove
+                next.Rotate 1, addScore player (removed + marble) scores
+            else
+                let next = (current.Rotate 1).Append marble
+                next, scores) (start, Map.empty)
+    finalScores |> Map.toList |> List.map snd |> List.max
 
 [<EntryPoint>]
 let main _ =
 
     let input = File.ReadAllText "input.txt"
     let players, maxMarble = 
-        match run (pint32 .>> pstring " players; last marble is worth " .>>. pint32 .>> pstring " points") input with
+        match run (pint64 .>> pstring " players; last marble is worth " .>>. pint64 .>> pstring " points") input with
         | Success (r, _, _) -> r
         | _ -> failwith "invalid input"
 
-    printfn "part 1: %i" <| part1 players maxMarble
-    printfn "part 2: %i" <| part2 0
+    printfn "part 1: %i" <| maxScore players maxMarble
+    printfn "part 2: %i" <| maxScore players (maxMarble*100L)
 
     0
--- Day9/readme.md
+++ Day9/readme.md
@@ -52,3 +52,9 @@ Here are a few more examples:
 ## Part One
 
 What is the winning Elf's score?
+
+## Part Two
+
+Amused by the speed of your answer, the Elves are curious:
+
+What would the new winning Elf's score be if the number of the last marble were 100 times larger?

19813cbcc32447424124be6beb698a7cb66d8da6
Chris Pritchard <chris.pritchard@edpay.nz>
2018-12-14 15:34:35 +1300

    interim work on part 1 of day 10

--- Day10/Program.fs
+++ Day10/Program.fs
@@ -9,7 +9,32 @@ let part2 _ = 0
 [<EntryPoint>]
 let main _ =
 
-    let input = File.ReadAllText "input.txt"
+    let pnum = spaces >>.pint32
+    let pcoord = (pnum .>> pstring ", ") .>>. pnum
+    let pline = (pstring "position=<" >>. pcoord) .>>. (pstring "> velocity=<" >>. pcoord .>> pchar '>')
+
+    let lines = 
+        File.ReadAllLines "input.txt"
+        |> Seq.map (run pline >> function Success (r, _, _) -> r | Failure (ex, _, _) -> failwith ex) 
+        |> Seq.toList
+
+    let tick = List.map (fun ((x, y), (dx, dy)) -> (x + dx, y + dy), (dx, dy))
+
+    let render stars = 
+        let (mx, my, maxx, maxy) = 
+            stars 
+            |> List.fold (fun (ox, oy, omx, omy) ((x, y), _) -> 
+                (if x < ox then x else ox),
+                (if y < oy then y else oy),
+                (if x > omx then x else omx),
+                (if y > omy then y else omy)) 
+                (0, 0, Int32.MaxValue, Int32.MaxValue)
+        let map = Map.ofList stars
+        [my..maxy] |> List.collect (fun y ->
+            "\n"::([mx..maxx] |> List.map (fun x -> match Map.tryFind (x, y) map with Some _ -> "#" | _ -> " ")))
+            |> System.String.Concat
+
+    lines |> tick |> tick |> tick |> render |> printfn "%s"
 
     printfn "part 1: %i" <| part1 ()
     printfn "part 2: %i" <| part2 ()

0a609f1a4bdfa8e2bcbc17f43cecd97a45b097a8
Christopher <chrispritchard.nz@gmail.com>
2018-12-15 08:20:31 +1300

    Successfully implemented part 1 of day 11

--- Day11/Program.fs
+++ Day11/Program.fs
@@ -1,6 +1,25 @@
 ﻿open System
 
+let power sn x y =
+    let rackId = x + 10
+    let power = ((rackId * y) + sn) * rackId
+    let hundreds = power / 100 % 10
+    hundreds - 5
+
 [<EntryPoint>]
 let main _ =
     let input = 8772
+    let grid = 
+        [1..300] |> List.collect (fun x -> 
+        [1..300] |> List.map (fun y -> 
+            (x, y), power input x y))
+            |> Map.ofList
+    let max =
+        [1..298] |> List.collect (fun x -> 
+        [1..298] |> List.map (fun y -> 
+            (x, y), [0..2] |> List.collect (fun dx ->
+            [0..2] |> List.map (fun dy -> grid.[x + dx, y + dy]))
+            |> List.sum))
+        |> List.maxBy snd
+    printfn "%i, %i" <|| fst max
     0

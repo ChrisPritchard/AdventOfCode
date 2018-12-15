7c940bc37a5d231ff0201769f7980433958fe943
Chris Pritchard <chrispritchard.nz@gmail.com>
2018-12-16 11:53:19 +1300

    Wrote parsers for day 12

---
+++ Day12/Program.fs
@@ -0,0 +1,25 @@
+﻿open System
+open System.IO
+open FParsec.CharParsers
+open FParsec
+
+[<EntryPoint>]
+let main _ =
+    
+    let input = File.ReadAllLines "input.txt"
+    let initialState = 
+        match run (pstring "initial state: " >>. many anyChar) input.[0] with 
+        | Success (r, _, _) -> r |> List.map ((=) '#')
+        | _ -> failwith "invalid line"
+
+    let lineParser line = 
+        match run ((parray 5 anyChar .>> pstring " => ") .>>. anyChar) line with
+        | Success ((pattern, result), _, _) -> 
+            pattern |> Array.toList |> List.map ((=) '#'), result = '#'
+        | _ -> failwith "invalid line"
+
+    let rules = input.[2..] |> Array.map lineParser |> Map.ofArray
+
+    printfn "%A" initialState
+    
+    0

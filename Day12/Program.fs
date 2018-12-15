ac66e3a9dd2a3124bc12db90785ec9f9fae2b5f1
Chris Pritchard <chrispritchard.nz@gmail.com>
2018-12-16 12:12:03 +1300

    wrote the advance method

--- Day12/Program.fs
+++ Day12/Program.fs
@@ -9,8 +9,15 @@ let main _ =
     let input = File.ReadAllLines "input.txt"
     let initialState = 
         match run (pstring "initial state: " >>. many anyChar) input.[0] with 
-        | Success (r, _, _) -> r |> List.map ((=) '#')
+        | Success (r, _, _) -> r |> List.mapi (fun i c -> i, c = '#')
         | _ -> failwith "invalid line"
+
+    let paddedState = 
+        ([-5..-1] |> List.map (fun i -> i, false))
+        @
+        initialState
+        @
+        ([1..5] |> List.map (fun i -> List.length initialState + i, false))
 
     let lineParser line = 
         match run ((parray 5 anyChar .>> pstring " => ") .>>. anyChar) line with
@@ -19,6 +26,17 @@ let main _ =
         | _ -> failwith "invalid line"
 
     let rules = input.[2..] |> Array.map lineParser |> Map.ofArray
+
+    let advance state rules =
+        let next = 
+            state 
+            |> List.windowed 5 
+            |> List.map (fun segment ->
+                let key = List.map snd segment 
+                match Map.tryFind key rules with
+                | Some n -> fst segment.[2], n
+                | _ -> segment.[2])
+        state.[0..1] @ next @ state.[state.Length - 2..]
 
     printfn "%A" initialState
     

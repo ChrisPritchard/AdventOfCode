0b0e07aba180e1be8ede392dbf77fa88e337ce4a
Christopher <chrispritchard.nz@gmail.com>
2018-12-16 08:38:11 +1300

    brute force impl

--- Day11/Program.fs
+++ Day11/Program.fs
@@ -15,7 +15,9 @@ let main _ =
     
     let squareVal size x y =
         [0..size-1] |> List.collect (fun dx ->
-        [0..size-1] |> List.map (fun dy -> grid.[x + dx, y + dy])) |> List.sum
+        [0..size-1] |> List.map (fun dy -> 
+            Map.tryFind (x + dx, y + dy) grid |> Option.defaultValue 0)) 
+        |> List.sum
 
     let part1 =
         [1..298] |> List.collect (fun x -> 
@@ -24,25 +26,12 @@ let main _ =
     
     printfn "part1: %i,%i" <|| fst part1
 
-    let sums = range |> List.fold (fun (sums : Map<int * int, int>) (x, y) ->
-        let sum = 
-            grid.[x, y]
-            + (Map.tryFind (x, y - 1) sums |> Option.defaultValue 0)
-            + (Map.tryFind (x - 1, y) sums |> Option.defaultValue 0)
-            - (Map.tryFind (x - 1, y - 1) sums |> Option.defaultValue 0)
-        Map.add (x, y) sum sums) (Map.empty<int * int, int> |> Map.add (0, 0) 0)
-    
     let part2 = 
-        range 
-        |> List.collect (fun (x, y) -> [0..300] |> List.map (fun s ->
-            let total = 
-                sums.[x, y]
-                 - (Map.tryFind (x, y - s) sums |> Option.defaultValue 0)
-                 - (Map.tryFind (x - s, y) sums |> Option.defaultValue 0)
-                 + (Map.tryFind (x - s, y - s) sums |> Option.defaultValue 0)
-            (total, x, y, s)))
-        |> List.maxBy (fun (total,_,_,_) -> total)
-        |> fun (_, x, y, s) -> x, y, s
+        [1..300] |> List.collect (fun s ->
+        [1..300] |> List.collect (fun x -> 
+        [1..300] |> List.map (fun y -> (x, y, s, squareVal s x y))))
+        |> List.maxBy (fun (_,_,_,t) -> t)
+        |> fun (x,y,s,_) -> x,y,s
 
     printfn "part2: %i,%i,%i" <||| part2
 

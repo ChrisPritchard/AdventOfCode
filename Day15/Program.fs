817c7fa8d78f764fbe5224c96b63322d8ce1cd2e
Chris Pritchard <chrispritchard.nz@gmail.com>
2018-12-16 21:24:43 +1300

    added astar for search

--- Day15/Program.fs
+++ Day15/Program.fs
@@ -1,10 +1,15 @@
 ﻿open System.IO
+open AStar
 
 type Fighter = 
     | Goblin of x: int * y: int * health: int 
     | Elf of x: int * y: int * health: int 
 
 let startHealth = 200
+
+let gScore _ _ = 1.
+let fScore (x, y) (gx, gy) = 
+    sqrt ((float gx - float x)**2. + (float gy - float y)**2.)
 
 [<EntryPoint>]
 let main _ =
@@ -25,5 +30,23 @@ let main _ =
         fighters |> List.sortBy (function 
             | Goblin (x, y, _) -> y, x 
             | Elf (x, y, _) -> y, x)
+    
+    let samePos (x, y) = 
+        function 
+        | Goblin (ox, oy, _) -> x = ox && y = oy
+        | Elf (ox, oy, _) -> x = ox && y = oy
+
+    let neighbours (x, y) =
+        [(-1,0);(1,0);(0,-1);(0,1)] |> Seq.filter (fun (dx, dy) ->
+            let nx, ny = x + dx, y + dy
+            match 
+                Set.contains (nx, ny) walls, 
+                List.tryFind (samePos (nx, ny)) fighters with
+            | false, None -> true
+            | _ -> false)
+
+    let path start goal = 
+        AStar.search start goal 
+        <| { neighbours = neighbours; gCost = gScore; fCost = fScore; maxIterations = None }
 
     0

0b925bc83d1b2a594e4855d50aba07bfe1af4915
Chris Pritchard <chrispritchard.nz@gmail.com>
2018-12-16 14:36:35 +1300

    interim work on part 1 of day 13

--- Day13/Program.fs
+++ Day13/Program.fs
@@ -1,11 +1,59 @@
 ﻿open System
 open System.IO
 
+let isCart = 
+    function 
+    | '^' | 'v'
+    | '<' | '>' -> true
+    | _ -> false
+
 let underrail =
     function
     | '^' | 'v' -> '|'
     | '<' | '>' -> '-'
     | c -> c
+
+let next ((x,y),c) =
+    match c with
+    | '^' -> x,y-1
+    | 'v' -> x,y+1
+    | '<' -> x-1,y
+    | '>' -> x+1,y
+    | _ -> failwith "invalid cart"
+
+let checkchange t =
+    function
+    | '^' ->
+        match t with 
+        | '/' -> '>'
+        | '\\' -> '<'
+        | '|' -> '^'
+        | _ -> 'X'
+    | 'v' ->
+        match t with 
+        | '/' -> '<'
+        | '\\' -> '>'
+        | '|' -> 'v'
+        | _ -> 'X'
+    | '<' ->
+        match t with 
+        | '/' -> 'v'
+        | '\\' -> '^'
+        | '-' -> '<'
+        | _ -> 'X'
+    | '>' ->
+        match t with 
+        | '/' -> '^'
+        | '\\' -> 'v'
+        | '-' -> '>'
+        | _ -> 'X'
+    | _ -> failwith "invalid cart"
+
+type Cart = {
+    pos: int * int
+    dir: char
+    inter: int
+}
 
 [<EntryPoint>]
 let main _ =
@@ -14,10 +62,22 @@ let main _ =
     let tiles =
         input |> Seq.mapi (fun y line -> 
         line |> Seq.mapi (fun x c -> (x, y), c))
-        |> Seq.collect id |> Seq.toList
+        |> Seq.collect id |> Set.toList
+
+    let rails = tiles |> List.map underrail |> Map.ofList
+    let carts = tiles |> List.filter isCart |> List.map (fun (p,c) -> 
+        { pos = p; dir = c; inter = 0 })
     
-    let carts = tiles |> List.filter (fun (_, c) -> 
-        match c with '>' | '<' | '^' | 'v' -> true | _ -> false)
-    let tileMap = tiles |> List.map (fun (p, c) -> p, underrail c) |> Map.ofList
+    let height, width = Seq.length input, Seq.length input.[0]
+    let orderedPoints = [0..height] |> List.collect (fun y -> [0..width] |> List.map (fun x -> x, y))
+
+    let rec firstCrash tiles = 
+        let next =
+            orderedPoints |> List.fold (fun state p -> 
+                match List.tryFind (fun c -> c.pos = p) carts with
+                | None -> state
+                | Some c -> 
+
+                else state)
 
     0

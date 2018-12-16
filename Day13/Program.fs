35a04162fb298f8dc38c693413ad6ff21e655d7e
Chris Pritchard <chrispritchard.nz@gmail.com>
2018-12-16 13:44:49 +1300

    interim work on part 1 of day 13

---
+++ Day13/Program.fs
@@ -0,0 +1,23 @@
+﻿open System
+open System.IO
+
+let underrail =
+    function
+    | '^' | 'v' -> '|'
+    | '<' | '>' -> '-'
+    | c -> c
+
+[<EntryPoint>]
+let main _ =
+    
+    let input = File.ReadAllLines "input.txt"
+    let tiles =
+        input |> Seq.mapi (fun y line -> 
+        line |> Seq.mapi (fun x c -> (x, y), c))
+        |> Seq.collect id |> Seq.toList
+    
+    let carts = tiles |> List.filter (fun (_, c) -> 
+        match c with '>' | '<' | '^' | 'v' -> true | _ -> false)
+    let tileMap = tiles |> List.map (fun (p, c) -> p, underrail c) |> Map.ofList
+
+    0

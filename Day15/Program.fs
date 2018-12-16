06fc16c2a42c0c51c672f0f388efdf3959ac2d14
Chris Pritchard <chrispritchard.nz@gmail.com>
2018-12-16 21:01:47 +1300

    implemented mapping of inputs to data

--- Day15/Program.fs
+++ Day15/Program.fs
@@ -1,6 +1,26 @@
 ﻿open System.IO
 
+type Fighter = {
+    pos: int * int
+    attack: int
+    health: int
+}
+
+let create pos = { pos = pos; attack = 3; health = 200 }
+
 [<EntryPoint>]
 let main _ =
-    let input = File.ReadAllText "input.txt"
+    let input = File.ReadAllLines "input.txt"
+
+    let walls, goblins, elves = 
+        input 
+        |> Seq.mapi (fun y -> Seq.mapi (fun x c -> (x, y), c))
+        |> Seq.collect id
+        |> Seq.fold (fun (w, g, e) (p,c) ->
+            match c with
+            | '#' -> Set.add p w, g, e
+            | 'G' -> w, (create p)::g, e
+            | 'E' -> w, g, (create p)::e
+            | _ -> w, g, e) (Set.empty, [], [])
+
     0

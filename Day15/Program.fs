a6820f0b9566718be21892f8c9061fc69e3abbd3
Chris Pritchard <chris.pritchard@edpay.nz>
2018-12-18 10:53:07 +1300

    clearing ready for new approach

--- Day15/Program.fs
+++ Day15/Program.fs
@@ -1,28 +1,16 @@
 ﻿open System
 open System.IO
-open AStar
 
 type Fighter = {
-    mutable x: int; mutable y: int; 
-    mutable health: int
+    x: int; 
+    y: int; 
+    health: int
+    attack: int
     kind: FighterKind
 } 
-with member __.Pos = __.x, __.y
 and FighterKind = Goblin | Elf
 
-let attack = 3
-
-let create x y kind = { x = x; y = y; health = 200; kind = kind }
-
-let gScore (_, y1) (_, y2) = 
-    if y1 = y2 then 0.9
-    else 1.
-let fScore (x, y) (gx, gy) = 
-    sqrt ((float gx - float x)**2. + (float gy - float y)**2.)
-
-let strikingDistance fighter enemy = 
-    (fighter.x = enemy.x || fighter.y = enemy.y) &&
-    abs (fighter.x - enemy.x) <= 1 && abs (fighter.y - enemy.y) <= 1
+let create x y kind = { x = x; y = y; health = 200; attack = 3; kind = kind }
 
 let render walls fighters = //()
     Console.CursorVisible <- false
@@ -37,7 +25,6 @@ let render walls fighters = //()
         Console.CursorLeft <- f.x
         Console.CursorTop <- f.y
         Console.Write (if f.kind = Goblin then 'G' else 'E')
-
 
 [<EntryPoint>]
 let main _ =
@@ -54,59 +41,8 @@ let main _ =
             | 'E' -> w, (create x y Elf)::f
             | _ -> w, f) (Set.empty, [])
 
-    let neighbours goal (x, y) =
-        [(-1,0);(1,0);(0,-1);(0,1)] 
-        |> Seq.map (fun (dx, dy) -> x + dx, y + dy)
-        |> Seq.filter (fun  (nx, ny) ->
-            match 
-                Set.contains (nx, ny) walls, 
-                List.tryFind (fun (f : Fighter) -> (nx, ny) <> goal && f.x = nx && f.y = ny) fighters with
-            | false, None -> true
-            | _ -> false)
-
-    let path fighter goal = 
-        AStar.search (fighter.x, fighter.y) goal { neighbours = neighbours goal; gCost = gScore; fCost = fScore; maxIterations = None }
-
-    let advance others fighter =
-        let enemyKind = match fighter.kind with Elf -> Goblin | _ -> Elf
-        let target = 
-            others |> List.filter 
-                (fun f -> f.kind = enemyKind && f.health > 0 && strikingDistance fighter f)
-                |> List.sortBy (fun f -> f.health, f.y, f.x)
-                |> List.tryHead
-        match target with
-        | Some e ->
-            e.health <- e.health - attack
-        | None ->
-            let goal = 
-                others 
-                |> List.filter (fun f -> f.kind = enemyKind && f.health > 0) 
-                |> List.map (fun e -> 
-                    match path fighter e.Pos with
-                    | Some p -> 
-                        Some (p |> Seq.rev |> Seq.toList, e)
-                    | None -> None)
-                |> List.choose id
-                |> List.sortBy (fun (p, _) -> 
-                    match p with (_::(x, y)::_) -> List.length p, y, x | _ -> 100,100,100)
-                |> List.tryHead
-            match goal with
-            | Some (_::(x, y)::_, _) -> 
-                fighter.x <- x
-                fighter.y <- y
-            | _ -> ()
-        fighter
-
     let rec game walls fighters turn =
-        render walls fighters
-        let next =
-            fighters 
-            |> List.sortBy (fun f -> f.y, f.x)
-            |> List.map (fun f -> if f.health > 0 then advance fighters f else f)
-            |> List.filter (fun f -> f.health > 0)
-        match next |> List.groupBy (fun f -> f.kind) |> List.length with
-        | 1 -> turn, next |> List.sumBy (fun f -> f.health)
-        | _ -> game walls next (turn + 1)
+        0, 0
 
     printfn "part 1: turn %i health %i" <|| game walls fighters 0
     0

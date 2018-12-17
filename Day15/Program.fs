fac1fbb7b1a3c5ad0719bb90e05cda8d08bfa19a
Chris Pritchard <chris.pritchard@edpay.nz>
2018-12-17 13:30:34 +1300

    Fixed pathing issue

--- Day15/Program.fs
+++ Day15/Program.fs
@@ -33,37 +33,37 @@ let main _ =
             | _ -> w, f) (Set.empty, [])
 
     let neighbours ignored (x, y) =
-        [(-1,0);(1,0);(0,-1);(0,1)] |> Seq.filter (fun (dx, dy) ->
-            let nx, ny = x + dx, y + dy
+        [(-1,0);(1,0);(0,-1);(0,1)] 
+        |> Seq.map (fun (dx, dy) -> x + dx, y + dy)
+        |> Seq.filter (fun  (nx, ny) ->
             match 
                 Set.contains (nx, ny) walls, 
-                List.tryFind (fun f -> ignored <> f && f.x = nx && f.y = ny) fighters with
+                List.tryFind (fun (f : Fighter) -> List.contains f.Pos ignored |> not && f.x = nx && f.y = ny) fighters with
             | false, None -> true
             | _ -> false)
 
     let path fighter goal = 
-        AStar.search (fighter.x, fighter.y) goal { neighbours = neighbours fighter; gCost = gScore; fCost = fScore; maxIterations = Some 10 }
-        |> Option.map Seq.toList
+        AStar.search (fighter.x, fighter.y) goal { neighbours = neighbours [fighter.Pos; goal]; gCost = gScore; fCost = fScore; maxIterations = Some 10 }
 
     let advance others fighter =
         let enemyKind = match fighter.kind with Elf -> Goblin | _ -> Elf
         let opponent = 
             others 
-            |> List.filter (fun f -> f.kind = enemyKind) 
+            |> List.filter (fun f -> f.kind = enemyKind && f.health > 0) 
             |> List.map (fun e -> 
                 match path fighter e.Pos with
                 | Some p -> 
-                    Some (p, e)
+                    Some (p |> Seq.toList, e)
                 | None -> None)
             |> List.choose id
             |> List.sortBy (fun (p, _) -> 
-                match p with ((x, y)::_) -> List.length p, y, x | _ -> 100,100,100)
+                match p with (_::(x, y)::_) -> List.length p, y, x | _ -> 100,100,100)
             |> List.tryHead
         match opponent with
-        | Some ([_], e) -> 
+        | Some ([_;_], e) -> 
             e.health <- e.health - attack
             fighter
-        | Some ((x, y)::_, _) -> 
+        | Some (_::(x, y)::_, _) -> 
             { fighter with x = x; y = y }
         | _ -> fighter
 
@@ -71,10 +71,10 @@ let main _ =
         let next =
             fighters 
             |> List.sortBy (fun f -> f.y, f.x)
-            |> List.map (advance fighters)
+            |> List.map (fun f -> if f.health > 0 then advance fighters f else f)
             |> List.filter (fun f -> f.health > 0)
-        match next with
-        | [_] -> turn
+        match next |> List.groupBy (fun f -> f.kind) |> List.length with
+        | 1 -> turn
         | _ -> game walls next (turn + 1)
 
     printfn "part 1: %i" <| game walls fighters 0

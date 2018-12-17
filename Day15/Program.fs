ec13099d841503f5b05b61c0d77bb1def31ade19
Chris Pritchard <chris.pritchard@edpay.nz>
2018-12-18 12:51:06 +1300

    Blanked fscore, and accounted for the dead in pathfinding

--- Day15/Program.fs
+++ Day15/Program.fs
@@ -17,15 +17,15 @@ let create x y kind = { x = x; y = y; health = 200; kind = kind }
 let gScore (_, y1) (_, y2) = 
     if y1 = y2 then 0.9
     else 1.
-let fScore (x, y) (gx, gy) = 
-    abs (x - gx) + abs (y - gy) |> float
+let fScore _ _ = 0.
+
 let strikingDistance fighter enemy = 
     (fighter.x = enemy.x || fighter.y = enemy.y) &&
     abs (fighter.x - enemy.x) <= 1 && abs (fighter.y - enemy.y) <= 1
 
-let render walls fighters = //()
+let render walls fighters turn = //()
     Console.CursorVisible <- false
-    System.Threading.Thread.Sleep 500
+    System.Threading.Thread.Sleep 200
     Console.Clear ()
     Console.CursorLeft <- 0
     for (x, y) in walls do
@@ -36,7 +36,15 @@ let render walls fighters = //()
         Console.CursorLeft <- f.x
         Console.CursorTop <- f.y
         Console.Write (if f.kind = Goblin then 'G' else 'E')
-
+    for row in fighters |> List.groupBy (fun f -> f.y) do
+        for (i, f) in snd row |> List.sortBy (fun f -> f.x) |> List.mapi (fun i f -> i, f) do
+        Console.CursorTop <- f.y
+        Console.CursorLeft <- 9 + i * 8
+        Console.Write (sprintf "%s (%i)" (if f.kind = Goblin then "G" else "E") f.health)
+    Console.CursorTop <- 9
+    Console.CursorLeft <- 2
+    Console.Write (sprintf "turn %i" turn)
+    Console.ReadKey true
 
 [<EntryPoint>]
 let main _ =
@@ -59,7 +67,7 @@ let main _ =
         |> Seq.filter (fun  (nx, ny) ->
             match 
                 Set.contains (nx, ny) walls, 
-                List.tryFind (fun (f : Fighter) -> (nx, ny) <> goal && f.x = nx && f.y = ny) fighters with
+                List.tryFind (fun (f : Fighter) -> f.health > 0 && (nx, ny) <> goal && f.x = nx && f.y = ny) fighters with
             | false, None -> true
             | _ -> false)
 
@@ -90,6 +98,10 @@ let main _ =
                     match p with (_::(x, y)::_) -> List.length p, y, x | _ -> 100,100,100)
                 |> List.tryHead
             match goal with
+            | Some ([_;(x, y);_], e) -> 
+                fighter.x <- x
+                fighter.y <- y
+                e.health <- e.health - attack
             | Some (_::(x, y)::_, _) -> 
                 fighter.x <- x
                 fighter.y <- y
@@ -97,15 +109,19 @@ let main _ =
         fighter
 
     let rec game walls fighters turn =
-        render walls fighters
         let next =
             fighters 
             |> List.sortBy (fun f -> f.y, f.x)
             |> List.map (fun f -> if f.health > 0 then advance fighters f else f)
             |> List.filter (fun f -> f.health > 0)
         match next |> List.groupBy (fun f -> f.kind) |> List.length with
-        | 1 -> turn, next |> List.sumBy (fun f -> f.health)
-        | _ -> game walls next (turn + 1)
+        | 1 -> 
+            //render walls next (turn + 1) |> ignore
+            turn + 1, next |> List.sumBy (fun f -> f.health)
+        | _ -> 
+            //render walls next (turn + 1) |> ignore
+            game walls next (turn + 1)
 
-    printfn "part 1: turn %i health %i" <|| game walls fighters 0
+    let turn, health = game walls fighters 0
+    printfn "part 1: turn %i health %i score %i" turn health (turn * health)
     0

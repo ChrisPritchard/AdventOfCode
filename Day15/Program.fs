1060d64350d48d0743e4346cc3d4852030c768b5
Chris Pritchard <chrispritchard.nz@gmail.com>
2018-12-19 21:57:37 +1300

    rework search to use bread first

--- Day15/Program.fs
+++ Day15/Program.fs
@@ -1,5 +1,4 @@
-﻿open System
-open System.IO
+﻿open System.IO
 
 type Fighter = {
     x: int; y: int; 
@@ -12,65 +11,49 @@ and FighterKind = Goblin | Elf
 
 let create x y kind = { x = x; y = y; health = 200; attack = 3; kind = kind }
 
-// let render walls fighters turn (width, height) =
-//     System.Threading.Thread.Sleep 100
-
-//     for y = 0 to height-1 do
-//         Console.CursorTop <- y   
-//         for x = 0 to width-1 do
-//             Console.CursorLeft <- x         
-//             let c =
-//                 if Set.contains (x, y) walls then '#'
-//                 else
-//                     match Array.tryFind (fun e -> e.health > 0 && e.x = x && e.y = y) fighters with
-//                     | Some f when f.kind = Goblin -> 'G'
-//                     | Some f when f.kind = Elf -> 'E'
-//                     | _ -> '.'
-//             Console.Write c
-//         Console.CursorLeft <- width + 1
-//         Console.Write "                                    "
-
-//     for row in fighters |> Seq.where (fun f -> f.health > 0) |> Seq.groupBy (fun f -> f.y) do
-//         for (i, f) in snd row |> Seq.sortBy (fun f -> f.x) |> Seq.mapi (fun i f -> i, f) do
-//         Console.CursorTop <- f.y
-//         Console.CursorLeft <- (width + 2) + i * 8
-//         Console.Write (sprintf "%s (%i)" (if f.kind = Goblin then "G" else "E") f.health)
-
-//     Console.CursorTop <- height + 1
-//     Console.CursorLeft <- 2
-//     Console.Write (sprintf "turn %i" turn)
-//     //Console.ReadKey true
-
-let blockers walls (fighters : seq<Fighter>) start enemies =
+let blockers walls (fighters : seq<Fighter>) start =
     fighters 
-    |> Seq.filter (fun f -> not (List.contains f enemies) && f.health > 0)
+    |> Seq.filter (fun f -> f.health > 0)
     |> Seq.map (fun f -> f.Pos)
     |> Set.ofSeq
     |> Set.union walls
     |> Set.add start
 
-let rec findPaths (fighter : Fighter) (enemies : seq<Fighter * int>) blockers =
+let findPaths (fighter : Fighter) (enemies : seq<Fighter * int>) blockers =
     let enemyMap = enemies |> Seq.map (fun (e, i) -> e.Pos, (e, i)) |> Map.ofSeq
-    let mutable maxLen = 0
-    let rec search (x, y) blockers soFar =
-        [
-            if maxLen <> 0 && List.length soFar >= maxLen then ()
-            else
-                let neighbours = 
-                    [-1,0; 1,0; 0,-1; 0,1]
-                    |> List.map (fun (dx, dy) -> x + dx, y + dy)
-                    |> List.filter (fun p -> not <| Set.contains p blockers)
-                let newBlockers = Set.union blockers <| Set.ofList neighbours
-                yield! neighbours |> Seq.collect (fun d -> 
-                    if enemyMap.ContainsKey d then 
-                        maxLen <- List.length soFar + 1
-                        [d::soFar |> List.rev]
-                    else search d newBlockers (d::soFar))
-        ]
-    search fighter.Pos blockers []
-    |> List.map (fun p -> 
-        let e, i = enemyMap.[p.[p.Length-1]]
-        p, e, i)
+
+    let expander (paths, closed, found) path =
+        let (x, y) = match path with [] -> fighter.Pos | head::_ -> head
+        let next = 
+            [-1, 0; 1, 0; 0, -1; 0, 1] 
+            |> List.map (fun (dx, dy) -> x + dx, y + dy)
+        let results = next |> List.filter (fun p -> Map.containsKey p enemyMap)
+        if List.isEmpty results then
+            let used = List.filter (fun p -> not <| Set.contains p closed) next
+            List.map (fun p -> p::path) used @ paths, 
+            Set.union closed <| Set.ofList used, 
+            found
+        else
+            let resultPaths = List.map (fun p -> p::path) results
+            paths, closed, resultPaths @ found
+
+    let rec expand (pathsSoFar : (int * int) list list) closedSoFar =
+        let (nextPaths, nextBlockers, results) =
+            pathsSoFar |> List.fold expander ([], closedSoFar, [])
+        if not <| List.isEmpty results then
+            Some results
+        else if not <| List.isEmpty nextPaths then
+            expand nextPaths nextBlockers
+        else
+            None
+
+    match expand [[]] blockers with
+    | None -> []
+    | Some results ->
+        results |> List.map (fun p -> 
+            let e, i = enemyMap.[List.head p]
+            List.rev p, e, i)
+        
 
 [<EntryPoint>]
 let main _ =
@@ -92,11 +75,7 @@ let main _ =
     let mutable index = 0;
     let mutable fighters = start |> List.sortBy (fun f -> f.y, f.x) |> List.toArray
 
-    // Console.Clear ()
-    // Console.CursorVisible <- false
-
     while not gameOver do
-        //render walls fighters turn (input.[0].Length, input.Length) |> ignore
         let fighter = fighters.[index]
         if fighter.health > 0 then
             let enemyKind = match fighter.kind with Elf -> Goblin | _ -> Elf
@@ -109,7 +88,7 @@ let main _ =
             if enemies.Length = 0 then
                 gameOver <- true
             else
-                let blockers = blockers walls fighters fighter.Pos (enemies |> Seq.map fst |> Seq.toList)
+                let blockers = blockers walls fighters fighter.Pos
                 let targets = findPaths fighter enemies blockers
                 let adjacent = 
                     targets 

10d5bb34c802312d267260326412b1a5c277a522
Chris Pritchard <chris.pritchard@edpay.nz>
2018-12-20 12:45:07 +1300

    interim work on part 2

--- Day15/Program.fs
+++ Day15/Program.fs
@@ -1,48 +1,14 @@
-﻿open System
-open System.IO
+﻿open System.IO
 
 type Fighter = {
     x: int; y: int; 
     health: int
-    attack: int
     kind: FighterKind
 } 
 with member __.Pos = __.x, __.y
 and FighterKind = Goblin | Elf
 
-let create x y kind = { x = x; y = y; health = 200; attack = 3; kind = kind }
-
-let render walls fighters current turn (width, height) =
-    //Console.Clear ()
-    Console.CursorVisible <- false
-    for y = 0 to height-1 do
-        Console.CursorTop <- y   
-        for x = 0 to width-1 do
-            Console.CursorLeft <- x         
-            let c =
-                if Set.contains (x, y) walls then '#'
-                else
-                    match Array.tryFind (fun e -> e.health > 0 && e.x = x && e.y = y) fighters with
-                    | Some f when Array.findIndex ((=) f) fighters = current -> 'X'
-                    | Some f when f.kind = Goblin -> 'G'
-                    | Some f when f.kind = Elf -> 'E'
-                    | _ -> '.'
-            Console.Write c
-        Console.CursorLeft <- width + 1
-        //Console.Write "                                    "
-
-    // for row in fighters |> Seq.where (fun f -> f.health > 0) |> Seq.groupBy (fun f -> f.y) do
-    //     for (i, f) in snd row |> Seq.sortBy (fun f -> f.x) |> Seq.mapi (fun i f -> i, f) do
-    //     Console.CursorTop <- f.y
-    //     Console.CursorLeft <- (width + 2) + i * 12
-    //     Console.Write (sprintf "%s (%i, %i: %i)" (if f.kind = Goblin then "G" else "E") f.x f.y f.health)
-
-    Console.CursorTop <- height + 1
-    Console.CursorLeft <- 2
-    Console.Write (sprintf "turn %i" turn)
-
-    // System.Threading.Thread.Sleep 33
-    Console.ReadKey true
+let create x y kind = { x = x; y = y; health = 200; kind = kind }
 
 let blockers walls (fighters : seq<Fighter>) start =
     fighters 
@@ -98,28 +64,13 @@ let strikeTarget (x, y) enemyMap =
     |> List.sortBy (fun (e, _) -> e.health, e.y, e.x)
     |> List.tryHead
 
-[<EntryPoint>]
-let main _ =
-    let input = File.ReadAllLines "input.txt"
-
-    let walls, start = 
-        input 
-        |> Seq.mapi (fun y -> Seq.mapi (fun x c -> x, y, c))
-        |> Seq.collect id
-        |> Seq.fold (fun (w, f) (x, y, c) ->
-            match c with
-            | '#' -> Set.add (x, y) w, f
-            | 'G' -> w, (create x y Goblin)::f
-            | 'E' -> w, (create x y Elf)::f
-            | _ -> w, f) (Set.empty, [])
-
+let runGame walls start (attack : Map<FighterKind, int>) = 
     let mutable gameOver = false
     let mutable turn = 0
     let mutable index = 0;
     let mutable fighters = start |> List.sortBy (fun f -> f.y, f.x) |> List.toArray
 
     while not gameOver do
-        //render walls fighters index turn (input.[0].Length, input.Length) |> ignore
         let fighter = fighters.[index]
         if fighter.health > 0 then
 
@@ -136,7 +87,7 @@ let main _ =
                 let enemyMap = enemies |> Seq.map (fun (e, i) -> e.Pos, (e, i)) |> Map.ofSeq
                 match strikeTarget fighter.Pos enemyMap with
                 | Some (e, i) ->
-                    fighters.[i] <- { e with health = e.health - fighter.attack }
+                    fighters.[i] <- { e with health = e.health - attack.[fighter.kind] }
                 | None ->
                     let blockers = blockers walls fighters fighter.Pos
                     let squares = 
@@ -152,7 +103,7 @@ let main _ =
                         fighters.[index] <- { fighter with x = x; y = y }
                         match strikeTarget (x, y) enemyMap with
                         | Some (e, i) ->
-                            fighters.[i] <- { e with health = e.health - fighter.attack }
+                            fighters.[i] <- { e with health = e.health - attack.[fighter.kind] }
                         | _ -> ()
                     | _ -> ()
 
@@ -162,7 +113,42 @@ let main _ =
             turn <- turn + 1
             fighters <- fighters |> Array.sortBy (fun f -> f.y, f.x)
 
-    let finalHealth = fighters |> Seq.filter (fun f -> f.health > 0) |> Seq.sumBy (fun f -> f.health)
-    printfn "part 1: turn %i health %i score %i" turn finalHealth (turn * finalHealth)
+    turn, fighters        
+
+[<EntryPoint>]
+let main _ =
+    let input = File.ReadAllLines "input.txt"
+
+    let walls, start = 
+        input 
+        |> Seq.mapi (fun y -> Seq.mapi (fun x c -> x, y, c))
+        |> Seq.collect id
+        |> Seq.fold (fun (w, f) (x, y, c) ->
+            match c with
+            | '#' -> Set.add (x, y) w, f
+            | 'G' -> w, (create x y Goblin)::f
+            | 'E' -> w, (create x y Elf)::f
+            | _ -> w, f) (Set.empty, [])
+
+    let part1Attack = [Goblin,3;Elf,3]|> Map.ofList
+    let part1Turn, part1Fighters = runGame walls start part1Attack
+    let finalHealth = part1Fighters |> Seq.filter (fun f -> f.health > 0) |> Seq.sumBy (fun f -> f.health)
+
+    printfn "part 1: turn %i health %i score %i" part1Turn finalHealth (part1Turn * finalHealth)
+
+    let elfCount (fighters : seq<Fighter>) = fighters |> Seq.sumBy (fun f -> if f.kind = Elf && f.health > 0 then 1 else 0)
+    let startCount = elfCount start
+    let mutable elfAttack = 4
+    let mutable found = false
+    while not found do
+        printfn "trying elf attack %i..." elfAttack
+        let part2Attack = [Goblin,3;Elf,elfAttack]|> Map.ofList
+        let part2Turn, part2Fighters = runGame walls start part2Attack
+        if elfCount part2Fighters = startCount then
+            let finalHealth = part2Fighters |> Seq.filter (fun f -> f.health > 0) |> Seq.sumBy (fun f -> f.health)
+            printfn "part 2: turn %i health %i score %i" part2Turn finalHealth (part2Turn * finalHealth)
+            found <- true
+        else
+            elfAttack <- elfAttack + 1
 
     0

a9a8cf49d9157a2a45b5d9efa5d50f97675dcb40
Christopher <chrispritchard.nz@gmail.com>
2018-12-21 08:21:53 +1300

    iterim work on a recursive, fold heavy implementation

--- Day15/Bandits.fs
+++ Day15/Bandits.fs
@@ -4,5 +4,54 @@ type GameResult =
     | Victory of map: string array * turn: int * totalHealth: int
     | ElfDeath of turn: int
 
+type Fighter = private {
+    kind: FighterKind
+    mutable pos: int * int
+    mutable health: int
+}
+with 
+    member __.X = match __.pos with (x, _) -> x
+    member __.Y = match __.pos with (_, y) -> y
+and FighterKind = private | Elf | Goblin
+
+let private create pos kind = { kind = kind; pos = pos; health = 200 }
+
+let private processMap map =
+    map 
+    |> Seq.mapi (fun y line -> line |> Seq.mapi (fun x c -> (x, y, c)))
+    |> Seq.collect id
+    |> Seq.fold (fun (walls, fighters) (x, y, c) -> 
+        match c with
+        | '#' -> Set.add (x, y) walls, fighters
+        | 'G' -> walls, (create (x, y) Goblin)::fighters
+        | 'E' -> walls, (create (x, y) Elf)::fighters
+        | _ -> walls, fighters) (Set.empty, [])
+
+let runFighterTurn (walls, fighters) (prev, gameOver) fighter =
+    if gameOver then
+        fighter::prev, gameOver
+    else
+        let enemies = fighters |> List.filter (fun f -> f.kind <> fighter.kind && f.health > 0)
+        if List.isEmpty enemies then
+            fighter::prev, true
+        else
+            //fighter::prev, true
+
+let runTurn gameState elfAttack shouldFailOnElfDeath =
+    fighters 
+    |> List.sortBy (fun f -> f.Y, f.X)
+    |> List.fold (runFighterTurn gameState) ([], false)
+
 let runGame startMap elfAttack shouldFailOnElfDeath =
-    Victory (startMap, 0, 0)
+    let walls, startFighters = processMap startMap
+
+    let rec runTurns fighters lastTurnCount =
+        let (newFighters, gameOver) = runTurn (walls, fighters) elfAttack shouldFailOnElfDeath
+        if gameOver then 
+            lastTurnCount, newFighters |> List.filter (fun f -> f.health > 0)
+        else
+            runTurns newFighters (lastTurnCount + 1)
+
+    let turn, finalFighters = runTurns startFighters 0
+    let finalMap = composeMap walls finalFighters
+    Victory (startMap, turn, finalFighters)

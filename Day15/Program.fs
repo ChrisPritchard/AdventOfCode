0ef6dae4eae4df8d08c63f31ff0347108032a6be
Christopher <chrispritchard.nz@gmail.com>
2018-12-21 08:00:57 +1300

    expanded program to print problem parts

--- Day15/Program.fs
+++ Day15/Program.fs
@@ -1,9 +1,24 @@
 ﻿open System.IO
+open Bandits
 
 [<EntryPoint>]
 let main _ =
     let input = File.ReadAllLines "input.txt"
 
-    printfn "part1: "
+    match runGame input 3 false with
+    | Victory (_, turn, health) ->
+        printfn "part1: turn %i health %i score %i" turn health (turn * health)
+    | _ -> failwith "Unexpected loss"
+
+    let part2 = 
+        [4..200] |> List.fold (fun prevResult elfAttack ->
+        match prevResult with
+        | _, ElfDeath _ -> elfAttack, runGame input elfAttack true
+        | _, _ -> prevResult) (0, ElfDeath 0)
+
+    match part2 with
+    | elfAttack, Victory (_, turn, health) ->
+        printfn "part2: attack %i turn %i health %i score %i" elfAttack turn health (turn * health)
+    | _ -> ()
 
     0

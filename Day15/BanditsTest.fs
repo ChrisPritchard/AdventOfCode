65f53796e27a471737697a5ae09c6f2d3e605188
Christopher <chrispritchard.nz@gmail.com>
2018-12-21 11:07:45 +1300

    added another test

--- Day15/BanditsTest.fs
+++ Day15/BanditsTest.fs
@@ -32,3 +32,32 @@ let ``First example`` () =
         turn |> should equal 47
         health |> should equal 590
     | _ -> failwith "Unexcepted loss"
+
+[<Fact>]
+let ``Second example`` () =
+    let map = [|
+        "#######"
+        "#G..#E#"
+        "#E#E.E#"
+        "#G.##.#"
+        "#...#E#"
+        "#...E.#"
+        "#######"
+    |]
+
+    let expected = [|
+        "#######"   
+        "#...#E#"
+        "#E#...#"
+        "#.E##.#"
+        "#E..#E#"
+        "#.....#"
+        "#######"
+    |]
+
+    match runGame map 3 false with
+    | Victory (result, turn, health) ->
+        result |> should equal expected 
+        turn |> should equal 37
+        health |> should equal 982
+    | _ -> failwith "Unexcepted loss"

f7be411604f19c1d539b6ee0b4b66a3fc6b637ce
Chris Pritchard <chrispritchard.nz@gmail.com>
2018-12-21 18:05:31 +1300

    successfully implemented part 1 of day 16 so much fucking easier than day 15, oh my GAWD

--- Day16/Program.fs
+++ Day16/Program.fs
@@ -27,10 +27,80 @@ let parseInput input =
         | Input list -> es, list::is) ([], [])
     |> fun (e, i) -> e, List.rev i
 
+let withC c registers cval = 
+    registers |> List.mapi (fun i v -> if i = c then cval else v)
+
+let addr a b c (registers: int list) =
+    registers.[a] + registers.[b] |> withC c registers
+
+let addi a b c (registers: int list) =
+    registers.[a] + b |> withC c registers
+
+let mulr a b c (registers: int list) =
+    registers.[a] * registers.[b] |> withC c registers
+
+let muli a b c (registers: int list) =
+    registers.[a] * b |> withC c registers
+
+let banr a b c (registers: int list) =
+    registers.[a] &&& registers.[b] |> withC c registers
+
+let bani a b c (registers: int list) =
+    registers.[a] &&& b |> withC c registers
+
+let borr a b c (registers: int list) =
+    registers.[a] ||| registers.[b] |> withC c registers
+
+let bori a b c (registers: int list) =
+    registers.[a] ||| b |> withC c registers
+
+let setr a _ c (registers: int list) =
+    registers.[a] |> withC c registers
+
+let seti a _ c (registers: int list) =
+    a |> withC c registers
+
+let gtir a b c (registers: int list) =
+    (if a > registers.[b] then 1 else 0) |> withC c registers
+
+let gtri a b c (registers: int list) =
+    (if registers.[a] > b then 1 else 0) |> withC c registers
+
+let gtrr a b c (registers: int list) =
+    (if registers.[a] > registers.[b] then 1 else 0) |> withC c registers
+
+let eqir a b c (registers: int list) =
+    (if a = registers.[b] then 1 else 0) |> withC c registers
+
+let eqri a b c (registers: int list) =
+    (if registers.[a] = b then 1 else 0) |> withC c registers
+
+let eqrr a b c (registers: int list) =
+    (if registers.[a] = registers.[b] then 1 else 0) |> withC c registers
+
+let ops = [
+    addr; addi; mulr; muli
+    banr; bani; borr; bori
+    setr; seti
+    gtir; gtri; gtrr
+    eqir; eqri; eqrr
+]
+
+let applies op (i: int list, before, after) =
+    let a, b, c = i.[1], i.[2], i.[3]
+    op a b c before = after
+
+let countApplies example = 
+    let result = ops |> List.sumBy (fun o -> if applies o example then 1 else 0)
+    result
+
 [<EntryPoint>]
 let main _ =
     
     let input = File.ReadAllText "input.txt"
-    let examples, instructions = parseInput input
+    let examples, _ = parseInput input
     
+    let part1 = examples |> List.sumBy (fun ex -> if (countApplies ex) >= 3 then 1 else 0)
+    printfn "part1: %i" part1
+
     0

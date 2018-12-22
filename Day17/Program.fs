d591c8cd1bda6c59a41af35af38cd1887d671701
Chris Pritchard <chrispritchard.nz@gmail.com>
2018-12-22 13:55:39 +1300

    initial naieve implementation

--- Day17/Program.fs
+++ Day17/Program.fs
@@ -30,7 +30,7 @@ let mapFromClay clay (miny, maxy, minx, maxx) =
         [x..w] |> List.iter (fun x ->
         [y..h] |> List.iter (fun y -> map.[1 + x-minx,1 + y-miny] <- Clay))
     clay |> List.iter updateMap
-    map
+    map, (500-minx + 1, 0)
 
 let renderMap (map: Tile [,]) = 
     [0..Array2D.length2 map - 1] |> List.map (fun y ->
@@ -53,6 +53,81 @@ let water map =
             | FallingWater | RestingWater -> 1
             | Spring -> 0))
 
+let spaceFree map (x, y) =
+    let maxWidth = Array2D.length1 map
+    let check found dx =
+        match found with
+        | Some _ -> found
+        | None ->
+            match map.[x + dx, y] with
+            | Sand -> Some <| Some (x + dx)
+            | Clay | FallingWater -> Some None
+            | _ -> None
+    match [-1..-1..-x] |> List.fold check None with
+    | Some (Some ox) -> Some (ox, y)
+    | _ -> 
+        match [1..(maxWidth-x)] |> List.fold check None with
+        | Some (Some ox) -> Some (ox, y)
+        | _ -> None
+
+let contained map (x, y) = 
+    let maxWidth = Array2D.length1 map
+    let check soFar dx =
+        match soFar with
+        | Some _ -> soFar
+        | None ->
+            match map.[x + dx, y] with
+            | Clay -> Some true
+            | _ -> 
+                match map.[x + dx, y + 1] with
+                | Clay | RestingWater -> None
+                | _ -> Some false
+    match [-1..-1..-x] |> List.fold check None, [1..(maxWidth-x)] |> List.fold check None with
+    | Some true, Some true -> true
+    | _ -> false
+
+let overflow map (x, y) next =
+    let maxWidth = Array2D.length1 map
+    let check soFar dx =
+        if soFar then 
+            match map.[x + dx, y] with
+            | Sand | FallingWater ->
+                map.[x + dx, y] <- FallingWater
+                match map.[x + dx, y + 1] with
+                | Sand | FallingWater -> 
+                    map.[x + dx, y + 1] <- FallingWater
+                    next (x + dx, y + 1)
+                    false
+                | _ -> true
+            | _ -> false
+        else false
+    [-1..-1..-x] |> List.fold check true |> ignore
+    [1..(maxWidth-x)] |> List.fold check true |> ignore
+
+let rec drop map (x, y) = 
+    if y = (Array2D.length2 map - 1) then ()
+    else
+        match map.[x, y + 1] with
+        | FallingWater -> drop map (x, y + 1)
+        | Clay -> map.[x, y] <- RestingWater
+        | Sand -> 
+            map.[x, y + 1] <- FallingWater
+            drop map (x, y + 1)
+        | RestingWater ->
+            match spaceFree map (x, y + 1) with
+            | Some (ox, oy) -> map.[ox, oy] <- RestingWater
+            | None ->
+                if contained map (x, y) 
+                then map.[x, y] <- RestingWater
+                else overflow map (x, y) (drop map)
+        | Spring -> ()
+
+let rec part1 map spring last1 last2 =
+    drop map spring
+    let count = water map
+    if count = last1 && count = last2 then count
+    else part1 map spring count last1
+
 [<EntryPoint>]
 let main _ =
 
@@ -61,7 +136,9 @@ let main _ =
     let clay = parseInput input
     let bounds = boundsByClay clay
 
-    let map = mapFromClay clay bounds
-    renderMap map |> List.iter (printfn "%s")
+    let map, spring = mapFromClay clay bounds
+    [1..200] |> List.iter (fun _ -> drop map spring)
+    System.IO.File.WriteAllLines ("out.txt", renderMap map)
+    //printfn "part 1: %i" <| part1 map spring 0 0 
 
     0

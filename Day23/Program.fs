11b35ceaf523ace14f12b0e19ccf58e507600734
Christopher <chrispritchard.nz@gmail.com>
2019-01-01 20:11:00 +1300

    implemented part 2 of day 23 using a reducing binary search

--- Day23/Program.fs
+++ Day23/Program.fs
@@ -23,21 +23,45 @@ let main _ =
         |> List.length 
 
     printfn "part 1: %i" inRange
+    
+    let countFor x y z dist = 
+        nanobots 
+            |> List.filter (fun (sx, sy, sz, sr) -> 
+                let distTo = distance sx sy sz x y z
+                (distTo - sr) / dist <= 0L)
+            |> List.length
+
+    let rec findBestPoint dist (minx, maxx, miny, maxy, minz, maxz) =
+        let candidates = 
+            [minx..dist..maxx] |> List.collect (fun x ->
+            [miny..dist..maxy] |> List.collect (fun y ->
+            [minz..dist..maxz] |> List.map (fun z -> x, y, z)))
+        let _, ox, oy, oz = 
+            candidates 
+            |> List.fold (
+                fun (count, ox, oy, oz) (x, y, z) ->
+                    let localCount = countFor x y z dist
+                    if localCount >= count then
+                        localCount, x, y, z
+                    else 
+                        count, ox, oy, oz)
+                (0, 0L, 0L, 0L)
+        if dist = 1L then abs ox + abs oy + abs oz
+        else
+            findBestPoint (dist / 2L) (ox - dist, ox + dist, oy - dist, oy + dist, oz - dist, oz + dist)
 
     let minx, maxx, miny, maxy, minz, maxz = 
         nanobots
         |> List.fold (fun (minx, maxx, miny, maxy, minz, maxz) (x, y, z, _) -> 
             min minx x, max maxx x, min miny y, max maxy y, min minz z, max maxz z)
             (Int64.MaxValue, 0L, Int64.MaxValue, 0L, Int64.MaxValue, 0L)
-    
+
     let rec findDistance dist =
         if dist > maxx - minx then dist
         else findDistance (dist * 2L)
     let startDistance = findDistance 1L
 
-    let sphereCount dist maxSoFar = 
-        [minx..dist..maxx] |> List.collect (fun x ->
-        [miny..dist..maxy] |> List.collect (fun y ->
-        [minz..dist..maxz] |> List.map (fun z -> x, y, z)))
+    let part2 = findBestPoint startDistance (minx, maxx, miny, maxy, minz, maxz)
+    printfn "part 2: %i" part2
 
     0

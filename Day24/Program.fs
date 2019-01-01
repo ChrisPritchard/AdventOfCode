8ac128263a35f0851444f8369e4f2e82bb5c8c6f
Christopher <chrispritchard.nz@gmail.com>
2019-01-02 07:43:49 +1300

    setup for part 2

--- Day24/Program.fs
+++ Day24/Program.fs
@@ -3,20 +3,6 @@
 open Model
 open Parsing
 
-type Group
-with 
-    member __.effectivePower () = __.units * __.attackStrength
-    member __.effectiveDamageTo other =
-        if List.contains __.attackType other.immunities then 0
-        else if List.contains __.attackType other.weaknesses then __.effectivePower () * 2
-        else __.effectivePower ()
-    member __.findTarget targets =
-        targets 
-        |> List.filter (fun group -> group.kind <> __.kind && group.units > 0)
-        |> List.sortByDescending (fun o -> __.effectiveDamageTo o, o.effectivePower (), o.initiative)
-        |> List.tryHead
-        |> Option.bind (fun other -> if __.effectiveDamageTo other = 0 then None else Some other)
-
 let runTurn groups = 
     let attackOrder =
         groups

2fda9a2d446a86f3245eecc378f9d73698e7847e
Christopher <chrispritchard.nz@gmail.com>
2019-01-01 21:23:27 +1300

    split up code for neatness. Implemented a target selector function

--- Day24/Program.fs
+++ Day24/Program.fs
@@ -1,59 +1,20 @@
-﻿open System
-open System.IO
-open FParsec.CharParsers
-open FParsec
+﻿open System.IO
 
-type AttackType = Slashing | Bludgeoning | Cold | Radiation | Fire
-type Group = {
-    units: int
-    hitpoints: int
-    weaknesses: AttackType list
-    immunities: AttackType list
-    attackType: AttackType
-    attackStrength: int
-    initiative: int
-}
+open Model
+open Parsing
 
-let parseInput input = 
-    let attackTypeFrom = 
-        function
-        | "slashing" -> Slashing
-        | "bludgeoning" -> Bludgeoning
-        | "cold" -> Cold
-        | "radiation" -> Radiation
-        | "fire" -> Fire
-        | _ -> failwith "attack type not recognised"
-
-    let pheader s = pstring s .>>. newline
-    let punitshp = (pint32 .>> pstring " units each with ") .>>. (pint32 .>> pstring " hit points ")
-    let pattackType = pstring "slashing" <|> pstring "bludgeoning" <|> pstring "cold" <|> pstring "radiation" <|> pstring "fire"
-    let pweakto = pstring "weak to " >>. sepBy1 pattackType (pstring ", ") |>> fun list -> true, List.map attackTypeFrom list
-    let pimmune = pstring "immune to " >>. sepBy1 pattackType (pstring ", ") |>> fun list -> false, List.map attackTypeFrom list
-    let pdefense = pchar '(' >>. sepBy1 (pweakto <|> pimmune) (pstring "; ") .>> pstring ") "
-    let pattack = pstring "with an attack that does " >>. pint32 .>>. (pchar ' ' >>. pattackType) .>>. (pstring " damage at initiative " >>. pint32)
-    let getDefenseType defenses isWeakTo =
-        match defenses |> Option.map (fun d -> 
-            List.tryPick (function | n, list when n = isWeakTo -> Some list | _ -> None) d) with 
-        | Some (Some list) -> list | _ -> []
-    let pgroup = 
-        punitshp .>>. opt pdefense .>>. pattack .>> opt newline
-        |>> fun (((units, hitpoints), defenses), ((attackStrength, attackType), initiative)) ->
-            let weaknesses = getDefenseType defenses true
-            let immunities = getDefenseType defenses false
-            {
-                units = units
-                hitpoints = hitpoints
-                weaknesses = weaknesses
-                immunities = immunities
-                attackType = attackTypeFrom attackType
-                attackStrength = attackStrength
-                initiative = initiative
-            }
-
-    let pall = (pheader "Immune System:" >>. many pgroup) .>>. (newline >>. pheader "Infection:" >>. many pgroup)
-    match run pall input with
-    | Success(r, _, _) -> r
-    | Failure(ex, _, _) -> failwith ex
+type Group
+with 
+    member __.effectivePower = __.units * __.attackStrength
+    member __.effectiveDamageTo other =
+        if List.contains __.attackType other.immunities then 0
+        else if List.contains __.attackType other.weaknesses then __.effectivePower * 2
+        else __.effectivePower
+    member __.findTarget targets =
+        targets 
+        |> List.sortByDescending (fun o -> __.effectiveDamageTo o, o.effectivePower, o.initiative)
+        |> List.tryHead
+        |> Option.bind (fun other -> if __.effectiveDamageTo other = 0 then None else Some other)
 
 [<EntryPoint>]
 let main _ =

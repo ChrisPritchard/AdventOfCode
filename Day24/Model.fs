f11eb7f934e08c9a12c048905e190e2b157121b1
Christopher <chrispritchard.nz@gmail.com>
2019-01-02 07:43:49 +1300

    setup for part 2

--- Day24/Model.fs
+++ Day24/Model.fs
@@ -14,3 +14,15 @@ type Group = {
     attackStrength: int
     initiative: int
 }
+with 
+    member __.effectivePower () = __.units * __.attackStrength
+    member __.effectiveDamageTo other =
+        if List.contains __.attackType other.immunities then 0
+        else if List.contains __.attackType other.weaknesses then __.effectivePower () * 2
+        else __.effectivePower ()
+    member __.findTarget targets =
+        targets 
+        |> List.filter (fun group -> group.kind <> __.kind && group.units > 0)
+        |> List.sortByDescending (fun o -> __.effectiveDamageTo o, o.effectivePower (), o.initiative)
+        |> List.tryHead
+        |> Option.bind (fun other -> if __.effectiveDamageTo other = 0 then None else Some other)

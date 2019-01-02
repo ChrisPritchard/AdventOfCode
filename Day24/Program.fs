7276742cee1db7f34f4c22963ddff707487fa7ae
Christopher <chrispritchard.nz@gmail.com>
2019-01-02 15:40:24 +1300

    implemented part 2, but getting the wrong answer

--- Day24/Program.fs
+++ Day24/Program.fs
@@ -37,18 +37,28 @@ let runTurn groups =
 
 let rec runGame groups = 
     let afterTurn = runTurn groups
-    let ofKind kind = List.tryFind (fun g -> g.kind = kind) afterTurn
-    if ofKind ImmuneSystem = None then
-        Infection, List.sumBy (fun g -> g.units) afterTurn
-    else if ofKind Infection = None then
-        ImmuneSystem, List.sumBy (fun g -> g.units) afterTurn
+    if afterTurn = groups then
+        None
     else
-        runGame afterTurn
+        let ofKind kind = List.tryFind (fun g -> g.kind = kind) afterTurn
+        if ofKind ImmuneSystem = None then
+            Some (Infection, List.sumBy (fun g -> g.units) afterTurn)
+        else if ofKind Infection = None then
+            Some (ImmuneSystem, List.sumBy (fun g -> g.units) afterTurn)
+        else
+            runGame afterTurn
 
 let boost amount groups =
     groups |> List.map (fun g -> 
         if g.kind = Infection then g 
         else { g with attackStrength = g.attackStrength + amount })
+
+let rec findMinBoost min max groups =
+    let mid = ((max - min) / 2) + min
+    match runGame <| boost mid groups with
+    | Some (ImmuneSystem, _) when max - min <= 2 -> mid
+    | Some (ImmuneSystem, _) -> findMinBoost min mid groups
+    | Some (Infection, _) | None -> findMinBoost mid max groups
 
 [<EntryPoint>]
 let main _ =
@@ -56,9 +66,11 @@ let main _ =
     let input = File.ReadAllText "input.txt"
     let groups = parseInput input
 
-    let _, part1 = runGame groups
-    printfn "part 1: %i" part1
+    match runGame groups with
+    | Some (_, part1) -> printfn "part 1: %i" part1
+    | _ -> failwith "stalemate for part 1"
 
-    let winner, _ = runGame <| boost 100000 groups
+    let part2 = findMinBoost 0 10000 groups
+    printfn "part 2: %i" part2
 
     0

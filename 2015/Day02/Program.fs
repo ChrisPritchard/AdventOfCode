7e028238ccb5a0a3711340b8278c11787e1ec96a
Christopher <chrispritchard.nz@gmail.com>
2019-01-03 09:56:49 +1300

    implemented day 2 of 2015

---
+++ 2015/Day02/Program.fs
@@ -0,0 +1,20 @@
+﻿
+open System.IO
+open System
+
+[<EntryPoint>]
+let main _ =
+
+    let input = File.ReadAllLines "input.txt"
+
+    let parseDims (s:string) = s.Split [|'x'|] |> Seq.map Int32.Parse |> Seq.sortBy id |> Seq.toList
+    let paper (dims: int list) = (dims.[0] * dims.[1]) * 3 + (dims.[0] * dims.[2] * 2) + (dims.[1] * dims.[2] * 2)
+    let ribbon (dims: int list) = (dims.[0] * 2) + (dims.[1] * 2) + (dims.[0] * dims.[1] * dims.[2])
+
+    let totalPaper = input |> Seq.sumBy (parseDims >> paper)
+    printfn "part 1: %i" totalPaper
+    
+    let totalRibbon = input |> Seq.sumBy (parseDims >> ribbon)
+    printfn "part 2: %i" totalRibbon
+
+    0

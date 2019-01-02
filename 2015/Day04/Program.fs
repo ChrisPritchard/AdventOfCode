73dfd03b59e9dd0b23e19614178e1eb8b6d9084f
Christopher <chrispritchard.nz@gmail.com>
2019-01-03 10:57:37 +1300

    interim work on part 1 of day 4, 2015

---
+++ 2015/Day04/Program.fs
@@ -0,0 +1,18 @@
+﻿
+[<EntryPoint>]
+let main _ =
+    let input = "ckczppom"
+
+    use md5 = System.Security.Cryptography.MD5.Create()
+    let gethash i = 
+        let text = input + string i
+        let bytes = System.Text.Encoding.UTF8.GetBytes text
+        md5.ComputeHash bytes
+
+    let part1 = 
+        Seq.initInfinite id |> Seq.find (fun i -> 
+        let hash = gethash i
+        hash.[0..4] = [|0uy;0uy;0uy;0uy;0uy|])
+    printfn "part 1: %i" part1
+
+    0

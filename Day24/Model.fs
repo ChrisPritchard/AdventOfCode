2a79e8df8186555eb7fdb943a675ff31e1985274
Christopher <chrispritchard.nz@gmail.com>
2019-01-02 15:05:33 +1300

    reworked to be immutable

--- Day24/Model.fs
+++ Day24/Model.fs
@@ -6,7 +6,7 @@ type AttackType = Slashing | Bludgeoning | Cold | Radiation | Fire
 
 type Group = {
     kind: GroupKind
-    mutable units: int
+    units: int
     hitpoints: int
     weaknesses: AttackType list
     immunities: AttackType list
@@ -15,14 +15,14 @@ type Group = {
     initiative: int
 }
 with 
-    member __.effectivePower () = __.units * __.attackStrength
+    member __.effectivePower = __.units * __.attackStrength
     member __.effectiveDamageTo other =
         if List.contains __.attackType other.immunities then 0
-        else if List.contains __.attackType other.weaknesses then __.effectivePower () * 2
-        else __.effectivePower ()
+        else if List.contains __.attackType other.weaknesses then __.effectivePower * 2
+        else __.effectivePower
     member __.findTarget targets =
         targets 
         |> List.filter (fun group -> group.kind <> __.kind && group.units > 0)
-        |> List.sortByDescending (fun o -> __.effectiveDamageTo o, o.effectivePower (), o.initiative)
+        |> List.sortByDescending (fun o -> __.effectiveDamageTo o, o.effectivePower, o.initiative)
         |> List.tryHead
         |> Option.bind (fun other -> if __.effectiveDamageTo other = 0 then None else Some other)

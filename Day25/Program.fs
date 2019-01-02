855a1b291635c99a762831c9dd222f03b53e0cbf
Christopher <chrispritchard.nz@gmail.com>
2019-01-02 16:28:20 +1300

    initial work for day 25 part 1

--- Day25/Program.fs
+++ Day25/Program.fs
@@ -1,8 +1,27 @@
-﻿// Learn more about F# at http://fsharp.org
-
-open System
+﻿open System
+open System.IO
 
 [<EntryPoint>]
-let main argv =
-    printfn "Hello World from F#!"
-    0 // return an integer exit code
+let main _ =
+    
+    let input = File.ReadAllLines "input.txt"
+    let coords = 
+        input 
+        |> Seq.map (fun s -> s.Split [|','|] |> Seq.map Int32.Parse |> Seq.toList) 
+        |> Seq.map (fun list -> list.[0], list.[1], list.[2], list.[3])
+        |> Seq.toList
+
+    let distance (x1,y1,z1,w1) (x2,y2,z2,w2) =
+        abs (x2 - x1) + abs (y2 - y1) + abs (z2 - z1) + abs (w2 - w1)
+
+    let folder coords soFar coord = 
+        if List.tryFind (fun set -> Set.contains coord set) soFar <> None then soFar
+        else
+            let newSet = coords |> List.filter (fun c -> distance c coord <= 3) |> Set.ofList
+            newSet::soFar
+
+    let constellations = coords |> List.fold (folder coords) []
+
+    printfn "part 1: %i" <| List.length constellations
+
+    0

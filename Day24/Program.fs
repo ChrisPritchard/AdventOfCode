a384c139af3cb3a1c3f5234e65d6e34d2fc7462b
Christopher <chrispritchard.nz@gmail.com>
2019-01-02 15:05:33 +1300

    reworked to be immutable

--- Day24/Program.fs
+++ Day24/Program.fs
@@ -4,9 +4,10 @@ open Model
 open Parsing
 
 let runTurn groups = 
+
     let attackOrder =
         groups
-        |> List.sortByDescending (fun (o: Group) -> o.effectivePower (), o.initiative)
+        |> List.sortByDescending (fun (o: Group) -> o.effectivePower, o.initiative)
         |> List.fold (fun (soFar, targets) group -> 
             let target = group.findTarget targets
             let remaining = 
@@ -16,21 +17,26 @@ let runTurn groups =
             (group, target)::soFar, remaining) ([], groups)
         |> fst
     
-    attackOrder 
-    |> List.sortByDescending (fun (a, _) -> a.initiative)
-    |> List.iter (fun (a, d) ->
-        if a.units <= 0 then ()
-        else
-            match d with
-            | Some other ->
-                let unitsLost = a.effectiveDamageTo other / other.hitpoints
-                other.units <- other.units - unitsLost
-            | _ -> ())
+    let unitsRemaining =
+        attackOrder 
+        |> List.sortByDescending (fun (a, _) -> a.initiative)
+        |> List.fold (fun units (a, d) ->
+            if Map.find a units <= 0 then units
+            else
+                let attacker = { a with units = Map.find a units }
+                match d with
+                | Some other ->
+                    let unitsLost = attacker.effectiveDamageTo other / other.hitpoints
+                    Map.add other (Map.find other units - unitsLost) units
+                | _ -> units)
+            (groups |> List.map (fun g -> g, g.units) |> Map.ofList)
 
-    groups
+    groups 
+    |> List.map (fun g -> { g with units = Map.find g unitsRemaining }) 
+    |> List.filter (fun g -> g.units > 0)
 
 let rec runGame groups = 
-    let afterTurn = runTurn groups |> List.filter (fun g -> g.units > 0)
+    let afterTurn = runTurn groups
     let ofKind kind = List.tryFind (fun g -> g.kind = kind) afterTurn
     if ofKind ImmuneSystem = None then
         Infection, List.sumBy (fun g -> g.units) afterTurn
@@ -42,7 +48,7 @@ let rec runGame groups =
 let boost amount groups =
     groups |> List.map (fun g -> 
         if g.kind = Infection then g 
-        else { g with hitpoints = g.hitpoints + amount })
+        else { g with attackStrength = g.attackStrength + amount })
 
 [<EntryPoint>]
 let main _ =
@@ -52,5 +58,7 @@ let main _ =
 
     let _, part1 = runGame groups
     printfn "part 1: %i" part1
+
+    let winner, _ = runGame <| boost 100000 groups
 
     0

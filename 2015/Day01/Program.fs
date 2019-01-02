28c4d0cb82a38248e17ade6df897ea52798a901b
Christopher <chrispritchard.nz@gmail.com>
2019-01-03 08:57:27 +1300

    implemented day 1 of 2015

---
+++ 2015/Day01/Program.fs
@@ -0,0 +1,21 @@
+﻿open System.IO
+
+[<EntryPoint>]
+let main _ =
+    
+    let input = File.ReadAllText "input.txt"
+
+    let part1 = input |> Seq.sumBy (function '(' -> 1 | _ -> -1)
+    printfn "part 1: %i" part1
+
+    let part2 = 
+        input 
+        |> Seq.fold (fun (floor, index) c -> 
+            if floor < 0 then floor, index
+            else
+                let next = floor + (match c with '(' -> 1 | _ -> -1)
+                next, index + 1) (0, 0)
+        |> snd
+    printfn "part 2: %i" part2
+
+    0

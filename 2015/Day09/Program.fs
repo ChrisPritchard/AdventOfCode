257cfce153342e6695eb7b1a5e65367b4acc479d
Christopher <chrispritchard.nz@gmail.com>
2019-01-03 20:46:10 +1300

    implemented part 1 of day 9. about to refactor for part 2

---
+++ 2015/Day09/Program.fs
@@ -0,0 +1,31 @@
+﻿open System
+open System.IO
+
+[<EntryPoint>]
+let main _ =
+
+    let input = File.ReadAllLines "input.txt"
+
+    let cities, travelTimes =
+        input
+        |> Array.fold (fun (cities, travelTimes) line ->
+            let line = line.Split [|' '|]
+            let newCity = Set.add line.[0] cities
+            let andCity = Set.add line.[2] newCity
+            let newTime = Map.add (line.[0], line.[2]) (Int32.Parse line.[4]) travelTimes
+            let andTime = Map.add (line.[2], line.[0]) (Int32.Parse line.[4]) newTime
+            andCity, andTime)
+            (Set.empty, Map.empty)
+
+    let rec findShortestTime =
+        function
+        | [] | [_] -> 0
+        | [city;other] -> travelTimes.[city, other]
+        | city::rest ->
+            rest |> List.map (fun other -> travelTimes.[city, other] + findShortestTime (other::(List.except [other] rest))) |> List.min
+
+    let cities = Set.toList cities
+    let part1 = cities |> List.map (fun city -> findShortestTime (city::(List.except [city] cities))) |> List.min
+    printfn "part 1: %i" part1
+
+    0

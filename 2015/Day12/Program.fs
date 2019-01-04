ab700e0ed1a456db3db6b44633a6c8bc9943862c
Christopher <chrispritchard.nz@gmail.com>
2019-01-04 16:35:26 +1300

    part 1 of day 12 using fparsec

---
+++ 2015/Day12/Program.fs
@@ -0,0 +1,16 @@
+﻿
+open System.IO
+open FParsec.Primitives
+open FParsec.CharParsers
+
+[<EntryPoint>]
+let main _ =
+
+    let input = File.ReadAllText "input.txt"
+
+    let pNumberSum = many (pint32 <|> (anyChar |>> fun _ -> 0)) |>> List.sum
+
+    let part1 = match run pNumberSum input with Success (r, _, _) -> r | Failure (ex, _, _) -> failwith ex
+    printfn "part 1: %i" part1
+
+    0

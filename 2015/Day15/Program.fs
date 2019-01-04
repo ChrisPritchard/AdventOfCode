2fb9bc46d7b2df0ab693598d321264b647b7bcc7
Christopher <chrispritchard.nz@gmail.com>
2019-01-04 19:24:47 +1300

    interim work on part 1, day 15 getting wrong answer

---
+++ 2015/Day15/Program.fs
@@ -0,0 +1,29 @@
+﻿
+[<EntryPoint>]
+let main _ =
+
+    let input = [
+        [4;-2;0;0;5]
+        [0;5;-1;0;8]
+        [-1;0;5;0;6]
+        [0;0;-2;2;1]
+    ]
+
+    let combinations = 
+        [0..100] |> List.collect (fun frosting ->
+        [0..100-frosting] |> List.collect (fun candy ->
+        [0..100-candy] |> List.collect (fun butterscotch ->
+        [0..100-butterscotch] |> List.map (fun sugar ->
+            [0..4] |> List.map (fun i -> 
+               max 0 ((frosting * input.[0].[i])
+               + (candy * input.[1].[i])
+               + (butterscotch * input.[2].[i])
+               + (sugar * input.[3].[i])))))))
+
+    let part1 = 
+        combinations 
+        |> List.map (List.take 4 >> List.fold (*) 1) 
+        |> List.max
+    printfn "part 1: %i" part1
+
+    0

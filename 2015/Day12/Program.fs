ca410dd6dd6eea3027d91fe99ffa755942dd775a
Christopher <chrispritchard.nz@gmail.com>
2019-01-04 17:45:04 +1300

    implemented part 2 of day 12

--- 2015/Day12/Program.fs
+++ 2015/Day12/Program.fs
@@ -2,6 +2,9 @@
 open System.IO
 open FParsec.Primitives
 open FParsec.CharParsers
+open Newtonsoft.Json
+open Newtonsoft.Json.Linq
+open System
 
 [<EntryPoint>]
 let main _ =
@@ -12,5 +15,30 @@ let main _ =
 
     let part1 = match run pNumberSum input with Success (r, _, _) -> r | Failure (ex, _, _) -> failwith ex
     printfn "part 1: %i" part1
+
+    let json = JArray.Parse input
+
+    let rec countJson (json: JToken) = 
+        match json.Type with
+        | JTokenType.Integer -> int json
+        | JTokenType.Array ->
+            let array = json :?> JArray
+            array |> Seq.sumBy countJson
+        | JTokenType.Object ->
+            let obj = json :?> JObject
+            let count = 
+                obj.Properties() 
+                |> Seq.fold (fun count (p: JProperty) -> 
+                    match count with 
+                    | None -> None 
+                    | Some c ->
+                        if p.Value.Type = JTokenType.String && string p.Value = "red" then None
+                        else Some (c + countJson p.Value)) 
+                    (Some 0)
+            match count with Some c -> c | _ -> 0
+        | _ -> 0
+
+    let part2 = countJson json
+    printfn "part 2: %i" part2
 
     0

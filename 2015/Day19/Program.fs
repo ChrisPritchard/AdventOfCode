99be9ae64b64c2e27d8d3bc5d3863fc6c2c891ef
Christopher <chrispritchard.nz@gmail.com>
2019-01-05 16:04:52 +1300

    implemented part 1 of day 19

---
+++ 2015/Day19/Program.fs
@@ -0,0 +1,29 @@
+﻿open System.IO
+open System
+open System.Text.RegularExpressions
+
+[<EntryPoint>]
+let main _ =
+
+    let input = File.ReadAllLines "input.txt"
+    let molecule = Array.last input
+    let mappings = 
+        Array.truncate (input.Length - 2) input 
+        |> Array.map (fun line -> 
+            let line = line.Split [|' '|]
+            line.[0], line.[2])
+
+    let indexes text toMatch = 
+        Regex.Matches (text, Regex.Escape toMatch) 
+        |> Seq.map (fun r -> r.Index) |> Seq.toList
+
+    let moleculesFrom (orig, repl) molecule =
+        indexes molecule orig
+        |> List.map (fun i ->
+            molecule.[0..i-1] + repl + molecule.[i + orig.Length..])
+
+    let allChanges = mappings |> Seq.collect (fun mapping -> moleculesFrom mapping molecule) |> Seq.distinct |> Seq.toList
+    let part1 = List.length allChanges
+    printfn "part 1: %i" part1
+
+    0

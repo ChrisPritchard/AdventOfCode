2b60163f850a90cd5b18c0acecf521bfdda3febe
Chris Pritchard <chrispritchard.nz@gmail.com>
2019-01-06 14:53:02 +1300

    completed day 20

--- 2015/Day20/Program.fs
+++ 2015/Day20/Program.fs
@@ -14,4 +14,14 @@ let main _ =
     let part1 = [2..3000000] |> List.find (presents >> (<=) input)
     printfn "part 1: %i" part1
 
+    let presents house =
+        [1..(house |> float |> sqrt |> int)] 
+        |> Seq.filter (fun n -> house % n = 0)
+        |> Seq.collect (fun n -> [n; house / n] |> Seq.filter (fun on -> house <= on * 50))
+        |> Seq.distinct
+        |> Seq.sumBy (fun n -> n * 11)
+
+    let part2 = [2..3000000] |> List.find (presents >> (<=) input)
+    printfn "part 2: %i" part2
+
     0

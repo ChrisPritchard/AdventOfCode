b5ba7f6c7f13caef91df37af79c623dce1ef184f
Chris Pritchard <chrispritchard.nz@gmail.com>
2019-01-07 11:51:00 +1300

    completed day 24

---
+++ 2015/Day24/Program.fs
@@ -0,0 +1,31 @@
+﻿open System
+open System.IO
+
+[<EntryPoint>]
+let main _ =
+
+    let input = File.ReadAllLines "input.txt"
+    let packages = input |> Seq.map (Int64.Parse) |> Seq.toList
+
+    let rec combinations maxLength list = 
+        match maxLength, list with
+        | 0, _ -> [[]]
+        | _, [] -> []
+        | k, (x::xs) -> 
+            List.map ((@) [x]) (combinations (k-1) xs) @ combinations k xs
+
+    let findEntanglement sectionCount =
+        let weight = List.sum packages / sectionCount
+        [1..List.length packages - 1] 
+        |> Seq.map (fun n -> 
+            combinations n packages
+            |> List.filter (fun set -> List.sum set = weight)
+            |> List.sortBy (fun set -> List.length set, set |> List.reduce (*))
+            |> List.tryHead)
+        |> Seq.pick id
+        |> List.reduce (*)
+
+    printfn "part 1: %i" <| findEntanglement 3L
+    printfn "part 2: %i" <| findEntanglement 4L
+
+    0

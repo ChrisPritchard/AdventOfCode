ce168c260183f095e46e75a3d930e9123094598b
Chris Pritchard <chrispritchard.nz@gmail.com>
2019-01-07 12:19:17 +1300

    Finished day 25 and all of 2015!

---
+++ 2015/Day25/Program.fs
@@ -0,0 +1,21 @@
+﻿
+[<EntryPoint>]
+let main _ =
+
+    let row, column = 2978, 3083
+
+    let rec findCode row column = 
+        let rec looper r c prev =
+            let next =
+                if r = 1 && c = 1 then 20151125L
+                else
+                    (prev * 252533L) % 33554393L
+            if r = row && c = column then next
+            else
+                let nr, nc = if r = 1 then c + 1, 1 else r - 1, c + 1
+                looper nr nc next
+        looper 1 1 0L
+
+    printfn "part 1: %i" <| findCode row column
+
+    0

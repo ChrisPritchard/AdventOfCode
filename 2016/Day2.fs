60c47e0866916bfb864693d435df5fa570246203
Chris Pritchard <chrispritchard.nz@gmail.com>
2019-03-20 11:16:20 +1300

    implemented day 2 part 2 of 2016

--- 2016/Day2.fs
+++ 2016/Day2.fs
@@ -31,6 +31,30 @@ Your puzzle input is the instructions from the document you found at the front d
 
 *)
 
+(*
+--- Part Two ---
+
+You finally arrive at the bathroom (it's a several minute walk from the lobby so visitors can behold the many fancy conference rooms and water coolers on this floor) and go to punch in the code. Much to your bladder's dismay, the keypad is not at all like you imagined it. Instead, you are confronted with the result of hundreds of man-hours of bathroom-keypad-design meetings:
+
+    1
+  2 3 4
+5 6 7 8 9
+  A B C
+    D
+
+You still start at "5" and stop when you're at an edge, but given the same instructions as above, the outcome is very different:
+
+    You start at "5" and don't move at all (up and left are both edges), ending at 5.
+    Continuing from "5", you move right twice and down three times (through "6", "7", "B", "D", "D"), ending at D.
+    Then, from "D", you move five more times (through "D", "B", "C", "C", "B"), ending at B.
+    Finally, after five more moves, you end at 3.
+
+So, given the actual keypad layout, the code would be 5DB3.
+
+Using the same instructions in your puzzle input, what is the correct bathroom code?
+
+*)
+
 module Day2
 
 open System
@@ -43,21 +67,21 @@ URLDDDLDRDDDURRRLURRRRLULURLDDUDRDUDDLURURLLRDURDDRLRUURLDLLRDLRUUURLRLDLDRUDDDU
 RLLDRDURRUDULLURLRLLURUDDLULUULRRRDRLULDDRLUDRDURLUULDUDDDDDUDDDDLDUDRDRRLRLRLURDURRURDLURDURRUUULULLUURDLURDUURRDLDLDDUURDDURLDDDRUURLDURRURULURLRRLUDDUDDDLLULUDUUUDRULLLLULLRDDRDLRDRRDRRDLDLDDUURRRDDULRUUURUDRDDLRLRLRRDLDRDLLDRRDLLUULUDLLUDUUDRDLURRRRULDRDRUDULRLLLLRRULDLDUUUURLDULDDLLDDRLLURLUDULURRRUULURDRUDLRLLLRDDLULLDRURDDLLDUDRUDRLRRLULLDRRDULDLRDDRDUURDRRRLRDLDUDDDLLUDURRUUULLDRLUDLDRRRRDDDLLRRDUURURLRURRDUDUURRDRRUDRLURLUDDDLUDUDRDRURRDDDDRDLRUDRDRLLDULRURULULDRLRLRRLDURRRUL
 """
 
-let keypad = 
-    [|
-        "123"
-        "456"
-        "789"
-    |] |> array2D
-
-let shift (x, y) dir =
-    match dir with
-    | 'U' -> x, max 0 (y - 1)
-    | 'R' -> min 2 (x + 1), y
-    | 'D' -> x, min 2 (y + 1)
-    | 'L' | _ -> max 0 (x - 1), y
-
 let part1 () =
+
+    let keypad = 
+        [|
+            "123"
+            "456"
+            "789"
+        |] |> array2D
+
+    let shift (x, y) dir =
+        match dir with
+        | 'U' -> x, max 0 (y - 1)
+        | 'R' -> min 2 (x + 1), y
+        | 'D' -> x, min 2 (y + 1)
+        | 'L' | _ -> max 0 (x - 1), y
     
     let lines = input.Split([|'\r';'\n'|], StringSplitOptions.RemoveEmptyEntries) |> Array.toList
 
@@ -69,3 +93,33 @@ let part1 () =
             walker (acc + string keypad.[fy, fx]) fx fy rest
     
     walker "" 1 1 lines
+
+let part2 () =
+    let keypad = 
+        [|
+            "  1  "
+            " 234 "
+            "56789"
+            " ABC "
+            "  D  "
+        |] |> array2D
+
+    let shift (x, y) dir =
+        let nx, ny = 
+            match dir with
+            | 'U' -> x, max 0 (y - 1)
+            | 'R' -> min 4 (x + 1), y
+            | 'D' -> x, min 4 (y + 1)
+            | 'L' | _ -> max 0 (x - 1), y
+        if keypad.[ny, nx] = ' ' then (x, y) else (nx, ny)
+            
+    let lines = input.Split([|'\r';'\n'|], StringSplitOptions.RemoveEmptyEntries) |> Array.toList
+
+    let rec walker acc x y lines =
+        match lines with
+        | [] -> acc
+        | next::rest ->
+            let (fx, fy) = ((x, y), next) ||> Seq.fold shift
+            walker (acc + string keypad.[fy, fx]) fx fy rest
+    
+    walker "" 0 2 lines
--- 2016/Program.fs
+++ 2016/Program.fs
@@ -13,5 +13,6 @@ let main _ =
     time 1 1 Day1.part1
     time 1 1 Day1.part2
     time 2 1 Day2.part1
+    time 2 2 Day2.part2
 
     0

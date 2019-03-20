9a102cab25ff4a694d72bfa0eda1d5b589bcbd2d
Chris Pritchard <chrispritchard.nz@gmail.com>
2019-03-20 21:58:00 +1300

    part 1 of day 6

--- 2016/Program.fs
+++ 2016/Program.fs
@@ -10,15 +10,16 @@ let main _ =
         timer.Stop()
         printfn "day%i part%i: %A (elapsed: %i ms)" day part result timer.ElapsedMilliseconds
 
-    time 1 1 Day1.part1
-    time 1 1 Day1.part2
-    time 2 1 Day2.part1
-    time 2 2 Day2.part2
-    time 3 1 Day3.part1
-    time 3 2 Day3.part2
-    time 4 1 Day4.part1
-    time 4 2 Day4.part2
+    //time 1 1 Day1.part1
+    //time 1 1 Day1.part2
+    //time 2 1 Day2.part1
+    //time 2 2 Day2.part2
+    //time 3 1 Day3.part1
+    //time 3 2 Day3.part2
+    //time 4 1 Day4.part1
+    //time 4 2 Day4.part2
     //time 5 1 Day5.part1
     //time 5 2 Day5.part2
+    time 6 1 Day6.part1
 
     0

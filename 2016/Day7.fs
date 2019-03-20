145cffdd5bd7bf7e15ad97c0020d42c1b647c661
Chris Pritchard <chris.pritchard@edpay.nz>
2019-03-21 09:58:24 +1300

    implemented part 1 of day 7

--- 2016/Day7.fs
+++ 2016/Day7.fs
@@ -15,25 +15,46 @@ For example:
 How many IPs in your puzzle input support TLS?
 *)
 
+(*
+--- Part Two ---
+
+You would also like to know which IPs support SSL (super-secret listening).
+
+An IP supports SSL if it has an Area-Broadcast Accessor, or ABA, anywhere in the supernet sequences (outside any square bracketed sections), and a corresponding Byte Allocation Block, or BAB, anywhere in the hypernet sequences. An ABA is any three-character sequence which consists of the same character twice with a different character between them, such as xyx or aba. A corresponding BAB is the same characters but in reversed positions: yxy and bab, respectively.
+
+For example:
+
+    aba[bab]xyz supports SSL (aba outside square brackets with corresponding bab within square brackets).
+    xyx[xyx]xyx does not support SSL (xyx, but no corresponding yxy).
+    aaa[kek]eke supports SSL (eke in supernet with corresponding kek in hypernet; the aaa sequence is not related, because the interior character must be different).
+    zazbz[bzb]cdb supports SSL (zaz has no corresponding aza, but zbz has a corresponding bzb, even though zaz and zbz overlap).
+
+How many IPs in your puzzle input support SSL?
+*)
+
 module Day7
 
-open System
+open Common
 open System.IO
 
 let input = File.ReadAllLines "Day7-input.txt"
 
-let ABBA s = 
-    true
+let ABBA (s: string) = 
+    [0..Seq.length s - 4] 
+    |> List.exists (fun i -> 
+        s.[i] = s.[i + 3]
+        && s.[i+1] = s.[i+2]
+        && s.[i] <> s.[i+1])
 
-let valid (line: string) = 
-    let segments = line.Split([|'[';']'|], StringSplitOptions.RemoveEmptyEntries)
-    let mutable normal, hypernet = false, false
+let valid line = 
+    let segments = split "[]" line
+    let mutable supernet, hypernet = false, false
     for i = 0 to segments.Length - 1 do
         if i % 2 = 0 then
-            if not normal && ABBA segments.[i] then normal <- true
+            if not supernet && ABBA segments.[i] then supernet <- true
         else
             if not hypernet && ABBA segments.[i] then hypernet <- true
-    normal && not hypernet
+    supernet && not hypernet
 
 let part1 () = 
     input |> Array.filter valid |> Array.length

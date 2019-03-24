d93a988cf7dd878617ac9c2d10c0c7567b4d5962
Chris Pritchard <chrispritchard.nz@gmail.com>
2019-03-24 20:27:35 +1300

    used a timing hack to solve part 2 of day 11

--- 2016/Program.fs
+++ 2016/Program.fs
@@ -30,7 +30,7 @@ let main _ =
     //time 9 2 Day9.part2
     //time 10 1 Day10.part1
     //time 10 2 Day10.part2
-    time 11 1 Day11.part1
-    //time 11 2 Day11.part2
+    //time 11 1 Day11.part1 // 5 min+ approx run time
+    //time 11 2 Day11.part2 // 5 min+ approx run time
 
     0

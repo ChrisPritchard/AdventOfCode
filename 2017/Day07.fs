76498bc76185f3579d46627a3715103584701e22
Christopher <chrispritchard.nz@gmail.com>
2019-04-18 07:20:28 +1200

    scaffolding for part 2 day 7

--- 2017/Day07.fs
+++ 2017/Day07.fs
@@ -46,6 +46,24 @@ tknk --- padx - havc
 In this example, tknk is at the bottom of the tower (the bottom program), and is holding up ugml, padx, and fwft. Those programs are, in turn, holding up other programs; in this example, none of those programs are holding up any other programs, and are all the tops of their own towers. (The actual tower balancing in front of you is much larger.)
 
 Before you're ready to help them, you need to make sure your information is correct. What is the name of the bottom program?
+
+--- Part Two ---
+
+The programs explain the situation: they can't get down. Rather, they could get down, if they weren't expending all of their energy trying to keep the tower balanced. Apparently, one program has the wrong weight, and until it's fixed, they're stuck here.
+
+For any program holding a disc, each program standing on that disc forms a sub-tower. Each of those sub-towers are supposed to be the same weight, or the disc itself isn't balanced. The weight of a tower is the sum of the weights of the programs in that tower.
+
+In the example above, this means that for ugml's disc to be balanced, gyxo, ebii, and jptl must all have the same weight, and they do: 61.
+
+However, for tknk to be balanced, each of the programs standing on its disc and all programs above it must each match. This means that the following sums must all be the same:
+
+    ugml + (gyxo + ebii + jptl) = 68 + (61 + 61 + 61) = 251
+    padx + (pbga + havc + qoyq) = 45 + (66 + 66 + 66) = 243
+    fwft + (ktlj + cntj + xhth) = 72 + (57 + 57 + 57) = 243
+
+As you can see, tknk's disc is unbalanced: ugml's stack is heavier than the other two. Even though the nodes above ugml are balanced, ugml itself is too heavy: it needs to be 8 units lighter for its stack to weigh 243 and keep the towers balanced. If this change were made, its weight would be 60.
+
+Given that exactly one program is the wrong weight, what would its weight need to be to balance the entire tower?
 *)
 
 module Day07
@@ -67,4 +85,5 @@ let part1 () =
     |> fst
 
 let part2 () =
+    let root = part1 ()
     0

5bb3e1be38e93d0ae365779d71e707e68faf2221
Christopher <chrispritchard.nz@gmail.com>
2019-04-18 18:23:41 +1200

    finished part 1 and start of part 2, day 8

--- 2017/Day08.fs
+++ 2017/Day08.fs
@@ -22,14 +22,46 @@ After this process, the largest value in any register is 1.
 You might also encounter <= (less than or equal to) or != (not equal to). However, the CPU doesn't have the bandwidth to tell you what all the registers are named, and leaves that to you to determine.
 
 What is the largest value in any register after completing the instructions in your puzzle input?
+
+--- Part Two ---
+
+To be safe, the CPU also needs to know the highest value held in any register during this process so that it can decide how much memory to allocate to these operations. For example, in the above instructions, the highest value ever held was 10 (in register c after the third instruction was evaluated).
 *)
 
 module Day08
 
-let input = System.IO.File.ReadAllLines "./inputs/day08.txt"
+open Common
+
+let input = 
+    System.IO.File.ReadAllLines "./inputs/day08.txt"
+    |> Array.map (splits [" ";"if"])
+
+let runCommand registers (line: string []) =
+    let condReg = Map.tryFind line.[3] registers |> Option.defaultValue 0
+    let condVal = int line.[5]
+    let shouldApply = 
+        match line.[4] with
+        | ">" -> condReg > condVal
+        | "<" -> condReg < condVal
+        | ">=" -> condReg >= condVal
+        | "<=" -> condReg <= condVal
+        | "==" -> condReg = condVal
+        | "!=" | _ -> condReg <> condVal
+    if shouldApply then 
+        let targetReg = Map.tryFind line.[0] registers |> Option.defaultValue 0
+        if line.[1] = "inc" then
+            Map.add line.[0] (targetReg + int line.[2]) registers
+        else
+            Map.add line.[0] (targetReg - int line.[2]) registers
+    else
+        registers
 
 let part1 () =
-    0
+
+    let finalRegs = Array.fold runCommand Map.empty input
+    finalRegs |> Map.toList |> Seq.map snd |> Seq.max
 
 let part2 () =
+    
+    let (regs, _) = Array.mapFold (runCommand >> fun r -> r, r) Map.empty input
     0

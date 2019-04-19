8974cf4324dba7edfdfc716c694f62c18141b6fd
Christopher <chrispritchard.nz@gmail.com>
2019-04-19 16:41:04 +1200

    prep for part 2

--- 2017/Day14.fs
+++ 2017/Day14.fs
@@ -27,6 +27,27 @@ V      V
 In this example, 8108 squares are used across the entire 128x128 grid.
 
 Given your actual key string, how many squares are used?
+
+--- Part Two ---
+
+Now, all the defragmenter needs to know is the number of regions. A region is a group of used squares that are all adjacent, not including diagonals. Every used square is in exactly one region: lone used squares form their own isolated regions, while several adjacent squares all count as a single region.
+
+In the example above, the following nine regions are visible, each marked with a distinct digit:
+
+11.2.3..-->
+.1.2.3.4   
+....5.6.   
+7.8.55.9   
+.88.5...   
+88..5..8   
+.8...8..   
+88.8.88.-->
+|      |   
+V      V   
+
+Of particular interest is the region marked 8; while it does not appear contiguous in this small view, all of the squares marked 8 are connected when considering the whole 128x128 grid. In total, in this example, 1242 regions are present.
+
+How many regions are present given your key string?
 *)
 
 module Day14
@@ -55,14 +76,21 @@ let binary (c: char) =
     let i = 
         if System.Char.IsDigit c then int c - int '0' 
         else int c - int 'a' + 10
-    System.Convert.ToString (i, 2)
+    System.Convert.ToString(i, 2).PadLeft(4, '0')
 
-let input = "hxtvlmkl"
+let input = "flqrgnkx"//"hxtvlmkl"
+
+let map = 
+    [|0..127|] 
+    |> Array.map (fun row -> 
+        hash (sprintf "%s-%i" input row) 
+        |> Seq.map binary 
+        |> String.concat "")
+    |> array2D
 
 let part1 () =
-    [0..127] 
-    |> Seq.collect (fun i -> 
-        hash (sprintf "%s-%i" input i) |> Seq.map binary |> Seq.collect id)
+    map
+    |> Seq.cast<char>
     |> Seq.filter (fun c -> c = '1')
     |> Seq.length
 

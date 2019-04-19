2278a8540f7364f4a8f9b4f3c1317e1754c60184
Christopher <chrispritchard.nz@gmail.com>
2019-04-19 18:47:30 +1200

    interim work part 1 day 16

--- 2017/Program.fs
+++ 2017/Program.fs
@@ -31,7 +31,9 @@ let main _ =
     //time 13 2 Day13.part2
     time 14 1 Day14.part1
     time 14 2 Day14.part2
-    time 15 1 Day15.part1
-    time 15 2 Day15.part2
+    //time 15 1 Day15.part1
+    //time 15 2 Day15.part2
+    time 16 1 Day16.part1
+    time 16 2 Day16.part2
 
     0

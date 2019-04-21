8dcb41191bb8a87ae40c62dc555879f89681be80
Christopher <chrispritchard.nz@gmail.com>
2019-04-21 18:38:31 +1200

    finished part 2 day 20

--- 2017/Day20.fs
+++ 2017/Day20.fs
@@ -33,6 +33,36 @@ p=<-8,0,0>, v=<-6,0,0>, a=<-2,0,0>                         (0)
 At this point, particle 1 will never be closer to <0,0,0> than particle 0, and so, in the long run, particle 0 will stay closest.
 
 Which particle will stay closest to position <0,0,0> in the long term?
+
+--- Part Two ---
+
+To simplify the problem further, the GPU would like to remove any particles that collide. Particles collide if their positions ever exactly match. Because particles are updated simultaneously, more than two particles can collide at the same time and place. Once particles collide, they are removed and cannot collide with anything else after that tick.
+
+For example:
+
+p=<-6,0,0>, v=< 3,0,0>, a=< 0,0,0>    
+p=<-4,0,0>, v=< 2,0,0>, a=< 0,0,0>    -6 -5 -4 -3 -2 -1  0  1  2  3
+p=<-2,0,0>, v=< 1,0,0>, a=< 0,0,0>    (0)   (1)   (2)            (3)
+p=< 3,0,0>, v=<-1,0,0>, a=< 0,0,0>
+
+p=<-3,0,0>, v=< 3,0,0>, a=< 0,0,0>    
+p=<-2,0,0>, v=< 2,0,0>, a=< 0,0,0>    -6 -5 -4 -3 -2 -1  0  1  2  3
+p=<-1,0,0>, v=< 1,0,0>, a=< 0,0,0>             (0)(1)(2)      (3)   
+p=< 2,0,0>, v=<-1,0,0>, a=< 0,0,0>
+
+p=< 0,0,0>, v=< 3,0,0>, a=< 0,0,0>    
+p=< 0,0,0>, v=< 2,0,0>, a=< 0,0,0>    -6 -5 -4 -3 -2 -1  0  1  2  3
+p=< 0,0,0>, v=< 1,0,0>, a=< 0,0,0>                       X (3)      
+p=< 1,0,0>, v=<-1,0,0>, a=< 0,0,0>
+
+------destroyed by collision------    
+------destroyed by collision------    -6 -5 -4 -3 -2 -1  0  1  2  3
+------destroyed by collision------                      (3)         
+p=< 0,0,0>, v=<-1,0,0>, a=< 0,0,0>
+
+In this example, particles 0, 1, and 2 are simultaneously destroyed at the time and place marked X. On the next tick, particle 3 passes through unharmed.
+
+How many particles are left after all collisions are resolved?
 *)
 
 module Day20
@@ -45,10 +75,10 @@ let input =
         line |> split "p=<,>va " |> Array.map int 
         |> fun a -> ((a.[0], a.[1], a.[2]), (a.[3], a.[4], a.[5]), (a.[6], a.[7], a.[8])))
 
-let advance ((xp, yp, zp), (xv, yv, zv), (xa, ya, za)) _ =
+let advance ((xp, yp, zp), (xv, yv, zv), (xa, ya, za)) =
     let xv, yv, zv = xv + xa, yv + ya, zv + za
     let xp, yp, zp = xp + xv, yp + yv, zp + zv
-    float (abs xp + abs yp + abs xp), ((xp, yp, zp), (xv, yv, zv), (xa, ya, za))
+    ((xp, yp, zp), (xv, yv, zv), (xa, ya, za))
 
 let part1 () =
 
@@ -71,4 +101,19 @@ let part1 () =
     |> Array.head |> fst
 
 let part2 () =
-    0
+
+    let start = Array.copy input |> Array.indexed
+    let iterations = 1000
+    let simulation = 
+        (start, [1..iterations])
+        ||> List.fold (fun particles _ ->
+            (Map.empty, particles) 
+            ||> Array.fold (fun map (index, particle) ->
+                let (np, nv, na) = advance particle
+                if Map.containsKey np map then
+                    Map.add np None map
+                else
+                    Map.add np (Some (index, (np, nv, na))) map)
+            |> Map.toArray |> Array.map snd |> Array.choose id)
+
+    simulation.Length

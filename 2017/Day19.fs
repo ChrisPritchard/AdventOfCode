58477a7c0e744d3531abb5c6a87c41d81a19bd75
Christopher <chrispritchard.nz@gmail.com>
2019-04-21 15:20:34 +1200

    part 2 of day 19

--- 2017/Day19.fs
+++ 2017/Day19.fs
@@ -25,6 +25,33 @@ Following the path to the end, the letters it sees on its path are ABCDEF.
 
 The little packet looks up at you, hoping you can help it find the way. What letters will it see (in the order it would see them) if it follows the path? (The routing diagram is very wide; make sure you view it without line wrapping.)
 
+--- Part Two ---
+
+The packet is curious how many steps it needs to go.
+
+For example, using the same routing diagram from the example above...
+
+     |          
+     |  +--+    
+     A  |  C    
+ F---|--|-E---+ 
+     |  |  |  D 
+     +B-+  +--+ 
+
+...the packet would go:
+
+    6 steps down (including the first line at the top of the diagram).
+    3 steps right.
+    4 steps up.
+    3 steps right.
+    4 steps down.
+    3 steps right.
+    2 steps up.
+    13 steps left (including the F it stops on).
+
+This would result in a total of 38 steps.
+
+How many steps does the packet need to go?
 *)
 
 module Day19
@@ -34,32 +61,31 @@ open Common
 let input = 
     System.IO.File.ReadAllLines "./inputs/day19.txt" |> array2D
 
+let rec follower x y last dir acc count =
+    match input.[y, x] with
+    | ' ' -> acc, count
+    | '|' when last = '|' ->
+        follower x (y + dir) last dir acc (count + 1)
+    | '|' when last = '-' ->
+        follower (x + dir) y last dir acc (count + 1)
+    | '-' when last = '-' ->
+        follower (x + dir) y last dir acc (count + 1)
+    | '-' when last = '|' ->
+        follower x (y + dir) last dir acc (count + 1)
+    | '+' when last = '|' ->
+        if input.[y, x - 1] = '-' then follower (x - 1) y '-' -1 acc (count + 1)
+        else follower (x + 1) y '-' 1 acc (count + 1)
+    | '+' when last = '-' ->
+        if input.[y - 1, x] = '|' then follower x (y - 1) '|' -1 acc (count + 1)
+        else follower x (y + 1) '|' 1 acc (count + 1)
+    | c when System.Char.IsLetter c && last = '|' ->
+        follower x (y + dir) last dir (c::acc) (count + 1)
+    | c when System.Char.IsLetter c && last = '-' ->
+        follower (x + dir) y last dir (c::acc) (count + 1)
+    | _ -> failwith "unrecognised tile"
+    
 let part1 () =
-
-    let rec follower x y last dir acc =
-        match input.[y, x] with
-        | ' ' -> acc
-        | '|' when last = '|' ->
-            follower x (y + dir) last dir acc
-        | '|' when last = '-' ->
-            follower (x + dir) y last dir acc
-        | '-' when last = '-' ->
-            follower (x + dir) y last dir acc
-        | '-' when last = '|' ->
-            follower x (y + dir) last dir acc
-        | '+' when last = '|' ->
-            if input.[y, x - 1] = '-' then follower (x - 1) y '-' -1 acc
-            else follower (x + 1) y '-' 1 acc
-        | '+' when last = '-' ->
-            if input.[y - 1, x] = '|' then follower x (y - 1) '|' -1 acc
-            else follower x (y + 1) '|' 1 acc
-        | c when System.Char.IsLetter c && last = '|' ->
-            follower x (y + dir) last dir (c::acc)
-        | c when System.Char.IsLetter c && last = '-' ->
-            follower (x + dir) y last dir (c::acc)
-        | _ -> failwith "unrecognised tile"
-
-    follower 99 0 '|' 1 [] |> List.rev |> asString
+    follower 99 0 '|' 1 [] 0 |> fst |> List.rev |> asString
 
 let part2 () =
-    ""
+    follower 99 0 '|' 1 [] 0 |> snd

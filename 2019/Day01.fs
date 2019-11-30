c33cf0f93997aa36a03aa2568390fe3f83e0027c
Christopher <chrispritchard.nz@gmail.com>
2019-11-30 13:54:15 +1300

    setup for 2019

---
+++ 2019/Day01.fs
@@ -0,0 +1,11 @@
+﻿module Day01
+
+open Common
+
+let input = System.IO.File.ReadAllLines("./inputs/day01.txt")
+
+let part1 () =
+    0
+
+let part2 () =
+    0
---
+++ 2019/Program.fs
@@ -0,0 +1,46 @@
+﻿open Common
+
+[<EntryPoint>]
+let main _ =
+    
+    time 1 1 Day01.part1
+    time 1 2 Day01.part2
+    // time 2 1 Day02.part1
+    // time 2 2 Day02.part2
+    // time 4 1 Day04.part1
+    // time 4 2 Day04.part2
+    // time 5 1 Day05.part1
+    // time 5 2 Day05.part2
+    // time 6 1 Day06.part1
+    // time 6 2 Day06.part2
+    // time 7 1 Day07.part1
+    // time 7 2 Day07.part2
+    // time 8 1 Day08.part1
+    // time 8 2 Day08.part2
+    // time 9 1 Day09.part1
+    // time 9 2 Day09.part2
+    // time 10 1 Day10.part1
+    // time 10 2 Day10.part2
+    // time 11 1 Day11.part1
+    // time 11 2 Day11.part2
+    // time 12 1 Day12.part1
+    // time 12 2 Day12.part2
+    // time 13 1 Day13.part1
+    // time 14 1 Day14.part1
+    // time 14 2 Day14.part2
+    // time 16 1 Day16.part1
+    // time 16 2 Day16.part2
+    // time 17 1 Day17.part1
+    // time 17 2 Day17.part2
+    // time 18 1 Day18.part1
+    // time 18 2 Day18.part2
+    // time 19 1 Day19.part1
+    // time 19 2 Day19.part2
+    // time 20 1 Day20.part1
+    // time 21 1 Day21.part1
+    // time 22 1 Day22.part1
+    // time 23 1 Day23.part1
+    // time 23 2 Day23.part2
+    // time 25 1 Day25.part1
+
+    0

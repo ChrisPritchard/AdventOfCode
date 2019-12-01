9ca7b29612f32c74497796dcc8a787d036dd751a
Christopher <chrispritchard.nz@gmail.com>
2019-12-01 18:09:49 +1300

    day 1 complete

--- 2019/Day01.fs
+++ 2019/Day01.fs
@@ -3,10 +3,16 @@
 open System.IO
 open Common
 
-let input = File.ReadAllLines("./inputs/day01.txt")
+let input = File.ReadAllLines ("./inputs/day01.txt")
 
 let part1 () =
-    input
+    input |> Array.sumBy (float >> fun v -> floor (v / 3.) - 2.) |> int
 
 let part2 () =
-    0
+
+    let rec processFuel acc v = 
+        let res = floor (v / 3.) - 2.
+        if res <= 0. then acc
+        else processFuel (acc + res) res
+
+    input |> Array.sumBy (float >> processFuel 0.) |> int

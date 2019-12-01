cc57e7f35475e430a25f3b50b40959da9ab09668
Christopher <chrispritchard.nz@gmail.com>
2019-12-02 08:19:46 +1300

    work on test data

--- 2019/AStar.fs
+++ 2019/AStar.fs
@@ -3,7 +3,7 @@
 open System
 open System.Collections.Generic
 
-let astar<'T when 'T : equality> (isGoal: 'T -> bool) (edges: 'T -> seq<'T * float>) (h: 'T -> float) (start: 'T) =
+let astar<'T when 'T : equality> (isGoal: 'T -> bool) (edges: 'T -> seq<'T>) (d: 'T -> 'T -> float) (h: 'T -> float) (start: 'T) =
     
     let cameFrom = Dictionary<'T, 'T>()
     let gScore = Dictionary<'T, float>()
@@ -34,8 +34,8 @@ let astar<'T when 'T : equality> (isGoal: 'T -> bool) (edges: 'T -> seq<'T * flo
             else
                 openSet.Remove current |> ignore
                 let edges = edges current
-                for (neighbour, d) in edges do
-                    let tentativeGscore = gScore.[current] + d
+                for neighbour in edges do
+                    let tentativeGscore = gScore.[current] + d current neighbour
                     let found, currentGscore = gScore.TryGetValue neighbour
                     if not found || tentativeGscore < currentGscore then
                         cameFrom.Add(neighbour, current)
@@ -45,3 +45,39 @@ let astar<'T when 'T : equality> (isGoal: 'T -> bool) (edges: 'T -> seq<'T * flo
                 searcher ()
 
     searcher ()
+
+// example of use:
+
+let testArray = 
+    array2D
+        [|
+            [|' ';' ';' ';' ';' ';' ';' ';' ';' ';' '|]
+            [|' ';' ';' ';' ';' ';' ';' ';' ';' ';' '|]
+            [|' ';' ';' ';' ';' ';' ';' ';' ';' ';' '|]
+            [|' ';' ';' ';' ';' ';' ';' ';'X';'X';' '|]
+            [|' ';' ';' ';' ';' ';' ';'X';'X';' ';' '|]
+            [|' ';' ';' ';' ';' ';' ';'X';' ';' ';' '|]
+            [|' ';' ';' ';' ';'X';'X';'X';' ';'X';'X'|]
+            [|' ';' ';' ';' ';'X';' ';' ';' ';'X';' '|]
+            [|' ';' ';' ';' ';'X';' ';'X';'X';'X';' '|]
+            [|' ';' ';' ';' ';'X';' ';'X';' ';' ';' '|]
+            [|' ';' ';' ';' ';'X';' ';'X';' ';' ';' '|]
+            [|' ';' ';' ';' ';'X';' ';' ';' ';'X';' '|]
+            [|' ';' ';' ';' ';' ';' ';'X';'X';'X';' '|]
+            [|' ';' ';' ';' ';' ';' ';'X';' ';' ';' '|]
+            [|' ';' ';' ';' ';' ';' ';'X';' ';' ';' '|]
+        |]
+
+let start = 0, 0
+let gx, gy = testArray.GetLength(1) - 1, testArray.GetLength(0) - 1
+let d _ _ = 1.
+let h (x, y) = sqrt ((float gx - float x) ** 2. + (float gy - float y) ** 2.)
+let isGoal = (=) (gx, gy)
+let edges (x, y) =
+    [-1, 0; 1, 0; 0, -1; 0, 1] 
+    |> Seq.map (fun (nx, ny) -> x + nx, y + ny)
+    |> Seq.filter (fun (dx, dy) -> 
+        dx >= 0 && dy >= 0 && dx < testArray.GetLength(1) && dy < testArray.GetLength(0)
+        && testArray.[dy, dx] = ' ')
+
+let path = astar isGoal edges d h start

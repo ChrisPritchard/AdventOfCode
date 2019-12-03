486b1199815752292e513fced904bd6d16b075e2
Christopher <chrispritchard.nz@gmail.com>
2019-12-03 16:44:17 +1300

    finished astar, fixing a bug with sortedset

--- 2019/AStar.fs
+++ 2019/AStar.fs
@@ -7,18 +7,13 @@ let astar<'T when 'T : equality> (isGoal: 'T -> bool) (edges: 'T -> seq<'T>) (d:
     
     let cameFrom = Dictionary<'T, 'T>()
     let gScore = Dictionary<'T, float>()
-    gScore.Add(start, 0.)
+    gScore.Add (start, 0.)
 
     let fScore = Dictionary<'T, float>()
-    fScore.Add(start, h start)
+    fScore.Add (start, h start)
 
-    let comparer a b =
-        let fA = fScore.TryGetValue a |> fun (exists, v) -> if exists then v else Double.MaxValue
-        let fB = fScore.TryGetValue b |> fun (exists, v) -> if exists then v else Double.MaxValue
-        fA - fB |> int
-    let sorter = Comparer<'T>.Create(Comparison<'T>(comparer))
-    let openSet = SortedSet<'T>(sorter)
-    openSet.Add (start) |> ignore
+    let openSet = HashSet<'T>()
+    openSet.Add start |> ignore
 
     let rec reconstructPath acc v =
         if cameFrom.ContainsKey v then
@@ -27,9 +22,10 @@ let astar<'T when 'T : equality> (isGoal: 'T -> bool) (edges: 'T -> seq<'T>) (d:
             v::acc
 
     let rec searcher () =
-        if openSet.Count = 0 then None
+        if openSet.Count = 0 then 
+            None
         else
-            let current = Seq.head openSet
+            let current = Seq.minBy (fun v -> if fScore.ContainsKey v then fScore.[v] else Double.MaxValue) openSet
             if isGoal current then Some (reconstructPath [] current)
             else
                 openSet.Remove current |> ignore
@@ -46,5 +42,46 @@ let astar<'T when 'T : equality> (isGoal: 'T -> bool) (edges: 'T -> seq<'T>) (d:
 
     searcher ()
 
-// example of use:
+    // example of use:
+    
+    (*
+    let testArray = 
+        array2D
+            [|
+                [|' ';' ';' ';' ';' ';' ';' ';' ';' ';' '|]
+                [|' ';' ';' ';' ';' ';' ';' ';' ';' ';' '|]
+                [|' ';' ';' ';' ';' ';' ';' ';' ';' ';' '|]
+                [|' ';' ';' ';' ';' ';' ';' ';'X';'X';' '|]
+                [|' ';' ';' ';' ';' ';' ';'X';'X';' ';' '|]
+                [|' ';' ';' ';' ';' ';' ';'X';' ';' ';' '|]
+                [|' ';' ';' ';' ';'X';'X';'X';' ';'X';'X'|]
+                [|' ';' ';' ';' ';'X';' ';' ';' ';'X';' '|]
+                [|' ';' ';' ';' ';'X';' ';'X';'X';'X';' '|]
+                [|' ';' ';' ';' ';'X';' ';'X';' ';' ';' '|]
+                [|' ';' ';' ';' ';'X';' ';'X';' ';' ';' '|]
+                [|' ';' ';' ';' ';'X';' ';' ';' ';'X';' '|]
+                [|' ';' ';' ';' ';'X';' ';'X';'X';'X';' '|]
+                [|' ';' ';' ';' ';'X';' ';'X';' ';' ';' '|]
+                [|' ';' ';' ';' ';'X';' ';'X';' ';' ';' '|]
+            |]
+    
+    let start = 0, 0
+    let gx, gy = testArray.GetLength(1) - 1, testArray.GetLength(0) - 1
+    let d _ _ = 1.
+    let h (x, y) = sqrt ((float gx - float x) ** 2. + (float gy - float y) ** 2.)
+    let isGoal = (=) (gx, gy)
+    let edges (x, y) =
+        [-1, 0; 1, 0; 0, -1; 0, 1] 
+        |> Seq.map (fun (nx, ny) -> x + nx, y + ny)
+        |> Seq.filter (fun (dx, dy) -> 
+            dx >= 0 && dy >= 0 && dx < testArray.GetLength(1) && dy < testArray.GetLength(0)
+            && testArray.[dy, dx] = ' ')
+    
+    let path = AStar.astar isGoal edges d h start |> Option.defaultValue []
 
+    for y = 0 to testArray.GetLength(0) - 1 do
+        for x = 0 to testArray.GetLength(1) - 1 do
+            if Seq.contains (x, y) path then printf "*"
+            else printf "%c" testArray.[y, x]
+        printfn ""
+*)

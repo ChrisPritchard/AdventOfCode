f978b55f2a7e2517315a16facca724e99675e5e6
Christopher <chrispritchard.nz@gmail.com>
2019-12-04 15:52:50 +1300

    cells line

--- 2019/Day04.fs
+++ 2019/Day04.fs
@@ -4,6 +4,7 @@ open System.IO
 open System.Collections.Generic
 
 let text = File.ReadAllText ("./inputs/day04.txt")
+let cells = text.Split ','
 let lines = File.ReadAllLines ("./inputs/day04.txt")
 
 let part1 () =

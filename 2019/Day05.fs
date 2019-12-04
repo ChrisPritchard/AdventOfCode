bccdd5a1e183d46e4e32c4ab449344eb3ef938e4
Christopher <chrispritchard.nz@gmail.com>
2019-12-05 07:44:39 +1300

    setup for day 5

---
+++ 2019/Day05.fs
@@ -0,0 +1,16 @@
+﻿module Day05
+
+open Common
+open System.IO
+open System.Collections.Generic
+
+let text = File.ReadAllText ("./inputs/day05.txt")
+let cells = text.Split ','
+let numbers = text |> split "-," |> Array.map int
+let lines = File.ReadAllLines ("./inputs/day05.txt")
+
+let part1 () =
+    text
+
+let part2 () =
+    text

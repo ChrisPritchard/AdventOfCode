c1d18c48355af02b0654bce19c98e7a236d72d14
Chris Pritchard <chrispritchard.nz@gmail.com>
2019-12-06 10:43:43 +1300

    prep for day 6

---
+++ 2019/Day06.fs
@@ -0,0 +1,16 @@
+﻿module Day06
+
+open Common
+open System.IO
+open System.Collections.Generic
+
+let text = File.ReadAllText ("./inputs/day06.txt")
+let cells = File.ReadAllText ("./inputs/day06.txt") |> split "-,"
+let numbers = File.ReadAllText ("./inputs/day06.txt") |> split "-," |> Array.map int
+let lines = File.ReadAllLines ("./inputs/day06.txt")
+
+let part1 () =
+    text
+
+let part2 () =
+    text

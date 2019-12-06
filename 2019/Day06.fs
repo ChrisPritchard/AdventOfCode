e876440f65799c3354024bb453c8eef93681c8f5
Christopher <chrispritchard.nz@gmail.com>
2019-12-07 10:32:18 +1300

    added an alternate implementation for day 6 part 2

--- 2019/Day06.fs
+++ 2019/Day06.fs
@@ -27,3 +27,23 @@ let part2 () =
             else orbiting |> Seq.ofArray)
         orbitMap.["YOU"]
     |> Option.defaultValue [] |> Seq.length |> fun i -> i - 1
+
+
+// while I did part 2 with a BFS, as I had my BFS utility ready, it could also have been done with a plain Seq zip like below:
+
+(*
+let part2 () =
+    let orbits = lines |> Array.map (fun s -> (s.Split ')').[1], (s.Split(')').[0]))
+    let orbitMap = orbits |> Map.ofArray
+
+    let rec path acc c =
+        if not (Map.containsKey c orbitMap) then 
+            c::acc
+        else
+            path (c::acc) orbitMap.[c]
+    
+    let pathA = path [] orbitMap.["SAN"]
+    let pathB = path [] orbitMap.["YOU"]
+    let meet = Seq.zip pathA pathB |> Seq.findIndex (fun (a, b) -> a <> b)
+    (List.length pathA + List.length pathB) - (2 * meet)
+*)

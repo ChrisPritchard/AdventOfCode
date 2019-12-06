a81df995b98c34975daa39198c75f10a0d089062
Christopher <chrispritchard.nz@gmail.com>
2019-12-06 18:28:25 +1300

    finished day 6

--- 2019/Program.fs
+++ 2019/Program.fs
@@ -10,29 +10,29 @@ let main _ =
     time 6 1 Day06.part1
     time 6 2 Day06.part2
 
-    printfn ""
+    //printfn ""
     
-    time 5 1 Day05.part1
-    time 5 2 Day05.part2
+    //time 5 1 Day05.part1
+    //time 5 2 Day05.part2
 
-    printfn ""
+    //printfn ""
     
-    time 4 1 Day04.part1
-    time 4 2 Day04.part2
+    //time 4 1 Day04.part1
+    //time 4 2 Day04.part2
 
-    printfn ""
+    //printfn ""
     
-    time 3 1 Day03.part1
-    time 3 2 Day03.part2
+    //time 3 1 Day03.part1
+    //time 3 2 Day03.part2
 
-    printfn ""
+    //printfn ""
     
-    time 2 1 Day02.part1
-    time 2 2 Day02.part2
+    //time 2 1 Day02.part1
+    //time 2 2 Day02.part2
 
-    printfn ""
+    //printfn ""
     
-    time 1 1 Day01.part1
-    time 1 2 Day01.part2
+    //time 1 1 Day01.part1
+    //time 1 2 Day01.part2
 
     0

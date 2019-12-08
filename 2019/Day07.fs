09a4a51e121f8cf67f30ce6f3d88db977aa7a370
Christopher <chrispritchard.nz@gmail.com>
2019-12-09 09:02:12 +1300

    removed queues from ops

--- 2019/Day07.fs
+++ 2019/Day07.fs
@@ -8,9 +8,9 @@ open System.Collections.Generic
 let startMemory = (File.ReadAllText ("./inputs/day07.txt")).Split ',' |> Array.map int
 //let startMemory = "3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,-5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10" |> split "," |> Array.map int
 
-type State = Running | Halted | Blocked of int * int[]
+type State = Running | Halted | Blocked
 
-let intcodeRun opcodes startIp (memory: int[]) input output =
+let intcodeRun opcodes startIp (memory: int[]) read write =
     let parseOp code = 
         let op = code % 100
         (op, code % 1000 / 100, code % 10000 / 1000)
@@ -18,13 +18,15 @@ let intcodeRun opcodes startIp (memory: int[]) input output =
         let opcode, mode1, mode2 = parseOp memory.[ip]
         let v1 () = let v = memory.[ip + 1] in if mode1 = 0 then memory.[v] else v
         let v2 () = let v = memory.[ip + 2] in if mode2 = 0 then memory.[v] else v
+        let set i v = let o = memory.[i] in memory.[o] <- v
         try
             let op = Map.find opcode opcodes
-            let nextIp, nextState = op ip memory v1 v2 input output
-            if nextState = Running then processor nextIp else nextState
+            let nextIp, nextState = op ip v1 v2 set read write
+            if nextState = Running then processor nextIp else nextState, nextIp
         with
-        | :? KeyNotFoundException | :? IndexOutOfRangeException  -> Halted
-    processor startIp
+        | :? KeyNotFoundException | :? IndexOutOfRangeException  -> Halted, ip
+    let finalState, finalIp = processor startIp
+    finalState, finalIp, memory
 
 let parse (mem: int[]) value mode =
     if mode = 0 then mem.[value] else value
@@ -33,50 +35,54 @@ let ops = Map.ofList [
     99, (fun ip _ _ _ _ _ -> // halt
         ip, Halted)
 
-    1, (fun ip (mem: int[]) v1 v2 _ _ -> // Add
-        mem.[mem.[ip + 3]] <- v1() + v2()
+    1, (fun ip v1 v2 set _ _ -> // Add
+        v1() + v2() |> set (ip + 3)
         ip + 4, Running)
 
-    2, (fun ip (mem: int[]) v1 v2 _ _ -> // Mul
-        mem.[mem.[ip + 3]] <- v1() * v2()
+    2, (fun ip v1 v2 set _ _ -> // Mul
+        v1() * v2() |> set (ip + 3)
         ip + 4, Running)
 
-    3, (fun ip (mem: int[]) _ _ (input: Queue<int>) _-> // Read or block
-        if input.Count = 0 then
-            ip, Blocked (ip, mem)
+    3, (fun ip _ _ set read _-> // Read or block
+        let (canRead, value) = read ()
+        if canRead then
+            set (ip + 1) value
+            ip + 2, Running
         else
-            mem.[mem.[ip + 1]] <- input.Dequeue ()
-            ip + 2, Running)
+            ip, Blocked)
 
-    4, (fun ip _ v1 _ _ (output: Queue<int>) -> // Write
-       output.Enqueue (v1())
+    4, (fun ip v1 _ _ _ write -> // Write
+       write <| v1()
        ip + 2, Running)
 
-    5, (fun ip _ v1 v2 _ _ -> // jump if greater
+    5, (fun ip v1 v2 _ _ _ -> // jump if greater
         let nextIp = if v1() > 0 then v2() else ip + 3
         nextIp, Running)
 
-    6, (fun ip _ v1 v2 _ _ -> // jump if equal
+    6, (fun ip v1 v2 _ _ _ -> // jump if equal
         let nextIp = if v1() = 0 then v2() else ip + 3
         nextIp, Running)
 
-    7, (fun ip (mem: int[]) v1 v2 _ _ -> // set if less
-        mem.[mem.[ip + 3]] <- if v1() < v2() then 1 else 0
+    7, (fun ip v1 v2 set _ _ -> // set if less
+        set (ip + 3) <| if v1() < v2() then 1 else 0
         ip + 4, Running)
 
-    8, (fun ip (mem: int[]) v1 v2 _ _ -> // set if equal
-        mem.[mem.[ip + 3]] <- if v1() = v2() then 1 else 0
+    8, (fun ip v1 v2 set _ _ -> // set if equal
+        set (ip + 3) <| if v1() = v2() then 1 else 0
         ip + 4, Running)
     ]
+
+let read (queue: Queue<int>) = 
+    fun () -> if queue.Count > 0 then true, queue.Dequeue () else false, 0
 
 let part1 () =
 
     let runWith phase previous = 
+        let memory = Array.copy startMemory
+
         let input = Queue<int>([phase;previous])
         let output = Queue<int>()
-
-        let memory = Array.copy startMemory
-        intcodeRun ops 0 memory input output |> ignore
+        intcodeRun ops 0 memory (read input) (output.Enqueue) |> ignore
         output.Dequeue ()
 
     let range = [0..4]
@@ -114,48 +120,41 @@ let part2 () =
                             let rec amplifier aState bState cState dState eState =
                                 let aState =
                                     match aState with
-                                    | Running ->
-                                        intcodeRun ops 0 (Array.copy startMemory) aInput bInput
-                                    | Blocked (ip, mem) ->
-                                        intcodeRun ops ip mem aInput bInput
-                                    | _ -> aState
+                                    | Halted, _, _ -> aState
+                                    | _, ip, mem ->
+                                        intcodeRun ops ip mem (read aInput) (bInput.Enqueue)
 
                                 let bState =
                                     match bState with
-                                    | Running ->
-                                        intcodeRun ops 0 (Array.copy startMemory) bInput cInput
-                                    | Blocked (ip, mem) ->
-                                        intcodeRun ops ip mem bInput cInput
-                                    | _ -> bState
+                                    | Halted, _, _ -> bState
+                                    | _, ip, mem ->
+                                        intcodeRun ops ip mem (read bInput) (cInput.Enqueue)
 
                                 let cState =
                                     match cState with
-                                    | Running ->
-                                        intcodeRun ops 0 (Array.copy startMemory) cInput dInput
-                                    | Blocked (ip, mem) ->
-                                        intcodeRun ops ip mem cInput dInput
-                                    | _ -> cState
+                                    | Halted, _, _ -> cState
+                                    | _, ip, mem ->
+                                        intcodeRun ops ip mem (read cInput) (dInput.Enqueue)
 
                                 let dState =
                                     match dState with
-                                    | Running ->
-                                        intcodeRun ops 0 (Array.copy startMemory) dInput eInput
-                                    | Blocked (ip, mem) ->
-                                        intcodeRun ops ip mem dInput eInput
-                                    | _ -> dState
+                                    | Halted, _, _ -> dState
+                                    | _, ip, mem ->
+                                        intcodeRun ops ip mem (read dInput) (eInput.Enqueue)
 
                                 let eState =
                                     match eState with
-                                    | Running ->
-                                        intcodeRun ops 0 (Array.copy startMemory) eInput aInput
-                                    | Blocked (ip, mem) ->
-                                        intcodeRun ops ip mem eInput aInput
-                                    | _ -> eState
+                                    | Halted, _, _ -> eState
+                                    | _, ip, mem ->
+                                        intcodeRun ops ip mem (read eInput) (aInput.Enqueue)
 
-                                if eState = Halted && aInput.Count > 0 then
-                                    aInput.Dequeue ()
-                                else
+                                match eState with
+                                | Halted, _, _ ->
+                                    if aInput.Count > 0 then aInput.Dequeue ()
+                                    else 0
+                                | _ ->
                                     amplifier aState bState cState dState eState
 
-                            yield amplifier Running Running Running Running Running
+                            let startState () = Running, 0, Array.copy startMemory
+                            yield amplifier (startState ()) (startState ()) (startState ()) (startState ()) (startState ())
     } |> Seq.max

97b07dbcfde8b31d49c262fda518def040432c35
Christopher <chrispritchard.nz@gmail.com>
2019-12-08 17:50:21 +1300

    prep day 8

---
+++ 2019/Day08.fs
@@ -0,0 +1,15 @@
+﻿module Day08
+
+open Common
+open System.IO
+open System.Collections.Generic
+
+let startMemory = (File.ReadAllText ("./inputs/day08.txt")).Split ',' |> Array.map int
+
+let part1 () =
+
+    0
+
+let part2 () =
+    
+    0

6f170acd2af031708773cd3622c8d551614ecf7d
Christopher <chrispritchard.nz@gmail.com>
2019-12-09 18:28:16 +1300

    finished day 9

--- 2019/Day09.fs
+++ 2019/Day09.fs
@@ -5,101 +5,103 @@ open System
 open System.IO
 open System.Collections.Generic
 
-let startMemory = (File.ReadAllText ("./inputs/day09.txt")).Split ',' |> Array.map int
-//let startMemory = "3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,-5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10" |> split "," |> Array.map int
+let startMemory = (File.ReadAllText ("./inputs/day09.txt")).Split ',' |> Array.map int64
+//let startMemory = "109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99" |> split "," |> Array.map (int64)
 
 type State = Running | Halted | Blocked
 
-let intcodeRun opcodes startIp (memory: int[]) read write =
+let intcodeRun opcodes startIp startRb (memory: int64[]) read write =
     let parseOp code = 
-        let op = code % 100
-        (op, code % 1000 / 100, code % 10000 / 1000)
-    let rec processor ip =
-        let opcode, mode1, mode2 = parseOp memory.[ip]
-        let v1 () = let v = memory.[ip + 1] in if mode1 = 0 then memory.[v] else v
-        let v2 () = let v = memory.[ip + 2] in if mode2 = 0 then memory.[v] else v
-        let set i v = let o = memory.[i] in memory.[o] <- v
-        try
-            let op = Map.find opcode opcodes
-            let nextIp, nextState = op ip v1 v2 set read write
-            if nextState = Running then processor nextIp else nextState, nextIp
-        with
-        | :? KeyNotFoundException | :? IndexOutOfRangeException  -> Halted, ip
-    let finalState, finalIp = processor startIp
+        let op = code % 100L
+        (op, code % 1000L / 100L, code % 10000L / 1000L, code % 100000L / 10000L)
+    let rec processor (ip: int) (rb: int) =
+        let opcode, mode1, mode2, mode3 = parseOp memory.[ip]
+        let v1 () = 
+            let v = memory.[int ip + 1] 
+            if mode1 = 0L then memory.[int v] 
+            elif mode1 = 2L then memory.[int (rb + int v)]
+            else v
+        let v2 () = 
+            let v = memory.[int ip + 2] 
+            if mode2 = 0L then memory.[int v] 
+            elif mode2 = 2L then memory.[int (rb + int v)]
+            else v
+        let set i idx v = 
+            let o = memory.[i]
+            let sidx = 
+                if idx = 1 && mode1 = 2L then rb + int o
+                elif idx = 2 && mode2 = 2L then rb + int o
+                elif idx = 3 && mode3 = 2L then rb + int o
+                else int o
+            memory.[sidx] <- v
+        let op = Map.find opcode opcodes
+        let nextIp, nextRb, nextState = op ip rb v1 v2 set read write
+        if nextState = Running then processor nextIp nextRb else nextState, nextIp
+    let finalState, finalIp = processor startIp startRb
     finalState, finalIp, memory
 
 let parse (mem: int[]) value mode =
     if mode = 0 then mem.[value] else value
 
 let ops = Map.ofList [
-    99, (fun ip _ _ _ _ _ -> // halt
-        ip, Halted)
+    99L, (fun ip (rb: int) _ _ _ _ _ -> // halt
+        ip, rb, Halted)
 
-    1, (fun ip v1 v2 set _ _ -> // Add
-        v1() + v2() |> set (ip + 3)
-        ip + 4, Running)
+    1L, (fun ip rb v1 v2 set _ _ -> // Add
+        v1() + v2() |> set (ip + 3) 3
+        ip + 4, rb, Running)
 
-    2, (fun ip v1 v2 set _ _ -> // Mul
-        v1() * v2() |> set (ip + 3)
-        ip + 4, Running)
+    2L, (fun ip rb v1 v2 set _ _ -> // Mul
+        v1() * v2() |> set (ip + 3) 3
+        ip + 4, rb, Running)
 
-    3, (fun ip _ _ set read _-> // Read or block
+    3L, (fun ip rb _ _ set read _-> // Read or block
         let (canRead, value) = read ()
         if canRead then
-            set (ip + 1) value
-            ip + 2, Running
+            set (ip + 1) 1 value
+            ip + 2, rb, Running
         else
-            ip, Blocked)
+            ip, rb, Blocked)
 
-    4, (fun ip v1 _ _ _ write -> // Write
+    4L, (fun ip rb v1 _ _ _ write -> // Write
        write <| v1()
-       ip + 2, Running)
+       ip + 2, rb, Running)
 
-    5, (fun ip v1 v2 _ _ _ -> // jump if greater
-        let nextIp = if v1() > 0 then v2() else ip + 3
-        nextIp, Running)
+    5L, (fun ip rb v1 v2 _ _ _ -> // jump if greater
+        let nextIp = if v1() > 0L then int (v2()) else ip + 3
+        nextIp, rb, Running)
 
-    6, (fun ip v1 v2 _ _ _ -> // jump if equal
-        let nextIp = if v1() = 0 then v2() else ip + 3
-        nextIp, Running)
+    6L, (fun ip rb v1 v2 _ _ _ -> // jump if equal
+        let nextIp = if v1() = 0L then int (v2()) else ip + 3
+        nextIp, rb, Running)
 
-    7, (fun ip v1 v2 set _ _ -> // set if less
-        set (ip + 3) <| if v1() < v2() then 1 else 0
-        ip + 4, Running)
+    7L, (fun ip rb v1 v2 set _ _ -> // set if less
+        set (ip + 3) 3 <| if v1() < v2() then 1L else 0L
+        ip + 4, rb, Running)
 
-    8, (fun ip v1 v2 set _ _ -> // set if equal
-        set (ip + 3) <| if v1() = v2() then 1 else 0
-        ip + 4, Running)
+    8L, (fun ip rb v1 v2 set _ _ -> // set if equal
+        set (ip + 3) 3 <| if v1() = v2() then 1L else 0L
+        ip + 4, rb, Running)
+
+    9L, (fun ip rb v1 _ _ _ _ -> // alter relative base
+        ip + 2, rb + int (v1()), Running)
     ]
 
-let read (queue: Queue<int>) = 
-    fun () -> if queue.Count > 0 then true, queue.Dequeue () else false, 0
+let read (queue: Queue<int64>) = 
+    fun () -> if queue.Count > 0 then true, queue.Dequeue () else false, 0L
 
 let part1 () =
 
-    let runWith phase previous = 
-        let memory = Array.copy startMemory
-
-        let input = Queue<int>([phase;previous])
-        let output = Queue<int>()
-        intcodeRun ops 0 memory (read input) (output.Enqueue) |> ignore
-        output.Dequeue ()
-
-    let range = [0..4]
-
-    seq {
-        for a in range do
-            let aRes = runWith a 0
-            for b in List.except [a] range do
-                let bRes = runWith b aRes
-                for c in List.except [a; b] range do
-                    let cRes = runWith c bRes
-                    for d in List.except [a; b; c] range do
-                        let dRes = runWith d cRes
-                        for e in List.except [a; b; c; d] range do
-                            yield runWith e dRes
-    } |> Seq.max
+    let input = Queue<int64>([1L])
+    let output = Queue<int64>()
+    let memory = Array.append (Array.copy startMemory) (Array.create 100000 0L)
+    intcodeRun ops 0 0 memory (read input) (output.Enqueue) |> ignore
+    output.Dequeue ()
 
 let part2 () =
     
-    0
+    let input = Queue<int64>([2L])
+    let output = Queue<int64>()
+    let memory = Array.append (Array.copy startMemory) (Array.create 100000 0L)
+    intcodeRun ops 0 0 memory (read input) (output.Enqueue) |> ignore
+    output.Dequeue ()

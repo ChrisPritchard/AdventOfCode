eeabcefd483f8ceba74837ca0967a2a0f740f908
Christopher <chrispritchard.nz@gmail.com>
2019-12-10 17:56:24 +1300

    prep for part 10

---
+++ 2019/Day10.fs
@@ -0,0 +1,20 @@
+﻿module Day10
+
+open Common
+open System.IO
+open System.Collections.Generic
+
+type T = int64
+type TQueue = Queue<T>
+let stot (o: string) = int64 o
+let itot (o: int) = int64 o
+
+let input = File.ReadAllText ("./inputs/day10.txt")
+    
+let part1 () =
+
+    0
+
+let part2 () =
+    
+    0

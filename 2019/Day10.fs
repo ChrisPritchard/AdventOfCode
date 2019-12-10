ac02c23f1a65dbc98bf41d24d9a23f33e656fcd7
Chris Pritchard <chrispritchard.nz@gmail.com>
2019-12-11 09:31:33 +1300

    inaccurate angle based version

--- 2019/Day10.fs
+++ 2019/Day10.fs
@@ -1,13 +1,6 @@
 ﻿module Day10
 
-open Common
 open System.IO
-open System.Collections.Generic
-
-type T = int64
-type TQueue = Queue<T>
-let stot (o: string) = int64 o
-let itot (o: int) = int64 o
 
 let input = File.ReadAllLines ("./inputs/day10.txt")
 let asteroids = 
@@ -19,24 +12,20 @@ let asteroids =
     
 let part1 () =
 
-    let cache = Dictionary<((int * int) * (int * int)), float>()
+    let angle (x1, y1) (x2, y2) =
+        ((float y2 - float y1) / (float x2 - float x1)) |> tanh
+
     let dist (x1, y1) (x2, y2) =
-        let key = (x1, y1), (x2, y2)
-        let keyi = (x2, y2), (x1, y1)
-        if cache.ContainsKey key then cache.[key]
-        elif cache.ContainsKey keyi then cache.[keyi]
-        else
-            let d = sqrt ((float x2 - float x1) ** 2. + (float y2 - float y1) ** 2.)
-            cache.Add (key, d)
-            d
-
-    let margin = 0.0001
-
+        (pown (x2 - x1) 2 + pown (y2 - y1) 2) |> float |> sqrt
+        
     let lineOfSight a b =
-        a <> b &&
-        asteroids 
-        |> Seq.exists (fun c -> c <> a && c <> b && abs ((dist a c + dist b c) - dist a b) <= margin)
-        |> not
+        if a = b then false
+        else
+            let angleAB = angle a b
+            let distAB = dist a b
+            asteroids 
+            |> Seq.exists (fun c -> c <> a && c <> b && angle a c = angleAB && dist a c < distAB)
+            |> not
 
     let maxReachable (x, y) =
         asteroids |> Seq.filter (lineOfSight (x, y)) |> Seq.length

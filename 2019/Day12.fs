b9df7f62ff727ff8cbb478deadc7996a07cceffd
Chris Pritchard <chrispritchard.nz@gmail.com>
2019-12-12 10:17:29 +1300

    setup for day12

---
+++ 2019/Day12.fs
@@ -0,0 +1,15 @@
+﻿module Day12
+
+open Common
+open System.IO
+open System.Collections.Generic
+
+let input = File.ReadAllText ("./inputs/day12.txt")
+
+let part1 () =    
+    
+    0
+
+let part2 () =
+    
+    0

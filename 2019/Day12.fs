cc9fa451e293b8d853288e7e4756dd1a91e484ee
Christopher <chrispritchard.nz@gmail.com>
2019-12-12 19:23:09 +1300

    finished part 1 of day 12

--- 2019/Day12.fs
+++ 2019/Day12.fs
@@ -4,11 +4,42 @@ open Common
 open System.IO
 open System.Collections.Generic
 
-let input = File.ReadAllText ("./inputs/day12.txt")
+let input = 
+    File.ReadAllLines ("./inputs/day12.txt") 
+    |> Array.map (fun line -> 
+        let parts = split "<x=, yz>" line
+        (int parts.[0], int parts.[1], int parts.[2]))
+
+let moons = input |> Array.map (fun p -> p, (0, 0, 0))
+
+let timeStep moons =
+    let appliedGravity = 
+        moons
+        |> Array.map (fun ((x, y, z), v) ->
+            let nv = 
+                (v, moons) ||> Array.fold (fun (vx, vy, vz) ((ox, oy, oz), _) ->
+                    (if ox > x then vx + 1 elif ox < x then vx - 1 else vx),
+                    (if oy > y then vy + 1 elif oy < y then vy - 1 else vy),
+                    (if oz > z then vz + 1 elif oz < z then vz - 1 else vz))
+            (x, y, z), nv)
+        
+    appliedGravity
+    |> Array.map (fun ((x, y, z), (vx, vy, vz)) ->
+        (x + vx, y + vy, z + vz), (vx, vy, vz))
+
+let energy moons = 
+    moons 
+    |> Array.sumBy (fun ((x, y, z), (vx, vy, vz)) -> 
+        (abs x + abs y + abs z) * (abs vx + abs vy + abs vz))
 
 let part1 () =    
-    
-    0
+
+    let rec runSteps rem moons =
+        if rem = 0 then moons
+        else
+            runSteps (rem - 1) (timeStep moons)
+
+    runSteps 1000 moons |> energy
 
 let part2 () =
     

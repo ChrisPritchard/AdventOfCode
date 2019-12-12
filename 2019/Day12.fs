3f12b11d9763caae6d80c5978760f0c08390a4ea
Chris Pritchard <chrispritchard.nz@gmail.com>
2019-12-13 08:40:37 +1300

    simple sdl renderer for day 12 part 2

--- 2019/Day12.fs
+++ 2019/Day12.fs
@@ -2,7 +2,9 @@
 
 open Common
 open System.IO
-open System.Collections.Generic
+open SDL
+open System
+open System.Runtime.InteropServices
 
 let input = 
     File.ReadAllLines ("./inputs/day12.txt") 
@@ -43,13 +45,58 @@ let part1 () =
 
 let part2 () =
 
-    let tracker = Dictionary<(((int * int * int) * (int * int * int))[]), uint64>()
+    let viewWidth, viewHeight = 1800, 1000
+    let asUint32 (r, g, b) = BitConverter.ToUInt32 (ReadOnlySpan [|b; g; r; 255uy|])
     
-    let rec counter acc moons =
-        let next = timeStep moons
-        if tracker.ContainsKey(next) then acc - tracker.[next]
-        else
-            tracker.Add (next, acc)
-            counter (acc + 1UL) next
+    let moonColours = 
+        [|
+            asUint32 (255uy, 0uy, 0uy)
+            asUint32 (0uy, 255uy, 0uy)
+            asUint32 (0uy, 0uy, 255uy)
+            asUint32 (255uy, 255uy, 255uy)
+        |]
+    let black = asUint32 (0uy, 0uy, 0uy)
 
-    counter 0UL moons
+    SDL_Init(SDL_INIT_VIDEO) |> ignore
+
+    let mutable window, renderer = IntPtr.Zero, IntPtr.Zero
+    let windowFlags = SDL_WindowFlags.SDL_WINDOW_SHOWN ||| SDL_WindowFlags.SDL_WINDOW_INPUT_FOCUS
+    SDL_CreateWindowAndRenderer(viewWidth, viewHeight, windowFlags, &window, &renderer) |> ignore
+
+    let texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING, viewWidth, viewHeight)
+    let frameBuffer = Array.create (viewWidth * viewHeight) black
+    let bufferPtr = IntPtr ((Marshal.UnsafeAddrOfPinnedArrayElement (frameBuffer, 0)).ToPointer ())
+    let mutable keyEvent = Unchecked.defaultof<SDL_KeyboardEvent>
+
+    let drawRect x y w h colour =
+        for dy = y to y + h - 1 do
+            let pos = (dy * viewWidth) + x
+            Array.fill frameBuffer pos w colour
+
+    let rec drawLoop moons =
+
+        let moons = timeStep moons
+        drawRect 0 0 viewWidth viewHeight black
+        moons |> Array.iteri (fun i ((x, y, _), _) ->
+            drawRect (x + viewWidth/2 - 1) (y + viewHeight/2 - 1) 3 3 moonColours.[i])
+
+        SDL_UpdateTexture(texture, IntPtr.Zero, bufferPtr, viewWidth * 4) |> ignore
+        SDL_RenderClear(renderer) |> ignore
+        SDL_RenderCopy(renderer, texture, IntPtr.Zero, IntPtr.Zero) |> ignore
+        SDL_RenderPresent(renderer) |> ignore
+
+        if SDL_PollEvent(&keyEvent) = 0 || (keyEvent.``type`` <> SDL_KEYDOWN && keyEvent.``type`` <> SDL_KEYUP) then
+            SDL_Delay 20ul
+            drawLoop moons
+        else if keyEvent.keysym.sym = SDLK_ESCAPE then 
+            ()
+
+    drawLoop moons
+
+    SDL_DestroyTexture(texture)
+    SDL_DestroyRenderer(renderer)
+    SDL_DestroyWindow(window)
+    SDL_Quit()
+
+    0
+    

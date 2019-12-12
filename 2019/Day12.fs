6f195d4ea5ede4aba3d55c2e69db0d6984390b39
Chris Pritchard <chrispritchard.nz@gmail.com>
2019-12-13 10:18:27 +1300

    solved part 2 day 12

--- 2019/Day12.fs
+++ 2019/Day12.fs
@@ -2,9 +2,6 @@
 
 open Common
 open System.IO
-open SDL
-open System
-open System.Runtime.InteropServices
 
 let input = 
     File.ReadAllLines ("./inputs/day12.txt") 
@@ -43,60 +40,32 @@ let part1 () =
 
     runSteps 1000 moons |> energy
 
+// ultimately I had to go to reddit to get the hint on calculating axii independently
+
 let part2 () =
 
-    let viewWidth, viewHeight = 1800, 1000
-    let asUint32 (r, g, b) = BitConverter.ToUInt32 (ReadOnlySpan [|b; g; r; 255uy|])
-    
-    let moonColours = 
+    let axis moons = 
         [|
-            asUint32 (255uy, 0uy, 0uy)
-            asUint32 (0uy, 255uy, 0uy)
-            asUint32 (0uy, 0uy, 255uy)
-            asUint32 (255uy, 255uy, 255uy)
+        (moons |> Array.map (fun ((x, _, _), (vx, _, _)) -> x, vx))
+        (moons |> Array.map (fun ((_, y, _), (_, vy, _)) -> y, vy))
+        (moons |> Array.map (fun ((_, _, z), (_, _, vz)) -> z, vz))
         |]
-    let black = asUint32 (0uy, 0uy, 0uy)
 
-    SDL_Init(SDL_INIT_VIDEO) |> ignore
+    let start = axis moons
 
-    let mutable window, renderer = IntPtr.Zero, IntPtr.Zero
-    let windowFlags = SDL_WindowFlags.SDL_WINDOW_SHOWN ||| SDL_WindowFlags.SDL_WINDOW_INPUT_FOCUS
-    SDL_CreateWindowAndRenderer(viewWidth, viewHeight, windowFlags, &window, &renderer) |> ignore
-
-    let texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING, viewWidth, viewHeight)
-    let frameBuffer = Array.create (viewWidth * viewHeight) black
-    let bufferPtr = IntPtr ((Marshal.UnsafeAddrOfPinnedArrayElement (frameBuffer, 0)).ToPointer ())
-    let mutable keyEvent = Unchecked.defaultof<SDL_KeyboardEvent>
-
-    let drawRect x y w h colour =
-        for dy = y to y + h - 1 do
-            let pos = (dy * viewWidth) + x
-            Array.fill frameBuffer pos w colour
-
-    let rec drawLoop moons =
-
-        let moons = timeStep moons
-        drawRect 0 0 viewWidth viewHeight black
-        moons |> Array.iteri (fun i ((x, y, _), _) ->
-            drawRect (x + viewWidth/2 - 1) (y + viewHeight/2 - 1) 3 3 moonColours.[i])
-
-        SDL_UpdateTexture(texture, IntPtr.Zero, bufferPtr, viewWidth * 4) |> ignore
-        SDL_RenderClear(renderer) |> ignore
-        SDL_RenderCopy(renderer, texture, IntPtr.Zero, IntPtr.Zero) |> ignore
-        SDL_RenderPresent(renderer) |> ignore
-
-        if SDL_PollEvent(&keyEvent) = 0 || (keyEvent.``type`` <> SDL_KEYDOWN && keyEvent.``type`` <> SDL_KEYUP) then
-            SDL_Delay 20ul
-            drawLoop moons
-        else if keyEvent.keysym.sym = SDLK_ESCAPE then 
-            ()
-
-    drawLoop moons
-
-    SDL_DestroyTexture(texture)
-    SDL_DestroyRenderer(renderer)
-    SDL_DestroyWindow(window)
-    SDL_Quit()
-
-    0
+    let rec findCycles acc cnt moons =
+        if Map.count acc = Array.length start then acc
+        else
+            let next = timeStep moons
+            let curr = axis next
+            let acc = 
+                (acc, Array.indexed curr) 
+                ||> Array.fold (fun acc (i, o) -> 
+                    if acc.ContainsKey i then acc elif o = start.[i] then Map.add i (cnt + 1UL) acc else acc)
+            findCycles acc (cnt + 1UL) next
     
+    let vCycles = findCycles Map.empty 0UL moons |> Map.toArray |> Array.map snd |> Array.sortDescending
+
+    // using def from https://en.wikipedia.org/wiki/Least_common_multiple
+    let rec gcd a b = if b = 0UL then a else gcd b (a % b)
+    vCycles |> Array.reduce (fun a b -> (a * b) / gcd a b) |> string

bb0eb233af2e4ccddcf5e9374d4ee559607821a9
Christopher <chrispritchard.nz@gmail.com>
2019-12-15 09:01:35 +1300

    finished day 14

--- 2019/Day14.fs
+++ 2019/Day14.fs
@@ -2,7 +2,6 @@
 
 open Common
 open System.IO
-open System.Collections.Generic
 
 let input = File.ReadAllLines "./inputs/day14.txt"
 
@@ -20,39 +19,59 @@ let transforms =
                     |> Array.map (fun b -> b.[1], int64 b.[0])))
     |> Map.ofArray
 
+let adder multiplier (ore, required) (other, needed) =
+    if other = "ORE" then ore + (needed * multiplier), required
+    else ore, (other, needed * multiplier)::required
+
+let replacer (ore, spare, required) (needed, amount) =
+    let trueAmount = amount - (Map.tryFind needed spare |> Option.defaultValue 0L)
+    if trueAmount < 0L then
+        (ore, Map.add needed (abs trueAmount) spare, required)
+    elif trueAmount = 0L then 
+        (ore, Map.remove needed spare, required)
+    else
+        let (amountMade, requirements) = transforms.[needed]
+        let multiplier = ceil (float trueAmount / float amountMade) |> int64
+        let spare = Map.add needed ((multiplier * amountMade) - trueAmount) spare
+        let ore, required = 
+            ((ore, required), requirements) 
+            ||> Array.fold (adder multiplier)
+        ore, spare, required
+
+let rec expander ore spare (required: (string * int64) list) =
+    let ore, spare, required = 
+        ((ore, spare, []), required)
+        ||> List.fold replacer
+    if List.length required = 0 then ore
+    else
+        expander ore spare required
+
 let part1 () =    
     
     let start = snd transforms.["FUEL"]
-
-    let adder multiplier (ore, required) (other, needed) =
-        if other = "ORE" then ore + (needed * multiplier), required
-        else ore, (other, needed * multiplier)::required
-    
-    let replacer (ore, spare, required) (needed, amount) =
-        let trueAmount = amount - (Map.tryFind needed spare |> Option.defaultValue 0L)
-        if trueAmount < 0L then
-            (ore, Map.add needed (abs trueAmount) spare, required)
-        elif trueAmount = 0L then 
-            (ore, Map.remove needed spare, required)
-        else
-            let (amountMade, requirements) = transforms.[needed]
-            let multiplier = ceil (float trueAmount / float amountMade) |> int64
-            let spare = Map.add needed ((multiplier * amountMade) - trueAmount) spare
-            let ore, required = 
-                ((ore, required), requirements) 
-                ||> Array.fold (adder multiplier)
-            ore, spare, required
-
-    let rec expander ore spare (required: (string * int64) list) =
-        let ore, spare, required = 
-            ((ore, spare, []), required)
-            ||> List.fold replacer
-        if List.length required = 0 then ore
-        else
-            expander ore spare required
-
     expander 0L Map.empty (List.ofArray start) |> string
 
 let part2 () =
 
-    0
+    let fuel n = 
+        let start = snd transforms.["FUEL"] |> Array.map (fun (s, on) -> s, n * on)
+        expander 0L Map.empty (List.ofArray start)
+
+    let target = 1000000000000L
+
+    let rec find min max =
+        if max - min < 50L then
+            [max..(-1L)..min] |> List.find (fun m -> fuel m <= target)
+        else
+            let mid = ((max - min) / 2L) + min
+            let ore = fuel mid
+            if ore < target then
+                find mid max
+            else //if ore > target then
+                find min mid
+    
+    let for0 = fuel 1L
+    let min = target / for0
+    let max = 2L * min
+
+    find min max |> string

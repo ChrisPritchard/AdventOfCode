2507e902f96f7eb2f3f56af36386ceb040e395df
Christopher <chrispritchard.nz@gmail.com>
2019-12-14 21:05:35 +1300

    transform of data

--- 2019/Day14.fs
+++ 2019/Day14.fs
@@ -6,9 +6,24 @@ open System.Collections.Generic
 
 let input = File.ReadAllLines "./inputs/day14.txt"
 
+let transforms =
+    input
+    |> Array.map (fun s -> 
+        s
+        |> split " ,=>"
+        |> fun a ->
+            a.[a.Length - 1], 
+                (int a.[a.Length - 2], 
+                    a 
+                    |> Array.truncate (a.Length - 2) 
+                    |> Array.chunkBySize 2 
+                    |> Array.map (fun b -> b.[1], int b.[0])
+                    |> Map.ofArray))
+    |> Map.ofArray
+
 let part1 () =    
     
-    input
+    transforms
 
 let part2 () =
 

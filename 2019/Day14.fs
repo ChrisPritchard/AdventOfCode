d4c9cc2b3dda0ac931690c89d0b6cb8815870831
Christopher <chrispritchard.nz@gmail.com>
2019-12-14 20:56:48 +1300

    prep for 14 with sample data

---
+++ 2019/Day14.fs
@@ -0,0 +1,15 @@
+﻿module Day14
+
+open Common
+open System.IO
+open System.Collections.Generic
+
+let input = File.ReadAllLines "./inputs/day14.txt"
+
+let part1 () =    
+    
+    input
+
+let part2 () =
+
+    0
--- 2019/Program.fs
+++ 2019/Program.fs
@@ -7,10 +7,15 @@ let main _ =
     printfn "==================="
     printfn ""
     
-    time 13 1 Day13.part1
-    time 13 2 Day13.part2
+    time 14 1 Day14.part1
+    time 14 2 Day14.part2
     
     printfn ""
+    
+    //time 13 1 Day13.part1
+    //time 13 2 Day13.part2
+    
+    //printfn ""
 
     //time 12 1 Day12.part1
     //time 12 2 Day12.part2

4eddc21379b15a6f349924e713e4361f254e9124
Christopher <chrispritchard.nz@gmail.com>
2019-12-16 10:18:54 +1300

    another attempt with reversals

--- 2019/Day15.fs
+++ 2019/Day15.fs
@@ -4,6 +4,8 @@ open System.IO
 
 let input = (File.ReadAllText ("./inputs/day15.txt")).Split ','
 
+type MoveAttempt = NotMoved | FoundEnd of int | NextMove of (int * int)
+
 let part1 () =
 
     let io = Intcode.IO.create ()
@@ -18,60 +20,73 @@ let part1 () =
                 x, y + 1
             ] |> Seq.filter (fun (x, y) -> (Map.tryFind (x, y) map |> Option.defaultValue '#') <> '#')
         BFS.bfs ((=) target) edges (0, 0) |> Option.defaultValue [] |> Seq.length
-
-    let tryDirection ((x, y), map, moved, result) (position, input) =
-        match moved, result with
-        | true, _ | _, Some _ -> ((x, y), map, moved, result)
-        | false, None ->
-            io.write input
-            Intcode.run 0L 0L mem io |> ignore
-            match io.read () with
-            | _, 0L -> 
-                let newMap = Map.add position '#' map
-                ((x, y), newMap, false, result)
-            | _, 1L -> 
-                let newMap = Map.add position '.' map
-                (position, newMap, true, result)
-            | _, 2L -> 
-                let newMap = Map.add position '.' map
-                let result = findResult position newMap
-                (position, newMap, true, Some result)
-            | _ -> failwith "invalidstate"
-                
+       
     let rec runner (x, y) map path = 
         let dirs = 
             [
-                (x, y - 1), 1L
-                (x, y + 1), 2L
-                (x - 1, y), 3L
-                (x + 1, y), 4L
+                (x, y - 1), 1L, 2L
+                (x, y + 1), 2L, 1L
+                (x - 1, y), 3L, 4L
+                (x + 1, y), 4L, 3L
             ]
-        let options = dirs |> List.filter (fun (t, _) -> Map.containsKey t map |> not)
+        let options = dirs |> List.filter (fun (t, _, _) -> 
+            if List.contains t path then false
+            else
+                if Map.containsKey t map then map.[t] <> '#' else true)
+
+        let forinput n = 
+            io.write n
+            Intcode.run 0L 0L mem io |> ignore
+            io.read () |> snd
 
         let backtrack () =
             match path with
             | last::rem ->
-                io.write (dirs |> List.find (fst >> (=) last) |> snd)
-                Intcode.run 0L 0L mem io |> ignore
-                io.read () |> ignore
-                last, map, rem, None
+                let n = dirs |> List.pick (fun (t, i, _) -> if t = last then Some i else None)
+                forinput n |> ignore
+                let nextMap = Map.add (x, y) '#' map
+                last, nextMap, rem, None
             | _ -> failwith "no path"
+
+        let tryDirection (map, result) (position, input, reverse) =
+            match result with
+            | FoundEnd _ -> (map, result)
+            | _ ->
+                match forinput input with
+                | 0L -> 
+                    let newMap = Map.add position '#' map
+                    (newMap, result)
+                | 1L -> 
+                    let newMap = Map.add position '.' map
+                    forinput reverse |> ignore
+                    (newMap, NextMove position)
+                | 2L -> 
+                    let newMap = Map.add position '.' map
+                    let pathLength = findResult position newMap
+                    (newMap, FoundEnd pathLength)
+                | _ -> failwith "invalidstate"
 
         let pos, map, path, result = 
             if List.length options = 0 then
                 backtrack ()
             else
-                let (nextPos, nextMap, _, result) =
-                    (((x, y),  map, false, None), options)
+                let (nextMap, result) =
+                    ((map, NotMoved), options)
                     ||> List.fold tryDirection
-                if nextPos = (x, y) then
-                    backtrack ()
-                else
-                    nextPos, nextMap, ((x, y)::path), result
+                match result with
+                | NotMoved -> backtrack ()
+                | NextMove nextPos ->
+                    let n = dirs |> List.pick (fun (t, i, _) -> if t = nextPos then Some i else None)
+                    forinput n |> ignore
+                    nextPos, nextMap, ((x, y)::path), None
+                | FoundEnd pathLength ->
+                    (x, y), nextMap, path, Some pathLength
 
         match result with
         | Some n -> n
         | None ->
+            printfn "%A" pos
+            System.Threading.Thread.Sleep 50
             runner pos map path
 
     let startMap = Map.empty.Add ((0, 0), '.')

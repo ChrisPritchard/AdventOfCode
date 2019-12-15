106b589cd32c7d0bcaa71626fe988b9dfc3e7b02
Christopher <chrispritchard.nz@gmail.com>
2019-12-16 08:01:37 +1300

    first cut for day 15

--- 2019/Day15.fs
+++ 2019/Day15.fs
@@ -1,16 +1,14 @@
 ﻿module Day15
 
-open Common
 open System.IO
-open System.Collections.Generic
 
 let input = (File.ReadAllText ("./inputs/day15.txt")).Split ','
 
-let inputStream = TQueue()
-let outputStream = TQueue()
-
 let part1 () =
 
+    let io = Intcode.IO.create ()
+    let mem = Intcode.memFrom input
+    
     let findResult target map = 
         let edges (x, y) = 
             [
@@ -20,70 +18,50 @@ let part1 () =
                 x, y + 1
             ] |> Seq.filter (fun (x, y) -> (Map.tryFind (x, y) map |> Option.defaultValue '#') <> '#')
         BFS.bfs ((=) target) edges (0, 0) |> Option.defaultValue [] |> Seq.length
+
+    let tryDirection ((x, y), ip, rb, mem, map, moved, result) (position, input) =
+        match moved, result with
+        | true, _ | _, Some _ -> ((x, y), ip, rb, mem, map, moved, result)
+        | false, None ->
+            io.write input
+            let _, ip, rb, mem = Intcode.run ip rb mem io
+            match io.read () with
+            | _, 0L -> 
+                let newMap = Map.add position '#' map
+                ((x, y), ip, rb, mem, newMap, false, result)
+            | _, 1L -> 
+                let newMap = Map.add position '.' map
+                (position, ip, rb, mem, newMap, true, result)
+            | _, 2L -> 
+                let newMap = Map.add position '.' map
+                let result = findResult position newMap
+                (position, ip, rb, mem, newMap, true, Some result)
+            | _ -> failwith "invalidstate"
     
     let rec runner (x, y) ip rb mem map = 
-        System.Console.Clear ()
-        System.Console.CursorLeft <- 0
-        System.Console.CursorTop <- 0
+        let dirs = 
+            [
+                (x, y - 1), 1L
+                (x, y + 1), 2L
+                (x - 1, y), 3L
+                (x + 1, y), 4L
+            ] |> List.filter (fun (t, _) -> (Map.tryFind t map |> Option.defaultValue '.') <> '#')
+        let (nextPos, ip, rb, mem, nextMap, _, result) =
+            (((x, y), ip, rb, mem, map, false, None), dirs)
+            ||> List.fold tryDirection
+        match result with
+        | Some n -> n
+        | None ->
+            runner nextPos ip rb mem nextMap
 
-        let render = 
-            asString <| seq {
-                yield '\n'
-                for oy = 0 to 80 do
-                    for ox = 0 to 80 do
-                        if (ox - 40, oy - 40) = (x, y) then yield 'D'
-                        else yield Map.tryFind (ox - 40, oy - 40) map |> Option.defaultValue '.'
-                    yield '\n'
-            }
-        printfn "%s" render
-
-        printfn "dir: "
-        let next = 
-            System.Console.ReadKey () 
-            |> function
-                | c when c.Key = System.ConsoleKey.UpArrow -> 1L
-                | c when c.Key = System.ConsoleKey.DownArrow -> 2L
-                | c when c.Key = System.ConsoleKey.LeftArrow -> 3L
-                | c when c.Key = System.ConsoleKey.RightArrow -> 4L
-                | _ -> failwith "invalid command"
-
-        inputStream.Enqueue next
-        let _, ip, rb, mem = intcodeRun ops ip rb mem (read inputStream) (outputStream.Enqueue)
-        let res = outputStream.Dequeue ()
-        let map, pos =
-            match res, next with
-            | r, n when r = t0 && n = t1 -> Map.add (x, y - 1) '#' map, (x, y)
-            | r, n when r = t0 && n = t2 -> Map.add (x, y + 1) '#' map, (x, y)
-            | r, n when r = t0 && n = t3 -> Map.add (x - 1, y) '#' map, (x, y)
-            | r, n when r = t0 && n = t4 -> Map.add (x + 1, y) '#' map, (x, y)
-            
-            | r, n when (r = t1 || r = t2) && n = t1 -> Map.add (x, y - 1) '.' map, (x, y - 1)
-            | r, n when (r = t1 || r = t2) && n = t2 -> Map.add (x, y + 1) '.' map, (x, y + 1)
-            | r, n when (r = t1 || r = t2) && n = t3 -> Map.add (x - 1, y) '.' map, (x - 1, y)
-            | r, n when (r = t1 || r = t2) && n = t4 -> Map.add (x + 1, y) '.' map, (x + 1, y)
-
-            | _, _ -> map, (x, y)
-
-        if res = t2 then
-            findResult pos map
-        else
-            runner pos ip rb mem map
-
-    let mem =
-        Array.indexed input 
-        |> Array.map (fun (k, v) -> itot k, v) 
-        |> dict 
-        |> Dictionary<T, T>
-
-    runner (0, 0) t0 t0 mem Map.empty
+    let startMap = Map.empty.Add ((0, 0), '.')
+    runner (0, 0) 0L 0L mem startMap
+        
 
 let part2 () =
 
-    let mem =
-        Array.indexed input 
-        |> Array.map (fun (k, v) -> itot k, v) 
-        |> dict 
-        |> Dictionary<T, T>
+    let io = Intcode.IO.create ()
+    let mem = Intcode.memFrom input
 
     0
     

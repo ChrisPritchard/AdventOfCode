8783b70a2a389ee6dc2f7e35bdd60bd28d3e191c
Chris Pritchard <chrispritchard.nz@gmail.com>
2019-12-17 11:45:59 +1300

    formatting and typos

--- 2019/Day16.fs
+++ 2019/Day16.fs
@@ -35,19 +35,20 @@ For part 2 below I had to go to reddit again to get the crucial insight, which i
 I'll explain it here how I thought through it, which resulted in the code below:
 
 - Any given digit is only calculated based on the numbers after it (except for the first digit);
-    this is because 0 is repeated by the digit index, plus 1, which skips all digits up to and including the target
+    this is because 0 is repeated by the digit index, plus 1, which skips all digits up to and including the target.
 - The 'offset' for part 2 is over the half way mark. This means that after the first zero, the next character in the base pattern '1'
     will be repeated for the rest of the number. The offset is large enough that the third base pattern '0' is never reached.
 
 Based on these two insights, and given we only need to consider the number at the offset for our result, we can simplify the run:
 
-- Only consider the digits from the offset onwards: the entire number up to this point can be ignored. This changes a 6.5 million digit number into a ~500k number.
-- Each digit in the reduced number is simply the sum of all numbers after it, mod 10
+- Only consider the digits from the offset onwards: the entire number up to this point can be ignored as it will have no affect on the offset digits.
+    This changes a 6.5 million digit number into a ~500k number.
+- Each digit in the reduced number is simply the sum of all numbers after it, mod 10.
 
 The solution therefore, presented below, is to:
 
 - Create the ~500k number (which is just the input, reversed, repeated 800 odd times, with a final append of the remainder). This is the new input.
-- Create a runner that computers the partial sums in reverse. Scanback in F# does this nicely.
+- Create a runner that computes the partial sums in reverse. scanBack in F# does this nicely.
 - Map the result mod 10.
 
 After a 100 runs, since we have already snipped off the irrelevant initial six million odd digits, the result is just like part 1: first 8 characters of the final number.

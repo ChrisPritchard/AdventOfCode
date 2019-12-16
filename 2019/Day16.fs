5cfe0c16a8c8c84d826d24344ed87c9594a04285
Chris Pritchard <chrispritchard.nz@gmail.com>
2019-12-17 10:04:53 +1300

    started experiements with cycles

--- 2019/Day16.fs
+++ 2019/Day16.fs
@@ -32,8 +32,32 @@ let part1 () =
 
 let part2 () =
         
-    let tic = input |> Seq.toArray |> Array.map (string >> int)
-    let ti = Array.init 10000 (fun _ -> Array.copy tic) |> Array.collect id
+    let ti = input |> Seq.toArray |> Array.map (string >> int)
+    let cycle i =
+        let loop = pown 4L (i + 1)
+        int64 ti.Length % loop
+
+    let test = [0..20] |> List.map cycle
       
-    //runner ti 100
-    0
+    let run (input: int[]) =
+        [|0..input.Length - 1|] 
+        |> Array.map (fun i ->
+            let i = i + 650
+            let total = 
+                Array.skip i input 
+                |> Array.chunkBySize (i + 1) 
+                |> Array.mapi (fun i a -> 
+                    match i % 4 with
+                    | 0 -> Array.sum a
+                    | 2 -> -1 * Array.sum a
+                    | _ -> 0)
+                |> Array.sum
+            abs (total % 10))
+
+    let rec runner current count =
+        if count = 0 then
+            current |> Array.truncate 8 |> Array.map (string >> char) |> asString
+        else
+            runner (run current) (count - 1)
+
+    runner ti 1

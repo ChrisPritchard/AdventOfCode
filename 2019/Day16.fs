b7eed98596cd6488bf0d5f5690ef409917ed23c0
Christopher <chrispritchard.nz@gmail.com>
2019-12-16 18:35:54 +1300

    solved part 1 day 16

--- 2019/Day16.fs
+++ 2019/Day16.fs
@@ -3,11 +3,31 @@
 open Common
 open System.IO
 
-let input = File.ReadAllLines "./inputs/day16.txt"
+let input = File.ReadAllText "./inputs/day16.txt"
 
-let part1 () =    
+let part1 () =
+
+    let basePattern = [|0; 1; 0; -1|]
+
+    let run (input: int[]) =
+        [|0..input.Length - 1|] 
+        |> Array.map (fun i ->
+            let pattern = 
+                Array.init (input.Length + 1) (fun j -> 
+                    let p = (j / (i + 1)) % basePattern.Length
+                    basePattern.[p])
+                |> Array.skip 1
+            let total = Array.map2 (*) input pattern |> Array.sum
+            abs (total % 10))
+
+    let rec runner current count =
+        if count = 0 then
+            current |> Array.truncate 8 |> Array.map (string >> char) |> asString
+        else
+            runner (run current) (count - 1)
     
-    0
+    let ti = input |> Seq.map (string >> int) |> Seq.toArray
+    runner ti 100
 
 let part2 () =
 

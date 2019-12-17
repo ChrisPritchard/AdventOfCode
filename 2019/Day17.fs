fc0646a8f77090509d809b3ac7d7124fddc48745
Christopher <chrispritchard.nz@gmail.com>
2019-12-17 18:52:10 +1300

    started work on map tracking

--- 2019/Day17.fs
+++ 2019/Day17.fs
@@ -24,10 +24,10 @@ let map = mapper [] []
 
 let part1 () =
 
-    //for y = 0 to map.Length - 1 do
-    //    for x = 0 to map.[y].Length - 1 do
-    //        printf "%c" map.[y].[x]
-    //    printfn ""
+    for y = 0 to map.Length - 1 do
+        for x = 0 to map.[y].Length - 1 do
+            printf "%c" map.[y].[x]
+        printfn ""
 
     let intersection x y =  
         if map.[y].[x] <> '#' then None
@@ -54,6 +54,38 @@ let part1 () =
     } |> Seq.sum
 
 let part2 () =
+
+    let start = (3, 0)
+    let goal = (24, 34)
+
+    let edges ((x, y), (lx, ly)) =
+        let options = 
+            [
+                -1, 0
+                1, 0
+                0, -1
+                0, 1
+            ] 
+            |> Seq.map (fun (dx, dy) -> x + dx, y + dy)
+            |> Seq.filter (fun (x, y) -> x >= 0 && y >= 0 && x < map.[0].Length && y < map.Length)
+            |> Seq.filter (fun (x, y) -> map.[y].[x] = '#' && (x, y) <> (lx, ly))
+            |> Seq.toArray
+        let straight = 
+            Array.tryFind (fun (x, y) -> x = lx || y = ly) options
+        seq {
+            match straight with
+            | Some o -> yield o, (x, y)
+            | None ->
+                yield! options |> Seq.map (fun o -> o, (x, y))
+        }
+
+    let isGoal ((x, y), _) = (x, y) = goal
+
+    let fullPath = BFS.run isGoal edges (start, start) |> Option.defaultValue []
+    let translated = 
+        ((['R'], 0), fullPath.[1..]) 
+        ||> List.fold (fun (acc, cnt) ((x, y), (lx, ly)) ->
+            if lx = x || ly = y)
 
     // calculate full path
         // find start and end

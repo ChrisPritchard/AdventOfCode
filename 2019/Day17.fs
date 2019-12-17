d0836d4a6f0a76f16ebcd3c4dd43e564f44d64c9
Christopher <chrispritchard.nz@gmail.com>
2019-12-17 18:15:50 +1300

    part 1 day 17

--- 2019/Day17.fs
+++ 2019/Day17.fs
@@ -3,11 +3,52 @@
 open Common
 open System.IO
 
-let input = File.ReadAllText "./inputs/day17.txt"
+let input = (File.ReadAllText ("./inputs/day17.txt")).Split ','
 
 let part1 () =
 
-    0
+    let mem = Intcode.memFrom input
+    let io = Intcode.IO.create ()
+    Intcode.run 0L 0L mem io |> ignore
+
+    let rec mapper acc line = 
+        let canRead, next = io.read ()
+        if not canRead then List.rev acc |> List.toArray
+        else
+            if next = 10L then
+                let line = List.rev line |> List.toArray
+                let acc = if line.Length > 0 then line::acc else acc
+                mapper acc []
+            elif next = 46L then
+                mapper acc ('.'::line)
+            else
+                mapper acc ('#'::line)
+
+    let map = mapper [] []
+
+    let intersection x y =  
+        if map.[y].[x] <> '#' then None
+        else
+            let isIntersection =
+                [
+                    -1, 0
+                    1, 0
+                    0, -1
+                    0, 1
+                ] 
+                |> Seq.map (fun (dx, dy) -> x + dx, y + dy)
+                |> Seq.filter (fun (x, y) -> x >= 0 && y >= 0 && x < map.[0].Length && y < map.Length)
+                |> Seq.filter (fun (x, y) -> map.[y].[x] = '#')
+                |> Seq.length >= 3
+            if isIntersection then Some (y * x) else None
+
+    seq {
+        for y = 0 to map.Length - 1 do
+            for x = 0 to map.[y].Length - 1 do
+                match intersection x y with
+                | Some n -> yield n
+                | None -> ()
+    } |> Seq.sum
 
 let part2 () =
 

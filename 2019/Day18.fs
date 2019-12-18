1fee11ae7e5f8477eb9f869a383f26e8ee702bde
Christopher <chrispritchard.nz@gmail.com>
2019-12-19 08:45:07 +1300

    quick rework to pre-calculate paths

--- 2019/Day18.fs
+++ 2019/Day18.fs
@@ -11,34 +11,49 @@ let map = input |> Array.map (fun s -> s.ToCharArray ())
 let start = 
     [0..map.Length - 1] 
     |> List.pick (fun y -> map.[y] |> Array.tryFindIndex (fun c -> c = '@') |> Option.map (fun x -> x, y))
-let keys = map |> Array.collect (fun line -> line |> Array.filter (fun c -> Char.IsLetter c && Char.IsLower c))
+let keys = 
+    map 
+    |> Array.indexed 
+    |> Array.collect (fun (y, line) -> 
+        line 
+        |> Array.indexed 
+        |> Array.filter (snd >> Char.IsLower) 
+        |> Array.map (fun (x, c) -> c, (x, y)))
 
 let part1 () =
 
-    let edges visited (x, y) =
-        [ 0, -1; 0, 1; 1, 0; -1, 0 ]
-        |> Seq.map (fun (dx, dy) -> x + dx, y + dy)
-        |> Seq.filter (fun (ox, oy) -> ox >= 0 && oy >= 0 && ox < map.[0].Length && oy < map.Length)
-        |> Seq.filter (fun (ox, oy) ->
-            let tile = map.[oy].[ox]
-            tile <> '#' && (not (Char.IsLetter tile) || Char.IsLower tile || Set.contains (Char.ToLower tile) visited))
+    // new approach:
+    // for each key, calculate distance to all other keys and start
+        // when calculating distance, also add up blocked doors
+        // also incidental keys collected?
+    // with this cache, then do a search to find shortest path?
 
-    let rec runner cnt visited start targets =
-        targets 
-        |> Array.filter (fun tile -> not (Set.contains tile visited))
-        |> Array.choose (fun tile -> 
-            BFS.run (fun (x, y) -> map.[y].[x] = tile) (edges visited) start)
-        |> Array.map (fun path ->
-            let (fx, fy) = List.last path
-            let tile = map.[fy].[fx]
-            cnt + path.Length - 1, Set.add tile visited, (fx, fy))
-        |> Array.map (fun (cnt, visited, start) ->
-            if Set.count visited = targets.Length then cnt
-            else
-                runner cnt visited start targets)
-        |> Array.min
+    let paths (start: char, startPos: int * int) others =
+        let edges (x, y) =
+            [ 0, -1; 0, 1; 1, 0; -1, 0 ]
+            |> Seq.map (fun (dx, dy) -> x + dx, y + dy)
+            |> Seq.filter (fun (ox, oy) -> ox >= 0 && oy >= 0 && ox < map.[0].Length && oy < map.Length)
+            |> Seq.filter (fun (ox, oy) ->
+                let tile = map.[oy].[ox]
+                tile <> '#')
+        others 
+        |> Array.choose (fun (goal, goalPos) ->
+            if goal = start then None
+            else 
+                BFS.run ((=) goalPos) edges startPos 
+                |> Option.map (fun path -> goal, path))
+        |> Array.map (fun (goal, path) ->
+            let blocks = 
+                path 
+                |> List.map (fun (x, y) -> map.[y].[x]) 
+                |> List.filter Char.IsUpper |> List.toArray
+            //let others = path |> List.map (fun (x, y) -> map.[y].[x]) |> List.filter Char.Lower
+            (start, goal), (path.Length - 1, blocks))
+        
+    let startOptions = paths ('@', start) keys
+    let keyPairs = keys |> Array.collect (fun key -> paths key keys)
 
-    runner 0 Set.empty start keys
+    0
 
 let part2 () =
 

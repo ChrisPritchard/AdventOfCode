27c171d23d69c229e578160dd239e013ebc32b41
Christopher <chrispritchard.nz@gmail.com>
2019-12-19 18:07:35 +1300

    part 1 complete

--- 2019/Day19.fs
+++ 2019/Day19.fs
@@ -4,11 +4,19 @@ open Common
 open System.IO
 open System.Collections.Generic
 
-let input = File.ReadAllText "./inputs/day19.txt"
+let input = File.ReadAllText "./inputs/day19.txt" |> split ","
 
 let part1 () = 
 
-    0
+    let io = Intcode.IO.create ()
+
+    [0L..49L] |> List.sumBy (fun y ->
+        [0L..49L] |> List.sumBy (fun x ->
+            io.write x
+            io.write y
+            let mem = Intcode.memFrom input
+            Intcode.run 0L 0L mem io |> ignore
+            io.read () |> snd)) |> string
 
 let part2 () =
 

6425033265b05a1dcfa391287b357589ca643eb4
Christopher Pritchard <chrispritchard.nz@gmail.com>
2019-12-21 17:57:54 +1300

    prep for day 21

---
+++ 2019/Day21.fs
@@ -0,0 +1,17 @@
+﻿module Day21
+
+open Common
+open System.IO
+
+let input = File.ReadAllText "./inputs/day21.txt" |> split ","
+
+let part1 () =
+
+    let io = Intcode.IO.create ()
+    let mem = Intcode.memFrom input
+    Intcode.run 0L 0L mem io |> ignore
+    io.read () |> snd |> (=) 1L
+
+let part2 () =
+
+    0

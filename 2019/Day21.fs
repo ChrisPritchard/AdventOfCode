711f8527954d268d83692639ac4640931c883895
Christopher Pritchard <chrispritchard.nz@gmail.com>
2019-12-21 20:41:30 +1300

    experiments with part 2

--- 2019/Day21.fs
+++ 2019/Day21.fs
@@ -5,16 +5,16 @@ open System.IO
 
 let input = File.ReadAllText "./inputs/day21.txt" |> split ","
 
-let part1 () =
+let run instructions =
+    let io = Intcode.IO.create ()
+    instructions |> Array.iter (fun s ->
+        s |> Seq.iter (int64 >> io.write)
+        io.write 10L)
+    let mem = Intcode.memFrom input
+    Intcode.run 0L 0L mem io |> ignore
+    io.output |> Seq.toArray
 
-    let run instructions =
-        let io = Intcode.IO.create ()
-        instructions |> Array.iter (fun s ->
-            s |> Seq.iter (int64 >> io.write)
-            io.write 10L)
-        let mem = Intcode.memFrom input
-        Intcode.run 0L 0L mem io |> ignore
-        io.output |> Seq.toArray
+let part1 () =
 
     let instructions = [|
         "NOT A J"
@@ -31,4 +31,23 @@ let part1 () =
 
 let part2 () =
 
+    let instructions = [|
+        "NOT A J"
+        "NOT B T"
+        "OR T J"
+        "NOT C T"
+        "OR T J"
+
+        "AND D J"
+
+        "AND H J"
+        "OR E J"
+        "AND I J"
+
+        "RUN"
+        |]
+
+    let result = run instructions
+    //Array.last result |> string
+    result |> Array.iter (char >> printf "%c")
     0

53591cefcefa2d15bb0d968ca8f0dddb569abc39
Christopher Pritchard <chrispritchard.nz@gmail.com>
2019-12-22 17:53:06 +1300

    prep for day 22

---
+++ 2019/Day22.fs
@@ -0,0 +1,16 @@
+﻿module Day22
+
+open Common
+open System
+open System.IO
+open System.Collections.Generic
+
+let input = File.ReadAllText "./inputs/day22.txt"
+
+let part1 () =
+
+    input + "1"
+
+let part2 () =
+
+    input + "2"

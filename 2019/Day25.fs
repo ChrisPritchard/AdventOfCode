22b1184455ba66d157e09746fc552eac4c1d39c7
Christopher Pritchard <chrispritchard.nz@gmail.com>
2019-12-25 10:18:08 +1300

    prep for day 25

---
+++ 2019/Day25.fs
@@ -0,0 +1,24 @@
+﻿module Day25
+
+open Common
+open System.IO
+open System.Collections.Generic
+
+let input = File.ReadAllText "./inputs/day25.txt" |> split ","
+
+let run instructions =
+    let io = Intcode.IO.create ()
+    instructions |> Array.iter (fun s ->
+        s |> Seq.iter (int64 >> io.write)
+        io.write 10L)
+    let mem = Intcode.memFrom input
+    Intcode.run 0L 0L mem io |> ignore
+    io.output |> Seq.toArray
+
+let part1 () =
+
+    0
+
+let part2 () =
+
+    0

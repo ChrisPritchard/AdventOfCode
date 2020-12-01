268ca1d59d98db283a3fada2e43a3ffedd2ee756
Chris Pritchard <chris@grislygrotto.nz>
2020-12-01 18:39:32 +1300

    refined solution, much faster

--- 2020/Day01.fs
+++ 2020/Day01.fs
@@ -1,26 +1,17 @@
 module Day01
 
 open System.IO
+open System.Collections
 
-let input = File.ReadAllLines ("./inputs/day01.txt")
+let input = File.ReadAllLines ("./inputs/day01.txt") |> Array.map int
 
 let part1 () =
     input 
-    |> Array.collect (fun a -> input |> Array.map (fun b -> int a, int b))
-    |> Array.find(fun (a, b) -> a + b = 2020)
-    |> fun (a, b) -> a * b
+    |> Array.pick (fun a ->
+        input |> Array.tryPick (fun b -> if a + b = 2020 then Some (a * b) else None))
 
 let part2 () =
     input 
-    |> Array.collect (fun a -> 
-        input |> Array.collect (fun b -> 
-            input |> Array.map (fun c -> int a, int b, int c)))
-    |> Array.find(fun (a, b, c) -> a + b + c = 2020)
-    |> fun (a, b, c) -> a * b * c
-
-    // let rec processFuel acc v = 
-    //     let res = floor (v / 3.) - 2.
-    //     if res <= 0. then acc
-    //     else processFuel (acc + res) res
-
-    // input |> Array.sumBy (float >> processFuel 0.) |> int
+    |> Array.pick (fun a ->
+        input |> Array.tryPick (fun b ->
+            input |> Array.tryPick (fun c -> if a + b + c = 2020 then Some (a * b * c) else None)))

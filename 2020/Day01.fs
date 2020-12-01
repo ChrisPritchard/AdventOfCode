8451587b78e4ff25d574beeb4894fc022d0a09b0
Chris Pritchard <chris@grislygrotto.nz>
2020-12-01 18:18:06 +1300

    first cut of day 1

--- 2020/Day01.fs
+++ 2020/Day01.fs
@@ -5,11 +5,18 @@ open System.IO
 let input = File.ReadAllLines ("./inputs/day01.txt")
 
 let part1 () =
-    input |> Array.sumBy (float >> fun v -> floor (v / 3.) - 2.) |> int
+    input 
+    |> Array.collect (fun a -> input |> Array.map (fun b -> int a, int b))
+    |> Array.find(fun (a, b) -> a + b = 2020)
+    |> fun (a, b) -> a * b
 
 let part2 () =
-
-    0
+    input 
+    |> Array.collect (fun a -> 
+        input |> Array.collect (fun b -> 
+            input |> Array.map (fun c -> int a, int b, int c)))
+    |> Array.find(fun (a, b, c) -> a + b + c = 2020)
+    |> fun (a, b, c) -> a * b * c
 
     // let rec processFuel acc v = 
     //     let res = floor (v / 3.) - 2.

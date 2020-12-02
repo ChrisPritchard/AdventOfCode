e610571a282fc2eaa0a92e9a06736fbcd5d81137
Chris Pritchard <chris@grislygrotto.nz>
2020-12-02 18:23:21 +1300

    refined day 2 and added a description

--- 2020/Day02.fs
+++ 2020/Day02.fs
@@ -3,26 +3,22 @@ module Day02
 open System.IO
 open Common
 
-let input = File.ReadAllLines ("./inputs/day02.txt")
+let input = 
+    File.ReadAllLines ("./inputs/day02.txt")
+    |> Array.map (fun s ->
+        let parts = split "- :" s
+        (int parts.[0], int parts.[1], char parts.[2], parts.[3]))
 
 let part1 () =
     input 
-    |> Array.filter (fun s ->
-        let parts = split "- :" s
-        let dict = parts.[3] |> Seq.countBy id |> Map.ofSeq
-        match Map.tryFind (char parts.[2]) dict with
-        | None -> false
-        | Some count ->
-            count >= int parts.[0] && count <= int parts.[1])
+    |> Array.filter (fun (n1, n2, c, s) ->
+        let count = s |> Seq.filter ((=) c) |> Seq.length
+        count >= n1 && count <= n2)
     |> Array.length
 
 let part2 () =
     input 
-    |> Array.filter (fun s ->
-        let parts = split "- :" s
-        let dict = parts.[3] |> Array.ofSeq
-        let target = char parts.[2]
-        let first = dict.[int parts.[0] - 1]
-        let second = dict.[int parts.[1] - 1]
-        first.Equals(target) <> second.Equals(target))
+    |> Array.filter (fun (n1, n2, c, s) ->
+        let dict = s |> Array.ofSeq
+        (dict.[n1 - 1] = c) <> (dict.[n2 - 1] = c))
     |> Array.length

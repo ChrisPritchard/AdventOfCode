cc300e1a429e2b862cfc9e2a30fc1989b49775df
Chris Pritchard <chris@grislygrotto.nz>
2020-12-02 18:11:52 +1300

    solved day2

--- 2020/Day02.fs
+++ 2020/Day02.fs
@@ -1,14 +1,28 @@
 module Day02
 
 open System.IO
-open System.Collections
+open Common
 
-let input = File.ReadAllLines ("./inputs/day02.txt") |> Array.map int
+let input = File.ReadAllLines ("./inputs/day02.txt")
 
 let part1 () =
     input 
-    |> Array.pick (fun a ->
-        input |> Array.tryPick (fun b -> if a + b = 2020 then Some (a * b) else None))
+    |> Array.filter (fun s ->
+        let parts = split "- :" s
+        let dict = parts.[3] |> Seq.countBy id |> Map.ofSeq
+        match Map.tryFind (char parts.[2]) dict with
+        | None -> false
+        | Some count ->
+            count >= int parts.[0] && count <= int parts.[1])
+    |> Array.length
 
 let part2 () =
-    0
+    input 
+    |> Array.filter (fun s ->
+        let parts = split "- :" s
+        let dict = parts.[3] |> Array.ofSeq
+        let target = char parts.[2]
+        let first = dict.[int parts.[0] - 1]
+        let second = dict.[int parts.[1] - 1]
+        first.Equals(target) <> second.Equals(target))
+    |> Array.length

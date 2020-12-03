3b0de1f2fac6bfa5449cac5f442a2b9db095ade8
Chris Pritchard <chris@grislygrotto.nz>
2020-12-03 18:55:49 +1300

    enabled all three days, as they're short

--- 2020/Program.fs
+++ 2020/Program.fs
@@ -10,11 +10,11 @@ let main argv =
     printfn "==================="
     printfn ""
     
-    // time 1 1 Day01.part1
-    // time 1 2 Day01.part2
+    time 1 1 Day01.part1
+    time 1 2 Day01.part2
 
-    // time 2 1 Day02.part1
-    // time 2 2 Day02.part2
+    time 2 1 Day02.part1
+    time 2 2 Day02.part2
 
     time 3 1 Day03.part1
     time 3 2 Day03.part2

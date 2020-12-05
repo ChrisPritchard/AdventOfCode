8c7c4705ad7af0045e9c5e57c02fdba25c72844a
Chris Pritchard <chris@grislygrotto.nz>
2020-12-05 18:20:02 +1300

    first cut day 5

--- 2020/Day05.fs
+++ 2020/Day05.fs
@@ -5,10 +5,45 @@ open System.IO
 open Common
 
 let input = 
-    (File.ReadAllText ("./inputs/day05.txt"))
+    File.ReadAllLines ("./inputs/day05.txt")
+
+let boardingPass (line: string) =
+    let mutable min, max, range, row, seat = 0, 127, 128, 0, 0
+    for i = 0 to 6 do
+        range <- range / 2
+        if line.[i] = 'F' then
+            max <- max - range
+            row <- max
+        else
+            min <- min + range
+            row <- min
+
+    min <- 0
+    max <- 7
+    range <- 8
+
+    for i = 7 to 9 do
+        range <- range / 2
+        if line.[i] = 'L' then
+            max <- max - range
+            seat <- max
+        else
+            min <- min + range
+            seat <- min
+    
+    row, seat, row * 8 + seat
 
 let part1 () =
-    0
+    input
+    |> Array.map boardingPass
+    |> Array.maxBy (fun (_, _, id) -> id)
 
 let part2 () =
-    0
+    let allRows =
+        input
+        |> Array.map boardingPass
+        |> Array.groupBy (fun (r, _, _) -> r)
+        |> Array.sortBy fst
+    let length = Array.length allRows
+    let missing = allRows |> Array.skip 1 |> Array.take (length - 2) |> Array.filter (fun (_, seats) -> Array.length seats <> 8)
+    missing

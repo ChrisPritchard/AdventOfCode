7da05af7013b4077bd48f39f616d50a3bf90854b
Chris Pritchard <chris@grislygrotto.nz>
2020-12-06 18:09:32 +1300

    solved day 6, first cut!

--- 2020/Day06.fs
+++ 2020/Day06.fs
@@ -1,12 +1,17 @@
 module Day06
 
+open Common
 open System.IO
 
 let input = 
-    File.ReadAllLines("./inputs/day06.txt")
+    File.ReadAllText("./inputs/day06.txt")
+    |> splitOn "\r\n\r\n"
 
 let part1 () = 
-    0
+    input |> Array.sumBy (fun g -> g |> Seq.filter (fun c -> System.Char.IsLetter(c)) |> Seq.distinct |> Seq.length)
 
 let part2 () =
-    0
+    input |> Array.sumBy (fun g -> 
+        let allc = g |> Seq.filter (fun c -> System.Char.IsLetter(c)) |> Seq.distinct
+        let rows = g |> split "\r\n" |> Array.map (Set.ofSeq)
+        allc |> Seq.filter (fun c -> rows |> Array.forall (Set.contains c)) |> Seq.length)

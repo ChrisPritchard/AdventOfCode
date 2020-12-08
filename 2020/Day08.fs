a97695b0a2a27a5996275681859aafb619db5619
Chris Pritchard <chris@grislygrotto.nz>
2020-12-08 18:07:09 +1300

    part 1 done

--- 2020/Day08.fs
+++ 2020/Day08.fs
@@ -6,10 +6,22 @@ open Common
 let input = File.ReadAllLines "./inputs/day08.txt"
 
 let processed () =
-    input
+    input |> Array.map (fun l -> split " +" l |> fun s -> s.[0], int s.[1])
 
 let part1 () =
-    processed ()
-
+    let instructions = processed ()
+    let rec processor ip acc visited =
+        if Set.contains ip visited then
+            acc
+        else
+            let visited = Set.add ip visited
+            let acc, ip = 
+                match instructions.[ip] with
+                | "acc", n -> acc + n, ip + 1
+                | "jmp", n -> acc, ip + n
+                | _ -> acc, ip + 1
+            processor ip acc visited
+    processor 0 0 Set.empty
+ 
 let part2 () =
     processed ()

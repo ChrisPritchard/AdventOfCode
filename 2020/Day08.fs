d48fa53ca4ca5309fd0eed239041a91996235838
Chris Pritchard <chris@grislygrotto.nz>
2020-12-08 18:20:16 +1300

    finished day 8!

--- 2020/Day08.fs
+++ 2020/Day08.fs
@@ -24,4 +24,26 @@ let part1 () =
     processor 0 0 Set.empty
  
 let part2 () =
-    processed ()
+    let instructions = processed ()
+    let rec processor ip acc visited faulty =
+        if Set.contains ip visited then
+            None
+        elif ip = Array.length instructions then
+            Some acc
+        else
+            let visited = Set.add ip visited
+            match instructions.[ip] with
+            | "acc", n ->
+                processor (ip + 1) (acc + n) visited faulty
+            | "jmp", n when not faulty ->
+                processor (ip + n) acc visited false
+            | "nop", _ when not faulty -> 
+                processor (ip + 1) acc visited false
+            | "jmp", n ->
+                processor (ip + n) acc visited true
+                |> Option.orElse (processor (ip + 1) acc visited false)
+            | _, n -> 
+                processor (ip + 1) acc visited true
+                |> Option.orElse (processor (ip + n) acc visited false)
+    processor 0 0 Set.empty true
+    

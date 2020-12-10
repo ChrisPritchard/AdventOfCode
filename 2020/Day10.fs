f0642f320f28cc7c626d65d10122314ff1900ddc
Chris Pritchard <chris@grislygrotto.nz>
2020-12-10 19:48:42 +1300

    work on part 2. will need to think

--- 2020/Day10.fs
+++ 2020/Day10.fs
@@ -1,6 +1,8 @@
 module Day10
 
+open System
 open System.IO
+open Common
 
 let input = File.ReadAllLines "./inputs/day10.txt"
 
@@ -23,18 +25,11 @@ let part1 () =
     |> fun m -> m.[1] * m.[3]
 
 let part2 () =
-    let processed = processed () |> List.ofArray
-    let last = processed.Length - 1
-    let mutable total = 1L
-    for i = 0 to last do
-        let start = processed.[i]
-        let future = [i + 1..last] |> List.tryFind (fun j -> let diff = processed.[j] - start in diff = 2 || diff = 3)
-        match future with
-        | None -> ()
-        | Some j ->
-            let diff = j - i
-            if diff = 3 then 
-                total <- total * 4L
-            elif diff = 2 then
-                total <- total * 2L
-    total        
+    let processed = 
+        processed () 
+        |> Array.windowed 2
+        |> Array.map (fun a -> string (a.[1] - a.[0]))
+        |> String.concat ""
+    let runs = processed |> split "3" |> Array.map String.length
+    let res = (1L, runs) ||> Array.fold (fun acc n -> acc * pown 2L (n - 1))
+    runs

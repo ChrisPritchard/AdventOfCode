1f2c2740718aee5617939f292c5d50559cbb9bcb
Christopher Pritchard <chrispritchard.nz@gmail.com>
2020-12-11 22:15:38 +1300

    start of day 11

--- 2020/Day11.fs
+++ 2020/Day11.fs
@@ -5,10 +5,41 @@ open System.IO
 let input = File.ReadAllLines "./inputs/day11.txt"
 
 let processed () =
-    input |> Array.map Seq.toArray
+    input 
+    |> Array.mapi (fun y line -> 
+        line |> Seq.toArray |> Array.mapi (fun x c -> (y, x), c))
+    |> Array.collect id
+    |> Map.ofArray
+
+let cardinals = 
+    [|
+        -1,-1;-1,0;-1,1;
+        0,-1;0,1;
+        1,-1;1,0;1,1;
+    |]
 
 let part1 () =
-    processed ()
+    let state (y, x) c a =
+        let occupied = 
+            cardinals 
+            |> Array.filter (fun (dy, dx) -> 
+                let p = (y + dy, x + dx)
+                Map.containsKey p a && a.[p] = 'L') 
+            |> Array.length
+        match c, occupied with
+        | 'L', 0 -> '#'
+        | '#', n when n >= 4 -> 'L'
+        | c, _ -> c
+
+    let rec iter a =
+        let n = Map.map (fun p v -> state p v a) a
+        if n = a then 
+            Map.filter (fun _ v -> v = '#') n
+            |> Map.count
+        else
+            iter n
+
+    iter (processed ())
 
 let part2 () =
     0

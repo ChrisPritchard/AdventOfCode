fc855bde91e8483c43e163f4899dc98793d3ac3a
Chris Pritchard <chris@grislygrotto.nz>
2020-12-13 19:07:25 +1300

    solved 13

--- 2020/Day13.fs
+++ 2020/Day13.fs
@@ -1,13 +1,50 @@
 module Day13
 
+open Common
+open System
 open System.IO
 
 let input = File.ReadAllLines "./inputs/day13.txt"
 
-let processed () = input
+let processed () = 
+    let earliest = int input.[0]
+    let ids = input.[1] |> split ","
+    earliest, ids
 
 let part1 () =
-    processed ()
+    let earliest, ids = processed ()
+    ids 
+    |> Array.filter (fun id -> id <> "x")
+    |> Array.map (fun id ->
+        let id = int id
+        if earliest % id = 0 then id, earliest 
+        else
+            let sub = earliest / id
+            id, (sub + 1) * id)
+    |> Array.minBy snd
+    |> fun (id, time) -> (time - earliest) * id
+    
+// technique from LizTheGrey again: https://www.twitch.tv/videos/835702252
 
 let part2 () =
-    0
+    let _, ids = processed()
+    let indexed = 
+        ids 
+        |> Array.indexed 
+        |> Array.filter (fun (_, id) -> id <> "x") 
+        |> Array.map (fun (i, id) -> uint64 i, uint64 id)
+        |> List.ofArray
+
+    let rec nextMin min index id prod =
+        if (min + index) % id = 0UL then min
+        else nextMin (min + prod) index id prod 
+
+    let rec searcher min prod numbers =
+        match numbers with
+        | [] -> min
+        | (index, id)::rest ->
+            let min = nextMin min index id prod
+            let prod = prod * id
+            searcher min prod rest
+
+    searcher 0UL 1UL indexed

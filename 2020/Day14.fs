f03764892c62630a01daf7de0bc29d55c9d6fab9
Chris Pritchard <chris@grislygrotto.nz>
2020-12-14 17:58:18 +1300

    prep for day14

---
+++ 2020/Day14.fs
@@ -0,0 +1,15 @@
+module Day14
+
+open Common
+open System.IO
+
+let input = File.ReadAllLines "./inputs/day14.txt"
+
+let processed () =
+    input
+
+let part1 () =
+    processed ()
+
+let part2 () =
+    0

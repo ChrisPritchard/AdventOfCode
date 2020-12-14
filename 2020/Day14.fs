dca2a02053e00df9f7de05f882d79188f432668a
Chris Pritchard <chris@grislygrotto.nz>
2020-12-14 21:13:28 +1300

    switched from convert to manual functions

--- 2020/Day14.fs
+++ 2020/Day14.fs
@@ -1,7 +1,6 @@
 module Day14
 
 open Common
-open System
 open System.IO
 
 let input = File.ReadAllLines "./inputs/day14.txt"
@@ -9,7 +8,22 @@ let input = File.ReadAllLines "./inputs/day14.txt"
 let processed () =
     input |> List.ofArray
 
-let rec asBinary (value: int64) = Convert.ToString (value, 2)
+// these two functions are marginally faster than the System.Convert equivalents
+
+let rec toBinary (value: int64) = 
+    if value < 2L then
+        string value
+    else
+        let divisor = value/2L
+        let remainder = string (value % 2L)
+        toBinary divisor + remainder
+
+let fromBinary (value: string) =
+    let len = value.Length - 1
+    [|0..value.Length-1|] 
+    |> Array.sumBy (fun i -> 
+        if value.[i] <> '1' then 0L
+        else pown 2L (len - i))
 
 let part1 () =
     let rec processor memory mask (instructions: string list) =
@@ -21,10 +35,10 @@ let part1 () =
         | s::rest ->
             let parts = s |> splits [|"mem[";"] = "|] |> Array.map int64
             let binary = 
-                ((asBinary parts.[1]).PadLeft mask.Length).Replace(" ", "0")
+                ((toBinary parts.[1]).PadLeft mask.Length).Replace(" ", "0")
                 |> Seq.mapi (fun i c -> if mask.[i] <> 'X' then mask.[i] else c)
                 |> asString
-            let value = Convert.ToInt64 (binary, 2)
+            let value = fromBinary binary
             processor (Map.add parts.[0] value memory) mask rest
     processor Map.empty Array.empty (processed ())
 
@@ -38,12 +52,12 @@ let part2 () =
         | s::rest ->
             let parts = s |> splits [|"mem[";"] = "|] |> Array.map int64
             let binary = 
-                ((asBinary parts.[0]).PadLeft mask.Length).Replace(" ", "0")
+                ((toBinary parts.[0]).PadLeft mask.Length).Replace(" ", "0")
                 |> Seq.toArray
                 |> Array.mapi (fun i c -> if mask.[i] = '0' then c else mask.[i])
             let rec updater (acc: string) i map =
                 if i >= binary.Length then
-                    let address = Convert.ToInt64 (acc, 2)
+                    let address = fromBinary acc
                     Map.add address parts.[1] map
                 else
                     let c = binary.[i]

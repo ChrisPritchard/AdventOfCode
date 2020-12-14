8148d8477f4cd770918ce957587e8df517b53792
Chris Pritchard <chris@grislygrotto.nz>
2020-12-14 18:54:07 +1300

    slight improvement

--- 2020/Day14.fs
+++ 2020/Day14.fs
@@ -9,13 +9,7 @@ let input = File.ReadAllLines "./inputs/day14.txt"
 let processed () =
     input |> List.ofArray
 
-let rec asBinary value =
-  if value < 2UL then
-    string value
-  else
-    let divisor = value/2UL
-    let remainder = string (value % 2UL)
-    asBinary divisor + remainder
+let rec asBinary (value: int64) = Convert.ToString (value, 2)
 
 let part1 () =
     let rec processor memory mask (instructions: string list) =
@@ -25,12 +19,12 @@ let part1 () =
         | s::rest when s.StartsWith "mask" ->
             processor memory (s.["mask = ".Length..] |> Seq.toArray) rest
         | s::rest ->
-            let parts = s |> splits [|"mem[";"] = "|] |> Array.map uint64
+            let parts = s |> splits [|"mem[";"] = "|] |> Array.map int64
             let binary = 
                 ((asBinary parts.[1]).PadLeft mask.Length).Replace(" ", "0")
                 |> Seq.mapi (fun i c -> if mask.[i] <> 'X' then mask.[i] else c)
                 |> asString
-            let value = Convert.ToUInt64 (binary.TrimStart([|'0'|]), 2)
+            let value = Convert.ToInt64 (binary, 2)
             processor (Map.add parts.[0] value memory) mask rest
     processor Map.empty Array.empty (processed ())
 
@@ -42,26 +36,21 @@ let part2 () =
         | s::rest when s.StartsWith "mask" ->
             processor memory (s.["mask = ".Length..] |> Seq.toArray) rest
         | s::rest ->
-            let parts = s |> splits [|"mem[";"] = "|] |> Array.map uint64
+            let parts = s |> splits [|"mem[";"] = "|] |> Array.map int64
             let binary = 
                 ((asBinary parts.[0]).PadLeft mask.Length).Replace(" ", "0")
                 |> Seq.toArray
                 |> Array.mapi (fun i c -> if mask.[i] = '0' then c else mask.[i])
-            let rec addresses (acc: string) i =
+            let rec updater (acc: string) i map =
                 if i >= binary.Length then
-                    [|
-                        Convert.ToUInt64 (acc.TrimStart([|'0'|]), 2)
-                    |]
+                    let address = Convert.ToInt64 (acc, 2)
+                    Map.add address parts.[1] map
                 else
                     let c = binary.[i]
                     if c = 'X' then
-                        Array.collect id [|
-                            addresses (acc + "0") (i+1)
-                            addresses (acc + "1") (i+1)
-                        |]
+                        let map = updater (acc + "0") (i+1) map
+                        updater (acc + "1") (i+1) map
                     else
-                        addresses (acc + string c) (i+1)
-            let allAddresses = addresses "" 0
-            let memory = (memory, allAddresses) ||> Array.fold (fun map address -> Map.add address parts.[1] map)
-            processor memory mask rest
+                        updater (acc + string c) (i+1) map
+            processor (updater "" 0 memory) mask rest
     processor Map.empty Array.empty (processed ())

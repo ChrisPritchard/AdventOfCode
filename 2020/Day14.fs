500073c283cfcbefa62633bc4bcf16c073a2a02d
Chris Pritchard <chris@grislygrotto.nz>
2020-12-14 18:38:34 +1300

    part 2 done and added description

--- 2020/Day14.fs
+++ 2020/Day14.fs
@@ -35,4 +35,33 @@ let part1 () =
     processor Map.empty Array.empty (processed ())
 
 let part2 () =
-    0
+    let rec processor memory mask (instructions: string list) =
+        match instructions with
+        | [] ->
+            Map.toArray memory |> Array.sumBy snd
+        | s::rest when s.StartsWith "mask" ->
+            processor memory (s.["mask = ".Length..] |> Seq.toArray) rest
+        | s::rest ->
+            let parts = s |> splits [|"mem[";"] = "|] |> Array.map uint64
+            let binary = 
+                ((asBinary parts.[0]).PadLeft mask.Length).Replace(" ", "0")
+                |> Seq.toArray
+                |> Array.mapi (fun i c -> if mask.[i] = '0' then c else mask.[i])
+            let rec addresses (acc: string) i =
+                if i >= binary.Length then
+                    [|
+                        Convert.ToUInt64 (acc.TrimStart([|'0'|]), 2)
+                    |]
+                else
+                    let c = binary.[i]
+                    if c = 'X' then
+                        Array.collect id [|
+                            addresses (acc + "0") (i+1)
+                            addresses (acc + "1") (i+1)
+                        |]
+                    else
+                        addresses (acc + string c) (i+1)
+            let allAddresses = addresses "" 0
+            let memory = (memory, allAddresses) ||> Array.fold (fun map address -> Map.add address parts.[1] map)
+            processor memory mask rest
+    processor Map.empty Array.empty (processed ())

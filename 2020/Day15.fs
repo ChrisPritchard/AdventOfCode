89df0f543ff7ab6b4266c84c8f50c4bb08a94ce3
Chris Pritchard <chris@grislygrotto.nz>
2020-12-16 07:49:48 +1300

    optimisation using mutabe collection

--- 2020/Day15.fs
+++ 2020/Day15.fs
@@ -2,6 +2,7 @@ module Day15
 
 open Common
 open System.IO
+open System.Collections.Generic
 
 let input = File.ReadAllLines "./inputs/day15.txt"
 
@@ -15,12 +16,15 @@ let part1 () =
         if i = 2020 then next else memory (i + 1) next (Map.add last (i - 1) acc)
     memory (nums.Length + 1) (nums.[nums.Length - 1]) acc
 
-// the below solves this in 60 seconds, so needs some optimisation
+// the below swaps the immutable map for a mutable dictionary, which improves runtime from 60s to about 2s
 
 let part2 () = 
     let nums = processed ()
-    let acc = nums |> Array.take (nums.Length - 1) |> Array.mapi (fun i n -> n, (i+1)) |> Map.ofArray
-    let rec memory i last acc =
-        let next = if Map.containsKey last acc then i - 1 - acc.[last] else 0
-        if i = 30000000 then next else memory (i + 1) next (Map.add last (i - 1) acc)
-    memory (nums.Length + 1) (nums.[nums.Length - 1]) acc
+    let acc = Dictionary<int, int>()
+    for (k, v) in nums |> Array.take (nums.Length - 1) |> Array.mapi (fun i n -> n, (i+1)) do
+        acc.Add(k, v)
+    let rec memory i last =
+        let next = if acc.ContainsKey last then i - 1 - acc.[last] else 0
+        acc.[last] <- i - 1
+        if i = 30000000 then next else memory (i + 1) next
+    memory (nums.Length + 1) (nums.[nums.Length - 1])

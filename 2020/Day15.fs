202ee77f178d035e2b34fe4d30b1091a8c1f76f0
Chris Pritchard <chris@grislygrotto.nz>
2020-12-16 14:26:27 +1300

    speed optimisation

--- 2020/Day15.fs
+++ 2020/Day15.fs
@@ -2,7 +2,6 @@ module Day15
 
 open Common
 open System.IO
-open System.Collections.Generic
 
 let input = File.ReadAllLines "./inputs/day15.txt"
 
@@ -16,15 +15,19 @@ let part1 () =
         if i = 2020 then next else memory (i + 1) next (Map.add last (i - 1) acc)
     memory (nums.Length + 1) (nums.[nums.Length - 1]) acc
 
-// the below swaps the immutable map for a mutable dictionary, which improves runtime from 60s to about 2s
+// in the below swapping the immutable map for a mutable dictionary improves runtime from 60s to about 2s
+// startomg the dictionary with an appropriate capacity (~4 mil) shaves off 400-600 ms
+// using an array big enough of o(1) lookups by index (~30 mil) drops it to ~600 ms
 
 let part2 () = 
     let nums = processed ()
-    let acc = Dictionary<int, int>(5000000) // initial capacity doubles speed
+    let acc = Array.create 30000000 0 // max number here - who cares about memory usage? :D
     for (k, v) in nums |> Array.take (nums.Length - 1) |> Array.mapi (fun i n -> n, (i+1)) do
-        acc.Add(k, v)
+        acc.[k] <- v
     let rec memory i last =
-        let next = if acc.ContainsKey last then i - 1 - acc.[last] else 0
+        if last >= acc.Length then 
+            printfn "%A" last
+        let next = if acc.[last] <> 0 then i - 1 - acc.[last] else 0
         acc.[last] <- i - 1
         if i = 30000000 then next else memory (i + 1) next
     memory (nums.Length + 1) (nums.[nums.Length - 1])

ec7148ded3489b9d25d0a812ff5c2bb45bf03d9f
Chris Pritchard <chris@grislygrotto.nz>
2020-12-18 09:17:06 +1300

    much faster pure mutable solution

--- 2020/Day17.fs
+++ 2020/Day17.fs
@@ -4,57 +4,70 @@ open System.IO
 
 let input = File.ReadAllLines "./inputs/day17.txt"
 
-let state p m = match Map.tryFind p m with Some s -> s | _ -> '.'
+let count = 6
+let xlen, ylen = input.Length, input.[0].Length
 
-let nextState p m adjacent = 
+let nextState c p i adjacent = 
     let active = 
-        adjacent p 
-        |> Array.map (fun op -> state op m) 
+        adjacent p i 
         |> Array.filter (fun s -> s = '#') 
         |> Array.length
-    match state p m with
+    match c with
     | '#' when active <> 2 && active <> 3 -> '.'
     | '.' when active = 3 -> '#'
     | s -> s
 
-let iter m adjacent = 
-    let positions =
-        Map.toArray m
-        |> Array.collect (fun (k, _) -> adjacent k |> Array.append [|k|])
-        |> Array.distinct
-    positions |> Array.map (fun p -> p, nextState p m adjacent) |> Map.ofArray
+let part1 () = 
+    let grids = 
+        [|0..count|]
+        |> Array.map (fun i ->
+            Array.init (xlen + count*2) (fun _ ->
+                Array.init (ylen + count*2) (fun _ ->
+                    Array.init (1 + count*2) (fun _ -> '.'))))
+    for x in [0..xlen-1] do
+        for y in [0..ylen-1] do
+            grids.[0].[x+count].[y+count].[count] <- input.[x].[y]
 
-let part1 () =
-    let processed =
-        input 
-        |> Array.mapi (fun y line -> 
-            line |> Seq.mapi (fun x c -> (x, y, 0), c) |> Seq.toArray)
-        |> Array.collect id
-        |> Map.ofArray
-
-    let cardinals = 
+    let cardinals =
         [|-1..1|] |> Array.collect (fun dx -> 
             [|-1..1|] |> Array.collect (fun dy ->
                 [|-1..1|] 
-                |> Array.filter (fun dz -> not (dx = 0 && dy = 0 && dz = 0))
+                |> Array.filter (fun dz -> not (dx = 0 && dy = 0 && dz = 0)) 
                 |> Array.map (fun dz -> dx, dy, dz)))
 
-    let adjacent (x, y, z) =
-        cardinals
-        |> Array.map (fun (dx, dy, dz) -> x + dx, y + dy, z + dz)
+    let adjacent (x, y, z) i =
+        cardinals 
+        |> Array.choose (fun (dx, dy, dz) -> 
+            let x, y, z = x + dx, y + dy, z + dz
+            if x < 0 || y < 0 || z < 0 then None
+            elif x >= grids.[i].Length || y >= grids.[i].[0].Length then None
+            elif z >= grids.[i].[0].[0].Length then None
+            else Some (grids.[i].[x].[y].[z]))
 
-    let result =
-        (processed, [0..5])
-        ||> List.fold (fun m _ -> iter m adjacent)
-    result |> Map.toArray |> Array.filter (fun (_, s) -> s = '#') |> Array.length
+    let mutable sum = 0
+    for i in [0..count-1] do
+        for x in [0..grids.[i].Length-1] do
+            for y in [0..grids.[i].[0].Length-1] do
+                for z in [0..grids.[i].[0].[0].Length-1] do
+                    let c = grids.[i].[x].[y].[z]
+                    let n = nextState c (x, y, z) i adjacent
+                    if i = 5 then
+                        if n = '#' then sum <- sum + 1
+                    else
+                        grids.[i+1].[x].[y].[z] <- n
+    sum
 
 let part2 () =
-    let processed =
-        input 
-        |> Array.mapi (fun y line -> 
-            line |> Seq.mapi (fun x c -> (x, y, 0, 0), c) |> Seq.toArray)
-        |> Array.collect id
-        |> Map.ofArray
+    let grids = 
+        [|0..count|]
+        |> Array.map (fun i ->
+            Array.init (xlen + count*2) (fun _ ->
+                Array.init (ylen + count*2) (fun _ ->
+                    Array.init (1 + count*2) (fun _ ->
+                        Array.init (1 + count*2) (fun _ -> '.')))))
+    for x in [0..xlen-1] do
+        for y in [0..ylen-1] do
+            grids.[0].[x+count].[y+count].[count].[count] <- input.[x].[y]
 
     let cardinals =
         [|-1..1|] |> Array.collect (fun dx -> 
@@ -64,11 +77,25 @@ let part2 () =
                     |> Array.filter (fun dw -> not (dx = 0 && dy = 0 && dz = 0 && dw = 0)) 
                     |> Array.map (fun dw -> dx, dy, dz, dw))))
 
-    let adjacent (x, y, z, w) =
+    let adjacent (x, y, z, w) i =
         cardinals 
-        |> Array.map (fun (dx, dy, dz, dw) -> x + dx, y + dy, z + dz, w + dw)
+        |> Array.choose (fun (dx, dy, dz, dw) -> 
+            let x, y, z, w = x + dx, y + dy, z + dz, w + dw
+            if x < 0 || y < 0 || z < 0 || w < 0 then None
+            elif x >= grids.[i].Length || y >= grids.[i].[0].Length then None
+            elif z >= grids.[i].[0].[0].Length || w >= grids.[i].[0].[0].[0].Length then None
+            else Some (grids.[i].[x].[y].[z].[w]))
 
-    let result =
-        (processed, [0..5])
-        ||> List.fold (fun m _ -> iter m adjacent)
-    result |> Map.toArray |> Array.filter (fun (_, s) -> s = '#') |> Array.length
+    let mutable sum = 0
+    for i in [0..count-1] do
+        for x in [0..grids.[i].Length-1] do
+            for y in [0..grids.[i].[0].Length-1] do
+                for z in [0..grids.[i].[0].[0].Length-1] do
+                    for w in [0..grids.[i].[0].[0].[0].Length-1] do
+                        let c = grids.[i].[x].[y].[z].[w]
+                        let n = nextState c (x, y, z, w) i adjacent
+                        if i = 5 then
+                            if n = '#' then sum <- sum + 1
+                        else
+                            grids.[i+1].[x].[y].[z].[w] <- n
+    sum

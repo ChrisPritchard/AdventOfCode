e0a82963b6b77406835072577f8a7037586ed529
Christopher Pritchard <chrispritchard.nz@gmail.com>
2020-12-18 21:16:18 +1300

    ugly solution to day 18

--- 2020/Day18.fs
+++ 2020/Day18.fs
@@ -1,36 +1,57 @@
 module Day18
 
+open Common
 open System
 open System.IO
 
 let input = File.ReadAllLines "./inputs/day18.txt"
 
 let processed () = 
-    input |> Array.map (fun line -> line.Replace(" ", "") |> List.ofSeq)
+    input |> Array.map (fun s -> s.Replace (")", " )") + " " |> List.ofSeq)
 
-let asInt (n: char) = uint64 n - 48UL
+let rec processor acc op curr rem =
+    //printfn "%A" acc
+    match rem with
+    | [] -> acc, []
+    | ')'::rem -> acc, rem
+    | '+'::rem -> processor acc '+' "" rem
+    | '*'::rem -> processor acc '*' "" rem
+    | '('::rem ->
+        let res, rem = processor 0UL ' ' "" rem
+        match op with
+        | '+' -> processor (acc + res) ' ' "" rem
+        | '*' -> processor (acc * res) ' ' "" rem
+        | _ -> processor res ' ' "" rem
+    | n::rem when Char.IsDigit n -> processor acc op (curr + string n) rem
+    | ' '::rem when curr <> "" ->
+        match op with
+        | '+' -> processor (acc + uint64 curr) ' ' "" rem
+        | '*' -> processor (acc * uint64 curr) ' ' "" rem
+        | _ -> processor (uint64 curr) ' ' "" rem
+    | ' '::rem -> processor acc op curr rem
+    | _ -> failwithf "unexpected state: %A" rem
 
 let part1 () =
-    let rec processor acc op rem =
-        match rem with
-        | [] -> acc, []
-        | ')'::rem -> acc, rem
-        | '+'::rem -> processor acc '+' rem
-        | '*'::rem -> processor acc '*' rem
-        | '('::rem ->
-            let res, rem = processor 0UL ' ' rem
-            match op with
-            | '+' -> processor (acc + res) ' ' rem
-            | '*' -> processor (acc * res) ' ' rem
-            | _ -> processor res ' ' rem
-        | n::rem when Char.IsDigit n -> 
-            match op with
-            | '+' -> processor (acc + asInt n) ' ' rem
-            | '*' -> processor (acc * asInt n) ' ' rem
-            | _ -> processor (asInt n) ' ' rem
-        | _ -> failwithf "unexpected state: %A" rem
     processed ()
-    |> Array.sumBy (processor 0UL ' ' >> fst)
+    |> Array.sumBy (processor 0UL ' ' "" >> fst)
+
+let precedence line =
+    let valueOf s = 
+        s 
+        |> split "*" 
+        |> Array.map (fun seg -> "("+seg+")") 
+        |> String.concat "*"
+        |> fun s -> s.Replace (")", " )") |> Seq.toList |> processor 0UL ' ' "" |> fst
+    let rec folder acc rem =
+        match rem with
+        | [] -> valueOf acc, []
+        | ')'::rem -> valueOf acc, rem
+        | '('::rem -> 
+            let v, rem = folder "" rem
+            folder (acc + string v) rem
+        | c::rem -> folder (acc + string c) rem
+    folder "" line |> fst
    
 let part2 () = 
-    0
+    processed ()
+    |> Array.sumBy precedence

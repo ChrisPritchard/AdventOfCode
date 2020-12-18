0fa8b15d4db73e90f651e427838a37867a036d91
Christopher Pritchard <chrispritchard.nz@gmail.com>
2020-12-18 18:46:44 +1300

    finished part 1

--- 2020/Day18.fs
+++ 2020/Day18.fs
@@ -8,9 +8,30 @@ let input = File.ReadAllLines "./inputs/day18.txt"
 let processed () = 
     input |> Array.map (fun line -> line.Replace(" ", "") |> List.ofSeq)
 
+let asInt (n: char) = uint64 n - 48UL
+
 let part1 () =
-    processed () |> fun a -> a.[0] |> printfn "%A"
-    0
+    let rec processor acc op rem =
+        match rem, op with
+        | [], _ -> acc, []
+        | ')'::rem, _ -> acc, rem
+        | '+'::rem, _ -> processor acc '+' rem
+        | '*'::rem, _ -> processor acc '*' rem
+        | '('::rem, '+' -> 
+            let res, rem = processor 0UL ' ' rem
+            processor (acc + res) ' '  rem
+        | '('::rem, '*' -> 
+            let res, rem = processor 0UL ' ' rem
+            processor (acc * res) ' ' rem
+        | '('::rem, _ -> 
+            let res, rem = processor 0UL ' ' rem
+            processor res ' ' rem
+        | n::rem, '+' when Char.IsDigit n -> processor (acc + asInt n) ' ' rem
+        | n::rem, '*' when Char.IsDigit n -> processor (acc * asInt n) ' ' rem
+        | n::rem, _ when Char.IsDigit n -> processor (asInt n) ' ' rem
+        | n::rem, _ -> failwith (sprintf "unexpected input: %A" n)
+    processed ()
+    |> Array.sumBy (fun line -> processor 0UL ' ' line |> fst)
    
 let part2 () = 
     0

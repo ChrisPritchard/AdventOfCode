9e1f88d1c3a3d181fdc85c7d1aa5807232873592
Christopher Pritchard <chrispritchard.nz@gmail.com>
2020-12-18 18:18:24 +1300

    beginning work on day 18

--- 2020/Day18.fs
+++ 2020/Day18.fs
@@ -1,14 +1,16 @@
 module Day18
 
+open System
 open System.IO
 
 let input = File.ReadAllLines "./inputs/day18.txt"
 
 let processed () = 
-    input
+    input |> Array.map (fun line -> line.Replace(" ", "") |> List.ofSeq)
 
 let part1 () =
-    input
+    processed () |> fun a -> a.[0] |> printfn "%A"
+    0
    
 let part2 () = 
     0

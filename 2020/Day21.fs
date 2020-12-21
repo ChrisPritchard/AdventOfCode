175eea755dc0970a80b6eb95371a20da98023f3c
Christopher Pritchard <chrispritchard.nz@gmail.com>
2020-12-21 20:31:49 +1300

    finished part 2 and added desc

--- 2020/Day21.fs
+++ 2020/Day21.fs
@@ -12,9 +12,7 @@ let processed () =
         |> splitOn " (contains "
         |> fun a -> splitOn " " a.[0], split " ,)" a.[1])
 
-let part1 () = 
-    let lines = processed ()
-    let all = lines |> Array.collect fst |> Array.distinct
+let ingredientAllergies lines =
     let candidates = 
         lines 
         |> Array.collect (fun (recipe, allergens) -> 
@@ -22,25 +20,26 @@ let part1 () =
         |> Array.groupBy fst
         |> Array.map (fun (allergen, sets) -> allergen, sets |> Array.map snd |> Array.reduce Set.intersect)
         |> Array.sortBy (snd >> Set.count)
-    let map =
-        (Map.empty, candidates)
-        ||> Array.fold (fun acc (allergen, options) ->
-            let ingredient = options |> Set.filter (fun ing -> not (Map.containsKey ing acc)) |> Set.toArray
-            //if ingredient.Length <> 1 then failwithf "assertion failed: %s: %A" allergen ingredient
-            Map.add ingredient.[0] allergen acc)
+        |> List.ofArray
+    let rec reducer acc rem = 
+        match rem with
+        | [] -> acc
+        | (allergen, options)::rem ->
+            if Set.count options <> 1 then failwithf "assertion failed: %s: %A" allergen options
+            let ingredient = Set.toArray options |> Array.head
+            let rem = rem |> List.map (fun (ing, opts) -> ing, Set.remove ingredient opts) |> List.sortBy (snd >> Set.count)
+            reducer (Map.add ingredient allergen acc) rem
+    reducer Map.empty candidates
+
+let part1 () = 
+    let lines = processed ()
+    let map = ingredientAllergies lines
+    let all = lines |> Array.collect fst |> Array.distinct
     let others = all |> Array.filter (fun ing -> not (Map.containsKey ing map))
     lines |> Array.sumBy (fun (recipe, _) ->
         others |> Array.filter (fun o -> Array.contains o recipe) |> Array.length)
 
-    // for each ingredient
-    // find recipes with that ingredient and intersect
-    // intersections are options
-    // if single option, exclude from variables
-
-    // map to ing -> line
-    // group by ing
-    // map to set intersect lines
-    // sort by intersect size
-    // fold to construct ingredient candidates
-
-let part2 () = 0
+let part2 () =
+    let lines = processed ()
+    let map = ingredientAllergies lines
+    map |> Map.toArray |> Array.sortBy snd |> Array.map fst |> String.concat ","

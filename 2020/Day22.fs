ab2bcb5f19289c8ab9441aad508c77dc2c5bf0ce
Christopher Pritchard <chrispritchard.nz@gmail.com>
2020-12-22 19:32:30 +1300

    solved part 2 day 22

--- 2020/Day22.fs
+++ 2020/Day22.fs
@@ -11,28 +11,53 @@ let processed () =
     |> Array.map (fun player -> 
         player 
         |> splitOn newline 
-        |> Array.tail 
-        |> String.concat "")
+        |> Array.tail
+        |> Array.map int)
     |> fun a -> a.[0], a.[1]
 
 let part1 () =
     let player1, player2 = processed ()
     let rec playGame p1 p2 =
         match p1, p2 with
-        | o, "" | "", o ->
-            o |> Seq.rev |> Seq.indexed |> Seq.sumBy (fun (i, c) -> (i+1) * (int c - 41))
+        | o, [||] | [||], o ->
+            o |> Array.rev |> Array.indexed |> Array.sumBy (fun (i, c) -> (i+1) * c)
         | p1, p2 ->
-            let p1c, p2c = string p1.[0], string p2.[0]
+            let p1c, p2c = p1.[0], p2.[0]
             let p1, p2 =
                 if p1c > p2c then
-                    p1 + p1c + p2c, p2.[1..]
+                    Array.append p1.[1..] [|p1c; p2c|], p2.[1..]
                 else
-                    p1.[1..], p2 + p2c + p1c
+                    p1.[1..], Array.append p2.[1..] [|p2c; p1c|]
             playGame p1 p2
     playGame player1 player2
 
-    // remove head of both
-    // append to sub of winner
+type Victory =
+    | Player1 of int []
+    | Player2 of int []
 
 let part2 () =
-    0
+    let player1, player2 = processed ()
+    let rec playGame p1 p2 acc =
+        if Set.contains (p1, p2) acc then
+            Player1 p1
+        else
+            let acc = Set.add (p1, p2) acc
+            match p1, p2 with
+            | o, [||] -> Player1 o
+            | [||], o -> Player2 o
+            | p1, p2 ->
+                let p1c, p2c = p1.[0], p2.[0]
+                let p1, p2 = 
+                    if p1c > p1.[1..].Length || p2c > p2.[1..].Length then
+                        if p1c > p2c then
+                            Array.append p1.[1..] [|p1c; p2c|], p2.[1..]
+                        else
+                            p1.[1..], Array.append p2.[1..] [|p2c; p1c|]
+                    else
+                        match playGame p1.[1..p1c] p2.[1..p2c] Set.empty with
+                        | Player1 _ -> Array.append p1.[1..] [|p1c; p2c|], p2.[1..]
+                        | Player2 _ -> p1.[1..], Array.append p2.[1..] [|p2c; p1c|]
+                playGame p1 p2 acc
+    match playGame player1 player2 Set.empty with
+    | Player1 o | Player2 o ->
+        o |> Array.rev |> Array.indexed |> Array.sumBy (fun (i, c) -> (i+1) * c)

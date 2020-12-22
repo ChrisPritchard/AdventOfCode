addc8f1abb2d23715ef671cae5a7535e409ab0c5
Christopher Pritchard <chrispritchard.nz@gmail.com>
2020-12-23 07:48:33 +1300

    prep for day23

---
+++ 2020/Day23.fs
@@ -0,0 +1,15 @@
+module Day23
+
+open Common
+open System.IO
+
+let input = File.ReadAllLines "./inputs/day23.txt"
+
+let processed () =
+    input
+
+let part1 () =
+    processed ()
+
+let part2 () =
+    0

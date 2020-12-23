378491436190bdaf1e37efe7874d9506e03fb88e
Christopher Pritchard <chrispritchard.nz@gmail.com>
2020-12-23 19:53:34 +1300

    optimised

--- 2020/Day23.fs
+++ 2020/Day23.fs
@@ -1,45 +1,42 @@
 module Day23
 
-open Common
 open System.IO
+open System.Collections.Generic
 
 let input = File.ReadAllLines "./inputs/day23.txt"
 
 let processed () =
-    input.[0] |> Seq.map (fun c -> int c - 48) |> Seq.toArray
+    let numbers = input.[0] |> Seq.map (fun c -> int c - 48) |> Seq.toArray
+    let next = Dictionary<int, int>()
+    for i in [0..numbers.Length-1] do
+        let right = if i = numbers.Length - 1 then 0 else i + 1
+        next.Add (numbers.[i], numbers.[right])
+    numbers.[0], Array.min numbers, Array.max numbers, next
 
 let part1 () =
-    let start = processed ()
-    let min, max = Array.min start, Array.max start
-    let rec round index (current: int []) c =
-        if c = 100 then current
+    let start, min, max, next = processed ()
+    let rec findDest n n1 n2 n3 =
+        if n < min then 
+            findDest max n1 n2 n3
+        elif n1 = n || n2 = n || n3 = n then
+            findDest (n - 1) n1 n2 n3
         else
-            let cup = current.[index]
-            let nextThree = current.[index+1..] |> Array.truncate 3
-            let nextThree, current = 
-                let short = 3 - nextThree.Length
-                if short = 0 then
-                    nextThree, Array.append current.[0..index] current.[index+4..]
-                else
-                    Array.append nextThree current.[0..short-1], current.[short..index]
-            let rec findDest n =
-                if n < min then 
-                    findDest max 
-                elif Array.contains n nextThree then
-                    findDest (n - 1)
-                else
-                    n
-            let destCup = findDest (cup - 1)
-            let dest = Array.findIndex (fun o -> o = destCup) current
-            let current = Array.concat [|current.[0..dest]; nextThree; current.[dest+1..]|]
-            let offset = Array.findIndex (fun o -> o = cup) current - index
-            let current = Array.append current.[offset..] current.[0..offset-1]
-            let index = if index = current.Length - 1 then 0 else index + 1
-            round index current (c + 1)
-    let final = round 0 start 0
-    let offset = Array.findIndex (fun o -> o = 1) final
-    Array.append final.[offset+1..] final.[0..offset-1]
-    |> Array.map string |> String.concat ""
+            n
+    let rec round current count =
+        if count = 100 then
+            let rec compiler acc n =
+                if n = 1 then acc else compiler (acc + string n) next.[n]
+            compiler "" next.[1]
+        else
+            let n1 = next.[current]
+            let n2 = next.[n1]
+            let n3 = next.[n2]
+            next.[current] <- next.[n3]
+            let dest = findDest (current - 1) n1 n2 n3
+            next.[n3] <- next.[dest]
+            next.[dest] <- n1
+            round next.[current] (count + 1)
+    round start 0
 
 let part2 () =
     0

06ef4d87ed26b27feaf0e228a331e02bc44c5a03
Chris Pritchard <chris@grislygrotto.nz>
2021-12-01 18:17:03 +1300

    put code for reading embedded files in common, and added the description

--- 2021/Day01.fs
+++ 2021/Day01.fs
@@ -1,24 +1,17 @@
 module Day01
 
-open System.Reflection
-open System.IO
 open Common
 
-let input = 
-    use s: Stream = Assembly.GetEntryAssembly().GetManifestResourceStream "2021.day01"
-    use r = new StreamReader(s)
-    r.ReadToEnd() |> split "\r\n"
-
-let processed () = Array.map int input
+let processed = readEmbedded "day01" |> Array.map int
 
 let part1 () =
-    processed () 
+    processed
     |> Array.windowed 2
     |> Array.filter (fun set -> set[1] > set[0])
     |> Array.length
 
 let part2 () =
-    processed () 
+    processed
     |> Array.windowed 3
     |> Array.map Array.sum
     |> Array.windowed 2

7edbd2dd8a688f9fdfdd2be54358f1944d87d9a0
Chris Pritchard <chris@grislygrotto.nz>
2021-12-01 21:30:56 +1300

    tweaks and experiments to make this faster

--- 2021/Program.fs
+++ 2021/Program.fs
@@ -8,11 +8,9 @@ let main argv =
     printfn "==================="
     printfn ""
     
-    time (fun () -> 
-        timeForDay 1 1 Day01.part1
-        timeForDay 1 2 Day01.part2
+    Day01.init()
+    timeForDay 1 1 Day01.part1
+    timeForDay 1 2 Day01.part2
 
-        0
-    ) |> fun (_, el) -> printfn "total time elapsed: %d ms" el
-
+    printfn ""
     0

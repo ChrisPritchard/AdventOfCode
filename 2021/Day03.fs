b8f5704ff2d63de15fe6876150bfc2777470da82
Chris Pritchard <chris@grislygrotto.nz>
2021-12-03 19:23:48 +1300

    finished all of day3

--- 2021/Day03.fs
+++ 2021/Day03.fs
@@ -9,29 +9,41 @@ let init () =
     processed |> Array.length |> ignore
 
 let part1 () =
-    let gammaS = 
+    let rates = 
         processed 
         |> Array.collect (fun s -> s.ToCharArray() |> Array.indexed)
         |> Array.groupBy fst
         |> Array.map 
             (fun (_, chars) -> 
                 chars |> Array.countBy (snd) |> Map.ofArray)
-        |> Array.map
-            (fun counts -> if counts['0'] > counts['1'] then '0' else '1')
-        |> asString
-    let gamma = Convert.ToInt32 (gammaS, 2)
-    let epsilonS = 
-        processed 
-        |> Array.collect (fun s -> s.ToCharArray() |> Array.indexed)
-        |> Array.groupBy fst
-        |> Array.map 
-            (fun (_, chars) -> 
-                chars |> Array.countBy (snd) |> Map.ofArray)
-        |> Array.map
-            (fun counts -> if counts['0'] < counts['1'] then '0' else '1')
-        |> asString
-    let episilon = Convert.ToInt32 (epsilonS, 2)
-    gamma * episilon
+        |> Array.fold 
+            (fun (gamma, epsilon) counts -> 
+                if counts['0'] > counts['1'] then 
+                    gamma + "0", epsilon + "1" 
+                else 
+                    gamma + "1", epsilon + "0")
+            ("", "")
+    Convert.ToInt32 (fst rates, 2) * Convert.ToInt32 (snd rates, 2)
 
 let part2 () =
-    0
+    let rates index (numbers: string[]) = 
+        numbers 
+        |> Array.groupBy (fun s -> s[index])
+        |> fun groups ->
+            if Array.length groups = 1 then
+                snd groups[0], snd groups[0]
+            else
+                let m = Map.ofArray groups
+                if Array.length m['0'] = Array.length m['1'] then
+                    m['1'], m['0']
+                else if Array.length m['1'] > Array.length m['0'] then
+                    m['1'], m['0']
+                else
+                    m['0'], m['1']
+    let rec finder index numbers (filter: string[] * string[] -> string[]) =
+        match numbers with
+        | [|n|] -> Convert.ToInt32 (n, 2)
+        | _ -> 
+            let next = filter (rates index numbers)
+            finder (index + 1) next filter
+    finder 0 processed (fst) * finder 0 processed (snd)

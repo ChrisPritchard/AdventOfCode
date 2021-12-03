97a5777d4630e0c3d6582f34c3f60dbe19bdd5a4
Chris Pritchard <chris@grislygrotto.nz>
2021-12-03 18:23:26 +1300

    first part done

--- 2021/Day03.fs
+++ 2021/Day03.fs
@@ -1,23 +1,37 @@
 module Day03
 
 open Common
+open System
 
-let processed = readEmbedded "day03" |> Array.map (fun s -> split " " s |> fun a -> a[0], int a[1])
+let processed = readEmbedded "day03"
 
 let init () =
     processed |> Array.length |> ignore
 
 let part1 () =
-    let mutable x = 0
-    let mutable y = 0
-    for (command, amt) in processed do
-        if command = "down" then
-            y <- y + amt
-        else if command = "up" then
-            y <- y - amt
-        else
-            x <- x + amt
-    x * y
+    let gammaS = 
+        processed 
+        |> Array.collect (fun s -> s.ToCharArray() |> Array.indexed)
+        |> Array.groupBy fst
+        |> Array.map 
+            (fun (_, chars) -> 
+                chars |> Array.countBy (snd) |> Map.ofArray)
+        |> Array.map
+            (fun counts -> if counts['0'] > counts['1'] then '0' else '1')
+        |> asString
+    let gamma = Convert.ToInt32 (gammaS, 2)
+    let epsilonS = 
+        processed 
+        |> Array.collect (fun s -> s.ToCharArray() |> Array.indexed)
+        |> Array.groupBy fst
+        |> Array.map 
+            (fun (_, chars) -> 
+                chars |> Array.countBy (snd) |> Map.ofArray)
+        |> Array.map
+            (fun counts -> if counts['0'] < counts['1'] then '0' else '1')
+        |> asString
+    let episilon = Convert.ToInt32 (epsilonS, 2)
+    gamma * episilon
 
 let part2 () =
     0

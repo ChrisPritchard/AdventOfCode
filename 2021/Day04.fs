95a59a7c1b8bfd4dd36baf347668de8e9553b03c
Chris Pritchard <chris@grislygrotto.nz>
2021-12-04 18:43:44 +1300

    finished day 4

--- 2021/Day04.fs
+++ 2021/Day04.fs
@@ -9,21 +9,93 @@ let init () =
     processed |> Array.length |> ignore
 
 let part1 () =
-    let rates = 
-        processed 
-        |> Array.collect (fun s -> s.ToCharArray() |> Array.indexed)
-        |> Array.groupBy fst
-        |> Array.map 
-            (fun (_, chars) -> 
-                chars |> Array.countBy (snd) |> Map.ofArray)
-        |> Array.fold 
-            (fun (gamma, epsilon) counts -> 
-                if counts['0'] > counts['1'] then 
-                    gamma + "0", epsilon + "1" 
-                else 
-                    gamma + "1", epsilon + "0")
-            ("", "")
-    Convert.ToInt32 (fst rates, 2) * Convert.ToInt32 (snd rates, 2)
+    let numbers = split "," processed[0] |> List.ofArray
+
+    let mutable boards = []
+    for i in [1..5..processed.Length-1] do
+        let board = processed[i..i+4] |> Array.map (split " ")
+        boards <- board::boards
+
+    let wins marked (board: string[][]) =
+        let mutable victory = false
+        for y = 0 to 4 do
+            let mutable unmarked = false
+            for x = 0 to 4 do 
+                if not (Set.contains (board[y][x]) marked) then
+                    unmarked <- true
+            if not unmarked then
+                victory <- true
+        for x = 0 to 4 do 
+            let mutable unmarked = false
+            for y = 0 to 4 do    
+                if not (Set.contains (board[y][x]) marked) then
+                    unmarked <- true
+            if not unmarked then
+                victory <- true
+        victory
+
+    let unmarked marked board = 
+        board |> Array.collect id |> Array.sumBy (fun s -> if Set.contains s marked then 0 else int s)
+
+    let rec play soFar (numbers: string list) boards =
+        match numbers with
+        | next::rem ->
+            let nextMarked = Set.add next soFar
+            let rec testBoards boards =
+                match boards with 
+                | b::rest ->
+                    if wins nextMarked b then
+                        Some ((unmarked nextMarked b) * int next)
+                    else
+                        testBoards rest
+                | _ -> None
+            match testBoards boards with
+            | Some n -> n
+            | _ -> play nextMarked rem boards
+        | _ -> failwith "shouldn't be reached"
+
+    play Set.empty numbers boards
 
 let part2 () =
-    0
+    let numbers = split "," processed[0] |> List.ofArray
+
+    let mutable boards = []
+    for i in [1..5..processed.Length-1] do
+        let board = processed[i..i+4] |> Array.map (split " ")
+        boards <- board::boards
+
+    let wins marked (board: string[][]) =
+        let mutable victory = false
+        for y = 0 to 4 do
+            let mutable unmarked = false
+            for x = 0 to 4 do 
+                if not (Set.contains (board[y][x]) marked) then
+                    unmarked <- true
+            if not unmarked then
+                victory <- true
+        for x = 0 to 4 do 
+            let mutable unmarked = false
+            for y = 0 to 4 do    
+                if not (Set.contains (board[y][x]) marked) then
+                    unmarked <- true
+            if not unmarked then
+                victory <- true
+        victory
+
+    let unmarked marked board = 
+        board |> Array.collect id |> Array.sumBy (fun s -> if Set.contains s marked then 0 else int s)
+
+    boards 
+    |> List.map (fun b ->
+        let rec count soFar numbers =
+            match numbers with
+            | next::rem ->
+                let nextMarked = Set.add next soFar
+                if wins nextMarked b then
+                    Set.count nextMarked, (unmarked nextMarked b) * int next
+                else
+                    count nextMarked rem
+            | _ -> failwith "shouldn't be reached"
+        count Set.empty numbers)
+    |> List.maxBy (fst)
+    |> snd

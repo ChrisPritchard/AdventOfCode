9ad53783782955c2b05a4b041cac4d713c43a328
Chris Pritchard <chris@grislygrotto.nz>
2021-12-07 20:35:26 +1300

    fixed sum performance

--- 2021/Day07.fs
+++ 2021/Day07.fs
@@ -14,4 +14,4 @@ let part1 () =
 
 let part2 () =
     let range = [|Array.min processed..Array.max processed|]
-    range |> Array.map (fun o -> processed |> Array.sumBy (fun s -> Array.sum [|1..abs (s - o)|])) |> Array.min
+    range |> Array.map (fun o -> processed |> Array.sumBy (fun s -> let n = abs (s - o) in (n * (n+1))/2)) |> Array.min

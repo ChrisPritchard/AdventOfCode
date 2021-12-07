42b25ce65e7c61c235bc7c8d19ae0900ea467b2b
Chris Pritchard <chris@grislygrotto.nz>
2021-12-07 20:32:26 +1300

    finished day 7

---
+++ 2021/Day07.fs
@@ -0,0 +1,17 @@
+module Day07
+
+open Common
+open System
+
+let processed = readEmbedded "day07" |> Array.head |> split "," |> Array.map int
+
+let init () =
+    processed |> Array.length |> ignore
+
+let part1 () =
+    let range = [|Array.min processed..Array.max processed|]
+    range |> Array.map (fun o -> processed |> Array.sumBy (fun s -> abs (s - o))) |> Array.min
+
+let part2 () =
+    let range = [|Array.min processed..Array.max processed|]
+    range |> Array.map (fun o -> processed |> Array.sumBy (fun s -> Array.sum [|1..abs (s - o)|])) |> Array.min

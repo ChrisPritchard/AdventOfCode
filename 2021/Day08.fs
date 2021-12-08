299ecb37035e96b8f3dd485a0a679360d709ab08
Chris Pritchard <chris@grislygrotto.nz>
2021-12-08 18:09:37 +1300

    finished part 1

--- 2021/Day08.fs
+++ 2021/Day08.fs
@@ -3,13 +3,16 @@ module Day08
 open Common
 open System
 
-let processed = readEmbedded "day08"
+let processed = readEmbedded "day08" |> Array.map (fun l -> let parts = split "|" l in split " " parts[0], split " " parts[1])
 
 let init () =
     processed |> Array.length |> ignore
 
 let part1 () =
-    0
+    processed
+    |> Array.collect snd
+    |> Array.countBy String.length
+    |> Array.sumBy (fun (l, c) -> if l = 2 || l = 4 || l = 3 || l = 7 then c else 0)
 
 let part2 () =
     0

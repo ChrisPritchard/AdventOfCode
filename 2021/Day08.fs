acdf972eb6992352ba099f79a592af12a7f3368d
Chris Pritchard <chris@grislygrotto.nz>
2021-12-08 17:55:56 +1300

    prep for day 8

---
+++ 2021/Day08.fs
@@ -0,0 +1,15 @@
+module Day08
+
+open Common
+open System
+
+let processed = readEmbedded "day08"
+
+let init () =
+    processed |> Array.length |> ignore
+
+let part1 () =
+    0
+
+let part2 () =
+    0

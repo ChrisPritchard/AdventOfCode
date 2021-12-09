bd5c25045b7966a02e653f6f88c26a518ca8b9a5
Chris Pritchard <chris@grislygrotto.nz>
2021-12-09 16:36:35 +1300

    prep for day 9

---
+++ 2021/Day09.fs
@@ -0,0 +1,14 @@
+module Day09
+
+open Common
+open System
+
+let processed = readEmbedded "day09"
+
+let init () =
+    processed |> Array.length |> ignore
+
+let part1 () =
+    0
+let part2 () =
+    0

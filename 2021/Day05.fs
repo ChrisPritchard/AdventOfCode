8106a898329a6e539e59d66687f7cdef65ca93fc
Chris Pritchard <chris@grislygrotto.nz>
2021-12-09 18:15:06 +1300

    finished part 1

--- 2021/Day05.fs
+++ 2021/Day05.fs
@@ -3,15 +3,13 @@ module Day05
 open Common
 open System
 
-let processed = readEmbedded "day05"
+let processed = readEmbedded "day05" |> Array.map (fun s ->
+    let parts = split ", ->" s |> Array.map int
+    parts[0],parts[1],parts[2],parts[3])
 
 let init () =
     processed |> Array.length |> ignore
 
-let ends (s: string) =
-    let parts = split ", ->" s |> Array.map int
-    parts[0],parts[1],parts[2],parts[3]
-    
 let asPoints (x1,y1,x2,y2) =
     let dx = if x1 < x2 then 1 else -1
     let dy = if y1 < y2 then 1 else -1
@@ -24,7 +22,6 @@ let asPoints (x1,y1,x2,y2) =
 
 let part1 () =
     processed 
-    |> Array.map ends
     |> Array.filter (fun (x1,y1,x2,y2) -> (x1 = x2 && y1 <> y2) || (x1 <> x2 && y1 = y2))
     |> Array.collect asPoints
     |> Array.groupBy id
@@ -33,7 +30,6 @@ let part1 () =
 
 let part2 () =
     processed 
-    |> Array.map ends
     |> Array.collect asPoints
     |> Array.groupBy id
     |> Array.filter (fun (_, a) -> Array.length a > 1)
--- 2021/Day09.fs
+++ 2021/Day09.fs
@@ -3,12 +3,26 @@ module Day09
 open Common
 open System
 
-let processed = readEmbedded "day09"
+let processed = readEmbedded "day09" |> Array.map (fun s -> s.ToCharArray() |> Array.map (fun c -> int c - int '0'))
 
 let init () =
     processed |> Array.length |> ignore
 
+let adjacent y x (m: int[][]) = 
+    [|-1,0; 0,1; 1,0; 0,-1|]
+    |> Array.map (fun (dy, dx) -> y + dy, x + dx)
+    |> Array.filter (fun (y, x) -> y >= 0 && x >= 0 && y < Array.length m && x < Array.length m[0])
+    |> Array.map (fun (y, x) -> m[y][x])
+
 let part1 () =
-    0
+    let mutable sum = 0
+    for y in [0.. Array.length processed - 1] do
+        for x in [0.. Array.length processed[0] - 1] do
+            let v = processed[y][x]
+            let neighbours = adjacent y x processed
+            if Array.forall (fun o -> o > v) neighbours then
+                sum <- sum + v + 1
+    sum
+
 let part2 () =
     0

3430073144b75e1a36bd5c13b4005aac7eee99fe
Chris Pritchard <chris@grislygrotto.nz>
2021-12-11 12:53:14 +1300

    prep for day 11

---
+++ 2021/Day11.fs
@@ -0,0 +1,15 @@
+module Day11
+
+open Common
+open System
+
+let processed = readEmbedded "day11"
+
+let init () =
+    processed |> Array.length |> ignore
+
+let part1 () =
+    0
+
+let part2 () =
+    0

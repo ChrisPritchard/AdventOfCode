ea6c9f3eaebc284ac8dadf912e644969793436ab
Chris Pritchard <chris@grislygrotto.nz>
2021-12-11 12:49:54 +1300

    finally finished day 10

--- 2021/Day10.fs
+++ 2021/Day10.fs
@@ -13,55 +13,34 @@ type Res =
     | Continue of char list
     | Result of char
 
-let part1 () =
+let beg c = Array.contains c [|'(';'[';'<';'{'|]
+let opp = Map.ofArray [|')','(';']','[';'}','{';'>','<'|]
 
-    let beg c = Array.contains c [|'(';'[';'<';'{'|]
-    let opp = function
-    | ')' -> '('
-    | ']' -> '['
-    | '>' -> '<'
-    | _ -> '{'
+let part1 () =
     let scores = Map.ofArray [|')',3;']',57;'}',1197;'>',25137|]
     let rec score queue rem =
-        match rem with
-        | [] -> 0
-        | c::rem when beg c ->
-            score (c::queue) rem
-        | c::rem ->
-            match queue with
-            | o::queue when o = opp c ->
-                score queue rem
-            | _ -> 
-                scores[c]
+        match rem, queue with
+        | [], _ -> 0
+        | c::rem, _ when beg c -> score (c::queue) rem
+        | c::rem, o::queue when o = opp[c] -> score queue rem
+        | c::rem, _ -> scores[c]                
 
     processed
     |> Array.sumBy (fun s -> score [] (List.ofSeq s))
 
 let part2 () =
-    0
+    let scores = Map.ofArray [|'(',1L;'[',2L;'{',3L;'<',4L|]
+    let rec score queue = queue |> List.fold (fun v c -> v * 5L + scores[c]) 0L |> Some
+    let rec find queue rem =
+        match rem, queue with
+        | [], [] -> None
+        | [], _ -> score queue
+        | c::rem, _ when beg c ->
+            find (c::queue) rem
+        | c::rem, o::queue when o = opp[c] -> find queue rem
+        | _ -> None
 
-// let test =
-//     let s = "c7afbc7a3ed8f0c719cd134fc7330241b186e53e97fe4a8056321812f51972f1"
-//     let t = "60b868"
-
-//     let md5 = System.Security.Cryptography.MD5.Create ()
-
-//     let hex (bytes: seq<byte>) =
-//         bytes
-//         |> Seq.map (fun b -> Convert.ToString(b, 16).PadLeft(2, '0'))
-//         |> String.concat ""
-
-//     let hexMd5Hash (s: string) = 
-//         let bytes = (s.ToCharArray ()) |> Array.map byte
-//         let hash = md5.ComputeHash (bytes) |> Array.map (fun b -> Convert.ToString(b, 16).PadLeft(2, '0'))
-//         (String.concat "" hash).ToLower()
-
-//     let rec finder i = 
-//         let p = s + string i
-//         let res = hexMd5Hash p
-//         if res.StartsWith t then
-//             string i
-//         else
-//             finder (i + 1)
-
-//     finder 0
+    processed
+    |> Array.choose (fun s -> find [] (List.ofSeq s))
+    |> Array.sort
+    |> fun a -> a[a.Length / 2]

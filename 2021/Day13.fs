9138a70d9a9e58b7ee65d62afbb6055b9be22763
Chris Pritchard <chris@grislygrotto.nz>
2021-12-13 19:08:40 +1300

    finished part 1

--- 2021/Day13.fs
+++ 2021/Day13.fs
@@ -3,13 +3,30 @@ module Day13
 open Common
 open System
 
-let processed = readEmbedded "day13" 
+let processed = 
+    readEmbedded "day13" 
+    |> Array.fold (fun (points, folds) (line: string) -> 
+        if line.Contains "," then 
+            let p = split "," line
+            Set.add (int p[0], int p[1]) points, folds
+        else
+            let p = split " =" line
+            points, Array.append folds [|p[2], int p[3]|]) 
+        (Set.empty, Array.empty)
 
 let init () =
-    processed |> Array.length |> ignore
+    snd processed |> Array.length |> ignore
 
 let part1 () =
-    0
+    let (fold_axis, fold_point) = (snd processed)[0]
+    if fold_axis = "y" then
+        let (top, bottom) = fst processed |> Set.partition (fun (_, y) -> y > fold_point)
+        let result = Set.union top (bottom |> Set.map (fun (x, y) -> x, fold_point - (y - fold_point)))
+        Set.count result
+    else
+        let (left, right) = fst processed |> Set.partition (fun (x, _) -> x > fold_point)
+        let result = Set.union left (right |> Set.map (fun (x, y) -> fold_point - (x - fold_point), y))
+        Set.count result
 
 let part2 () =
     0

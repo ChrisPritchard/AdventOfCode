5a1a90cd8324cc7280cead001e7728ce5785dac5
Chris Pritchard <chris@grislygrotto.nz>
2021-12-15 08:50:35 +1300

    finished day 14

--- 2021/Day14.fs
+++ 2021/Day14.fs
@@ -21,9 +21,22 @@ let part1 () =
     res[res.Length - 1] - res[0]
     
 let part2 () =
+    let rules = snd processed
     let apply_step current =
-        // go through all rules. find matches, generates
+        let mutable next = Map.empty
+        for (k, v) in Map.toArray current do
+            match Map.tryFind k rules with
+            | None -> next <- Map.add k v next
+            | Some c -> 
+                let n1 = [|k[0];c|]
+                let n2 = [|c;k[1]|]
+                let v1 = match Map.tryFind n1 next with None -> v | Some n -> n + v
+                let v2 = match Map.tryFind n2 next with None -> v | Some n -> n + v
+                next <- next |> Map.add n1 v1 |> Map.add n2 v2
+        next
 
-    let start = (fst processed) |> Array.windowed 2 |> Array.map (fun a -> a, 1L) |> Map.ofArray
-    let res = [1..10] |> List.fold (fun c _ -> apply_step c) start |> Map.toArray |> Array.sortBy snd
-    snd res[res.Length - 1] - snd res[0]
+    let start = (fst processed) |> Array.windowed 2 |> Array.groupBy id |> Array.map (fun (a, v) -> a, int64 v.Length) |> Map.ofArray
+    let folded = [1..40] |> List.fold (fun c _ -> apply_step c) start |> Map.toArray
+    let res = folded |> Array.map (fun (k, v) -> k[1], v) |> Array.groupBy fst |> Array.map (fun (k, v) -> k, Array.map snd v |> Array.sum) |> Array.sortBy snd
+    (snd (res[res.Length - 1])) - (snd res[0])
+    

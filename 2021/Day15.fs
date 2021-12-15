7f62b8e7c69f4d8cc10fc1cbca79c4f3227632e9
Chris Pritchard <chris@grislygrotto.nz>
2021-12-15 21:57:19 +1300

    started 15

--- 2021/Day15.fs
+++ 2021/Day15.fs
@@ -3,13 +3,13 @@ module Day15
 open Common
 open System
 
-let processed = readEmbedded "day15" 
+let processed = readEmbedded "day15" |> Array.map (fun s -> s.ToCharArray() |> Array.map (fun c -> int c - int '0'))
 
 let init () =
     processed |> Array.length |> ignore
 
 let part1 () =
-    0
+    Int64.MaxValue
     
 let part2 () =
     0

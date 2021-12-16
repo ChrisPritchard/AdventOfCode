494089cb1fe45397cc2ad70bd0fb2d86702715d1
Chris Pritchard <chris@grislygrotto.nz>
2021-12-16 17:18:08 +1300

    removed redundant code

--- 2021/Day15.fs
+++ 2021/Day15.fs
@@ -11,7 +11,8 @@ let init () =
 let height = processed.Length
 let width = processed[0].Length
 
-type Heap(maxSize, comparer) =
+type PriorityQueue(maxSize, comparer) =
+    // actually just a binary heap, but same diff
     let data = Array.zeroCreate maxSize
     let mutable nextIndex = 0
 
@@ -56,11 +57,7 @@ type Heap(maxSize, comparer) =
 
         frontItem
 
-let part1 () =
-    
-    let ty = height - 1
-    let tx = width - 1
-
+let totalRisk ty tx getRisk =
     let gScores = [|0..ty|] |> Array.map (fun _ -> [|0..tx|] |> Array.map (fun _ -> Int32.MaxValue))
     let fScores = [|0..ty|] |> Array.map (fun _ -> [|0..tx|] |> Array.map (fun _ -> Int32.MaxValue))
     let h = 
@@ -76,58 +73,12 @@ let part1 () =
     gScores[0][0] <- 0
     fScores[0][0] <- h[0][0]
 
-    let openSet = Heap(width * height, fun (x1, y1) (x2, y2) -> fScores[x1][y1] - fScores[x2][y2])
+    let openSet = PriorityQueue(width * height, fun (x1, y1) (x2, y2) -> fScores[x1][y1] - fScores[x2][y2])
     openSet.Enqueue (0, 0)
 
     let mutable found = false
     while not found do
         let (cy, cx) = openSet.Dequeue()
-        if (cy, cx) = (ty, tx) then
-            found <- true
-        else
-            for (ny, nx) in n[cy][cx] do
-                let tentative_gScore = gScores[cy][cx] + processed[ny][nx]
-                if tentative_gScore < gScores[ny][nx] then
-                    gScores[ny][nx] <- tentative_gScore
-                    fScores[ny][nx] <- tentative_gScore + h[ny][nx]
-                    openSet.Enqueue(ny, nx)
-
-    gScores[ty][tx]
-    
-let part2 () =
-
-    let getRisk y x = 
-        let raw = processed[y % height][x % width]
-        let my = y / height
-        let mx = x / width
-        let n = (raw + my + mx)
-        if n > 9 then n - 9
-        else n
-
-    let ty = height * 5 - 1
-    let tx = width * 5 - 1
-
-    let gScores = [|0..ty|] |> Array.map (fun _ -> [|0..tx|] |> Array.map (fun _ -> Int32.MaxValue))
-    let fScores = [|0..ty|] |> Array.map (fun _ -> [|0..tx|] |> Array.map (fun _ -> Int32.MaxValue))
-    let h = 
-        let h (y1, x1) (y2, x2) = ((float x2 - float x1)**2 + (float y2 - float y1)**2) |> sqrt |> int
-        [|0..ty|] |> Array.map (fun y -> [|0..tx|] |> Array.map (fun x -> h (y, x) (ty, tx)))
-    let n =
-        let nextTo (y, x) = 
-            [|0,-1;-1,0;1,0;0,1|] 
-            |> Array.map (fun (dy, dx) -> y + dy, x + dx) 
-            |> Array.filter (fun (y, x) -> y >= 0 && y <= ty && x >= 0 && x <= tx)
-        [|0..ty|] |> Array.map (fun y -> [|0..tx|] |> Array.map (fun x -> nextTo (y, x)))
-
-    gScores[0][0] <- 0
-    fScores[0][0] <- h[0][0]
-
-    let openSet = Heap(width * 5 * height * 5, fun (x1, y1) (x2, y2) -> fScores[x1][y1] - fScores[x2][y2])
-    openSet.Enqueue (0, 0)
-
-    let mutable found = false
-    while not found do
-        let (cy, cx) = openSet.Dequeue ()
         if (cy, cx) = (ty, tx) then
             found <- true
         else
@@ -139,3 +90,18 @@ let part2 () =
                     openSet.Enqueue(ny, nx)
 
     gScores[ty][tx]
+
+let part1 () =
+    totalRisk (height - 1) (width - 1) (fun y x -> processed[y][x])
+    
+let part2 () =
+
+    let getRisk y x = 
+        let raw = processed[y % height][x % width]
+        let my = y / height
+        let mx = x / width
+        let n = (raw + my + mx)
+        if n > 9 then n - 9
+        else n
+
+    totalRisk (height * 5 - 1) (width * 5 - 1) getRisk

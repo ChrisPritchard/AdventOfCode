b353c7e968f48ed0e77a714aabe4a2f6992eac28
Chris Pritchard <chris@grislygrotto.nz>
2021-12-19 08:27:36 +1300

    interim rework using an array

--- 2021/Day18.fs
+++ 2021/Day18.fs
@@ -9,95 +9,65 @@ let init () =
     processed |> Array.length |> ignore
 
 let add a b =
-    let mutable n = "[" + a + "," + b + "]"
-    let mutable reduced = true
+    let mutable res = ("[" + a + "," + b + "]").ToCharArray() |> Array.map string
 
+    let explodeAt index = 
+        let left = int res[index + 1]
+        let right = int res[index + 3]
+
+        let mutable j = index - 1
+        let mutable found = false
+        while j >= 0 && not found do
+            if Char.IsDigit (res[j][0]) then
+                res[j] <- string (int res[j] + left)
+                found <- true
+        j <- index + 5
+        found <- false
+        while j < res.Length && not found do
+            if Char.IsDigit (res[j][0]) then
+                res[j] <- string (int res[j] + right)
+                found <- true
+
+        res <- Array.concat [| res[0..index-1] ; [|"0"|] ; res[index+5..] |]
+
+    let splitAt index = 
+        let value = float res[index]
+        let (left, right) = int (floor (value / 2.)), int (ceil (value / 2.))
+        res <- Array.concat [| res[0..index-1] ; [|"[";string left;",";string right;"]"|] ; res[index+1..] |]
+
+    let mutable reduced = true
     while reduced do
         reduced <- false
 
+        // test for explodes
         let mutable level = 0
-        let mutable lastRegular = -1
         let mutable i = 0
-
-        while i < n.Length && not reduced do
-            match n[i] with
-            | '[' when level < 4 -> 
+        while i < res.Length && not reduced do
+            match res[i] with
+            | "[" when level < 4 ->
                 level <- level + 1
-            | '[' when level = 4 -> 
-
-                let (left, right, nextStart) =
-                    if n[i+4] = ']' then asInt n[i+1], asInt n[i+3], 5
-                    else if n[i+2] = ',' then asInt n[i+1], int n[i+3..i+4], 6
-                    else if n[i+5] = ']' then int n[i+1..i+2], asInt n[i+4], 6
-                    else int n[i+1..i+2], int n[i+4..i+5], 7
-
-                let prev = 
-                    if lastRegular <> -1 then
-                        let lastN = asInt n[lastRegular]
-                        let newN = lastN + left
-                        n[0..(lastRegular - 1)] + string newN + n[lastRegular+1..i-1]
-                    else
-                        n[0..i-1]
-
-                let mutable next = n[i+nextStart..]
-                let mutable j = 0
-                while j < next.Length do
-                    if Char.IsDigit next[j] then
-                        if Char.IsDigit next[j + 1] then
-                            let nextN = int next[j..j+1]
-                            let newN = nextN + right
-                            next <- next[0..(j - 1)] + string newN + next[(j+2)..]
-                        else
-                            let nextN = asInt next[j]
-                            let newN = nextN + right
-                            next <- next[0..(j - 1)] + string newN + next[(j+1)..]
-                        j <- next.Length
-                    j <- j + 1
-
-                n <- prev + "0" + next
-                
+            | "[" ->
+                explodeAt i
                 reduced <- true
-
-            | ']' -> 
+            | "]" ->
                 level <- level - 1
-            | c when Char.IsDigit c && lastRegular <> i - 1 -> 
-                lastRegular <- i
-
-            | _ -> () // ignore others
-
+            | _ -> 
+                ()
             i <- i + 1
 
-        lastRegular <- -1
+        // test for splits
         i <- 0
-
-        while i < n.Length && not reduced do
-            match n[i] with
-            | c when Char.IsDigit c && lastRegular <> i - 1 -> 
-                lastRegular <- i
-            | c when Char.IsDigit c ->
-
-                // split
-                let number = float (asString [n[i-1];c])
-                let left = int (floor (number / 2.))
-                let right = int (ceil (number / 2.))
-
-                if left < 0 || right < 0 then
-                    failwith "unexpected split"
-
-                n <- n[0..i-2] + "[" + string left + "," + string right + "]" + n[(i+1)..]
+        while i < res.Length && not reduced do
+            if Char.IsDigit (res[i][0]) && int res[i] > 9 then
+                splitAt i
                 reduced <- true
-
-            | _ -> () // ignore others
-
             i <- i + 1
-        
-        printfn "%s\n" n
-    n
-
+            
+    res |> String.concat ""
 
 let part1 () =
-    processed |> Array.reduce add
-    //add "[[[[4,3],4],4],[7,[[8,4],9]]]" "[1,1]"
+    //processed |> Array.reduce add
+    add "[[[[4,3],4],4],[7,[[8,4],9]]]" "[1,1]"
 
 let part2 () =
     0

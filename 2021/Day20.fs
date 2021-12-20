21e4d9ef1cff3334cd23c1c155d3ef87a1f4df65
Chris Pritchard <chris@grislygrotto.nz>
2021-12-20 19:14:17 +1300

    finished day 20

--- 2021/Day20.fs
+++ 2021/Day20.fs
@@ -8,38 +8,54 @@ let processed = readEmbedded "day20"
 let init () =
     processed |> Array.length |> ignore
 
-let part1 () =
-    let algo = processed[0].ToCharArray()
+let algo = processed[0].ToCharArray()
 
+let index first (x, y) m =
+    let defaultChar = if not first && algo[0] = '#' then '1' else '0'
+    [|x-1, y-1; x, y-1; x+1, y-1; x-1, y; x,y; x+1, y; x-1, y+1; x, y+1; x+1, y+1|]
+    |> Array.map (fun p -> match Map.tryFind p m with Some '#' -> '1' | Some '.' -> '0' | _ -> defaultChar)
+    |> asString
+    |> fun s -> Convert.ToInt32(s, 2)
+
+let enhance first m = 
+    m |> Map.fold (fun c k _ -> Map.add k (algo[index first k m]) c) Map.empty
+
+// used for debugging
+// let render m = 
+//     let mutable res = "\n"
+//     let a = Map.toArray m
+//     let xrange = a |> Array.map (fst >> fst) |> Array.sort
+//     let yrange = a |> Array.map (fst >> snd) |> Array.sort
+//     for y in [yrange[0]..yrange[yrange.Length - 1]] do
+//         for x in [xrange[0]..xrange[xrange.Length - 1]] do
+//             match Map.tryFind (x, y) m with 
+//             | Some '#' -> res <- res + "#"
+//             | _ -> res <- res + "."
+//         res <- res + "\n"
+//     res
+
+let part1 () =
+    
     let pic = 
-        processed[1..] 
-        |> Array.mapi (fun y s -> s.ToCharArray() |> Array.mapi (fun x c -> (x, y), c))
+        let rowLen = processed[1].Length
+        let headerFooter = [|Array.create (rowLen + 4) '.' |> asString|]
+        Array.concat [|headerFooter; headerFooter; processed[1..]; headerFooter; headerFooter|]
+        |> Array.mapi (fun y s -> (".." + s + "..").ToCharArray() |> Array.mapi (fun x c -> (x, y), c))
         |> Array.collect id
         |> Map.ofArray
-
-    let index (x, y) m =
-        [|x-1, y-1; x, y-1; x+1, y+1; x-1, y; x,y; x+1, y; x-1, y+1; x, y+1; x+1, y+1|]
-        |> Array.map (fun p -> match Map.tryFind p m with Some '#' -> '1' | _ -> '0')
-        |> asString
-        |> fun s -> Convert.ToInt32(s, 2)
-
-    let enhance m = 
-        m |> Map.fold (fun c k _ -> Map.add k (algo[index k m]) c) Map.empty
-
-    let render m = 
-        let mutable res = ""
-        let a = Map.toArray m
-        let xrange = a |> Array.map (fst >> fst) |> Array.sort
-        let yrange = a |> Array.map (fst >> snd) |> Array.sort
-        for y in yrange do
-            for x in xrange do
-                match Map.tryFind (x, y) m with 
-                | Some '#' -> res <- res + "#"
-                | _ -> res <- res + "."
-            res <- res + "\n"
-        res
-
-    pic |> enhance |> enhance |> render
+  
+    pic |> enhance true |> enhance false |> Map.toArray |> Array.filter (fun (_,c) -> c = '#') |> Array.length
 
 let part2 () =
-    0
+    
+    let pic = 
+        let rowLen = processed[1].Length
+        let headerFooter = Array.create 50 (Array.create (rowLen + 100) '.' |> asString)
+        let prefixPostfix = Array.create 50 '.' |> asString
+        Array.concat [|headerFooter; processed[1..]; headerFooter|]
+        |> Array.mapi (fun y s -> (prefixPostfix + s + prefixPostfix).ToCharArray() |> Array.mapi (fun x c -> (x, y), c))
+        |> Array.collect id
+        |> Map.ofArray
+  
+    [1..50] |> List.fold (fun m i -> enhance (i % 2 = 1) m) pic
+    |> Map.toArray |> Array.filter (fun (_,c) -> c = '#') |> Array.length

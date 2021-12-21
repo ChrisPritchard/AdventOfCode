251422622e05f09786da687d0377b5409ee37565
Chris Pritchard <chris@grislygrotto.nz>
2021-12-21 17:56:04 +1300

    prep for day 21

---
+++ 2021/Day21.fs
@@ -0,0 +1,15 @@
+module Day21
+
+open Common
+open System
+
+let processed = readEmbedded "day21"
+
+let init () =
+    processed |> Array.length |> ignore
+
+let part1 () =
+    0
+
+let part2 () =
+    0
--- 2021/Program.fs
+++ 2021/Program.fs
@@ -10,105 +10,110 @@ let main argv =
     
     let mutable total = 0.0
     
-    Day01.init()
+    // Day01.init()
     
-    total <- total + timeForDay 1 1 Day01.part1
-    total <- total + timeForDay 1 2 Day01.part2
+    // total <- total + timeForDay 1 1 Day01.part1
+    // total <- total + timeForDay 1 2 Day01.part2
     
-    Day02.init()
+    // Day02.init()
         
-    total <- total + timeForDay 2 1 Day02.part1
-    total <- total + timeForDay 2 2 Day02.part2
+    // total <- total + timeForDay 2 1 Day02.part1
+    // total <- total + timeForDay 2 2 Day02.part2
     
-    Day03.init()
+    // Day03.init()
         
-    total <- total + timeForDay 3 1 Day03.part1
-    total <- total + timeForDay 3 2 Day03.part2
+    // total <- total + timeForDay 3 1 Day03.part1
+    // total <- total + timeForDay 3 2 Day03.part2
     
-    Day04.init()
+    // Day04.init()
         
-    total <- total + timeForDay 4 1 Day04.part1
-    total <- total + timeForDay 4 2 Day04.part2
+    // total <- total + timeForDay 4 1 Day04.part1
+    // total <- total + timeForDay 4 2 Day04.part2
     
-    Day05.init()
+    // Day05.init()
         
-    total <- total + timeForDay 5 1 Day05.part1
-    total <- total + timeForDay 5 2 Day05.part2
+    // total <- total + timeForDay 5 1 Day05.part1
+    // total <- total + timeForDay 5 2 Day05.part2
     
-    Day06.init()
+    // Day06.init()
         
-    total <- total + timeForDay 6 1 Day06.part1
-    total <- total + timeForDay 6 2 Day06.part2
+    // total <- total + timeForDay 6 1 Day06.part1
+    // total <- total + timeForDay 6 2 Day06.part2
     
-    Day07.init()
+    // Day07.init()
         
-    total <- total + timeForDay 7 1 Day07.part1
-    total <- total + timeForDay 7 2 Day07.part2
+    // total <- total + timeForDay 7 1 Day07.part1
+    // total <- total + timeForDay 7 2 Day07.part2
     
-    Day08.init()
+    // Day08.init()
         
-    total <- total + timeForDay 8 1 Day08.part1
-    total <- total + timeForDay 8 2 Day08.part2
+    // total <- total + timeForDay 8 1 Day08.part1
+    // total <- total + timeForDay 8 2 Day08.part2
     
-    Day09.init()
+    // Day09.init()
         
-    total <- total + timeForDay 9 1 Day09.part1
-    total <- total + timeForDay 9 2 Day09.part2
+    // total <- total + timeForDay 9 1 Day09.part1
+    // total <- total + timeForDay 9 2 Day09.part2
     
-    Day10.init()
+    // Day10.init()
         
-    total <- total + timeForDay 10 1 Day10.part1
-    total <- total + timeForDay 10 2 Day10.part2
+    // total <- total + timeForDay 10 1 Day10.part1
+    // total <- total + timeForDay 10 2 Day10.part2
     
-    Day11.init()
+    // Day11.init()
         
-    total <- total + timeForDay 11 1 Day11.part1
-    total <- total + timeForDay 11 2 Day11.part2
+    // total <- total + timeForDay 11 1 Day11.part1
+    // total <- total + timeForDay 11 2 Day11.part2
     
-    Day12.init()
+    // Day12.init()
         
-    total <- total + timeForDay 12 1 Day12.part1
-    total <- total + timeForDay 12 2 Day12.part2
+    // total <- total + timeForDay 12 1 Day12.part1
+    // total <- total + timeForDay 12 2 Day12.part2
     
-    Day13.init()
+    // Day13.init()
         
-    total <- total + timeForDay 13 1 Day13.part1
-    total <- total + timeForDay 13 2 Day13.part2
+    // total <- total + timeForDay 13 1 Day13.part1
+    // total <- total + timeForDay 13 2 Day13.part2
     
-    Day14.init()
+    // Day14.init()
         
-    total <- total + timeForDay 14 1 Day14.part1
-    total <- total + timeForDay 14 2 Day14.part2
+    // total <- total + timeForDay 14 1 Day14.part1
+    // total <- total + timeForDay 14 2 Day14.part2
     
-    Day15.init()
+    // Day15.init()
         
-    total <- total + timeForDay 15 1 Day15.part1
-    total <- total + timeForDay 15 2 Day15.part2
+    // total <- total + timeForDay 15 1 Day15.part1
+    // total <- total + timeForDay 15 2 Day15.part2
     
-    Day16.init()
+    // Day16.init()
         
-    total <- total + timeForDay 16 1 Day16.part1
-    total <- total + timeForDay 16 2 Day16.part2
+    // total <- total + timeForDay 16 1 Day16.part1
+    // total <- total + timeForDay 16 2 Day16.part2
     
-    Day17.init()
+    // Day17.init()
         
-    total <- total + timeForDay 17 1 Day17.part1
-    total <- total + timeForDay 17 2 Day17.part2
+    // total <- total + timeForDay 17 1 Day17.part1
+    // total <- total + timeForDay 17 2 Day17.part2
     
-    Day18.init()
+    // Day18.init()
         
-    total <- total + timeForDay 18 1 Day18.part1
-    total <- total + timeForDay 18 2 Day18.part2
+    // total <- total + timeForDay 18 1 Day18.part1
+    // total <- total + timeForDay 18 2 Day18.part2
     
     Day19.init()
         
     total <- total + timeForDay 19 1 Day19.part1
     total <- total + timeForDay 19 2 Day19.part2
     
-    Day20.init()
+    // Day20.init()
         
-    total <- total + timeForDay 20 1 Day20.part1
-    total <- total + timeForDay 20 2 Day20.part2
+    // total <- total + timeForDay 20 1 Day20.part1
+    // total <- total + timeForDay 20 2 Day20.part2
+    
+    Day21.init()
+        
+    total <- total + timeForDay 21 1 Day21.part1
+    total <- total + timeForDay 21 2 Day21.part2
 
     printfn ""
     printfn "total time elapsed: %f ms" total

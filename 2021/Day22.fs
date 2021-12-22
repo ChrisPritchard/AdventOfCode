13bfc6bbcc96dad565345e0b0b3a35745fd2413a
Christopher Pritchard <chrispritchard.nz@gmail.com>
2021-12-23 08:12:56 +1300

    couple of util functions

--- 2021/Day22.fs
+++ 2021/Day22.fs
@@ -9,9 +9,17 @@ let init () =
     processed |> Array.length |> ignore
 
 let coords = processed |> Array.map (fun s -> 
-    s.Replace("on", "1").Replace("off","0") |> split " x=.,yz" |> Array.map int)
+    s.Replace("on", "1").Replace("off","0") 
+    |> split " x=.,yz" 
+    |> Array.map int 
+    |> fun a -> a[0] = 1, (a[1],a[2],a[3],a[4],a[4],a[5]))
+
+let size (x1,x2,y1,y2,z1,z2) =
+    (x2-x1) * (y2-y1) * (z2-z1)
 
 let part1 () =
+    let cap (x1,x2,y1,y2,z1,z2) =
+        max -50 x1, min 50 x2, max -50 y1, min 50 y2, max -50 z1, min 50 z2
     coords
 
 let part2 () =

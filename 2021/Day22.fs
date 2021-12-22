05acab8357924f6f0c45ac1a0202aa701c4c4f92
Christopher Pritchard <chrispritchard.nz@gmail.com>
2021-12-23 10:44:39 +1300

    more work

--- 2021/Day22.fs
+++ 2021/Day22.fs
@@ -17,10 +17,25 @@ let coords = processed |> Array.map (fun s ->
 let size (x1,x2,y1,y2,z1,z2) =
     (x2-x1) * (y2-y1) * (z2-z1)
 
+let intersect (x1,x2,y1,y2,z1,z2) (xx1,xx2,yy1,yy2,zz1,zz2) =
+    if x1 > xx2 || y1 > yy2 || z1 > zz2 || x2 < xx1 || y2 < yy1 || z2 < zz1 then
+        0, 0, 0, 0, 0, 0 // no overlap
+    else
+        max x1 xx1, min x2 xx2, max y1 yy1, min y2 yy2, max z1 zz1, min z2 zz2
+
 let part1 () =
     let cap (x1,x2,y1,y2,z1,z2) =
         max -50 x1, min 50 x2, max -50 y1, min 50 y2, max -50 z1, min 50 z2
-    coords
+    
+    let mutable count = 0
+    for i = [0..coords.Length - 1] do
+        let (on, coord) = coords[i]
+        let coord = cap coord
+        let sized = size coord
+        if on then
+            // go through previous 'on's and find intersections
+            // only add for size - intersection
+            // if there is an 
 
 let part2 () =
     0

da154944522605b103807870cb2346b473b01e7a
Chris Pritchard <chris@grislygrotto.nz>
2022-02-14 12:09:01 +1300

    finished day 22 parts 1 and 2

--- 2021/Day22.fs
+++ 2021/Day22.fs
@@ -17,29 +17,29 @@ let coords = processed |> Array.map (fun s ->
     |> fun a -> a[0] = 1, (a[1],a[2],a[3],a[4],a[5],a[6]))
 
 let size ((x1,x2,y1,y2,z1,z2): Coord) =
-    (x2-x1) * (y2-y1) * (z2-z1)
+    (x2-x1+1L) * (y2-y1+1L) * (z2-z1+1L)
 
 let rec cull shapes target =
     let (tx1,tx2,ty1,ty2,tz1,tz2) = target
     shapes
     |> Array.collect (fun shape ->
         let (x1,x2,y1,y2,z1,z2) = shape
-        if x2 <= tx1 || y2 <= ty1 || z2 <= tz1 || x1 >= tx2 || y1 >= ty2 || z1 >= tz2 then
+        if x2 < tx1 || y2 < ty1 || z2 < tz1 || x1 > tx2 || y1 > ty2 || z1 > tz2 then
             [|shape|]   // completely outside
         else if x1 >= tx1 && x2 <= tx2 && y1 >= ty1 && y2 <= ty2 && z1 >= tz1 && z2 <= tz2 then
             Array.empty // completely inside
         else if x1 < tx1 then
-            Array.append [|x1, tx1, y1, y2, z1, z2|] (cull [|tx1, x2, y1, y2, z1, z2|] target)
+            Array.append [|x1, tx1-1L, y1, y2, z1, z2|] (cull [|tx1, x2, y1, y2, z1, z2|] target)
         else if x2 > tx2 then
-            Array.append [|tx2, x2, y1, y2, z1, z2|] (cull [|x1, tx2, y1, y2, z1, z2|] target)
+            Array.append [|tx2+1L, x2, y1, y2, z1, z2|] (cull [|x1, tx2, y1, y2, z1, z2|] target)
         else if y1 < ty1 then
-            Array.append [|x1, x2, y1, ty1, z1, z2|] (cull [|x1, x2, ty1, y2, z1, z2|] target)
+            Array.append [|x1, x2, y1, ty1-1L, z1, z2|] (cull [|x1, x2, ty1, y2, z1, z2|] target)
         else if y2 > ty2 then
-            Array.append [|x1, x2, ty2, y2, z1, z2|] (cull [|x1, x2, y1, ty2, z1, z2|] target)
+            Array.append [|x1, x2, ty2+1L, y2, z1, z2|] (cull [|x1, x2, y1, ty2, z1, z2|] target)
         else if z1 < tz1 then
-            Array.append [|x1, x2, y1, y2, z1, tz1|] (cull [|x1, x2, y1, y2, tz1, z2|] target)
+            Array.append [|x1, x2, y1, y2, z1, tz1-1L|] (cull [|x1, x2, y1, y2, tz1, z2|] target)
         else if z2 > tz2 then
-            Array.append [|x1, x2, y1, y2, tz2, z2|] (cull [|x1, x2, y1, y2, z1, tz2|] target)
+            Array.append [|x1, x2, y1, y2, tz2+1L, z2|] (cull [|x1, x2, y1, y2, z1, tz2|] target)
         else
             failwith "should not be reached")
 
@@ -55,6 +55,10 @@ let instructionFolder acc (on, coord) =
 let part1 () =
 
     coords
+    |> Array.filter (fun (_, (x1,x2,y1,y2,z1,z2)) -> 
+        not (x1 > 50L || x2 < -50L || y1 > 50L || y2 < -50L || z1 > 50L || z2 < -50L))
+    |> Array.map (fun (on, (x1,x2,y1,y2,z1,z2)) ->
+        on, (max x1 -50L, min x2 50L, max y1 -50L, min y2 50L, max z1 -50L, min z2 50L))
     |> Array.fold instructionFolder Array.empty
     |> Array.sumBy size
 

0197da104bfe674fab9e0d75dda4d299a7782349
Chris Pritchard <chris@grislygrotto.nz>
2022-02-14 08:36:42 +1300

    experiments reworking this so its more efficient (and works)

--- 2021/Day22.fs
+++ 2021/Day22.fs
@@ -20,81 +20,46 @@ let size ((x1,x2,y1,y2,z1,z2): Coord) =
     (x2-x1) * (y2-y1) * (z2-z1)
 
 let part1 () =
-    let cap (x1,x2,y1,y2,z1,z2) =
-        max -50L x1, min 50L x2, max -50L y1, min 50L y2, max -50L z1, min 50L z2
 
-    let rec cull (toSplit: Coord) (against: Coord) keepInner  =
-        let (x1,x2,y1,y2,z1,z2) = toSplit
-        let (xx1,xx2,yy1,yy2,zz1,zz2) = against
-        if x1 > xx2 || y1 > yy2 || z1 > zz2 || x2 < xx1 || y2 < yy1 || z2 < zz1 then
-            if keepInner then
-                [|toSplit; against|] // no overlap
+    let rec cull shapes target =
+        let (tx1,tx2,ty1,ty2,tz1,tz2) = target
+        shapes
+        |> Array.collect (fun shape ->
+            let (x1,x2,y1,y2,z1,z2) = shape
+            if x2 <= tx1 || y2 <= ty1 || z2 <= tz1 || x1 >= tx2 || y1 >= ty2 || z1 >= tz2 then
+                [|shape|]   // completely outside
+            else if x1 >= tx1 && x2 <= tx2 && y1 >= ty1 && y2 <= ty2 && z1 >= tz1 && z2 <= tz2 then
+                Array.empty // completely inside
+            else if x1 < tx1 && x2 <= tx2 then
+                Array.append [|x1, tx1, y1, y2, z1, z2|] (cull [|tx1, x2, y1, y2, z1, z2|] target)
+            else if x2 > tx2 && x1 >= tx1 then
+                Array.append [|tx2, x2, y1, y2, z1, z2|] (cull [|x1, tx2, y1, y2, z1, z2|] target)
+            else if y1 < ty1 && y2 <= ty2 then
+                Array.append [|x1, x2, y1, ty1, z1, z2|] (cull [|x1, x2, ty1, y2, z1, z2|] target)
+            else if y2 > ty2 && y1 >= ty1 then
+                Array.append [|x1, x2, ty2, y2, z1, z2|] (cull [|x1, x2, y1, ty2, z1, z2|] target)
+            else if z1 < tz1 && x2 <= tz2 then
+                Array.append [|x1, x2, y1, y2, z1, tz1|] (cull [|x1, x2, y1, y2, tz1, z2|] target)
+            else if z2 > tz2 && z1 >= tz1 then
+                Array.append [|x1, x2, y1, y2, tz2, z2|] (cull [|x1, x2, y1, y2, z1, tz2|] target)
             else
-                [|toSplit|]
-        else if x1 < xx1 then
-            Array.append [|x1,xx1,y1,y2,z1,z2|] (cull (xx1,x2,y1,y2,z1,z2) against keepInner)
-        else if x2 > xx2 then
-            Array.append [|xx2,x2,y1,y2,z1,z2|] (cull (x1,xx2,y1,y2,z1,z2) against keepInner)
-        else if y1 < yy1 then
-            Array.append [|x1,x2,y1,yy1,z1,z2|] (cull (x1,x2,yy1,y2,z1,z2) against keepInner)
-        else if y2 > yy2 then
-            Array.append [|x2,x2,yy2,y2,z1,z2|] (cull (x1,x2,y1,yy2,z1,z2) against keepInner) 
-        else if z1 < zz1 then
-            Array.append [|x1,x2,y1,y2,z1,zz1|] (cull (x1,x2,y1,y2,zz1,z2) against keepInner) 
-        else if z2 > zz2 then
-            Array.append [|x1,x2,y1,y2,zz2,z2|] (cull (x1,x2,y1,y2,z1,zz2) against keepInner) 
-        else if keepInner then
-            [|against|] // assume toSplit is inside against
-        else
-            Array.empty
-
-    let rec remove (toRemove: Coord) (from: Coord)  =
-        let (x1,x2,y1,y2,z1,z2) = toRemove
-        let (xx1,xx2,yy1,yy2,zz1,zz2) = from
-        if x1 > xx2 || y1 > yy2 || z1 > zz2 || x2 < xx1 || y2 < yy1 || z2 < zz1 then
-            [|from|] // no overlap
-        else if x1 < xx1 then
-            remove (xx1,x2,y1,y2,z1,z2) from
-        else if x2 > xx2 then
-            remove (x1,xx2,y1,y2,z1,z2) from
-        else if y1 < yy1 then
-            remove (x1,x2,yy1,y2,z1,z2) from
-        else if y2 > yy2 then
-            remove (x1,x2,y1,yy2,z1,z2) from
-        else if z1 < zz1 then
-            remove (x1,x2,y1,y2,zz1,z2) from
-        else if z2 > zz2 then
-            remove (x1,x2,y1,y2,z1,zz2) from
-        else
-            // if toRemove is inside... do the split op but discard the inner
-            cull from toRemove false
-
-    let rec noOverlaps shape (others: Coord[]) =
-        let mutable j = 0
-        let mutable lastCull = Array.empty
-        while j < others.Length && lastCull.Length <= 2 do
-            lastCull <- cull shape others[j] true
-            j <- j + 1
-        if lastCull.Length = 1 then
-            others
-        else if lastCull.Length = 2 then
-            Array.append others [|lastCull[1]|]
-        else
-            (lastCull[0..lastCull.Length - 2], others)
-            ||> Array.fold (fun others shape -> noOverlaps shape others)
-
-    let all = 
-        coords
-        |> Array.fold (fun (acc: Coord[]) (on, coord: Coord) ->
-            if on then 
-                if Array.isEmpty acc then
-                    [|coord|]
-                else
-                    noOverlaps coord acc
-            else
-                Array.collect (fun o -> remove coord o) acc) Array.empty
+                failwith "should not be reached")
     
-    all |> Array.sumBy size
+    let instructionFolder acc (on, coord) =
+        if on && Array.isEmpty acc then [|coord|]
+        else
+            if on then 
+                Array.fold cull [|coord|] acc
+            else
+                Array.fold cull acc [|coord|]
+
+    let cap (on, (x1,x2,y1,y2,z1,z2)) =
+        on, (max -50L x1, min 50L x2, max -50L y1, min 50L y2, max -50L z1, min 50L z2)
+
+    coords
+    |> Array.map cap
+    |> Array.fold instructionFolder Array.empty
+    |> Array.sumBy size
 
 let part2 () =
     0

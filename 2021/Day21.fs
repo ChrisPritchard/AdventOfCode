441602432cb31acb99e02149b3d312c1be49094c
Chris Pritchard <chris@grislygrotto.nz>
2022-02-13 22:28:42 +1300

    finished part 2 day 21

--- 2021/Day21.fs
+++ 2021/Day21.fs
@@ -34,28 +34,42 @@ let part1 () =
     play 0 1 starts[0] starts[1] 0 0
 
 let part2 () =
-    // dice can fall in 27 different ways
-    // a mere eight roles will result 282 billion possibilities, so arrays are out of the question
-    // math might make it easier:
-        // each player will likely win in two or three rounds
-        // in the 27 dice options, there are only 7 values: 3 - 9
-        // for a given starting position, this can result in 7 scores after round one, e.g.
-            // if p1 starts at 4, their score can be 7,8,9,10,1,2,3 after round 1
-            // after round 2 this becomes:
-                // from 7: 17,8,9,10,11,12,13
-                // from 8: 9,10,11,12,13,14,15
-                // from 9: 11,12,13,14,15,16,17
-                // from 10: 13,14,15,16,17,18,19
-                // from 1: 5,6,7,8,9,10,11
-                // from 2: 7,8,9,10,11,12,3
-                // from 3: 9,10,11,12,13,4,5
-            // after round 3...
 
-    // another approach might be to calculate all possible positions from a given position
-    // 1: 4,5,6,7,8,9,10
-    // 2: 5,6,7,8,9,10,1
-    // 3: 6,7,8,9,10,1,2
-    // with this it can be calculated from whatever position, in how many possibilities they will have what score...
-    // 
+    // memoize dice, p1pos, p2pos, p1score, p2score to win counts for each
+    let dict = System.Collections.Generic.Dictionary<int * int * int * int * int * int, int64 * int64>()
 
-    0
+    let rec play idx acc p1 p2 p1s p2s = 
+        if dict.ContainsKey((idx, acc, p1, p2, p1s, p2s)) then
+            dict[(idx, acc, p1, p2, p1s, p2s)]
+        else
+            let res = 
+                if p1s >= 21 then 1L, 0L
+                else if p2s >= 21 then 0L, 1L
+                else if idx <> 2 && idx <> 5 then
+                    let (q1p1, q1p2) = play (idx + 1) (acc + 1) p1 p2 p1s p2s
+                    let (q2p1, q2p2) = play (idx + 1) (acc + 2) p1 p2 p1s p2s
+                    let (q3p1, q3p2) = play (idx + 1) (acc + 3) p1 p2 p1s p2s
+                    q1p1 + q2p1 + q3p1, q1p2 + q2p2 + q3p2
+                else
+                    let p = if idx < 3 then p1 else p2
+                    let cap v = if v > 10 then v - 10 else v
+                    let s1 = cap (p + acc + 1)
+                    let s2 = cap (p + acc + 2)
+                    let s3 = cap (p + acc + 3)
+                    let nd = (idx + 1) % 6
+                    if idx < 3 then
+                        let (q1p1, q1p2) = play nd 0 s1 p2 (p1s + s1) p2s
+                        let (q2p1, q2p2) = play nd 0 s2 p2 (p1s + s2) p2s
+                        let (q3p1, q3p2) = play nd 0 s3 p2 (p1s + s3) p2s
+                        q1p1 + q2p1 + q3p1, q1p2 + q2p2 + q3p2
+                    else
+                        let (q1p1, q1p2) = play nd 0 p1 s1 p1s (p2s + s1)
+                        let (q2p1, q2p2) = play nd 0 p1 s2 p1s (p2s + s2)
+                        let (q3p1, q3p2) = play nd 0 p1 s3 p1s (p2s + s3)
+                        q1p1 + q2p1 + q3p1, q1p2 + q2p2 + q3p2
+            dict.Add((idx, acc, p1, p2, p1s, p2s), res)
+            res
+
+    let p1, p2 = 
+        play 0 0 starts[0] starts[1] 0 0
+    if p1 > p2 then p1 else p2

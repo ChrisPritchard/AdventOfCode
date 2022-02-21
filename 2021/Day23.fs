5f2c061a366f692e7cb08c7eefb64c39321faf89
Chris Pritchard <chris@grislygrotto.nz>
2022-02-22 10:56:21 +1300

    first test

--- 2021/Day23.fs
+++ 2021/Day23.fs
@@ -27,6 +27,18 @@ let part1 () =
     let effectivePos col = 
         let neutral = Char.ToLower col
         1.5 + float (int neutral - int 'a')
+    
+    let dist start target = 
+        let col = 
+            if Char.IsUpper start || Char.IsUpper target then 1.5
+            else 0.5
+        let start = if Char.IsLetter start then effectivePos start else float start - float '0'
+        let target = if Char.IsLetter target then effectivePos target else float target - float '0'
+        if target > start then (target - start) + col
+        else (start - target) + col
+
+    let costByDist index start target = 
+        dist start target |> int |> cost index
 
     let blocked start target line =
         let start = if Char.IsLetter start then effectivePos start else float start
@@ -38,25 +50,41 @@ let part1 () =
             (start < target && digit <= target && digit > start)
             || (target < start && digit < start && digit >= target))
 
-    let dist start target = 
-        let col = 
-            if Char.IsUpper start || Char.IsUpper target then 1.5
-            else 0.5
-        let start = if Char.IsLetter start then effectivePos start else float start
-        let target = if Char.IsLetter target then effectivePos target else float target
-        if target > start then (target - start) + col
-        else (start - target) + col
+    let newLine (line: string) index (newPos: char) =
+        line[0..index-1] + Char.ToString newPos + line[index+1..]
 
     let next (line: string) =
-        []
+        [|0..7|] |> Array.collect (fun index ->
+            let targetCol = char ((index / 2) + int 'a')
+            let c = line[index]
+            if Char.IsLetter c then
+                if c = Char.ToUpper targetCol then Array.empty // current index is in right place
+                else if c = targetCol && index % 2 = 1 && line[index - 1] = Char.ToUpper targetCol then Array.empty // ditto
+                else 
+                    [|'0'..'6'|] 
+                    |> Array.filter (fun target -> not (blocked c target line))
+                    |> Array.map (fun target -> 
+                        costByDist index c target, newLine line index target)
+            else
+                if blocked c targetCol line then Array.empty
+                else
+                    let upper = Char.ToUpper targetCol
+                    if not (line.Contains(Char.ToString upper)) then 
+                        [|costByDist index c upper, newLine line index upper|] // col is empty
+                    else if line.Contains(Char.ToString targetCol) then Array.empty // col is full
+                    else if line.IndexOf (Char.ToString upper) = index / 2 then 
+                        [|costByDist index c targetCol, newLine line index targetCol|] // top of col is target
+                    else Array.empty)
 
         // collect options for each char, track with cost and new line
         // collect options for each new line
         // ultimately if a line is a success then it returns cost
+
         // let options = collect options.
         // if options empty, then none
         // else options choose next options, min
         // memoize result?
+        // could be line -> options -> map to next -> min or none
 
         // if up then
             // test path to col, if blocked then no target
@@ -69,15 +97,7 @@ let part1 () =
             // each up not blocked should be tested
             // for each check if path to position is blocked
 
-        // each given start state can calculate options, the cost to reach said option, and then recurse, trying to find the min cost
-        
-
-        // check each pair, keeping track of index
-        // for each pair, calculate positions, index determines where it should be, up or col determines what section
-
-    // the result of each possible move can be run as a new line with a cost
-    // victory is simply matching A0A1B0B1C0C1D0D1    
-    0
+    next "ADacbCBd"
 
 let part2 () =
     

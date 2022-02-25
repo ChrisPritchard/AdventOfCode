cdd3544a3bfaf97aa770851066247786d788eac0
Christopher Pritchard <chrispritchard.nz@gmail.com>
2022-02-25 21:14:15 +1300

    work on revising to be adaptable

--- 2021/Day23.fs
+++ 2021/Day23.fs
@@ -3,6 +3,11 @@ module Day23
 open System
 open System.Collections.Generic
 
+// need to remake this to be more adaptive to longer cols
+// switch from 1 char to two? T and index, A/B/C/D and index?
+// bDACBdac becomes B1D2A2C2B2D1A1C1
+// target would be A1A2B1B2C1C2D1D2 or AABBCCDD with numbers removed
+
 //let processed = "ADacbCBd" // test data
 let processed = "bDACBdac" // my data
 
@@ -11,62 +16,65 @@ let init () =
 
 let part1 () =
 
-    let cost index dist = (pown 10 (index / 2)) * dist
+    let start = "b1d2a2c2b2d1a1c1"
+    let colDepth = 2
 
-    let effectivePos col = 
-        let neutral = Char.ToLower col
-        2 + (int neutral - int 'a')*2
+    let cost (cat: char) dist = (pown 10 (int cat - int 'a')) * dist
+
+    let effectivePos (col, _) = 
+        2 + (int col - int 'a')*2
     
-    let isCol c = Char.IsLetter c && c <> 'x'
-    let top c = if Char.IsLetter c then 10 else int c - int '0'
+    let isCol (cat, _) = cat <> 't'
+    let pos (_, c) = if Char.IsLetter c then 10 else int c - int '0'
+    let posChar i = if i = 10 then 'x' else char (int '0' + i)
 
     let dist start target = 
         let colStart = 
             if not (isCol start) then 0
-            else if Char.IsUpper start then 2
-            else 1
+            else pos start
         let colTarget = 
             if not (isCol target) then 0
-            else if Char.IsUpper target then 2
-            else 1
-        let start = if isCol start then effectivePos start else top start
-        let target = if isCol target then effectivePos target else top target
+            else pos target
+        let start = if isCol start then effectivePos start else pos start
+        let target = if isCol target then effectivePos target else pos target
         if target > start then (target - start) + colStart + colTarget
         else (start - target) + colStart + colTarget
 
-    let costByDist index start target = 
-        dist start target |> int |> cost index
+    let costByDist cat start target = 
+        dist start target |> cost cat
+    
+    let parts (line: string) =
+        line |> Seq.toArray |> Array.chunkBySize 2 |> Array.map (fun a -> a[0], a[1])
 
     let blocked start target (line: string) =
-        let sv = if isCol start then effectivePos start else top start
-        let tv = if isCol target then effectivePos target else top target
+        let sv = if isCol start then effectivePos start else pos start
+        let tv = if isCol target then effectivePos target else pos target
+        let line = parts line
         let topBlocked = 
             line 
             |> Seq.filter (isCol >> not)
-            |> Seq.map top
+            |> Seq.map pos
             |> Seq.exists (fun digit -> 
                 (sv < tv && digit <= tv && digit > sv)
                 || (tv < sv && digit < sv && digit >= tv))
         if topBlocked then true
         else
-            if Char.IsUpper start && line.Contains (Char.ToLower start) then true
-            else if Char.IsUpper target && line.Contains (Char.ToLower target) then true
-            else if isCol target && line.Contains target then true
+            if isCol start && Array.exists (fun p -> fst p = fst start && pos p < pos start) line then true
+            else if isCol target && Array.exists (fun p -> fst p = fst target && pos p < pos target) line then true
             else false
 
-    let newLine (line: string) index (newPos: char) =
-        line[0..index-1] + Char.ToString newPos + line[index+1..]
+    let newLine (line: string) index (newCat: char, newPos: char) =
+        line[0..(index-1)*2] + Char.ToString newCat + Char.ToString newPos + line[(index+1)*2..]
 
     let nextMemo = Dictionary<string, (int * string)[]>()
 
     let next (line: string) =
+        let parts = parts line
         [|0..7|] |> Array.collect (fun index ->
-            let targetCol = char ((index / 2) + int 'a')
-            let upper = Char.ToUpper targetCol
-            let c = line[index]
+            let cat = char ((index / 2) + int 'a')
+            let c = parts[index]
             if isCol c then
-                if c = upper then Array.empty // current index is in right place
-                else if c = targetCol && ((index % 2 = 0 && line[index + 1] = upper) || (index % 2 = 1 && line[index - 1] = upper)) then Array.empty // ditto
+                if fst c = cat && [pos c..colDepth] |> List.forall (fun i -> Array.contains (cat, posChar i) parts) then Array.empty // current index is in right place
                 else if not (blocked c upper line) then 
                     [|costByDist index c upper, newLine line index upper|]
                 else if not (blocked c targetCol line) then
@@ -116,7 +124,7 @@ let part1 () =
                         else None)
                 minToWin lowestCost (Array.append nextOptions rem)
 
-    minToWin Int32.MaxValue [|0, processed|]
+    minToWin Int32.MaxValue [|0, start|]
 
 let part2 () =
     0

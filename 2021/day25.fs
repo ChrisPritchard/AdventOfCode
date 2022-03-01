475fa470afe68626ab916a74d051c35f0a76b251
Chris Pritchard <chris@grislygrotto.nz>
2022-03-01 16:23:00 +1300

    prep for day 25

---
+++ 2021/day25.fs
@@ -0,0 +1,17 @@
+module Day25
+
+open Common
+open System
+
+let processed = readEmbedded "day25"
+
+let init () =
+    processed |> Array.length |> ignore
+
+let part1 () =
+
+    0
+
+let part2 () =
+    
+    0

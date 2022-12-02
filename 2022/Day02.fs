395a28db4f407ad7c714ead170942879f81f580b
Christopher <chris@grislygrotto.nz>
2022-12-03 08:48:28 +1300

    fixed my logic bug for day 2 part 2 paper is 2 score, not 3, and vice versa for scissors

--- 2022/Day02.fs
+++ 2022/Day02.fs
@@ -25,14 +25,14 @@ let part2 () =
     ||> 
         Seq.fold (fun s (v: string) ->
             match v with
-            | "A X" -> s + 2 + 0 // rock scissors, lose
+            | "A X" -> s + 3 + 0 // rock scissors, lose
             | "A Y" -> s + 1 + 3 // rock rock, draw
-            | "A Z" -> s + 3 + 6 // rock paper, win
+            | "A Z" -> s + 2 + 6 // rock paper, win
             | "B X" -> s + 1 + 0 // paper rock, lose
-            | "B Y" -> s + 3 + 3 // paper paper, draw
-            | "B Z" -> s + 2 + 6 // paper scissors, win
-            | "C X" -> s + 3 + 0 // scissors paper, lose
-            | "C Y" -> s + 2 + 3 // scissors scissors, draw
+            | "B Y" -> s + 2 + 3 // paper paper, draw
+            | "B Z" -> s + 3 + 6 // paper scissors, win
+            | "C X" -> s + 2 + 0 // scissors paper, lose
+            | "C Y" -> s + 3 + 3 // scissors scissors, draw
             | "C Z" -> s + 1 + 6 // scissors rock, win
             | _ -> s // shouldnt happen 
             )

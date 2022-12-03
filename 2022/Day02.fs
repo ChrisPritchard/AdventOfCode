dd474de4f72fc160841a5a1fe6ab1cbc5049d6a4
Christopher <chris@grislygrotto.nz>
2022-12-03 13:25:17 +1300

    removed magic numbers from day 2

--- 2022/Day02.fs
+++ 2022/Day02.fs
@@ -3,21 +3,24 @@ module Day02
 open Common
 open System
 
+let rock, paper, scissors = 1, 2, 3
+let loss, draw, win = 0, 3, 6
+
 let part1 () =
     (0, readEmbeddedRaw "day02")
     ||> 
         Seq.fold (fun s (v: string) ->
             match v with
-            | "A X" -> s + 1 + 3 // rock rock, draw
-            | "A Y" -> s + 2 + 6 // rock paper, win
-            | "A Z" -> s + 3 + 0 // rock scissors, loss
-            | "B X" -> s + 1 + 0 // paper rock, loss
-            | "B Y" -> s + 2 + 3 // paper paper, draw
-            | "B Z" -> s + 3 + 6 // paper scissors, win
-            | "C X" -> s + 1 + 6 // scissors rock, win
-            | "C Y" -> s + 2 + 0 // scissors paper, loss
-            | "C Z" -> s + 3 + 3 // scissors scissors, draw
-            | _ -> s // shouldnt happen 
+            | "A X" -> s + rock + draw // rock rock, draw
+            | "A Y" -> s + paper + win // rock paper, win
+            | "A Z" -> s + scissors + loss // rock scissors, loss
+            | "B X" -> s + rock + loss // paper rock, loss
+            | "B Y" -> s + paper + draw // paper paper, draw
+            | "B Z" -> s + scissors + win // paper scissors, win
+            | "C X" -> s + rock + win // scissors rock, win
+            | "C Y" -> s + paper + loss // scissors paper, loss
+            | "C Z" -> s + scissors + draw // scissors scissors, draw
+            | _ -> failwith "shouldnt happen"
             )
 
 let part2 () =
@@ -25,15 +28,15 @@ let part2 () =
     ||> 
         Seq.fold (fun s (v: string) ->
             match v with
-            | "A X" -> s + 3 + 0 // rock scissors, lose
-            | "A Y" -> s + 1 + 3 // rock rock, draw
-            | "A Z" -> s + 2 + 6 // rock paper, win
-            | "B X" -> s + 1 + 0 // paper rock, lose
-            | "B Y" -> s + 2 + 3 // paper paper, draw
-            | "B Z" -> s + 3 + 6 // paper scissors, win
-            | "C X" -> s + 2 + 0 // scissors paper, lose
-            | "C Y" -> s + 3 + 3 // scissors scissors, draw
-            | "C Z" -> s + 1 + 6 // scissors rock, win
-            | _ -> s // shouldnt happen 
+            | "A X" -> s + scissors + loss // rock scissors, lose
+            | "A Y" -> s + rock + draw // rock rock, draw
+            | "A Z" -> s + paper + win // rock paper, win
+            | "B X" -> s + rock + loss // paper rock, lose
+            | "B Y" -> s + paper + draw // paper paper, draw
+            | "B Z" -> s + scissors + win // paper scissors, win
+            | "C X" -> s + paper + loss // scissors paper, lose
+            | "C Y" -> s + scissors + draw // scissors scissors, draw
+            | "C Z" -> s + rock + win // scissors rock, win
+            | _ -> failwith "shouldnt happen"
             )
 

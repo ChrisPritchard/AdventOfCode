36b971c923c9e66acc6fea3e12d62d2b07fab187
Christopher <chris@grislygrotto.nz>
2022-12-03 19:15:49 +1300

    finished day 3

--- 2022/Program.fs
+++ 2022/Program.fs
@@ -14,6 +14,9 @@ let main _ =
     
     total <- total + timeForDay 2 1 Day02.part1
     total <- total + timeForDay 2 2 Day02.part2
+    
+    total <- total + timeForDay 3 1 Day03.part1
+    total <- total + timeForDay 3 2 Day03.part2
 
     printfn ""
     printfn "total time elapsed: %f ms" total

77cfc0ae655203fe22a05dc5bac44bccd8d5b664
Christopher <chris@grislygrotto.nz>
2022-12-03 19:18:09 +1300

    slightly reworked day 3

--- 2022/Day03.fs
+++ 2022/Day03.fs
@@ -3,25 +3,24 @@ module Day03
 open Common
 open System
 
+let score s =
+    let item = Set.toArray s |> fun a -> a[0]
+    if int item <= int 'Z' then
+        int item - int 'A' + 27
+    else
+        int item - int 'a' + 1
+
 let part1 () =
     readEmbeddedRaw "day03"
     |> Seq.sumBy (fun (line: string) -> 
         let left = Seq.take (line.Length / 2) line |> Set.ofSeq
         let right = Seq.skip (line.Length / 2) line |> Set.ofSeq
-        let shared = Set.intersect left right |> Set.toArray |> fun a -> a[0]
-        if int shared <= int 'Z' then
-            int shared - int 'A' + 27
-        else
-            int shared - int 'a' + 1)
+        Set.intersect left right |> score)
 
 let part2 () =
     readEmbeddedRaw "day03"
     |> Seq.map Set.ofSeq
     |> Seq.chunkBySize 3
-    |> Seq.map (Set.intersectMany >> Set.toArray >> fun a -> a[0])
-    |> Seq.sumBy (fun shared -> 
-        if int shared <= int 'Z' then
-            int shared - int 'A' + 27
-        else
-            int shared - int 'a' + 1)
+    |> Seq.map Set.intersectMany
+    |> Seq.sumBy score
 

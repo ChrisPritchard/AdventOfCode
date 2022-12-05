4f7a52fa57e303d7ae5ff4c96c7b690f05a40d5f
Christopher <chris@grislygrotto.nz>
2022-12-05 18:38:40 +1300

    creating stacks from the provided data

--- 2022/Day05.fs
+++ 2022/Day05.fs
@@ -3,8 +3,37 @@ module Day05
 open Common
 open System
 
+type State =
+    | Reading of string list
+    | Stacks of (char list) []
+
+let createStacks (lines: string list) = 
+    let bottom = List.head lines
+    let stacks = Array.create ((bottom.Length + 1) / 4) List.empty
+    (stacks, lines)
+    ||> List.fold (fun stacks line ->
+        stacks |> Array.mapi (fun i s ->
+            let index = (i * 4) + 1
+            if index >= line.Length then s 
+            else if line[index] = ' ' then s
+            else line[index]::s))
+    |> Stacks
+
 let part1 () =
-    readEmbeddedRaw "day05"
+    (Reading [], readEmbeddedRaw "day05")
+    ||> Seq.fold (fun state line ->
+        match state with
+        | Reading acc ->
+            if line = "" then
+                createStacks acc
+            else if not (Char.IsNumber(line[1])) then
+                Reading (line::acc)
+            else
+                state
+        | Stacks stacks ->
+            state
+    )
+
 
 let part2 () =
     readEmbeddedRaw "day05"

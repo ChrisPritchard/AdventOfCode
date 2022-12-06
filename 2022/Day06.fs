fe13379bfcafe0ad7b290a529ed5c506afff26f6
Christopher <chris@grislygrotto.nz>
2022-12-06 19:55:46 +1300

    refined day 6 solution

--- 2022/Day06.fs
+++ 2022/Day06.fs
@@ -5,11 +5,19 @@ open System
 
 let part1And2 () =
     let line = (readEmbedded "day06")[0]
-    let endOfHeader = 
-        [4..line.Length] |> Seq.find (fun i -> 
-            line[(i-4)..i-1] |> Set.ofSeq |> Seq.length |> (=) 4)
-    let endOfMessage = 
-        [14..line.Length] |> Seq.find (fun i -> 
-            line[(i-14)..i-1] |> Set.ofSeq |> Seq.length |> (=) 14)
-    endOfHeader, endOfMessage
+    let findPattern n =
+        line |> Seq.windowed n |> Seq.map (Set.ofSeq >> Seq.length) |> Seq.findIndex (fun s -> s = n) |> (+) n
+    findPattern 4, findPattern 14
+
+// original
+
+// let part1And2 () =
+//     let line = (readEmbedded "day06")[0]
+//     let endOfHeader = 
+//         [4..line.Length] |> Seq.find (fun i -> 
+//             line[(i-4)..i-1] |> Set.ofSeq |> Seq.length |> (=) 4)
+//     let endOfMessage = 
+//         [14..line.Length] |> Seq.find (fun i -> 
+//             line[(i-14)..i-1] |> Set.ofSeq |> Seq.length |> (=) 14)
+//     endOfHeader, endOfMessage
 

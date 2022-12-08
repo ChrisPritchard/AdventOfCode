f6b6615cd613744ab0035744c886601b206fe257
Christopher <chris@grislygrotto.nz>
2022-12-09 11:26:42 +1300

    wording and formatting fixes

--- 2022/Day08.fs
+++ 2022/Day08.fs
@@ -24,20 +24,21 @@ let part1 () =
     let horizontal = 
         (Set.empty, [1..grid.Length - 2])
         ||> List.fold (fun visible row ->
-            let half1 = sightTester visible (grid[row][0]) (1, row) (1, 0)
-            let half2 = sightTester half1 (grid[row][grid[0].Length - 1]) (grid[0].Length - 2, row) (-1, 0)
-            half2)
+            let fromLeft = sightTester visible (grid[row][0]) (1, row) (1, 0)
+            let fromRight = sightTester fromLeft (grid[row][grid[0].Length - 1]) (grid[0].Length - 2, row) (-1, 0)
+            fromRight)
     let withVertical = 
         (horizontal, [1..grid[0].Length - 2])
         ||> List.fold (fun visible col ->
-            let half1 = sightTester visible (grid[0][col]) (col, 1) (0, 1)
-            let half2 = sightTester half1 (grid[grid.Length - 1][col]) (col, grid.Length - 2) (0, -1)
-            half2)
+            let fromTop = sightTester visible (grid[0][col]) (col, 1) (0, 1)
+            let fromBottom = sightTester fromTop (grid[grid.Length - 1][col]) (col, grid.Length - 2) (0, -1)
+            fromBottom)
 
     Set.count withVertical + outside
 
 let part2() =
     let grid = readEmbedded "day08"
+    
     let rec crawl maxHeight count (x, y) (dx, dy) =
         let (nx, ny) = (x + dx, y + dy)
         if nx < 0 || ny < 0 || nx = grid[0].Length || ny = grid.Length then count

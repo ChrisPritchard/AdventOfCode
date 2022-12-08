542d563b859f788c22c71785c35fb22375ee1384
Christopher <chris@grislygrotto.nz>
2022-12-09 11:30:34 +1300

    slight tweaking to be a little more concise

--- 2022/Day08.fs
+++ 2022/Day08.fs
@@ -1,14 +1,13 @@
 module Day08
 
 open Common
-open System
 
 let part1 () =
     let grid = readEmbedded "day08"
     
     let outside = grid.Length * 2 + (grid[0].Length - 2) * 2 
 
-    let rec sightTester visible lastHighest (x, y) (dx, dy) =
+    let rec sightTester lastHighest (x, y) (dx, dy) visible =
         let visible, heighest =
             let height = grid[y][x]
             if height > lastHighest then
@@ -19,26 +18,26 @@ let part1 () =
         else
             let (nx, ny) = (x + dx, y + dy)
             if nx = 0 || ny = 0 || nx = grid[0].Length - 1 || ny = grid.Length - 1 then visible
-            else sightTester visible heighest (nx, ny) (dx, dy)
+            else sightTester heighest (nx, ny) (dx, dy) visible
 
     let horizontal = 
         (Set.empty, [1..grid.Length - 2])
         ||> List.fold (fun visible row ->
-            let fromLeft = sightTester visible (grid[row][0]) (1, row) (1, 0)
-            let fromRight = sightTester fromLeft (grid[row][grid[0].Length - 1]) (grid[0].Length - 2, row) (-1, 0)
-            fromRight)
+            visible 
+            |> sightTester (grid[row][0]) (1, row) (1, 0) 
+            |> sightTester (grid[row][grid[0].Length - 1]) (grid[0].Length - 2, row) (-1, 0))
     let withVertical = 
         (horizontal, [1..grid[0].Length - 2])
         ||> List.fold (fun visible col ->
-            let fromTop = sightTester visible (grid[0][col]) (col, 1) (0, 1)
-            let fromBottom = sightTester fromTop (grid[grid.Length - 1][col]) (col, grid.Length - 2) (0, -1)
-            fromBottom)
+            visible 
+            |> sightTester (grid[0][col]) (col, 1) (0, 1) 
+            |> sightTester (grid[grid.Length - 1][col]) (col, grid.Length - 2) (0, -1))
 
     Set.count withVertical + outside
 
 let part2() =
     let grid = readEmbedded "day08"
-    
+
     let rec crawl maxHeight count (x, y) (dx, dy) =
         let (nx, ny) = (x + dx, y + dy)
         if nx < 0 || ny < 0 || nx = grid[0].Length || ny = grid.Length then count
@@ -52,10 +51,8 @@ let part2() =
     [0..grid.Length - 1] |> Seq.collect (fun y ->
         [0..grid[y].Length - 1] |> Seq.map (fun x -> 
             let height = grid[y][x]
-            let res = 
-                crawl height 0 (x, y) (-1, 0) * 
-                crawl height 0 (x, y) (1, 0) * 
-                crawl height 0 (x, y) (0, -1) * 
-                crawl height 0 (x, y) (0, 1)
-            res))
+            crawl height 0 (x, y) (-1, 0) * 
+            crawl height 0 (x, y) (1, 0) * 
+            crawl height 0 (x, y) (0, -1) * 
+            crawl height 0 (x, y) (0, 1)))
             |> Seq.max

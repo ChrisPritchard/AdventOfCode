98feae2d4ec263c598974f53f5b60f26857fe06a
Christopher <chris@grislygrotto.nz>
2022-12-11 11:50:18 +1300

    finished part 1 of day 10

--- 2022/Day10.fs
+++ 2022/Day10.fs
@@ -1,9 +1,24 @@
 module Day10
 
 open Common
+open System
 
 let part1() =
-    0
+    ((0, (1, 1)), readEmbeddedRaw "day10")
+    ||> Seq.fold (fun (sum, (X, cycle)) instruction ->
+        if instruction = "noop" then
+            let cycle = cycle + 1
+            let sum = if (cycle - 20) % 40 = 0 then sum + X * cycle else sum
+            sum, (X, cycle)
+        else
+            let toAdd = Int32.Parse (instruction.Substring(5))
+            let cycle = cycle + 2
+            let sum = 
+                if (cycle - 20) % 40 = 0 then sum + (X + toAdd) * cycle
+                else if (cycle - 20) % 40 = 1 then sum + X * (cycle - 1)
+                else sum  
+            sum, (X + toAdd, cycle))
+    |> fst
 
 let part2() =
     0

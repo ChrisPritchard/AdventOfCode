90f32d7150e17be71b8b6c274c34e2ec9e33bc66
Christopher <chris@grislygrotto.nz>
2022-12-11 12:42:13 +1300

    finished day 10

--- 2022/Day10.fs
+++ 2022/Day10.fs
@@ -24,23 +24,23 @@ let part2() =
     // six 40 bool arrays
     // track position of x each cycle - if cycle x is +- 1 of cycle then mark cycle position as positive
     let litPixels =
-        ((Set.empty, (1, 1)), readEmbeddedRaw "day10")
+        ((Set.empty, (1, 0)), readEmbeddedRaw "day10")
         ||> Seq.fold (fun (lit, (X, cycle)) instruction ->
             if instruction = "noop" then
                 let cycle = cycle + 1
-                let lit = if abs (X - cycle) <= 1 then Set.add cycle lit else lit
+                let lit = if abs (X - (cycle % 40)) <= 1 then Set.add cycle lit else lit
                 lit, (X, cycle)
             else
                 let toAdd = Int32.Parse (instruction.Substring(5))
                 let cycle = cycle + 2
                 let lit = 
-                    if abs ((X + toAdd) - cycle) <= 1 then Set.add cycle lit else lit
+                    if abs ((X + toAdd) - (cycle % 40)) <= 1 then Set.add cycle lit else lit
                 let lit = 
-                    if abs (X - (cycle - 1)) <= 1 then Set.add (cycle - 1) lit else lit
+                    if abs (X - ((cycle - 1) % 40)) <= 1 then Set.add (cycle - 1) lit else lit
                 lit, (X + toAdd, cycle))
         |> fst
     for y in [0..5] do
-        for x in [0..49] do
+        for x in [0..39] do
             let p = y * 40 + x
             if Set.contains p litPixels then printf "#" else printf "."
         printfn ""

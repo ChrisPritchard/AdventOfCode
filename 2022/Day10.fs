47d6a96d1a313958a3aa83be679b0225c81078c2
Christopher <chris@grislygrotto.nz>
2022-12-12 07:24:38 +1300

    changed part 2 to create the string in one go

--- 2022/Day10.fs
+++ 2022/Day10.fs
@@ -22,30 +22,21 @@ let part1() =
 
 let part2() =
     // track position of x each cycle - if cycle x is +- 1 of cycle then mark cycle position as positive
-    let litPixels =
-        ((Set.empty, (1, 0)), readEmbeddedRaw "day10")
-        ||> Seq.fold (fun (lit, (X, cycle)) instruction ->
-            if instruction = "noop" then
-                let cycle = cycle + 1
-                let lit = if abs (X - (cycle % 40)) <= 1 then Set.add cycle lit else lit
-                lit, (X, cycle)
-            else
-                let toAdd = Int32.Parse (instruction.Substring(5))
-                let cycle = cycle + 2
-                let lit = 
-                    if abs ((X + toAdd) - (cycle % 40)) <= 1 then Set.add cycle lit else lit
-                let lit = 
-                    if abs (X - ((cycle - 1) % 40)) <= 1 then Set.add (cycle - 1) lit else lit
-                lit, (X + toAdd, cycle))
-        |> fst
-
-    let res =
-        [0..5] 
-        |> Seq.map (fun y -> 
-            [0..39] 
-            |> Seq.map (fun x -> 
-                if Set.contains (y * 40 + x) litPixels then '#' else ' ')
-            |> asString)
-        |> String.concat "\n\t"
-    "\n\t" + res
-    
+    (("\n\t ", (1, 0)), readEmbeddedRaw "day10")
+    ||> Seq.fold (fun (lit, (X, cycle)) instruction ->
+        if instruction = "noop" then
+            let cycle = cycle + 1
+            let lit = if cycle % 40 = 0 then lit + "\n\t" else lit
+            let lit = if abs (X - (cycle % 40)) <= 1 then lit + "#" else lit + " "
+            lit, (X, cycle)
+        else
+            let toAdd = Int32.Parse (instruction.Substring(5))
+            let cycle = cycle + 2
+            let lit = 
+                let lit = if (cycle - 1) % 40 = 0 then lit + "\n\t" else lit
+                if abs (X - ((cycle - 1) % 40)) <= 1 then lit + "#" else lit + " "
+            let lit = 
+                let lit = if cycle % 40 = 0 then lit + "\n\t" else lit
+                if abs ((X + toAdd) - (cycle % 40)) <= 1 then lit + "#" else lit + " "
+            lit, (X + toAdd, cycle))
+    |> fst

de5858e1d5134635e192aed05c52418297ab3138
Christopher <chris@grislygrotto.nz>
2022-12-12 21:32:16 +1300

    start of day 12

---
+++ 2022/Day12.fs
@@ -0,0 +1,11 @@
+module Day12
+
+let part1 () =
+    let map = readEmbedded "day12"
+    // find end, assign 0
+    // for each position next to end, assign dist to nearest set
+    // if can't be set, skip
+    // else use a*
+
+let part2 () =
+    0

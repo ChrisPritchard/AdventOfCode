45e5e55521588b14b58d4f48a95fb42cfa727a33
Christopher <chris@grislygrotto.nz>
2022-12-17 10:39:04 +1300

    experimentation with day 16

--- 2022/Day16.fs
+++ 2022/Day16.fs
@@ -1,9 +1,60 @@
 module Day16
 
 open Common
+open System.Collections.Generic
+
+let bfs<'T when 'T : equality> (isGoal: 'T -> bool) (edges: 'T -> seq<'T>) (start: 'T) =
+    let queue = Queue<'T>()
+    let discovered = HashSet<'T>()
+    let parents = Dictionary<'T, 'T>()
+    
+    discovered.Add start |> ignore
+    queue.Enqueue start
+
+    let rec reconstructPath acc v =
+        if parents.ContainsKey v then
+            reconstructPath (v::acc) parents.[v]
+        else
+            v::acc
+
+    let rec searcher () =
+        if queue.Count = 0 then None
+        else
+            let v = queue.Dequeue ()
+            if isGoal v then
+                reconstructPath [] v |> Some
+            else
+                let edges = edges v
+                for w in edges do
+                    if not (discovered.Contains w) then
+                        discovered.Add w |> ignore
+                        parents.Add (w, v)
+                        queue.Enqueue w
+                searcher ()
+            
+    searcher ()
 
 let part1 () =
-    0
+    let input = 
+        readEmbeddedRaw "day16"
+        |> Seq.map (fun line ->
+            line
+                .Replace("Valve ", "")
+                .Replace(" has flow rate=", ",")
+                .Replace(" has flow rate=", ",")
+                .Replace("; tunnels lead to valves ", ",")
+                .Replace("; tunnel leads to valve ", ",")
+            |> split ",")
+    
+    input 
+    |> Seq.choose (fun a -> 
+        if a[1] = "0" then None 
+        else Some (int a[1]))
+    |> fun s -> [Seq.length s..(-1)..1] |> List.reduce (*)
+
+    // while there are too many possible paths to test them all, there are less valves with actual pressure values
+    // you would never visit a pressure valve more than once. 720 in the source data, 2004310016 in the real data 
+    //  (which might be barely attainable, especially with eager discarding of ineffient paths)
 
 let part2 () =
     0

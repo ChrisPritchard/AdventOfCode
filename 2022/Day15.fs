eaf8d8aaf2430a2a24597b4ca0d6c2b403317fda
Christopher <chris@grislygrotto.nz>
2022-12-17 19:05:36 +1300

    working, but slow, day 15 part 1 (faster than before)

--- 2022/Day15.fs
+++ 2022/Day15.fs
@@ -7,12 +7,13 @@ let dist (x1, y1) (x2, y2) =
 
 let onRow row ((sx, sy), d) =
     if sy <= row && sy + d >= row then
-        let rd = row - sy // if sy is 4, row is 7, and d is 9, then the d for row 7 will be 9 - 3 = 6
+        let rd = d - (row - sy) // if sy is 4, row is 7, and d is 9, then the d for row 7 will be 9 - 3 = 6
         Some (sx - rd, sx + rd)
     else if sy >= row && sy - d <= row then
-        let rd = sy - row
+        let rd = d - (sy - row)
         Some (sx - rd, sx + rd)
     else None
+
 
 let part1 () =
     let input = 
@@ -23,14 +24,13 @@ let part1 () =
     let sensorDistances = Seq.map (fun (s, b) -> s, dist s b) input
     let rowToTest = 2000000
     let ranges = sensorDistances |> Seq.choose (onRow rowToTest)
-    let onLine = input |> Seq.collect (fun (s, b) -> [s;b]) |> Seq.filter (snd >> (=) rowToTest) |> Seq.length
+    let onLine = input |> Seq.collect (fun (s, b) -> [s;b]) |> Seq.distinct |> Seq.filter (snd >> (=) rowToTest) |> Seq.length
 
-    // take a set of ranges, sort by min
-    // take the last max. if thats greater than the current max, then skip the current and continue
-    // if its less than the current max, 
-
-    ranges
-    |> Seq.toArray
+    ranges 
+    |> Seq.map (fun (a, b) -> set [a..b])
+    |> Seq.reduce Set.union
+    |> Set.count
+    |> fun total -> total - onLine
 
 let part2 () =
     0

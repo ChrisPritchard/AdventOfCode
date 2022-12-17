e26d02cbd9c875ebf83b947927e6592e177b5b03
Christopher <chris@grislygrotto.nz>
2022-12-17 21:50:29 +1300

    finished part 2 (albeit very slow, 3 mins~)

--- 2022/Day15.fs
+++ 2022/Day15.fs
@@ -16,7 +16,7 @@ let onRow row ((sx, sy), d) =
 
 let subtract (min1, max1) (min2, max2) =
     if max1 < min2 || min1 > max2 then [min1, max1] // no overlap
-    else if min1 < min2 && max1 > max2 then [min1,max2] // full overlap
+    else if min1 < min2 && max1 > max2 then [min1,min2-1;max2+1,max1] // full overlap
     else if max1 = min2 || (min1 < min2 && max1 < max2) then [min1, min2-1] // overlap left
     else if min1 = max2 || (min1 > min2 && max1 > max2) then [max2+1, max1] // overlap right
     else [] // overlapped
@@ -31,7 +31,8 @@ let part1 () =
     let sensorDistances = Seq.map (fun (s, b) -> s, dist s b) input
     let rowToTest = 2000000
     let ranges = sensorDistances |> Seq.choose (onRow rowToTest) |> List.ofSeq
-    let onLine = input |> Seq.collect (fun (s, b) -> [s;b]) |> Seq.filter (snd >> (=) rowToTest) |> Seq.length
+
+    let onLine = input |> Seq.collect (fun (s, b) -> [s;b]) |> Seq.filter (snd >> (=) rowToTest) |> Seq.distinct |> Seq.length
 
     let rec reduce acc =
         function 
@@ -48,4 +49,22 @@ let part1 () =
     |> fun total -> total - onLine
 
 let part2 () =
-    0
+    let input = 
+        readEmbeddedRaw "day15"
+        |> Seq.map (split "Sensor at x=, y=: closest beacon is at x=, y=" >> Array.map int)
+        |> Seq.map (fun a -> (a[0], a[1]), (a[2], a[3]))
+
+    let sensorDistances = Seq.map (fun (s, b) -> s, dist s b) input
+    let max = 4000000
+
+    let rec findBeacon line = 
+        let ranges = sensorDistances |> Seq.choose (onRow line) |> List.ofSeq
+        let free =
+            ([0, max], ranges) 
+            ||> List.fold (fun acc r2 -> 
+                acc |> List.collect (fun s -> subtract s r2))
+        if List.isEmpty free then findBeacon (line + 1)
+        else if List.length free > 1 then failwith "expect only a single value"
+        else int64 (fst (List.head free)) * int64 max + int64 line
+    
+    findBeacon 0

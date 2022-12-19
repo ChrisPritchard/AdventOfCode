242ca743461bd4122ce89444ae96854a7b88876d
Christopher <chris@grislygrotto.nz>
2022-12-19 17:39:22 +1300

    finished part 1, day 18

--- 2022/Day18.fs
+++ 2022/Day18.fs
@@ -2,8 +2,22 @@ module Day18
 
 open Common
 
+// coord system: x,y,z,facing, with the last being one of 6 values? only 3 are needed, and required to gen duplicates
+// this could be represented using a 2 bits, then each of the others with 8 bits? or just use tuples
+
 let part1 () =
-    0
+    readEmbeddedRaw "day18"
+    |> Seq.collect (fun line -> 
+        let x, y, z = line |> split "," |> Array.map int |> fun a -> a[0], a[1], a[2]
+        seq [
+            x, y, z, 0 // up
+            x, y, z-1, 0 // down
+            x, y, z, 1 // north
+            x, y+1, z, 1 // south
+            x, y, z, 2 // west
+            x+1, y, z, 2 // east
+        ])
+    |> Seq.countBy id |> Seq.filter (snd >> (=) 1) |> Seq.length
 
 let part2 () =
     0

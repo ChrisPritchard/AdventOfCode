5417f1edcefb60c111073f9c6f0bb6f0b05eaaaa
Christopher <chris@grislygrotto.nz>
2022-12-19 22:14:49 +1300

    partial work day 19

--- 2022/Day19.fs
+++ 2022/Day19.fs
@@ -2,31 +2,57 @@ module Day19
 
 open Common
 
+type costs = {
+    oreBotOreCost: int
+    clayBotOreCost: int
+    obsidianBotOreCost: int
+    obsidianBotClayCost: int
+    geodeBotOreCost: int 
+    geodeBotObsidianCost: int
+}
+
 let part1 () =
     let minutes = 24
 
-    let rec automate minutes 
+    let rec automate minute
         (oreBots, clayBots, obsidianBots, geodeBots) 
-        (ore, clay, obsidians, geodes) 
-        (oreBotOreCost, clayBotOreCost, obsidianBotOreCost, obsidianBotClayCost, geodeBotOreCost, geodeBotObsidianCost) =
+        (ore, clay, obsidian, geodes) 
+        (botCosts: costs) =
 
-        if minutes = 0 then geodes
+        if minute = 0 then 
+            geodes
         else
-            automate (minutes - 1) 
-                (oreBots, clayBots, obsidianBots, geodeBots) 
-                (ore, clay, obsidians, geodes) 
-                (oreBotOreCost, clayBotOreCost, obsidianBotOreCost, obsidianBotClayCost, geodeBotOreCost, geodeBotObsidianCost)
+            let res = 
+                seq [
+                    // dont build anything
+                    yield automate (minute - 1) (oreBots, clayBots, obsidianBots, geodeBots) (ore + oreBots, clay + clayBots, obsidian + obsidianBots, geodes + geodeBots) botCosts
+                    
+                    if ore >= botCosts.oreBotOreCost then 
+                        yield automate (minute - 1) (oreBots + 1, clayBots, obsidianBots, geodeBots) (ore + oreBots - botCosts.oreBotOreCost, clay + clayBots, obsidian + obsidianBots, geodes + geodeBots) botCosts
+
+                    if ore >= botCosts.clayBotOreCost then 
+                        yield automate (minute - 1) (oreBots, clayBots + 1, obsidianBots, geodeBots) (ore + oreBots - botCosts.clayBotOreCost, clay + clayBots, obsidian + obsidianBots, geodes + geodeBots) botCosts
+
+                    if ore >= botCosts.obsidianBotOreCost && clay >= botCosts.obsidianBotClayCost then 
+                        yield automate (minute - 1) (oreBots, clayBots, obsidianBots + 1, geodeBots) (ore + oreBots - botCosts.obsidianBotOreCost, clay + clayBots - botCosts.obsidianBotClayCost, obsidian + obsidianBots, geodes + geodeBots) botCosts
+
+                    if ore >= botCosts.geodeBotOreCost && obsidian >= botCosts.geodeBotObsidianCost then 
+                        yield automate (minute - 1) (oreBots, clayBots, obsidianBots, geodeBots + 1) (ore + oreBots - botCosts.geodeBotOreCost, clay + clayBots, obsidian + obsidianBots - botCosts.geodeBotObsidianCost, geodes + geodeBots) botCosts
+                ] 
+            res |> Seq.max
 
     readEmbeddedRaw "day19"
     |> Seq.map (fun line ->
         line |> split " :" |> (fun a -> 
             let id = int a[1]
-            id * automate minutes (1, 0, 0, 0) (0, 0, 0, 0) (int a[6], int a[12], int a[18], int a[21], int a[27], int a[30])))
-
-    // this could be manually processed, however there need to be decisions on when to build what
-    // one way this could be done would be by calculating 'tails': 
-    //  maximum number of bots is 24 = 1 + 23 (one built each minute except the last, when its pointless)
-    //  the first ore bot will mine 24 ore over its lifetime...
+            let botCosts = { 
+                oreBotOreCost = int a[6]
+                clayBotOreCost = int a[12]
+                obsidianBotOreCost = int a[18]
+                obsidianBotClayCost = int a[21]
+                geodeBotOreCost = int a[27]
+                geodeBotObsidianCost = int a[30] }
+            id * automate minutes (1, 0, 0, 0) (0, 0, 0, 0) botCosts))
 
 let part2 () =
     0

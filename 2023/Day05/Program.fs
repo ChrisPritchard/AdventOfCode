5aa87314c72323647b31c341a20c48e83f0dd7d6
Christopher <chris@grislygrotto.nz>
2023-12-06 12:05:10 +1300

    work on p2

--- 2023/Day05/Program.fs
+++ 2023/Day05/Program.fs
@@ -2,6 +2,7 @@ let input = Input.value
 
 let splitOn (a: char[]) (s: System.String) = s.Split (a, System.StringSplitOptions.RemoveEmptyEntries)
 
+// dest source length
 let numbersFrom s = splitOn [|' '|] s |> Array.map int64
 let conversionMap s = 
     splitOn [|'\n'|] s
@@ -34,5 +35,43 @@ for step in steps do
 let smallest = Array.min seeds
 printfn "Part 1: %d" smallest
 
-// dest source length
-// start with num, find range where num sits (assume no overlap), translate
+let mutable seed_ranges = parts[0] |> splitOn [|'\n'|] |> Array.head |> numbersFrom |> Array.chunkBySize 2 |> Array.map (fun a -> a[0], a[0] + a[1] - 1L)
+
+let test_range_against (r1, r2) (dest, source, range) =
+    if r1 >= source && r2 < source + range then
+        [|
+            dest + (r1 - source), dest + (r2 - source)
+        |]
+    else if r1 < source && r2 >= source && r2 < source + range then
+        [|
+            r1, source - 1L
+            dest, dest + (r2 - source)
+        |]
+    else if r1 >= source && r1 < source + range && r2 >= source + range then
+        [|
+            dest + (r1 - source), dest + range - 1L
+            source + range, r2
+        |]
+    else if r1 < source && r2 >= source + range then
+        [|
+            r1, source - 1L
+            dest, dest + range - 1L
+            source + range, r2
+        |]
+    else
+        [||]
+
+//printfn "start: %A" seed_ranges
+for step in steps do
+    //printfn "step: %A" step
+    seed_ranges <- seed_ranges |> Array.collect (fun range -> 
+        let new_ranges = step |> Array.collect (test_range_against range)
+        if Array.isEmpty new_ranges then [|range|] else new_ranges)
+    //printfn "result: %A" seed_ranges
+
+let smallest_from_ranges = seed_ranges |> Array.map fst |> Array.min
+
+printfn "Part 2: %d" smallest_from_ranges
+
+// let range = (81L, 94L)
+// printfn "test: %A" <| test_range_against range (18L, 25L, 70L)

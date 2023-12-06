89ea44c456b704876e259c192d5723ddc8ed7f9d
Christopher <chris@grislygrotto.nz>
2023-12-06 18:25:09 +1300

    day 6 done

---
+++ 2023/Day06/Program.fs
@@ -0,0 +1,26 @@
+let input = Input.value
+
+let splitOn (a: char[]) (s: System.String) = s.Split (a, System.StringSplitOptions.RemoveEmptyEntries)
+
+let races = 
+    let lines = splitOn [|'\n'|] input
+    let times = splitOn [|' '|] lines[0] |> Array.skip 1 |> Array.map int
+    let records = splitOn [|' '|] lines[1] |> Array.skip 1 |> Array.map int
+    Array.zip times records
+
+let winning_counts time record = 
+    [|0..time - 1|] |> Array.filter (fun time_held -> (time - time_held) * time_held > record) |> Array.length
+
+let total = races |> Array.map (fun (time, record) -> winning_counts time record |> int64) |> Array.reduce (*)
+
+printfn "Part 1: %d" total
+
+let (longer_time, longerRecord) = 
+    let lines = splitOn [|'\n'|] input
+    let time = splitOn [|':'|] lines[0] |> fun s -> s[1].Replace(" ", "") |> int64
+    let record = splitOn [|':'|] lines[1] |> fun s -> s[1].Replace(" ", "") |> int64
+    time, record
+
+let win_chances = [|0L..longer_time - 1L|] |> Array.filter (fun time_held -> (longer_time - time_held) * time_held > longerRecord) |> Array.length
+
+printfn "Part 2: %d" win_chances

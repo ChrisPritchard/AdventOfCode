510fe56bc6496316fe65d6aa7908986c29f719e8
Christopher <chris@grislygrotto.nz>
2023-12-06 18:39:48 +1300

    much faster solution

--- 2023/Day06/Program.fs
+++ 2023/Day06/Program.fs
@@ -8,10 +8,16 @@ let races =
     let records = splitOn [|' '|] lines[1] |> Array.skip 1 |> Array.map int
     Array.zip times records
 
-let winning_counts time record = 
-    [|0..time - 1|] |> Array.filter (fun time_held -> (time - time_held) * time_held > record) |> Array.length
+let rec find_wins so_far current_time max_time record = 
+    let is_win = (max_time - current_time) * current_time > record
+    if is_win then
+        find_wins (so_far + 1) (current_time + 1L) max_time record
+    else if so_far = 0 then
+        find_wins so_far (current_time + 1L) max_time record
+    else
+        so_far
 
-let total = races |> Array.map (fun (time, record) -> winning_counts time record |> int64) |> Array.reduce (*)
+let total = races |> Array.map (fun (time, record) -> find_wins 0 0L time record |> int64) |> Array.reduce (*)
 
 printfn "Part 1: %d" total
 
@@ -21,6 +27,6 @@ let (longer_time, longerRecord) =
     let record = splitOn [|':'|] lines[1] |> fun s -> s[1].Replace(" ", "") |> int64
     time, record
 
-let win_chances = [|0L..longer_time - 1L|] |> Array.filter (fun time_held -> (longer_time - time_held) * time_held > longerRecord) |> Array.length
+let win_chances = find_wins 0 0L longer_time longerRecord
 
 printfn "Part 2: %d" win_chances

0ffd689c2649232dde6cae595dcbc2b6d0127148
Christopher <chris@grislygrotto.nz>
2023-12-08 16:07:13 +1300

    finished day 7

--- 2023/get-description.sh
+++ 2023/get-description.sh
@@ -3,4 +3,4 @@ year=2023
 cookie="session=53616c7465645f5fbfd281cafeb37d70d432c41db6e0725d4f200d0ad02747e711be3efb26f46531d8d1694596adaa1e5f3c7c2928485626d84d3d010239ef0e"
 
 day=$(date +%-d)
-curl -s --cookie $cookie https://adventofcode.com/$year/day/$day | pandoc -f html -t markdown -o Day$(date +%d)/readme.md
+curl -s --cookie $cookie https://adventofcode.com/$year/day/$day | pandoc -f html -t markdown -o Day0$day/readme.md

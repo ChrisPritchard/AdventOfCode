7a0a399390f0a7ea37487e09508425e6770727ec
Christopher <chris@grislygrotto.nz>
2023-12-08 19:21:59 +1300

    cleaned up the code a little

--- 2023/Day08/Program.fs
+++ 2023/Day08/Program.fs
@@ -7,36 +7,22 @@ let map = lines |> Array.skip 2 |> Array.map (fun s ->
     let parts = s.Split ([|' ';'=';'(';',';')'|], System.StringSplitOptions.RemoveEmptyEntries)
     parts[0], (parts[1], parts[2])) |> Map.ofArray
 
-let rec follow_route index count pos = 
+let rec follow_route index count pos is_target = 
     let options = map[pos]
     let next = if instructions[index] = 'L' then fst options else snd options
-    if next = "ZZZ" then count + 1
+    if is_target next then count + 1
     else
         let next_index = index + 1
         if next_index = instructions.Length then
-            follow_route 0 (count + 1) next
+            follow_route 0 (count + 1) next is_target
         else
-            follow_route next_index (count + 1) next
+            follow_route next_index (count + 1) next is_target
 
-let path_count = follow_route 0 0 "AAA"
+let path_count = follow_route 0 0 "AAA" ((=) "ZZZ")
 printfn "Part 1: %d" path_count
 
 let starting_nodes = map.Keys |> Seq.toArray |> Array.filter (fun k -> k[2] = 'A')
-
-let rec find_z_dist index count pos = 
-    let options = map[pos]
-    let next = if instructions[index] = 'L' then fst options else snd options
-    if next[2] = 'Z' then count + 1
-    else
-        let next_index = index + 1
-        if next_index = instructions.Length then
-            find_z_dist 0 (count + 1) next
-        else
-            find_z_dist next_index (count + 1) next
-
-let cycles = starting_nodes |> Array.map (fun start -> find_z_dist 0 0 start |> uint64)
-
+let cycles = starting_nodes |> Array.map (fun start -> follow_route 0 0 start (fun s -> s[2] = 'Z') |> uint64)
 let rec gcd a b = if b = 0UL then a else gcd b (a % b)
 let lcm = cycles |> Array.reduce (fun a b -> (a * b) / gcd a b)
-
 printfn "Part 2: %d" lcm

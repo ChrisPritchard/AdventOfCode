cb0c719d09cea27d30cd1213da11c7925618866e
Christopher <chris@grislygrotto.nz>
2023-12-10 12:06:51 +1300

    finished day 9, prep for day 10

---
+++ 2023/Day09/Program.fs
@@ -0,0 +1,19 @@
+let input = Input.value
+
+let lines = input.Split [|'\n'|] |> Array.map (fun line -> line.Split [|' '|] |> Array.map int64)
+
+let rec find_next (line: int64[]) =
+    let diffs = line |> Array.windowed 2 |> Array.map (fun window -> window[1] - window[0])
+    if Array.forall ((=) 0L) diffs then line[line.Length - 1]
+    else line[line.Length - 1] + find_next diffs
+
+let sum = Array.sumBy find_next lines
+printfn "Part 1: %d" sum
+
+let rec find_prev (line: int64[]) =
+    let diffs = line |> Array.windowed 2 |> Array.map (fun window -> window[1] - window[0])
+    if Array.forall ((=) 0L) diffs then line[0]
+    else line[0] - find_prev diffs
+
+let sum2 = Array.sumBy find_prev lines
+printfn "Part 2: %d" sum2
--- 2023/create-day.sh
+++ 2023/create-day.sh
@@ -3,7 +3,7 @@ year=2023
 cookie="session=53616c7465645f5fbfd281cafeb37d70d432c41db6e0725d4f200d0ad02747e711be3efb26f46531d8d1694596adaa1e5f3c7c2928485626d84d3d010239ef0e"
 
 day=$(date +%-d)
-foldername=Day0$day
+foldername=Day$day
 mkdir $foldername
 
 echo -e "<Project Sdk=\"Microsoft.NET.Sdk\">\n\t<PropertyGroup>\n\t\t<OutputType>Exe</OutputType>\n\t\t<TargetFramework>net8.0</TargetFramework>\n\t</PropertyGroup>\n\t<ItemGroup>" > $foldername/$foldername.fsproj

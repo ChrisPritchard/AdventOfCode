ba1b332208746c387e9727b5b9d90c95ab42190d
Christopher <chris@grislygrotto.nz>
2023-12-13 21:23:22 +1300

    work on part 2, needs more

--- 2023/Day12/Program.fs
+++ 2023/Day12/Program.fs
@@ -26,19 +26,34 @@ let part1 = springs |> Array.sumBy (fun (mask, integrity) ->
 
 printfn "Part 1: %d" part1
 
-// let bigger_springs = springs |> Array.map (fun (mask, integrity) -> String.init 4 (fun _ -> mask + "?") + mask, [|0..4|] |> Array.collect (fun _ -> integrity))
+let bigger_springs = springs |> Array.map (fun (mask, integrity) -> String.init 4 (fun _ -> mask + "?") + mask, [|0..4|] |> Array.collect (fun _ -> integrity))
 
-// let mask_is c = 
+let mask_is c_list (mask: string) = Seq.forall (fun c -> Array.contains c c_list) mask
 
-// let rec valid_positions total_len mask integrity =
-//     if Array.isEmpty integrity then
-//         if total_len = 0 && mask then 1 else if 
+let rec valid_positions total_len mask integrity =
+    if Array.isEmpty integrity then
+        if total_len = 0 || mask_is [|'?';'.'|] mask then 1 
+        else 0 // mask has some mandatory '#' remaining, so something is in the wrong position
+    else
+        let required_space = integrity[1..] |> Array.sumBy (fun n -> n + 1)
+        let possible_positions = total_len - required_space - (integrity[0] - 1)
+        let relevant_mask = mask[0..total_len - required_space]
+        [|0..possible_positions-1|] |> Array.filter (fun pos -> 
+            let start_valid = pos = 0 || mask_is [|'?';'.'|] relevant_mask[0..pos-1]
+            let pos_valid = mask_is [|'?';'#'|] relevant_mask[pos..integrity[0]-1]
+            let next_valid = integrity.Length = 1 || mask.Length = relevant_mask.Length || (let c = mask[relevant_mask.Length] in c = '.' || c = '?')
+            start_valid && pos_valid && next_valid
+        ) |> Array.sumBy (fun pos ->
+            let size = pos + integrity[0]
+            let full_size = if integrity.Length > 1 then size + 1 else size
+            let rem_mask = if mask.Length > relevant_mask.Length then mask[relevant_mask.Length..] else ""
+            valid_positions (total_len - full_size) rem_mask integrity[1..])
 
 // let part2 = bigger_springs |> Array.sumBy (fun (mask, integrity) ->
 //     let len = mask.Length
 //     positions len integrity |> Array.filter (fun possible -> Seq.forall2 (fun m p -> m = '?' || m = p) mask possible) |> Array.length)
 
-// printfn "Part 2: %d" part2
+printfn "Part 2: %d" (valid_positions 12 "?###????????" [|3;2;1|])
 
 // could improve this dramatically by not actually creating the strings, and finding someway to filter out by mask as we go
 // these are connected: strings only exist so they can be checked by the mask. a mask could be applied to the calculated space, per section

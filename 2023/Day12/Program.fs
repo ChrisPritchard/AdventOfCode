a0956d4fc41c835c84a00fd11751062b62313705
Christopher <chris@grislygrotto.nz>
2023-12-13 20:51:56 +1300

    part 1 done

--- 2023/Day12/Program.fs
+++ 2023/Day12/Program.fs
@@ -4,22 +4,41 @@ let springs =
     input.Split [|'\n'|] |> Array.map (fun line -> 
         let parts = line.Split [|' '|]
 
-        parts[0].ToCharArray (), (parts[1].Split [|','|] |> Array.map int))
+        parts[0], (parts[1].Split [|','|] |> Array.map int))
 
-let rec positions total_len integrity: int = 
-    if Array.isEmpty integrity then 1
+let make_str (c: char) len = String.init len (fun _ -> c.ToString())
+
+let rec positions total_len integrity =
+    if Array.isEmpty integrity then
+        if total_len = 0 then [|""|] else [|make_str '.' total_len|]
     else
-        let required_space = integrity |> Array.skip 1 |> Array.sumBy (fun i -> i + 1)
-        let possible_positions = total_len - required_space - (integrity[0] - 1)
-        [|0..possible_positions-1|] |> Array.sumBy (fun pos -> 
-            let new_len = total_len - (pos + integrity[0] + 1)
-            positions new_len integrity[1..])
+        let required_space = integrity[1..] |> Array.sumBy (fun n -> n + 1) // e.g. 3,2 would me a min of .###.##, or seven spaces
+        let possible_positions = total_len - required_space - (integrity[0] - 1) // e.g. if size is 2, total_len is 6, required is 3, then there should be two possible spaces
+        [|0..possible_positions-1|] |> Array.collect (fun pos ->
+            let marks = make_str '#' integrity[0];
+            let prefix = if pos = 0 then marks else make_str '.' pos + marks
+            let full_prefix = if integrity.Length > 1 then prefix + "." else prefix
+            positions (total_len - full_prefix.Length) integrity[1..] |> Array.map (fun s -> full_prefix + s))
 
-let ptest1 = positions 12 [|3;2;1|]
-printfn "%d" ptest1
+let part1 = springs |> Array.sumBy (fun (mask, integrity) ->
+    let len = mask.Length
+    positions len integrity |> Array.filter (fun possible -> Seq.forall2 (fun m p -> m = '?' || m = p) mask possible) |> Array.length)
 
-// ????..???.???#??##?. 1,1,1,1,1,6
-let ptest2 = positions 20 [|1;1;1;1;1;6|]
-printfn "%d" ptest2
+printfn "Part 1: %d" part1
 
-// for any given position, how can we determine if it fits the mask?
+// let bigger_springs = springs |> Array.map (fun (mask, integrity) -> String.init 4 (fun _ -> mask + "?") + mask, [|0..4|] |> Array.collect (fun _ -> integrity))
+
+// let mask_is c = 
+
+// let rec valid_positions total_len mask integrity =
+//     if Array.isEmpty integrity then
+//         if total_len = 0 && mask then 1 else if 
+
+// let part2 = bigger_springs |> Array.sumBy (fun (mask, integrity) ->
+//     let len = mask.Length
+//     positions len integrity |> Array.filter (fun possible -> Seq.forall2 (fun m p -> m = '?' || m = p) mask possible) |> Array.length)
+
+// printfn "Part 2: %d" part2
+
+// could improve this dramatically by not actually creating the strings, and finding someway to filter out by mask as we go
+// these are connected: strings only exist so they can be checked by the mask. a mask could be applied to the calculated space, per section

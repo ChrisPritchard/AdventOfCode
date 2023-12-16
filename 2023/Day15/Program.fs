b039064159ea723c1cc95a6faaf7d56d04c1aaa6
Christopher <chris@grislygrotto.nz>
2023-12-16 15:29:33 +1300

    part 1 done day 15

--- 2023/Day15/Program.fs
+++ 2023/Day15/Program.fs
@@ -1,4 +1,14 @@
 let input = Input.value
 
-printfn "%s" input
+let segments = input.Split [|','|]
 
+let hash (s: string) = 
+    let mutable total = 0
+    for c in s do
+        total <- total + int c
+        total <- total * 17
+        total <- total % 256
+    total
+
+let part1 = segments |> Array.sumBy hash
+printfn "Part 1: %d" part1

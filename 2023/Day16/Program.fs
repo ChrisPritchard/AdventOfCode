8d3ece2de588db0306057c3a6f98e8bcd90c580e
Christopher <chris@grislygrotto.nz>
2023-12-18 05:52:30 +1300

    finished part 1, invalid input error

--- 2023/Day16/Program.fs
+++ 2023/Day16/Program.fs
@@ -2,55 +2,41 @@ let input = Input.value
 
 let grid = input.Split [|'\n'|]
 
-let mutable illuminated = Set.empty
-
 type Dir = North | East | South | West
 
-let rec light_beam x y dir =
-    let nx, ny = 
-        match dir with
-        | North -> x, y - 1
-        | East -> x + 1, y
-        | South -> x, y + 1
-        | West -> x - 1, y
-    if nx < 0 || ny < 0 || ny = grid.Length || nx = grid[ny].Length || Set.contains (nx, ny, dir) illuminated then ()
-    else
-        illuminated <- Set.add (nx, ny, dir) illuminated
-        match grid[ny][nx], dir with
-        | '|', West | '|', East -> 
-            light_beam nx ny North
-            light_beam nx ny South
-        | '-', North | '-', South ->
-            light_beam nx ny East
-            light_beam nx ny West
-        | '\\', East ->
-            light_beam nx ny South
-        | '\\', North ->
-            light_beam nx ny West
-        | '\\', West ->
-            light_beam nx ny North
-        | '\\', South ->
-            light_beam nx ny East
-        | '/', East ->
-            light_beam nx ny North
-        | '/', North ->
-            light_beam nx ny East
-        | '/', West ->
-            light_beam nx ny South
-        | '/', South ->
-            light_beam nx ny West
-        | _ -> 
-            light_beam nx ny dir
+let next_dirs (c: char) dir = 
+    match c, dir with
+    | '|', West | '|', East -> [North;South]
+    | '-', North | '-', South -> [East;West]
+    | '\\', East ->[South]
+    | '\\', South -> [East]
+    | '\\', North -> [West]
+    | '\\', West -> [North]
+    | '/', East -> [North]
+    | '/', North -> [East]
+    | '/', West -> [South]
+    | '/', South -> [West]
+    | _ ->  [dir]
 
-light_beam -1 0 East
+let rec light_beam queue beams =
+    match queue with
+    | (x, y, dir)::rem ->
+        if Set.contains (x, y, dir) beams || y < 0 || x < 0 || y = grid.Length || x = grid[y].Length then
+            light_beam rem beams
+        else
+            let new_illuminated = Set.add (x, y, dir) beams
+            let new_tiles = next_dirs (grid[y][x]) dir |> List.map (fun d -> 
+                match d with
+                | North -> x, y - 1, d
+                | West -> x - 1, y, d
+                | South ->  x, y + 1, d
+                | East ->  x + 1, y, d)
+            let new_queue = List.append new_tiles rem
+            light_beam new_queue new_illuminated
+    | [] -> beams
 
-let just_illuminated = Set.map (fun (x, y, dir) -> x, y) illuminated
+let beams = light_beam [0, 0, East] Set.empty
+let just_illuminated = Set.map (fun (x, y, dir) -> x, y) beams
 let part1 = just_illuminated |> Set.count
-
-// for i in 0..grid.Length-1 do
-//     for j in 0..grid[i].Length-1 do
-//         if Set.contains (j, i) just_illuminated then printf "#"
-//         else printf "%c" (grid[i][j])
-//     printf "\n"
 
 printfn "Part 1: %d" part1

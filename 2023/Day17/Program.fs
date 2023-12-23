647a63e5fb7d8a8a08312f0e776a0a720a7cf65c
Christopher <chris@grislygrotto.nz>
2023-12-23 20:27:19 +1300

    refinement. missing something

--- 2023/Day17/Program.fs
+++ 2023/Day17/Program.fs
@@ -5,37 +5,36 @@ let input = System.IO.File.ReadAllLines "input.txt"
 open System
 open System.Collections.Generic
 
-let djikstra<'T when 'T : equality> (all_points: 'T seq) (isGoal: 'T -> bool) (edges: Dictionary<'T, 'T> -> 'T -> seq<'T>) (d: 'T -> 'T -> float) (start: 'T) =
+let djikstra<'T when 'T : equality> (vertices: 'T seq) (target: 'T -> bool) (edges: Dictionary<'T, 'T> -> 'T -> seq<'T>) (edge: 'T -> 'T -> float) (source: 'T) =
     
-    let previous = Dictionary<'T, 'T>()
-    let distances = Dictionary<'T, float>()
-    let queue = HashSet<'T>()
-    for point in all_points do 
-        distances[point] <- infinity
-        queue.Add point |> ignore
-    distances[start] <- 0.
+    let prev = Dictionary<'T, 'T>()
+    let dist = Dictionary<'T, float>()
+    let Q = HashSet<'T>()
+    for v in vertices do 
+        dist[v] <- infinity
+        Q.Add v |> ignore
+    dist[source] <- 0.
 
     let rec searcher () = 
-        if queue.Count = 0 then None
+        if Q.Count = 0 then None
         else
-            let u = Seq.minBy (fun v -> distances[v]) queue
+            let u = Seq.minBy (fun v -> dist[v]) Q
 
-            if isGoal u then
-                let mutable total_path = [u]
-                let mutable current = u
-                while previous.ContainsKey current do
-                    current <- previous[current]
-                    total_path <- current::total_path
-                Some total_path
+            if target u then
+                let mutable S = [u]
+                let mutable u = u
+                while prev.ContainsKey u do
+                    u <- prev[u]
+                    S <- u::S
+                Some S
             else
-                queue.Remove u |> ignore
+                Q.Remove u |> ignore
 
-                let neighbours = edges previous u
-                for v in neighbours |> Seq.filter (fun v -> queue.Contains v) do
-                    let alt = distances[u] + d u v
-                    if alt < distances[v] then
-                        distances[v] <- alt
-                        previous[v] <- u
+                for v in edges prev u |> Seq.filter (fun v -> Q.Contains v) do
+                    let alt = dist[u] + edge u v
+                    if alt < dist[v] then
+                        dist[v] <- alt
+                        prev[v] <- u
                 searcher()
     searcher ()
 
@@ -46,32 +45,34 @@ let add (x1, y1) (x2, y2) =
 let sub (x1, y1) (x2, y2) = 
     x1 - x2, y1 - y2
 
-let neighbours (came_from: Dictionary<int * int, int * int>) point = 
+let last_four_points (came_from: Dictionary<int * int, int * int>) current = 
     // custom to challenge: can only turn left, right, or forward
     // and only forward if not already 3 blocks in a row forward
     // to calculate, need to find the last three tiles if possible, and calculate their offsets
-    let previous = if came_from.ContainsKey point then Some came_from[point] else None
+    let previous = if came_from.ContainsKey current then Some came_from[current] else None
     let prev_previous = previous |> Option.bind (fun p -> if came_from.ContainsKey p then Some came_from[p] else None)
     let prev_prev_previous = prev_previous |> Option.bind (fun p -> if came_from.ContainsKey p then Some came_from[p] else None)
-    let last_four = [Some point; previous; prev_previous; prev_prev_previous] |> List.choose id
+    [Some current; previous; prev_previous; prev_prev_previous] |> List.choose id
 
+let neighbours (last_four: (int * int) list) =
+    let point = last_four[0]
     if last_four.Length = 1 then
         [-1,0; 1,0; 0,-1; 0,1] |> List.map (add point) |> List.filter is_valid |> Seq.ofList
     else
+        let previous = last_four[1]
         let in_a_line = 
             last_four.Length = 4 && 
             (last_four |> List.map fst |> List.distinct |> List.length = 1
             || last_four |> List.map snd |> List.distinct |> List.length = 1)
-        let forward = add point (sub point previous.Value)
+        let forward = add point (sub point previous)
         [-1,0; 1,0; 0,-1; 0,1] |> List.map (add point) |> List.filter (fun neighbour -> 
-            is_valid neighbour && neighbour <> previous.Value && (not in_a_line || neighbour <> forward)) |> Seq.ofList
+            is_valid neighbour && neighbour <> previous && (not in_a_line || neighbour <> forward)) |> Seq.ofList
 
 let d_score (x, y) = float (input[y][x] - '0')
 let all_points = [|0..input.Length-1|] |> Array.collect (fun y -> [|0..input[y].Length-1|] |> Array.map (fun x -> x, y))
 let goal = input[input.Length - 1].Length - 1, input.Length - 1
 
-//let path = a_star (0, 0) goal (distance goal) d_score neighbours
-let path = djikstra all_points ((=) goal) neighbours (fun _ p -> d_score p) (0, 0)
+let path = djikstra all_points ((=) goal) (fun a p -> last_four_points a p |> neighbours) (fun _ p -> d_score p) (0, 0)
 let heat_loss = path.Value |> List.skip 1 |> List.sumBy (d_score >> int)
 
 printfn "%d" heat_loss

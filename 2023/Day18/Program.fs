823d3715edd9299c5c04d49a69cefbe209cfce09
Christopher <chris@grislygrotto.nz>
2023-12-29 15:17:26 +1300

    finished day 18

--- 2023/Day18/Program.fs
+++ 2023/Day18/Program.fs
@@ -1,42 +1,53 @@
 let input = System.IO.File.ReadAllLines "input.txt"
 
-let rec crawler index pos top_left bottom_right dug =
-    let add (x, y) (x2, y2) = x + x2, y + y2
-    if index = input.Length then
-        dug, top_left, bottom_right
-    else
-        let dir, dist, color = let parts = input[index].Split [|' '|] in parts[0], int parts[1], parts[2][1..parts[2].Length - 2]
-        let delta = match dir with "U" -> 0,-1 | "D" -> 0,1 | "L" -> -1,0 | "R" | _ -> 1,0 
-        let mutable new_pos = pos
-        let mutable new_dug = dug
-        for n in 1..dist do 
-            new_pos <- add new_pos delta
-            new_dug <- Set.add new_pos new_dug
-        let dx, dy = new_pos
-        let new_top_left = let tx, ty = top_left in min tx dx, min ty dy
-        let new_bottom_right = let bx, by = bottom_right in max bx dx, max by dy
-        crawler (index + 1) new_pos new_top_left new_bottom_right new_dug
+// for part 2, part 1's 'crawler' solution will not work
+// instead get points and calculate shoelace formula: https://en.wikipedia.org/wiki/Shoelace_formula
+// then use area with picks theorem to get interior points: https://en.wikipedia.org/wiki/Pick%27s_theorem
 
-let dug, (tx, ty), (bx, by) = crawler 0 (0,0) (1,1) (-1,-1) Set.empty
+let shoe_lace (points: (int64 * int64)[]) = 
+    let mutable sum = points |> Array.windowed 2 |> Array.map (fun a -> a[0], a[1]) |> Array.sumBy (fun ((x1, y1), (x2, y2)) ->  (x1 * y2) - (x2 * y1))
+    let (x1, y1), (x2, y2) = Array.last points, Array.head points
+    sum <- sum + ((x1 * y2) - (x2 * y1))
+    abs sum / 2L
 
-let rec filler queue dug inside =
-    //printfn "%A" queue
-    match queue with
-    | [] -> inside
-    | (x, y)::rem ->
-        let new_inside = Set.add (x, y) inside
-        let neighbours = [0,-1; 0,1; -1,0; 1,0] |> List.map (fun (dx, dy) -> x + dx, y + dy) |> List.filter (fun p -> not (Set.contains p dug) && not (List.contains p rem) && not (Set.contains p new_inside))
-        filler (List.append neighbours rem) dug new_inside
+let rev_picks boundary_points area = 
+    area + 1L - (boundary_points / 2L)
 
-let (sx, sy) = 
-    [tx..bx] |> List.find (fun x -> dug.Contains (x, ty)) |> fun x -> x, ty
-let inside = filler [sx+1,sy+1] dug Set.empty
-printfn "%A" (tx, ty)
+let part1_points = 
+    input 
+    |> Array.map (fun line -> let parts = line.Split [|' '|] in parts[0], int64 parts[1])
+    |> Array.mapFold (fun (x, y) (dir, dist) -> 
+        let (dx, dy) = match dir with "U" -> 0L,-1L | "D" -> 0,1 | "L" -> -1,0 | "R" | _ -> 1,0 
+        let next = x + dist * dx, y + dist * dy
+        next, next) (0, 0)
+    |> fst
 
-for y in ty..by do
-    for x in tx..bx do
-        if (x, y) = (tx, ty) then printf "S" else if Set.contains (x, y) dug then printf "#" else if Set.contains (x, y) inside then printf "X" else printf "."
-    printf "\n"
+let part1_boundary = input |> Array.sumBy (fun line -> let parts = line.Split [|' '|] in int64 parts[1])
 
-let part1 = dug.Count + inside.Count
+let part1 = 
+    let area = shoe_lace part1_points
+    let interior = rev_picks part1_boundary area
+    part1_boundary + interior
+
 printfn "Part 1: %d" part1
+
+let part2_points = 
+    input 
+    |> Array.map (fun line -> let parts = line.Split [|' '|] in parts[2])
+    |> Array.mapFold (fun (x, y) code -> 
+        let dir = code[7]
+        let (dx, dy) = match dir with '0' -> 1L,0L | '1' -> 0,1 | '2' -> -1,0 | '3' | _ -> 0,-1
+        let dist = System.Convert.ToInt64(code[2..6], 16)
+        let next = int64 x + dist * dx, int64 y + dist * dy
+        next, next) (0, 0)
+    |> fst
+
+let part2_boundary = input |> Array.sumBy (fun line -> 
+    let parts = line.Split [|' '|] in System.Convert.ToInt64(parts[2][2..6], 16))
+
+let part2 = 
+    let area = shoe_lace part2_points
+    let interior = rev_picks part2_boundary area
+    part2_boundary + interior
+
+printfn "Part 2: %d" part2

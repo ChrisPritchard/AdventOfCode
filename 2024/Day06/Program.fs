b24b9f293a9dede0a9d27a462aa0c8151e923a46
= <=>
2024-12-07 11:53:40 +1300

    partial work on day 6 part 2

--- 2024/Day06/Program.fs
+++ 2024/Day06/Program.fs
@@ -30,3 +30,56 @@ let path = march (Set.empty.Add starting_position) starting_position 0
 let sum = Set.count path
 
 printfn "Part 1: %d" sum
+
+// for part 2, calculate a vector at each point and also march a line in the turn direction
+// if the marched line will hit and turn to match an existing vector, we have a loop
+
+let rec marcher (x, y) dir =
+    let (dx, dy) =
+        match dir with
+        | 0 -> 0, -1
+        | 1 -> 1, 0
+        | 2 -> 0, 1
+        | _ -> -1, 0
+
+    let nx, ny = x + dx, y + dy
+
+    if nx < 0 || nx = input[0].Length || ny < 0 || ny = input.Length then
+        None
+    else
+        match input[ny][nx] with
+        | '.'
+        | '^' -> marcher (nx, ny) dir
+        | _ -> Some((nx, ny), if dir = 3 then 0 else dir + 1)
+
+let rec check_turn (x, y) dir =
+    marcher (x, y) (if dir = 3 then 0 else dir + 1)
+
+let rec new_march loops (acc: Set<(int * int) * int>) (x, y) dir =
+    let (dx, dy) =
+        match dir with
+        | 0 -> 0, -1
+        | 1 -> 1, 0
+        | 2 -> 0, 1
+        | _ -> -1, 0
+
+    let nx, ny = x + dx, y + dy
+
+    if nx < 0 || nx = input[0].Length || ny < 0 || ny = input.Length then
+        loops
+    else
+        match input[ny][nx] with
+        | '.'
+        | '^' ->
+            let new_loops =
+                match check_turn (nx, ny) dir with
+                | Some vector when acc.Contains vector -> loops + 1
+                | _ -> loops
+
+            new_march new_loops (Set.add ((nx, ny), dir) acc) (nx, ny) dir
+        | _ -> new_march loops (Set.add ((nx, ny), dir) acc) (x, y) (if dir = 3 then 0 else dir + 1)
+
+let loop_possibles =
+    new_march 0 (Set.empty.Add(starting_position, 0)) starting_position 0
+
+printfn "Part 2: %d" loop_possibles

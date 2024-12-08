7db91c35196bc454e1a3f33e19b466ad496c2bc1
= <=>
2024-12-08 14:42:38 +1300

    solved day 6 yus!

--- 2024/Day06/Program.fs
+++ 2024/Day06/Program.fs
@@ -6,81 +6,88 @@ let starting_position =
     |> Array.pick (fun (i, line) ->
         line
         |> Seq.indexed
-        |> Seq.tryPick (fun (j, c) -> if c = '^' then Some(j, i) else None))
+        |> Seq.tryPick (fun (j, c) -> if c = '^' then Some(j, i, 0) else None))
 
-let blocks =
-    input
-    |> Array.indexed
-    |> Seq.collect (fun (i, line) ->
-        line
-        |> Seq.indexed
-        |> Seq.choose (fun (j, c) -> if c = '#' then Some(j, i) else None))
-    |> Set.ofSeq
+let mutable blocks =
+    input |> Array.map (fun line -> line |> Seq.map ((=) '#') |> Array.ofSeq)
+
+let find_turn (x, y, dir) =
+    if dir = 0 then
+        [| (y - 1) .. (-1) .. 0 |]
+        |> Array.tryPick (fun i -> if blocks[i][x] then Some(x, i + 1, 1) else None)
+    else if dir = 1 then
+        [| (x + 1) .. input[0].Length - 1 |]
+        |> Array.tryPick (fun i -> if blocks[y][i] then Some(i - 1, y, 2) else None)
+    else if dir = 2 then
+        [| (y + 1) .. input.Length - 1 |]
+        |> Array.tryPick (fun i -> if blocks[i][x] then Some(x, i - 1, 3) else None)
+    else
+        [| (x - 1) .. (-1) .. 0 |]
+        |> Array.tryPick (fun i -> if blocks[y][i] then Some(i + 1, y, 0) else None)
+
+let rec find_path visited turns pos =
+    match find_turn pos with
+    | None -> Some(List.rev turns)
+    | Some t when Set.contains t visited -> None
+    | Some t ->
+        let new_visited = Set.add t visited
+        find_path new_visited (t :: turns) t
+
+let all_turns = find_path Set.empty [ starting_position ] starting_position
 
 let delta =
     function
-    | 0 -> 0, -1
-    | 1 -> 1, 0
-    | 2 -> 0, 1
+    | (_, _, 0) -> 0, -1
+    | (_, _, 1) -> 1, 0
+    | (_, _, 2) -> 0, 1
     | _ -> -1, 0
 
-let turn dir = if dir = 3 then 0 else dir + 1
+let points_between (x, y, dir) (nx, ny, _) =
+    let dx, dy =
+        match dir with
+        | 0 -> 0, -1
+        | 1 -> 1, 0
+        | 2 -> 0, 1
+        | _ -> -1, 0
 
-type Pathing =
-    | Normal of ((int * int) * int) list
-    | Loop
+    let is_out_of_bounds (x, y) =
+        x < 0 || y < 0 || x = input[0].Length || y = input.Length
 
-let rec march blockers acc (x, y) dir =
+    let rec counter acc (x, y) =
+        let next = x + dx, y + dy
 
-    let (dx, dy) = delta dir
-    let nx, ny = x + dx, y + dy
-
-    let add x y dir = ((x, y), dir) :: acc
-
-    if nx < 0 || nx = input[0].Length || ny < 0 || ny = input.Length then
-        Normal(List.rev acc)
-    else if List.contains ((nx, ny), dir) acc then
-        Loop
-    else if Set.contains (nx, ny) blockers then
-        march blockers acc (x, y) (turn dir)
-    else
-        march blockers (add nx ny dir) (nx, ny) dir
-
-let path =
-    match march blocks [ starting_position, 0 ] starting_position 0 with
-    | Normal path -> path
-    | _ -> failwith "loop for part 1"
-
-let sum = List.map fst path |> Set.ofList |> Set.count
-printfn "Part 1: %d" sum
-
-let path_array = Array.ofList path
-
-let folder (loops, acc, blockers_tested) index =
-    let new_acc = path_array[index] :: acc
-
-    if snd path[index] <> snd path_array[index + 1] then
-        loops, new_acc, blockers_tested // skip existing turns
-    else
-        // printfn "testing %A" path_array[index]
-
-        let (x, y), dir = path_array[index]
-        let ahead = let dx, dy = delta dir in x + dx, y + dy
-
-        if Set.contains ahead blockers_tested then
-            loops, new_acc, blockers_tested
+        if next = (nx, ny) || is_out_of_bounds next then
+            Set.ofList acc
         else
-            let new_blockers_tested = Set.add ahead blockers_tested
-            let custom_blocks = Set.add ahead blocks
-            let new_dir = turn dir
+            counter (next :: acc) next
 
-            match march custom_blocks new_acc (x, y) new_dir with
-            | Loop ->
-                // printfn "loop found with blocker at %A" ahead
-                loops + 1, new_acc, new_blockers_tested
-            | _ -> loops, new_acc, new_blockers_tested
+    counter [ x, y ] (x, y)
 
-let loops, _, _ =
-    [ 0 .. path_array.Length - 2 ] |> List.fold folder (0, [], Set.empty)
+let rec find_all_points acc pos turns =
+    match turns with
+    | [] -> points_between pos (-1, -1, -1) |> Set.union acc
+    | n :: rest ->
+        let next = points_between pos n |> Set.union acc
+        find_all_points next n rest
+
+let all_points =
+    find_all_points Set.empty starting_position (List.skip 1 all_turns.Value)
+
+printfn "Part 1: %A" (Set.count all_points)
+
+let mutable loops = 0
+
+let (sx, sy, _) = starting_position
+
+for point in all_points do
+    if point <> (sx, sy) then
+        let (x, y) = point
+        blocks[y][x] <- true
+
+        match find_path Set.empty [ starting_position ] starting_position with
+        | None -> loops <- loops + 1
+        | Some _ -> ()
+
+        blocks[y][x] <- false
 
 printfn "Part 2: %d" loops

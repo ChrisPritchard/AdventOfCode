41e9c8a2c84efb347db9c946fd3b7fcde576a8d4
= <=>
2024-12-21 21:59:35 +1300

    refactored to include parents, need to optimise

--- 2024/Day16/Program.fs
+++ 2024/Day16/Program.fs
@@ -1,57 +1,78 @@
 let input = System.IO.File.ReadAllLines "input.txt"
 
-type Coords = int * int
+type Vector = int * int * char
+
 
 [<Struct>]
 type QueueItem =
-    { point: Coords
-      direction: char
-      cost: int }
+    { vector: Vector
+      cost: int
+      parent: Vector }
 
-let target: Coords = input[0].Length - 2, 1
-let memo = System.Collections.Generic.Dictionary<Coords, int>()
+[<Struct>]
+type PathItem = { cost: int; parents: Vector list }
+
+let is_target (v: Vector) =
+    let (x, y, _) = v in x = input[0].Length - 2 && y = 1
+
+let memo = System.Collections.Generic.Dictionary<Vector, PathItem>()
+
+let mutable best_cost = System.Int32.MaxValue
 
 let rec path_finder (queue: QueueItem list) =
     match queue with
     | [] -> ()
-    | head :: remainder when memo.ContainsKey target && memo[target] < head.cost -> path_finder remainder
+    | head :: remainder when best_cost < head.cost -> path_finder remainder
     | head :: remainder ->
-        if memo.ContainsKey head.point && memo[head.point] < head.cost then
-            path_finder remainder
-        else if head.point = target then
-            memo[head.point] <- head.cost
+        if memo.ContainsKey head.vector && memo[head.vector].cost < head.cost then
             path_finder remainder
         else
-            memo[head.point] <- head.cost
-            let neighbours = find_neighbours head
-            let new_queue = List.append neighbours remainder
-            path_finder new_queue
+            if memo.ContainsKey head.vector then
+                if memo[head.vector].cost > head.cost then
+                    memo[head.vector] <-
+                        { cost = head.cost
+                          parents = [ head.parent ] }
+                else
+                    memo[head.vector] <-
+                        { cost = head.cost
+                          parents = head.parent :: memo[head.vector].parents }
+            else
+                memo[head.vector] <-
+                    { cost = head.cost
+                      parents = [ head.parent ] }
+
+            if is_target head.vector then
+                best_cost <- min best_cost head.cost
+                path_finder remainder
+            else
+                let neighbours = find_neighbours head
+                let new_queue = List.append neighbours remainder
+                path_finder new_queue
 
 and find_neighbours node : QueueItem list =
-    let (x, y) = node.point
 
     let candidates =
-        match node.direction with
-        | '>' ->
-            [ (x + 1, y), node.direction, node.cost + 1
-              (x, y - 1), '^', node.cost + 1001
-              (x, y + 1), 'v', node.cost + 1001 ]
-        | '<' ->
-            [ (x - 1, y), node.direction, node.cost + 1
-              (x, y - 1), '^', node.cost + 1001
-              (x, y + 1), 'v', node.cost + 1001 ]
-        | '^' ->
-            [ (x, y - 1), node.direction, node.cost + 1
-              (x - 1, y), '<', node.cost + 1001
-              (x + 1, y), '>', node.cost + 1001 ]
-        | 'v'
-        | _ ->
-            [ (x, y + 1), node.direction, node.cost + 1
-              (x - 1, y), '<', node.cost + 1001
-              (x + 1, y), '>', node.cost + 1001 ]
+        match node.vector with
+        | x, y, '>' ->
+            [ (x + 1, y, '>'), node.cost + 1
+              (x, y - 1, '^'), node.cost + 1001
+              (x, y + 1, 'v'), node.cost + 1001 ]
+        | x, y, '<' ->
+            [ (x - 1, y, '<'), node.cost + 1
+              (x, y - 1, '^'), node.cost + 1001
+              (x, y + 1, 'v'), node.cost + 1001 ]
+        | x, y, '^' ->
+            [ (x, y - 1, '^'), node.cost + 1
+              (x - 1, y, '<'), node.cost + 1001
+              (x + 1, y, '>'), node.cost + 1001 ]
+        | x, y, 'v'
+        | x, y, _ ->
+            [ (x, y + 1, 'v'), node.cost + 1
+              (x - 1, y, '<'), node.cost + 1001
+              (x + 1, y, '>'), node.cost + 1001 ]
 
     candidates
-    |> List.choose (fun ((nx, ny), direction, cost) ->
+    |> List.choose (fun ((nx, ny, direction), cost) ->
         if
             nx < 0
             || ny < 0
@@ -62,17 +83,16 @@ and find_neighbours node : QueueItem list =
             None
         else
             Some(
-                { point = (nx, ny)
-                  direction = direction
-                  cost = cost }
+                { vector = (nx, ny, direction)
+                  cost = cost
+                  parent = node.vector }
             ))
 
-let start = 1, input.Length - 2
+let start = 1, input.Length - 2, '>'
 
 path_finder
-    [ { point = start
-        direction = '>'
-        cost = 0 } ]
+    [ { vector = start
+        cost = 0
+        parent = -1, -1, ' ' } ]
 
-let best_cost = memo[target]
 printfn "Part 1: %d" best_cost

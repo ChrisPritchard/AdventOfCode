1e55d34ec6a472bb8f2290e483aa724261198840
= <=>
2025-02-25 21:36:41 +1300

    full part 1 but not getting the right result with test data

--- 2024/Day21/Program.fs
+++ 2024/Day21/Program.fs
@@ -2,16 +2,16 @@ let input = System.IO.File.ReadAllLines "input.txt"
 
 let final_keypad =
     function
-    | 7 -> 0, 0
-    | 8 -> 1, 0
-    | 9 -> 2, 0
-    | 4 -> 0, 1
-    | 5 -> 1, 1
-    | 6 -> 2, 1
-    | 1 -> 0, 2
-    | 2 -> 1, 2
-    | 3 -> 2, 2
-    | 0 -> 1, 3
+    | '7' -> 0, 0
+    | '8' -> 1, 0
+    | '9' -> 2, 0
+    | '4' -> 0, 1
+    | '5' -> 1, 1
+    | '6' -> 2, 1
+    | '1' -> 0, 2
+    | '2' -> 1, 2
+    | '3' -> 2, 2
+    | '0' -> 1, 3
     | _ -> 2, 3 // used for 'A' character
 
 let robot_keypad =
@@ -41,15 +41,24 @@ let path_steps (x1, y1) (x2, y2) =
 
     result + "A"
 
-let start_pos = 2, 3
-
 let rec find_path keypad acc pos rem =
     match rem with
     | "" -> acc
     | s ->
         let n = s[0]
-        let target = keypad (if n = 'A' then -1 else int n - int '0')
+        let target = keypad n
         let new_acc = acc + path_steps pos target
         find_path keypad new_acc target s[1..]
 
-printfn "%A" (find_path final_keypad "" start_pos "029A")
+let full_path target_sequence =
+    find_path final_keypad "" (2, 3) target_sequence
+    |> find_path robot_keypad "" (2, 0)
+    |> find_path robot_keypad "" (2, 0)
+
+let sub_result (target_sequence: string) =
+    let index_num = System.Int32.Parse target_sequence[0 .. target_sequence.Length - 2]
+    let full_path = full_path target_sequence
+    printfn "%s: %d * %d = %d, %s" target_sequence index_num full_path.Length (index_num * full_path.Length) full_path
+    index_num * full_path.Length
+
+printfn "Part 1: %d" (Array.sumBy sub_result input)

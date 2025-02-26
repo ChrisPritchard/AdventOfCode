ec408430b45e69f68b064f5135249acf17c78dbb
= <=>
2025-02-26 15:46:42 +1300

    fixed to avoid dead space

--- 2024/Day21/Program.fs
+++ 2024/Day21/Program.fs
@@ -22,38 +22,40 @@ let robot_keypad =
     | '>' -> 2, 1
     | _ -> 2, 0 // used for 'A' charactger
 
-let path_steps (x1, y1) (x2, y2) =
+let path_steps (x1, y1) (x2, y2) bad_row =
     let mutable result = ""
 
     let horizontal = x1 - x2
 
-    if horizontal > 0 then
-        result <- result + System.String('<', horizontal)
-    else
-        result <- result + System.String('>', abs horizontal)
+    let across =
+        if horizontal > 0 then
+            System.String('<', horizontal)
+        else
+            System.String('>', abs horizontal)
 
     let vertical = y1 - y2
 
-    if vertical > 0 then
-        result <- result + System.String('^', vertical)
-    else
-        result <- result + System.String('v', abs vertical)
+    let through =
+        if vertical > 0 then
+            System.String('^', vertical)
+        else
+            System.String('v', abs vertical)
 
-    result + "A"
+    (if y1 = bad_row then through + across else across + through) + "A"
 
-let rec find_path keypad acc pos rem =
+let rec find_path keypad bad_row acc pos rem =
     match rem with
     | "" -> acc
     | s ->
         let n = s[0]
         let target = keypad n
-        let new_acc = acc + path_steps pos target
-        find_path keypad new_acc target s[1..]
+        let new_acc = acc + path_steps pos target bad_row
+        find_path keypad bad_row new_acc target s[1..]
 
 let full_path target_sequence =
-    find_path final_keypad "" (2, 3) target_sequence
-    |> find_path robot_keypad "" (2, 0)
-    |> find_path robot_keypad "" (2, 0)
+    find_path final_keypad 3 "" (2, 3) target_sequence
+    |> find_path robot_keypad 0 "" (2, 0)
+    |> find_path robot_keypad 0 "" (2, 0)
 
 let sub_result (target_sequence: string) =
     let index_num = System.Int32.Parse target_sequence[0 .. target_sequence.Length - 2]

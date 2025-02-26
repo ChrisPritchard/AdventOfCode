443092e6e12c591d079c97e817366c3f3d85ec53
= <=>
2025-02-26 18:38:09 +1300

    fixed by exploring all possibles

--- 2024/Day21/Program.fs
+++ 2024/Day21/Program.fs
@@ -18,29 +18,25 @@ let robot_keypad =
     [ '^', (1, 0); '<', (0, 1); 'v', (1, 1); '>', (2, 1); 'A', (2, 0) ]
     |> Map.ofList
 
-let path_steps (x1, y1) (x2, y2) =
+let rec path_step_options (dx, dy) (x, y) keypad acc =
+    let valid p = Map.exists (fun _ v -> v = p) keypad
 
-    let mutable result = ""
+    seq {
+        if dx < x && valid (dx + 1, dy) then
+            yield! path_step_options (dx + 1, dy) (x, y) keypad (acc + ">")
+        else if dx > x && valid (dx - 1, dy) then
+            yield! path_step_options (dx - 1, dy) (x, y) keypad (acc + "<")
 
-    let horizontal = x1 - x2
+        if dy < y && valid (dx, dy + 1) then
+            yield! path_step_options (dx, dy + 1) (x, y) keypad (acc + "v")
+        else if dy > y && valid (dx, dy - 1) then
+            yield! path_step_options (dx, dy - 1) (x, y) keypad (acc + "^")
 
-    let across =
-        if horizontal > 0 then
-            System.String('<', horizontal)
-        else
-            System.String('>', abs horizontal)
+        if dx = x && dy = y then
+            yield acc + "A"
+    }
+    |> Seq.toArray
 
-    let vertical = y1 - y2
-
-    let through =
-        if vertical > 0 then
-            System.String('^', vertical)
-        else
-            System.String('v', abs vertical)
-
-    (if y1 = bad_row then through + across else across + through) + "A"
-
-let path_step_options start target keypad = [||]
 
 let find_path keypad start_pos (to_type_options: string[]) =
     to_type_options
@@ -54,7 +50,8 @@ let find_path keypad start_pos (to_type_options: string[]) =
 
             acc <-
                 acc
-                |> Array.collect (fun so_far -> path_step_options pos target keypad |> Array.map (fun s -> so_far + s))
+                |> Array.collect (fun so_far ->
+                    path_step_options pos target keypad "" |> Array.map (fun s -> so_far + s))
 
             pos <- target
 

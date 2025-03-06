b1944ddd178f14c29c57cc39b6d5bd9749cbc06f
= <=>
2025-03-06 19:37:44 +1300

    finished day 23

--- 2024/Day23/Program.fs
+++ 2024/Day23/Program.fs
@@ -22,3 +22,38 @@ let t_comp_sets =
     |> Array.length
 
 printfn "Part 1: %d" t_comp_sets
+
+open System.Collections.Generic
+
+let sets = List<HashSet<string>>()
+
+for connection in input do
+    let a, b = let parts = connection.Split "-" in parts[0], parts[1]
+
+    match sets |> Seq.tryFind (fun s -> s.Contains a || s.Contains b) with
+    | None ->
+        let new_set = HashSet<string>()
+        let _ = new_set.Add a
+        let _ = new_set.Add b
+        sets.Add new_set
+    | Some s ->
+        let _ = s.Add a
+        let _ = s.Add b
+        ()
+
+let password =
+    sets
+    |> Seq.filter (fun s ->
+        let all_comps = Seq.toArray s
+
+        all_comps
+        |> Array.forall (fun comp ->
+            all_comps
+            |> Array.filter (fun other -> other <> comp)
+            |> Array.forall (fun other -> as_map[comp].Contains other)))
+    |> Seq.sortByDescending (fun s -> s.Count)
+    |> Seq.head
+    |> Seq.sort
+    |> String.concat ","
+
+printfn "Part 2: %s" password

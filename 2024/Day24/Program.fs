dc5c8e0bfb8b6f82d7a588f18e6526d9194672bc
= <=>
2025-03-09 22:23:09 +1300

    work on a gate swapping mechanic

--- 2024/Day24/Program.fs
+++ 2024/Day24/Program.fs
@@ -12,56 +12,131 @@ let input_folder (wires, circuits) (line: string) =
     else
         wires, circuits
 
-let input_wires, input_circuits =
-    Array.fold input_folder ([], []) input
-    |> fun (w, c) -> Map.ofList w, Array.ofList c
+let input_wires, input_circuits = Array.fold input_folder ([], []) input
 
-let rec processor wires (circuits: (string * string * string * string)[]) =
-    let next, rem =
-        circuits
-        |> Array.partition (fun (i1, i2, _, _) -> Map.containsKey i1 wires && Map.containsKey i2 wires)
+let mutable full_circuit = Map.empty
+let mutable gate_log = fun out -> ()
+let mutable stack_depth = 0
 
-    if next.Length = 0 then
-        wires
-    else
+let max_depth_of_stack = 100
 
-        let circuit_runner (new_wires: Map<string, bool>) valid_circuit =
-            let i1, i2, op, out = valid_circuit
+for i1, i2, op, out in input_circuits do
+    full_circuit <-
+        Map.add
+            out
+            (fun () ->
+                gate_log out
+                stack_depth <- stack_depth + 1
 
-            let v =
+                if stack_depth > max_depth_of_stack then
+                    failwith "loop detected"
+
                 match op with
-                | "AND" -> new_wires[i1] && new_wires[i2]
-                | "OR" -> new_wires[i1] || new_wires[i2]
-                | "XOR" -> new_wires[i1] <> new_wires[i2]
-                | s -> failwithf "invalid op: %s" s
+                | "AND" -> full_circuit[i1]() && full_circuit[i2]()
+                | "OR" -> full_circuit[i1]() || full_circuit[i2]()
+                | "XOR" -> full_circuit[i1]() <> full_circuit[i2]()
+                | _ -> failwithf "invalid op: %s" op)
+            full_circuit
 
-            Map.add out v new_wires
+let get_output i =
+    stack_depth <- 0
+    full_circuit[sprintf "z%02d" i]()
 
-        let new_wires = Array.fold circuit_runner wires next
-        processor new_wires rem
+let set_input (key: char) (bits: bool array) =
+    for i, b in Array.indexed bits do
+        full_circuit <- Map.add (sprintf "%c%02d" key i) (fun () -> b) full_circuit
 
-let final_wires = processor input_wires input_circuits
+let x_s =
+    input_wires
+    |> Seq.filter (fun (k, _) -> k.StartsWith "x")
+    |> Seq.sort
+    |> Seq.map snd
+    |> Seq.toArray
 
-let find_number start_char =
-    final_wires
-    |> Map.toArray
-    |> Array.filter (fun (k, _) -> k[0] = start_char)
-    |> Array.sortBy fst
-    |> Array.map (snd >> fun v -> if v then '1' else '0')
+let y_s =
+    input_wires
+    |> Seq.filter (fun (k, _) -> k.StartsWith "y")
+    |> Seq.sort
+    |> Seq.map snd
+    |> Seq.toArray
+
+set_input 'x' x_s
+set_input 'y' y_s
+
+let out_length = x_s.Length + 1
+
+let z_s = Array.init out_length get_output
+
+let as_num =
+    z_s
     |> Array.rev
+    |> Array.map (fun v -> if v then '1' else '0')
     |> System.String
     |> fun s -> System.Convert.ToUInt64(s, 2)
 
-printfn "Part 1: %d" (find_number 'z')
+printfn "Part 1: %d" as_num
 
-let input_1 = find_number 'x'
-let input_2 = find_number 'y'
-let current_out = find_number 'z'
-let expected_out = input_1 + input_2
+// set_input 'y' (Array.init y_s.Length (fun i -> if i = 0 then true else false))
+// set_input 'x' (Array.init x_s.Length (fun i -> false))
 
-printfn "%d" input_1
-printfn "%d" input_2
-printfn "current out:  %d" current_out
-printfn "%s" <| System.Convert.ToString(int64 current_out, 2)
-printfn "expected out: %d" expected_out
-printfn "%s" <| System.Convert.ToString(int64 expected_out, 2)
+// let test_out = Array.init out_length get_output
+// printfn "%A" test_out
+
+let mutable swapped: string list = []
+
+let check up_to =
+    let new_z = Array.init out_length get_output
+
+    let rec adder i carry =
+        let expected = x_s[i] <> y_s[i] <> carry
+        let new_carry = x_s[i] && y_s[i] || x_s[i] && carry || y_s[i] && carry
+
+        if new_z[i] <> expected then false
+        else if i = up_to then true
+        else adder (i + 1) new_carry
+
+    adder 0 false
+
+let mutable gates = []
+
+gate_log <-
+    fun o ->
+        if not (o.StartsWith "z") && not (List.contains o swapped) then
+            gates <- o :: gates
+
+for i in 0 .. out_length - 2 do
+    gates <- []
+
+    if not (check i) then
+        printfn "failed at bit %i" i
+
+        let possible_pairs =
+            [ for i in 0 .. gates.Length - 2 do
+                  for j in i + 1 .. gates.Length - 1 do
+                      yield gates[i], gates[j] ]
+
+        let rec find_swap =
+            function
+            | [] -> failwith "couldn't find correction"
+            | (a, b) :: rem ->
+                let a_f = full_circuit[a]
+                let b_f = full_circuit[b]
+                full_circuit <- Map.add a b_f full_circuit
+                full_circuit <- Map.add b a_f full_circuit
+
+                let result =
+                    try
+                        check i
+                    with _ ->
+                        false
+
+                if result then
+                    swapped <- a :: b :: swapped
+                    printfn "fixed with swap %s <-> %s" a b
+                    ()
+                else
+                    full_circuit <- Map.add a a_f full_circuit
+                    full_circuit <- Map.add b b_f full_circuit
+                    find_swap rem
+
+        find_swap possible_pairs
